92d0ebae6f383b545b5d05245b0e61b9ae5f75d5
Mapi Carrillo <mp.carrillo@yahoo.es>
2018-04-13 17:39:53 +0200

    Falta última interacción y maquetar

---
+++ awesome-project/bin/seeds.js
@@ -0,0 +1,27 @@
+require("dotenv").config();
+
+const mongoose = require('mongoose');
+const Movie = require('../models/Movie');
+const movies = require('./movie_data');
+const dbURL = process.env.DBURL;
+
+mongoose.connect(dbURL).then(() =>{
+    console.log(`Connected to db ${dbURL}`);
+    //Movie.collection.drop();
+    console.log(movies)
+    movies.forEach( movie => {
+        console.log(movie)
+            let movie_new = new Movie ({
+                title: movie.title,
+                director: movie.director,
+                starts: movie.starts,
+                image: movie.image,
+                description: movie.description,
+                showtimes: movie.showtimes
+            })
+            .save()
+            .then( () => console.log("Created movie"))
+        })
+    })
+    //mongoose.disconnect();
+
