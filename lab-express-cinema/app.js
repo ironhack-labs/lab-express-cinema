139d1ed0e9fd1f31aadb699aa2ad16e02e2dc291
Tanis Pan <e.pan.gar@gmail.com>
2018-04-13 19:33:59 +0200

    5

--- lab-express-cinema/app.js
+++ lab-express-cinema/app.js
@@ -8,11 +8,12 @@ const hbs          = require('hbs');
 const mongoose     = require('mongoose');
 const logger       = require('morgan');
 const path         = require('path');
+const router = express.Router();
 
 
 mongoose.Promise = Promise;
 mongoose
-  .connect('mongodb://localhost/lab-express-cinema', {useMongoClient: true})
+  .connect('mongodb://localhost/IronCinema', {useMongoClient: true})
   .then(() => {
     console.log('Connected to Mongo!')
   }).catch(err => {
@@ -40,6 +41,7 @@ app.use(require('node-sass-middleware')({
       
 
 app.set('views', path.join(__dirname, 'views'));
+
 app.set('view engine', 'hbs');
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
@@ -49,10 +51,14 @@ app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
 // default value for title local
 app.locals.title = 'Express - Generated with IronGenerator';
 
+app.get("/", (req, res) => {
+  res.render("index");
+});
 
-
-const index = require('./routes/index');
-app.use('/', index);
+const indexRouter = require('./routes/index');
+app.use('/', indexRouter);
+const moviesRouter= require('./routes/movies');
+app.use('/movies', moviesRouter);
 
 
 module.exports = app;
