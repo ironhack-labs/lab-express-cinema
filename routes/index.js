19d1464ff5da021b10f4a55871ce11ffbd8ce2ca
Garxoza <garxoza@gmail.com>
2018-06-18 09:09:30 +0200

    done

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,36 @@
-const express = require('express');
-const router  = express.Router();
+const express = require("express");
+const router = express.Router();
+const Movie = require("../models/Movie");
 
 /* GET home page */
-router.get('/', (req, res, next) => {
-  res.render('index');
+router.get("/", (req, res, next) => {
+  res.render("index");
 });
+
+router.get("/movies", (req, res, next) => {
+  Movie.find({})
+    .then(movies => {
+      console.log("DEBUG movies", movies);
+
+      res.render("movies", { movies: movies });
+    })
+    .catch(err => {
+      throw err;
+    });
+});
+
+router.get("/movies/:movieId", (req, res, next) => {
+  let myMovie = req.params.movieId;
+  console.log("DEBUG myMovie", myMovie);
+  Movie.findOne({ _id: myMovie })
+    .then(movieOne => {
+      console.log("DEBUG MOVIE", movieOne);
+      res.render("movie-detail", movieOne);
+    })
+    .catch(err => {
+      throw err;
+    });
+});
+
 
 module.exports = router;
