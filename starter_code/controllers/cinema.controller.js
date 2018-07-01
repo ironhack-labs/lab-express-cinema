c4fdbf4ff14985ede737192a9766629df4d5a31a
Paula Quitián <pquitian@idealista.com>
2018-07-01 20:41:28 +0200

    [feat]MOvie Detail

--- starter_code/controllers/cinema.controller.js
+++ starter_code/controllers/cinema.controller.js
@@ -4,7 +4,19 @@ const Movie = require('../models/cinema.model');
 module.exports.list = (req, res, next) => {
     Movie.find()
         .then(movies => {
-            res.render('movies', {movies});
+            res.render('movies/index', {movies, title: 'Ironhack Movies' });
+        })
+        .catch(err => {
+            next(err);
+        });
+};
+
+module.exports.views = (req, res, next) => {
+    const id = req.params.id;
+
+    Movie.findById(id)
+        .then(movie => {
+            res.render('movies/movie', {movie, title: 'Movie Detail' });
         })
         .catch(err => {
             next(err);
--- starter_code/routes/movies.js
+++ starter_code/routes/movies.js
@@ -3,6 +3,9 @@ const router = express.Router();
 const cinemaController = require('../controllers/cinema.controller');
 
 /* GET movies listing. */
+
 router.get('/', cinemaController.list);
+router.get('/:id', cinemaController.views);
+
 
 module.exports = router;
