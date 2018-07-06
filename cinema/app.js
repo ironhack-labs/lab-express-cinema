359fa33c86d80432f0b4d800228c55d77b9519e8
Junior Mendez-Mateo <junior_abigail@hotmail.com>
2018-07-06 14:34:39 -0400

    homepage styles completed

--- cinema/app.js
+++ cinema/app.js
@@ -48,7 +48,7 @@ app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
 
 
 // default value for title local
-app.locals.title = 'Ironhack Cinema';
+app.locals.title = 'Cinema Ironhack';
 
 const index = require('./routes/index');
 app.use('/', index);
