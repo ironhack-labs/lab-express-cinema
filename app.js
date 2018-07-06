8494afae42a0816248ae5c2229250d699cd7dc22
Samuraty <alexandra.garciagasco@gmail.com>
2018-07-06 17:07:35 +0200

    Casi

--- app.js
+++ app.js
@@ -9,10 +9,10 @@ const mongoose     = require('mongoose');
 const logger       = require('morgan');
 const path         = require('path');
 
-
+const dbName = process.env.DBURL;
 mongoose.Promise = Promise;
 mongoose
-  .connect('mongodb://localhost/lab-express-cinema', {useMongoClient: true})
+  .connect('mongodb://localhost/'+dbName, {useMongoClient: true})
   .then(() => {
     console.log('Connected to Mongo!')
   }).catch(err => {
@@ -47,7 +47,7 @@ app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
 
 
 // default value for title local
-app.locals.title = 'Express - Generated with IronGenerator';
+app.locals.title = 'Cinema Ironhack';
 
 
 
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,31 @@
 const express = require('express');
 const router  = express.Router();
+const Movie = require('../models/Movie');
+
 
 /* GET home page */
 router.get('/', (req, res, next) => {
   res.render('index');
 });
 
+
+/* GET movies page */
+router.get('/movies', (req, res, next) => {
+  Movie.find({}).then(movies => {
+    console.log(movies)
+    res.render('movies', {movies});
+  });
+});
+
+/* GET themovies:id page */
+router.get('/themovie/:id', (req, res) => {
+
+  Movie.findById({_id: req.params.id})
+  .then(movies => {
+    console.log(movies)
+    res.render('themovie');
+  });
+});
+
 module.exports = router;
+
