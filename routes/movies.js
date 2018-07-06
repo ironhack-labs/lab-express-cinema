c14630a05458f5c6992b724739c01d864ef73c92
AlexZarrella <37054157+AlexZarrella@users.noreply.github.com>
2018-07-06 15:47:29 -0400

    Add files via upload

---
+++ routes/movies.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const router  = express.Router();
+const Movie        = require('../models/movies');
+
+
+router.get('/movies', (req, res, next)=>{
+  Movie.find()
+.then((listOfMovies)=>{
+  res.render('movies', {moviesArray: listOfMovies})
+
+})
+.catch((err)=>{
+  res.send(err);
+})
+})
+
+router.get('/movies/:id', (req, res, next)=>{
+  const theID = req.params.id;
+  Movie.findById(theID)
+  .then((theMovie)=>{
+      res.render('singleMovie', {movie: theMovie})
+  })
+  .catch((err)=>{
+      res.send(err)
+  })
+});
+
+
+
+
+
+
+module.exports = router;
