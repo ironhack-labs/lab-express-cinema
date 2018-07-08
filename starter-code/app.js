a72ccd2d3797812e35812f295e196145923875b4
Montse Badia <mbadia_11@hotmail.com>
2018-07-08 14:47:40 +0200

    added link to movie-details route

--- starter-code/app.js
+++ starter-code/app.js
@@ -28,7 +28,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/movies', moviesRouter); // no need to require books/:id
+app.use('/movies', moviesRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
--- starter-code/routes/movies.js
+++ starter-code/routes/movies.js
@@ -8,8 +8,19 @@ const Movie = require('../models/movie.js');
 router.get('/', (req, res, next) => {
   Movie.find()
     .then(allMovies => {
-      console.log(allMovies);
+      // console.log(allMovies);
       res.render('movies', {allMovies: allMovies});
+    })
+    .catch(error => {
+      console.log(error);
+    });
+});
+
+router.get('/:id', (req, res, next) => {
+  const movieId = req.params.id; // Why params.id??????????????????
+  Movie.findById(movieId)
+    .then(movie => {
+      res.render('movie-details', {movie: movie});
     })
     .catch(error => {
       console.log(error);
