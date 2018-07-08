a110c2c66da76a75bbae07480b3a21587865e29c
Julien Chil Hagopian <julienchilhagopian@gmail.com>
2018-07-08 16:30:15 +0200

    installed starting packages

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,12 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+  const data = {
+    title: 'bichito'
+  };
+  // res.render('index', { title: 'Express' });
+  res.render('index', data);
 });
 
 module.exports = router;
