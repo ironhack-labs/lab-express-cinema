93438c624f3ad39f8f643768f49376d7c5b5cf35
mabvmex <mabvmex@gmail.com>
2018-07-09 22:20:12 -0500

    done

--- app.js
+++ app.js
@@ -41,7 +41,7 @@ app.use(require('node-sass-middleware')({
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public'))); //Aqui nazco siempre.
 app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
 
 
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,37 @@
 const express = require('express');
 const router  = express.Router();
+const Movie = require('../models/movie');
 
 /* GET home page */
 router.get('/', (req, res, next) => {
   res.render('index');
 });
+
+
+//Ruta a movies
+router.get('/movies', (req, res, next)=>{
+  Movie.find()
+  .then(movies=>{
+    res.render('movies', {movies})
+    console.log(movies.length)
+  })
+  .catch(err=>{
+    console.log("error >>> " + err)
+  })
+});
+
+
+//Rutas para detalles movie
+router.get('/movies/:id', (req, res, neex)=>{
+  let movieId = req.params.id;
+  Movie.findOne({'_id' : movieId})
+  .then(movie => {
+    res.render('movie_detail', {movie})
+  .catch(err=>{
+    console.log('error >>> ' + err)
+  })
+})
+
+})
 
 module.exports = router;
