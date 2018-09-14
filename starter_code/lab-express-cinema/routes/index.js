e56a56d87e5cd8a811049cc063935754e0e2d337
Cristian Tejedor <Tejedor92@gmail.com>
2018-09-14 17:25:52 +0200

    done

---
+++ starter_code/lab-express-cinema/routes/index.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const router  = express.Router();
+const Movie=require("../models/Movie")
+
+/* GET home page */
+router.get('/', (req, res, next) => {
+  res.render('index');
+});
+
+router.get("/movies",(req,res,next)=>{
+  Movie.find({}).then(movies=>{
+    res.render("movies",{movies})
+  })
+})
+
+router.get("/movies/:id",(req,res,next)=>{
+  let movieId=req.params.id
+  Movie.findById(movieId)
+    .then( (movie)=>{
+      res.render("info",{movie})
+    })
+})
+
+
+
+
+
+
+
+
+
+module.exports = router;
