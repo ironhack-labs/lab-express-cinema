42e222cbe858be35ec9c5b597202ac5d8f0563ef
RaulG <guiaraul@gmail.com>
2018-09-14 16:58:26 +0200

    Styles Done

--- starter_code/lab-express-cinema/routes/index.js
+++ starter_code/lab-express-cinema/routes/index.js
@@ -5,12 +5,18 @@ const Movie = require('../models/Movie')
 router.get('/', (req, res, next) => {
   res.render('home');
 });
-router.get('/movies', (req, res, next)=>{
+router.get('/movies', (req, res, next) => {
   Movie.find()
-  .then(movies=>{
-    res.render('movies',{movies})
+    .then(movies => {
+      res.render('movies', { movies })
 
-  })
+    })
+})
+router.get('/movie/:id', (req, res, next) => {
+  Movie.findById(req.params.id)
+    .then(movie => {
+      res.render('movie', movie)
+    })
 })
 
 module.exports = router;
