214178dc9b84bbe38af50645dba22d211d576d96
agdwm <almudenaguerraswm@gmail.com>
2018-09-14 16:57:28 +0200

    Iteration 4 done

--- starter_code/lab-express-cinema/app.js
+++ starter_code/lab-express-cinema/app.js
@@ -1,23 +1,26 @@
 require('dotenv').config();
 
-const bodyParser   = require('body-parser');
+const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
-const express      = require('express');
-const favicon      = require('serve-favicon');
-const hbs          = require('hbs');
-const mongoose     = require('mongoose');
-const logger       = require('morgan');
-const path         = require('path');
+const express = require('express');
+const favicon = require('serve-favicon');
+const hbs = require('hbs');
+const mongoose = require('mongoose');
+const logger = require('morgan');
+const path = require('path');
+const Movie = require('./models/Movie');
 
 
 mongoose
-  .connect('mongodb://localhost/lab-express-cinema', {useNewUrlParser: true})
-  .then(x => {
-    console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
-  })
-  .catch(err => {
-    console.error('Error connecting to mongo', err)
-  });
+	.connect('mongodb://localhost/lab-express-cinema', {
+		useNewUrlParser: true
+	})
+	.then(x => {
+		console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
+	})
+	.catch(err => {
+		console.error('Error connecting to mongo', err)
+	});
 
 const app_name = require('./package.json').name;
 const debug = require('debug')(`${app_name}:${path.basename(__filename).split('.')[0]}`);
@@ -27,17 +30,19 @@ const app = express();
 // Middleware Setup
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({
+	extended: false
+}));
 app.use(cookieParser());
 
 // Express View engine setup
 
 app.use(require('node-sass-middleware')({
-  src:  path.join(__dirname, 'public'),
-  dest: path.join(__dirname, 'public'),
-  sourceMap: true
+	src: path.join(__dirname, 'public'),
+	dest: path.join(__dirname, 'public'),
+	sourceMap: true
 }));
-      
+
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
@@ -49,7 +54,15 @@ app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
 // default value for title local
 app.locals.title = 'Express - Generated with IronGenerator';
 
-
+app.get('/movies', (req, res, next) => {
+	Movie.find({})
+		.then(movies => {
+			res.render('movies', {movies})
+		})
+		.catch(e => {
+			console.log('Something went wrong', e);
+		})
+});
 
 const index = require('./routes/index');
 app.use('/', index);
--- starter_code/lab-express-cinema/routes/index.js
+++ starter_code/lab-express-cinema/routes/index.js
@@ -6,4 +6,6 @@ router.get('/', (req, res, next) => {
   res.render('index', {title: 'Cinema Ironhack'});
 });
 
+
+
 module.exports = router;
