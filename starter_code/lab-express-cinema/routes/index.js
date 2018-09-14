757e41847036c6a8cf5a53e55f3a886255027dea
RaulG <guiaraul@gmail.com>
2018-09-14 15:33:15 +0200

    List the movies

--- starter_code/lab-express-cinema/routes/index.js
+++ starter_code/lab-express-cinema/routes/index.js
@@ -1,9 +1,16 @@
 const express = require('express');
-const router  = express.Router();
-
+const router = express.Router();
+const Movie = require('../models/Movie')
 /* GET home page */
 router.get('/', (req, res, next) => {
   res.render('home');
 });
+router.get('/movies', (req, res, next)=>{
+  Movie.find()
+  .then(movies=>{
+    res.render('movies',{movies})
+
+  })
+})
 
 module.exports = router;
