838febdeef2d3e58c70e454efb1a11a976471544
Gonzalo Quiroz <kiyegoo@gmail.com>
2018-10-03 00:39:04 -0500

    Se finaliza funcionalidad principal, solo falta pagina de inicio

---
+++ cinema-ironhack/routes/index.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const router = express.Router();
+const Movie = require("../models/Movie");
+
+/* GET home page */
+router.get("/", (req, res, next) => {
+  res.render("index");
+});
+
+router.get("/movies", (req, res) => {
+  Movie.find({})
+    .then(movies => {
+      console.log(movies);
+
+      res.render("movies", {
+        header: "Ironhack Cinema",
+        subtitle: "Click on the movie to checkthe showtimes!",
+        movies
+      });
+    })
+    .catch(err => {
+      throw err;
+    });
+});
+
+router.get("/movie/:id", (req, res) => {
+  Movie.findById(req.params.id)
+    .then(movie => {
+      res.render("movieDetails", movie);
+    })
+    .catch(err => {
+      throw err;
+    });
+});
+
+module.exports = router;
