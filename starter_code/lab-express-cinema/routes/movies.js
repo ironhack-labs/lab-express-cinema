f582a0f4e1d29c5ba4c6afbc56cb03fd29957dcb
Cesar Lugo <cesarlugo00@hotmail.com>
2018-10-14 14:32:08 +0200

    conected data Base

--- starter_code/lab-express-cinema/routes/movies.js
+++ starter_code/lab-express-cinema/routes/movies.js
@@ -1,8 +1,33 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+
+mongoose.connect('mongodb://localhost/cinemaApp',{
+  useNewUrlParser: true,
+});
+
+
+const movieSchema = new Schema ({
+  title: String,
+  director: String,
+  image: String,
+  stars: Array,
+  description: String,
+  showtimes: Array
+})
+
+const Movie = mongoose.model("Movie", movieSchema);
+const moviesArray = [];
+
 
 /* GET movies page. */
 router.get('/', function(req, res, next) {
+  Movie.find()
+    .then(movie =>
+      res.render('movies', {movie}))
+
   res.render('movies', { title: 'Movies' });
 });
 
