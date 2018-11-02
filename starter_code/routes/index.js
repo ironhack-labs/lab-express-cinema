bd7f1eb59a2fb1379bd5774346f90c621e8b7638
Johanna <johanna.skuhra@gmail.com>
2018-11-02 15:59:48 +0100

    done

--- starter_code/routes/index.js
+++ starter_code/routes/index.js
@@ -1,9 +1,25 @@
 const express = require('express');
-const router  = express.Router();
+const router = express.Router();
+const Movie = require('../models/movie');
 
 /* GET home page */
 router.get('/', (req, res, next) => {
   res.render('index');
 });
+
+router.get('/movies', (req, res, next) => {
+  Movie.find()
+    .then(movies => {
+      console.log("test", movies)
+      res.render("movies", { movies: movies })
+    })
+    .catch(error => {
+      console.log(error)
+    })
+
+});
+
+
+
 
 module.exports = router;
