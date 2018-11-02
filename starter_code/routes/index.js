df9795f9666c241a06b69cde4255e3f84ab081a1
Felicitas Deutges <deutges.ironhack@gmail.com>
2018-11-02 14:43:45 +0100

    Done

--- starter_code/routes/index.js
+++ starter_code/routes/index.js
@@ -1,9 +1,33 @@
 const express = require('express');
-const router  = express.Router();
+const router = express.Router();
+const Movies = require('../models/Movie')
 
 /* GET home page */
 router.get('/', (req, res, next) => {
   res.render('index');
 });
+
+router.get('/movies', (req, res, next) => {
+  Movies.find()
+    .then(moviesFromDb => {
+      console.log("rendering", moviesFromDb)
+      res.render('movies', {
+        moviesInput: moviesFromDb
+      })
+    })
+    .catch(err => console.log(err))
+});
+
+router.get('/movies/:id', (req, res, next) => {
+  let id = req.params.id //correct??
+  Movies.findById(id)
+    .then(movieFromDb => {
+      console.log(movieFromDb)
+      res.render('selectedMovie', {
+        movie: movieFromDb
+      })
+    })
+    .catch(err => console.log(err))
+})
 
 module.exports = router;
