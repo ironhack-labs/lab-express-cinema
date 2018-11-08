084dee55bf94c4d8f55d02a73dba33501c32dfd5
MacBook Air <macbookair@MacBooks-MacBook-Air.local>
2018-11-08 19:27:19 -0200

    [SAO FT1 W4D4] Cinema | Joao Caetano

--- starter_code/routes/index.js
+++ starter_code/routes/index.js
@@ -2,8 +2,23 @@ const express = require('express');
 const router  = express.Router();
 
 /* GET home page */
-router.get('/', (req, res, next) => {
+router.get('../views/index.hbs', (req, res, next) => {
   res.render('index');
 });
+
+module.exports = router;
+
+
+//get Movies
+router.get('/views/movies', (req, res, next) => {
+  Movie.find()
+    .then(movies => {
+      res.render("movies", { movies });
+    })
+    .catch(error => {
+      console.log(error)
+    })
+});
+
 
 module.exports = router;
