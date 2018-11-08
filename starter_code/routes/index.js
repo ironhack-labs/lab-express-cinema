adf0532e7d85cebaba9089e71bb676eaf4bec4d0
Sarah Connor <sarahchamorro@gmail.com>
2018-11-08 21:18:57 -0200

    finish db schema and document display, missing partials and styles

--- starter_code/routes/index.js
+++ starter_code/routes/index.js
@@ -1,9 +1,30 @@
 const express = require('express');
 const router  = express.Router();
+const Movie   = require('../models/Movie.js');
 
 /* GET home page */
 router.get('/', (req, res, next) => {
   res.render('index');
+});
+
+router.get('/movies', (req, res, next) => {
+  Movie.find()
+    .then(movies => {
+      res.render('movies', {movies});
+    })
+    .catch(error => {
+      res.render('error');
+    })
+});
+
+router.get('/movies/:id', (req, res, next) => {
+  const movieId = req.params.id;
+  Movie.find({_id: movieId})
+    .then(movieInfo => {
+      const movie = movieInfo[0];
+      console.log(movie);
+      res.render('movie-details', {movie});
+    })
 });
 
 module.exports = router;
