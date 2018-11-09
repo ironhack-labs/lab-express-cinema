954cf1a6b5d97866467afe4da5daea4cb76b012f
Andre Amancio <amancioandre@gmail.com>
2018-11-08 22:14:47 -0200

    backend done

--- starter_code/routes/index.js
+++ starter_code/routes/index.js
@@ -1,9 +1,31 @@
 const express = require('express');
 const router  = express.Router();
 
+const Movie = require('../models/movies')
+
 /* GET home page */
 router.get('/', (req, res, next) => {
   res.render('index');
 });
+
+router.get('/movies', (req, res, next) => {
+  Movie.find()
+    .then(movies => {
+      res.send(movies)
+    })
+    .catch(err => {
+      console.log(err)
+    })
+})
+
+router.get('/movies/:movieId', (req, res, next) => {
+  Movie.findOne( { _id: req.params.movieId })
+    .then(movie => {
+      res.send(movie)
+    })
+    .catch(err => {
+      console.log('fueh')
+    })
+})
 
 module.exports = router;
