04571ce81c7868ac94e42f9775fcc53fcc936159
amine <bouamama.amine@bcg.com>
2018-11-12 15:06:32 +0100

    done

--- starter_code/routes/index.js
+++ starter_code/routes/index.js
@@ -8,17 +8,27 @@ router.get('/', (req, res, next) => {
 });
 
 router.get('/movies', (req, res, next) => {
-
   Movie
     .find({}, 'title image')
     .then(movies => {
       res.render('movies', {movies});
     })
     .catch(err => {
-
+      next(err)
     });
-
-
 });
+
+
+router.get('/movie/:movieId', (req, res, next) => {
+
+  const movieId = req.params.movieId;
+  Movie.
+    findById(movieId)
+    .then(movie => {
+      res.render('movie', movie);
+    })
+    .catch(err => next(err));
+});
+
 
 module.exports = router;
