42b82d1eea9fc76ca6d96a7370cde40e42d26d22
aemartos <aemartos@gmail.com>
2018-11-14 23:55:19 +0100

    Set routes and views info

--- starter_code/routes/index.js
+++ starter_code/routes/index.js
@@ -1,9 +1,44 @@
 const express = require('express');
 const router  = express.Router();
+const Movie = require('../models/Movie');
+const app_name = require('../package.json').name;
+const debug = require('debug')(`${app_name}:indexRouter`);
 
 /* GET home page */
 router.get('/', (req, res, next) => {
-  res.render('index');
+  debug(process.env.DEBUG);
+  let data = {
+    name: "home"
+  };
+  res.render('index',{data});
 });
+
+/* GET movies page */
+router.get('/movies', (req, res, next) => {
+  debug(process.env.DEBUG);
+  let data = {
+    name: "movies"
+  };
+  Movie.find().then(movies => {
+    res.render('movies',{movies, data});
+    console.log(movies);
+  })
+});
+
+/* GET individual movie page */
+router.get('/movies/:movieId/movieInfo', (req, res, next) => {
+  console.log(req.params.movieId);
+  let data = {
+    name: "movieInfo",
+    movieId: req.params.movieId
+  };
+  Movie.findOne({_id: data.movieId}).then(movie => {
+    res.render('movieInfo',{movie, data});
+  })
+});
+
+
+
+
 
 module.exports = router;
