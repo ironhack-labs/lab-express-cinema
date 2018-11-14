9e75e8e0fc323914118270db3b9c574940af4a54
AaronReina <aaron.ivan.reina@gmail.com>
2018-11-14 21:08:21 +0100

    posible fallo en el seed

--- starter_code/routes/index.js
+++ starter_code/routes/index.js
@@ -1,9 +1,28 @@
 const express = require('express');
 const router  = express.Router();
+const Movie = require('./models/Movie.js');
 
 /* GET home page */
 router.get('/', (req, res, next) => {
   res.render('index');
 });
+
+
+router.get('/movies', (req, res, next) => {
+  Movie.find()
+  .then(data => {  console.log(Movie)
+  res.render('movies',{data})})
+});
+
+// router.get('/movie_info/:id', (req, res, next) => {
+//   let movieId = req.params.id;
+//   Movie.findOne({'_id': movieId})
+//   .then(data => {
+//   res.render('movie_info', { data });
+//   })
+//   .catch(error => {
+//     console.log(error)
+//   })
+// });
 
 module.exports = router;
