894dfe424944a4953e4be076e8cd7239a802c124
Teresa Romero <tromerolozano@gmail.com>
2018-11-14 21:32:09 +0100

    done no css

--- starter_code/routes/index.js
+++ starter_code/routes/index.js
@@ -1,9 +1,27 @@
-const express = require('express');
-const router  = express.Router();
+const express = require("express");
+const router = express.Router();
+const Movie = require("../models/Movie");
 
 /* GET home page */
-router.get('/', (req, res, next) => {
-  res.render('index');
+router.get("/", (req, res, next) => {
+  res.render("index");
+});
+
+router.get("/movies", (req, res, next) => {
+  Movie.find()
+    .then(movies => {
+      res.render("movies", { movies });
+    })
+    .catch(err => {
+      console.log("error");
+    });
+});
+
+router.get("/movie/:id", (req, res, next) => {
+  let movieId = req.params.id;
+  Movie.findOne({ _id: movieId }).then(movie => {
+    res.render("movie", { movie });
+  });
 });
 
 module.exports = router;
