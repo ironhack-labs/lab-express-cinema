d609178f4bab09c6284798fe7505d470e2cfbe38
David Diez <ddiezr@gmail.com>
2018-11-16 00:23:03 +0100

    Second Commit . Web finished all styles complete

--- starter_code/models/Movie.js
+++ starter_code/models/Movie.js
@@ -2,25 +2,25 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const MovieSchema = new Schema({
-    title: {
-        type: String
-    },
-    director: {
-        type: String
-    },
-    stars: {
-        type: Array
-    },
-    image: {
-        type: String,
-        required: true
-    },
-    description: {
-        type: String
-    },
-    showtimes: {
-        type: Array
-    }
+  title: {
+    type: String
+  },
+  director: {
+    type: String
+  },
+  stars: {
+    type: Array
+  },
+  image: {
+    type: String,
+    required: true
+  },
+  description: {
+    type: String
+  },
+  showtimes: {
+    type: Array
+  }
 });
 
 const Movie = mongoose.model('Movie', MovieSchema);
