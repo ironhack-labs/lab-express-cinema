86b3201482fc18d459ac0c1bea93d71b58f631ab
Sandra Luengo <luchosandra@gmail.com>
2018-11-15 20:38:48 +0100

    Iteration 2

--- starter_code/routes/index.js
+++ starter_code/routes/index.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const router  = express.Router();
 
+module.exports = router;
+
 /* GET home page */
 router.get('/', (req, res, next) => {
   res.render('index');
 });
 
-module.exports = router;
+
+router.get('/movies', (req, res, next) => {
+  res.render('movies');
+});
+
