f60cf90210905e6d944b76f311fea7e997f642a8
Anna Zhuravleva <helloboysandgirls.az@gmail.com>
2018-11-15 22:20:03 +0100

    Iteration 3

--- starter_code/routes/index.js
+++ starter_code/routes/index.js
@@ -1,9 +1,18 @@
 const express = require('express');
 const router  = express.Router();
+const Movie = require('../models/Movie.js');
 
 /* GET home page */
 router.get('/', (req, res, next) => {
   res.render('index');
+});
+
+router.get('/movies', (req, res, next)=> {
+
+  Movie.find()
+  .then(movies => res.render('movies', {movies}))
+  .catch(err => { console.log('An error happened:', err) });
+ 
 });
 
 module.exports = router;
