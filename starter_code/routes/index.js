d7380a3c6ba0c9a50d21aa4b1e4edd352464dd46
Xiaomi Notebook Pro <xiaomi@gmailcom>
2018-11-16 09:13:36 +0100

    DE done!

--- starter_code/routes/index.js
+++ starter_code/routes/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router  = express.Router();
 const Movies = require('../models/Movie');
+const mongoose = require('mongoose');
 
 /* GET home page */
 router.get('/', (req, res, next) => {
