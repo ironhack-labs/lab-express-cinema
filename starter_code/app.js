0d46a707c6a09bc0309463082752c33fa8714fea
Fer Perez Ayala <ferp@protonmail.ch>
2018-11-16 01:04:08 +0100

    Making progress on the database calls

--- starter_code/app.js
+++ starter_code/app.js
@@ -11,7 +11,7 @@ const path         = require('path');
 
 
 mongoose
-  .connect('mongodb://localhost/starter-code', {useNewUrlParser: true})
+  .connect('mongodb://localhost/cinema-ironhack', {useNewUrlParser: true})
   .then(x => {
     console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
   })
@@ -47,7 +47,7 @@ app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
 
 
 // default value for title local
-app.locals.title = 'Express - Generated with IronGenerator';
+app.locals.title = 'Cinema Ironhack';
 
 
 
@@ -56,3 +56,4 @@ app.use('/', index);
 
 
 module.exports = app;
+
--- starter_code/routes/index.js
+++ starter_code/routes/index.js
@@ -1,9 +1,22 @@
 const express = require('express');
 const router  = express.Router();
+const Movie = require('../models/Movie.js');
 
 /* GET home page */
 router.get('/', (req, res, next) => {
   res.render('index');
 });
+
+router.get('/movies', (req, res, next) => {
+
+  Movie.find({})
+      .then(movies => {
+        console.log(movies);
+          res.render("movies", { movies });
+      })
+      .catch(error => {
+          console.log('Error loading the movies from the database:' + err)
+      })
+})
 
 module.exports = router;
