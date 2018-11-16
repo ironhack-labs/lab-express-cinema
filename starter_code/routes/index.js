fae9f346814e1f232be5c57ecad698af149397e3
cgandiaf <c.gandia.f@gmail.com>
2018-11-16 09:27:45 +0100

    Pelis

--- starter_code/routes/index.js
+++ starter_code/routes/index.js
@@ -1,9 +1,25 @@
 const express = require('express');
-const router  = express.Router();
+const router = express.Router();
+const Movies = require('../models/Movie');
+
+//console.log(movies);
 
 /* GET home page */
 router.get('/', (req, res, next) => {
   res.render('index');
 });
+
+router.get('/movies', (req, res, next) => {
+  Movies.find({})
+    .then(movies => {
+      //console.log(movies);
+      res.render('movies', {movies});
+    })
+    .catch(err => {
+      console.log(`Error al cargar las pelis ${err}`)
+    })
+
+});
+
 
 module.exports = router;
