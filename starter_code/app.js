20ecccaa2af83e011c7cb4cf454d32a88ff31723
Rene Manzano <rene@Renes-MacBook.local>
2018-11-22 20:11:12 -0600

    Done

--- starter_code/app.js
+++ starter_code/app.js
@@ -11,7 +11,7 @@ const path         = require('path');
 
 
 mongoose
-  .connect('mongodb://localhost/starter-code', {useNewUrlParser: true})
+  .connect('mongodb://localhost/movies', {useNewUrlParser: true})
   .then(x => {
     console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
   })
@@ -54,5 +54,8 @@ app.locals.title = 'Express - Generated with IronGenerator';
 const index = require('./routes/index');
 app.use('/', index);
 
+const PORT = process.env.PORT || 3000
+
+app.listen(PORT, () => { console.log(`localhost:${PORT}`) })
 
 module.exports = app;
