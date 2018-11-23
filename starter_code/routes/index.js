fdf8983b1c271800139bc6a4d5826feb65d1981f
Carlos Rivera <carlos.rivera.cabrera@gmail.com>
2018-11-22 20:53:07 -0600

    Iteration 3 | List the Movies. Done

--- starter_code/routes/index.js
+++ starter_code/routes/index.js
@@ -1,9 +1,22 @@
 const express = require('express');
 const router  = express.Router();
+const Movie = require('../models/Movie')
 
 /* GET home page */
 router.get('/', (req, res, next) => {
   res.render('index');
 });
 
+router.get('/movies', (req, res, next) => {
+  Movie.find()
+  .then(movies=>{
+    // console.log(movies)
+    res.render('movies',{movies});
+    // res.send(movies)
+  })
+  .catch(err=>{
+    res.render('error')
+    // res.send(err)
+  })
+});
 module.exports = router;
