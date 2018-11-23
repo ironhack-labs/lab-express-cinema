b65ccf0a2398442ae22f8d55a63234b64d5764d3
conviertemx <ro@convierte.mx>
2018-11-23 08:56:57 -0600

    almost done

--- starter_code/routes/index.js
+++ starter_code/routes/index.js
@@ -1,9 +1,27 @@
 const express = require('express');
 const router  = express.Router();
+const Movie    = require('../models/Movie');
 
 /* GET home page */
 router.get('/', (req, res, next) => {
-  res.render('index');
+  Movie.find()
+  .then(movies => {
+    res.render('index', { movies });
+  })
+  .catch(err => {
+    res.render('index')
+  })
+});
+
+router.get('/movies/:id', (req, res) => {
+  const { id } = req.params; //the best way to get the id info is using req.params. 
+  Movie.findById(id)
+  .then(movie => {
+    res.render('movie', movie);
+  })
+  .catch(err => {
+    res.render('detail');
+  })
 });
 
 module.exports = router;
