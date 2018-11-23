25738846b510c7099c8a558a8ef48d6c11ed45f1
MefitHp <mefitDev@gmail.com>
2018-11-22 21:08:34 -0600

    Done

---
+++ starter_code/models/Movie.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const movieSchema = new Schema({
+    title: String,
+    director: String,
+    stars: Array,
+    image: String,
+    description: String,
+    showtimes: Array,
+}, {
+        timestamps: {
+            createdAt: true,
+            updatedAt: true,
+        },
+        versionKey: false
+    })
+
+module.exports = mongoose.model('Movie', movieSchema)
--- starter_code/routes/index.js
+++ starter_code/routes/index.js
@@ -1,9 +1,29 @@
 const express = require('express');
-const router  = express.Router();
+const router = express.Router();
+const Movie = require('../models/Movie')
 
 /* GET home page */
 router.get('/', (req, res, next) => {
   res.render('index');
 });
+
+router.get('/movies', (req, res, next) => {
+  Movie.find()
+    .then(movies => {
+      res.render('movies', { movies });
+    }).catch(err => {
+      console.log(err)
+    })
+});
+
+router.get('/movies/:id', (req, res) => {
+  const { id } = req.params
+  Movie.findById(id)
+    .then(movie => {
+      res.render('movie-detail', movie)
+    }).catch(err => {
+      console.log(err)
+    })
+})
 
 module.exports = router;
