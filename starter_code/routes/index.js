5042489ceb391d023d1df356100406fc6bfa2a13
Gerardo <gera@MacBook-de-Gerardo.local>
2018-11-22 20:58:41 -0600

    Iteración final, estilos añadidos

--- starter_code/routes/index.js
+++ starter_code/routes/index.js
@@ -1,9 +1,33 @@
-const express = require('express');
-const router  = express.Router();
+const express = require('express')
+const routers  = express.Router()
+const Movie = require('../models/Movie')
 
 /* GET home page */
-router.get('/', (req, res, next) => {
-  res.render('index');
-});
+routers.get('/', (req, res, next) => {
+    res.render('index')
+})
 
-module.exports = router;
+routers.get('/movies', (req, res, next) => {
+  Movie.find()
+  .then( movies => {
+    res.render('movies',{movies})
+    
+  })
+  .catch( error => {
+    res.render('movies')
+  })
+})
+
+routers.get('/movie/:id', (req,res) => {
+  const {id} = req.params
+  Movie.findById(id)
+  .then( movie => {
+    console.log(movie)
+    res.render('movie',movie)
+  })
+  .catch( err => {
+    console.log(err)
+  })
+})
+
+module.exports = routers
