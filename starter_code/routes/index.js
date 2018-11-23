fb89f2d77094b252b175d437e985a8ebad408e69
sanmiranda <sanmiranda@hotmail.com>
2018-11-22 22:55:49 -0600

    va version con poco diseño

--- starter_code/routes/index.js
+++ starter_code/routes/index.js
@@ -1,9 +1,34 @@
 const express = require('express');
 const router  = express.Router();
+const Movie = require('../models/Movie')
+
 
 /* GET home page */
 router.get('/', (req, res, next) => {
   res.render('index');
 });
+
+
+router.get('/list', (req, res, next) => {
+Movie.find()
+.then(movies=>{
+  res.render('movies', {movies})
+})
+.catch(err=>{
+  res.render('movies')
+})
+
+router.get('/movies/:id', (req, res) => {
+  const { id } = req.params;
+  Movie.findById(id)
+  .then(movie => {
+    res.render('detail', movie);
+  })
+  .catch(err => {
+    res.render('detail');
+  })
+});
+
+})
 
 module.exports = router;
