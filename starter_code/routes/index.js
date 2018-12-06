804a92d50d6494289d616fa49a1360df7b0e8f17
Merwan Guerrib <merwanguerrib@gmail.com>
2018-12-06 21:47:50 +0100

    iteration 4 done

--- starter_code/routes/index.js
+++ starter_code/routes/index.js
@@ -1,19 +1,32 @@
-const express = require('express');
-const router  = express.Router();
-const Movie = require('../models/Movie')
+const express = require("express");
+const router = express.Router();
+const Movie = require("../models/Movie");
+
 /* GET home page */
-router.get('/', (req, res, next) => {
-  res.render('index');
+router.get("/", (req, res, next) => {
+  res.render("index");
 });
 
-router.get('/movies', (req, res, next) => {
+/* GET movies */
+router.get("/movies", (req, res, next) => {
   Movie.find()
-  .then( movies => {
-  res.render('movies', {list : movies});
-})
-  .catch (error => {
-    next (error)
-  });
+    .then(movies => {
+      res.render("movies", { list: movies });
+    })
+    .catch(error => {
+      next(error);
+    });
+});
+
+//GET /movie/:id
+router.get('/movie/:id', (req, res, next) => {
+  Movie.findOne({_id:req.params.id})
+    .then(movie => {
+      res.render("movie", {movie});
+    })
+    .catch(error => {
+      next(error);
+    });
 });
 
 module.exports = router;
