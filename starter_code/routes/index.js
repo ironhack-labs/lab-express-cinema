01a3d8c74bb9386bb14c55c6b2819f2a7a3ac01a
Antoine Ironhack <antoineironhack@MacBook-Pro-de-Antoine.local>
2018-12-06 21:55:58 +0100

    first-push

--- starter_code/routes/index.js
+++ starter_code/routes/index.js
@@ -1,9 +1,31 @@
 const express = require('express');
 const router  = express.Router();
+const Movie   = require('../models/movie')
 
 /* GET home page */
 router.get('/', (req, res, next) => {
   res.render('index');
 });
+
+router.get('/movies', (req, res, next) => {
+  Movie.find()
+  .then(movies=>{
+    res.render('movies', {movies});
+  })
+  .catch(err=>{
+    next(err)
+  });
+});
+
+router.get('/movie/:id', (req, res, next) => {
+  Movie.findOne({_id:req.params.id})
+  .then(movie=>{
+    res.render('movie', {movie});
+  })
+  .catch(err=>{
+    next(err)
+  })
+});
+
 
 module.exports = router;
