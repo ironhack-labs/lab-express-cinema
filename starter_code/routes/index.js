7a7167718c1655e4f63fb1c049239bc0d4dcf3fb
Juliana <juliana.jeanmarie972@gmail.com>
2018-12-06 21:32:54 +0100

    done

--- starter_code/routes/index.js
+++ starter_code/routes/index.js
@@ -1,9 +1,34 @@
-const express = require('express');
-const router  = express.Router();
+const express = require("express");
+const router = express.Router();
+
+const Movie = require("../models/Movie");
 
 /* GET home page */
-router.get('/', (req, res, next) => {
-  res.render('index');
+router.get("/", (req, res, next) => {
+  res.render("index", { title: "Cinema IronHack" });
+});
+
+router.get("/movies", (req, res, next) => {
+  Movie.find({}, function(err, movies) {
+    if (!err) {
+      console.log(movies);
+      res.render("movies", { movies });
+    } else {
+      throw err;
+    }
+  });
+});
+
+router.get("/movie/:id", (req, res, next) => {
+  Movie.findById(req.params.id, function(err, movie) {
+    console.log("id: ", req.params.id);
+    if (!err) {
+      console.log("one movie : ", movie);
+      res.render("movie", movie);
+    } else {
+      throw err;
+    }
+  });
 });
 
 module.exports = router;
