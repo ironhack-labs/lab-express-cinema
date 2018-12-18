ab8000bfdc9b2a6dda9abf43be38385171f76abe
StephGidde <steph.gidde@gmx.de>
2018-12-18 21:43:49 +0100

    almost done

---
+++ starter_code/models/Movie.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const movieSchema = new Schema({
+  title: String,
+  director: String,
+  image: String,
+  description: String
+});
+
+const Movie = mongoose.model("Movie", movieSchema);
+module.exports = Movie;
--- starter_code/routes/index.js
+++ starter_code/routes/index.js
@@ -1,9 +1,22 @@
-const express = require('express');
-const router  = express.Router();
+const express = require("express");
+const router = express.Router();
+const Movie = require("../models/Movie");
 
 /* GET home page */
-router.get('/', (req, res, next) => {
-  res.render('index');
+router.get("/", (req, res, next) => {
+  res.render("index");
+});
+
+router.get("/movies", (req, res) => {
+  Movie.find().then(movies => {
+    res.render("movies", { moviesArr: movies });
+  });
+});
+
+router.get("/movieinfo/:movieid", (req, res) => {
+  Movie.findOne({ _id: req.params.movieid }).then(movies => {
+    res.render("movieinfo", { moviesArr: movies });
+  });
 });
 
 module.exports = router;
