18d5ed85db8572f874aec9d9b1a1d7282340ed37
Hendrik <hendrik.wermser@ironhack.com>
2018-12-18 21:49:37 +0100

    done.

--- starter_code/routes/index.js
+++ starter_code/routes/index.js
@@ -1,9 +1,23 @@
 const express = require('express');
 const router  = express.Router();
 
+const Movie = require('../models/Movie');
+
 /* GET home page */
-router.get('/', (req, res, next) => {
+router.get('/', (req, res) => {
   res.render('index');
 });
+
+router.get('/movies', (req, res) => {
+  Movie.find().then((movies) => {
+    res.render('movies', { movies })
+  })  
+})
+
+router.get('/movie/:movie_id', (req, res) => {
+  Movie.findById(req.params.movie_id).then((movie) => {
+    res.render('movie', movie)
+  })
+})
 
 module.exports = router;
