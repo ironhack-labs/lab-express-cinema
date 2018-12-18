4e7b6532e523764d4ab05dc7639c81a1b316a024
beckyxde <beckyxde@users.noreply.github.com>
2018-12-18 22:42:14 +0100

    done

--- starter_code/routes/index.js
+++ starter_code/routes/index.js
@@ -1,9 +1,22 @@
 const express = require('express');
 const router  = express.Router();
+const Movie = require('../models/Movie')
 
 /* GET home page */
 router.get('/', (req, res, next) => {
   res.render('index');
 });
+
+router.get('/movies', (req, res) => {
+  Movie.find().then((movies) => {
+    res.render('movies', {movies}) 
+  })
+})
+
+router.get('/movies/:id', (req, res) => {
+  Book.findOne({_id: req.params.id}).then((movie) => {
+    res.render('movie', {movie}) 
+  })
+})
 
 module.exports = router;
