eeeb3e868e4fce729b635416bce147bdc08962f2
AB Rao <anirudhrao24@gmail.com>
2018-12-18 22:45:32 +0100

    done

---
+++ starter_code/models/Movie.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+const Schema   = mongoose.Schema;
+
+const moviesSchema = new Schema({
+  title: String,
+  director: String,
+  stars: [String],
+  image: String,
+  description: String,
+  showtimes: [String]
+});
+
+const Movies = mongoose.model("movies", moviesSchema);
+
+module.exports = Movies;
--- starter_code/routes/index.js
+++ starter_code/routes/index.js
@@ -1,9 +1,33 @@
 const express = require('express');
 const router  = express.Router();
+const Movie = require('../models/Movie.js');
+
 
 /* GET home page */
 router.get('/', (req, res, next) => {
   res.render('index');
+});
+
+router.get('/movies', (req, res) => {
+  Movie.find().then(movies => {
+      //console.log(books);
+      res.render('movies',{moviesArray: movies})
+    })
+    .catch(error => {
+      console.log(error)
+    })
+});
+
+router.get('/movies/:id', (req, res) => {
+//  console.log(req.params.id);
+
+  Movie.findOne({_id: req.params.id}).then(document => {
+      //console.log(books);
+      res.render('moviePage',{movie: document})
+    })
+    .catch(error => {
+      console.log(error)
+    })
 });
 
 module.exports = router;
