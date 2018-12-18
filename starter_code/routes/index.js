1cd76ec274a406917fc1a2ad5c5f932c4cc60772
Juan Ignacio Benito <juanignaciobenito@Juans-MacBook-Air.local>
2018-12-18 21:49:16 +0100

    done

--- starter_code/routes/index.js
+++ starter_code/routes/index.js
@@ -1,9 +1,22 @@
 const express = require('express');
 const router  = express.Router();
+const Movie = require('../models/movie')
 
 /* GET home page */
 router.get('/', (req, res, next) => {
   res.render('index');
 });
+
+router.get('/movies',(req,res)=>{
+  Movie.find().then((movies)=>{
+    res.render('movies',{moviesArray:movies})
+  })
+})
+
+router.get('/movies/:movieId',(req,res)=>{
+  Movie.findOne({_id:req.params.movieId}).then((movie)=>{
+    res.render('description',{movie})
+  })
+})
 
 module.exports = router;
