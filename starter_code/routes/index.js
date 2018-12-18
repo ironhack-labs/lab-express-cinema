8b3a5ac5352ca0e1290d5117405cf580ff9ef343
Christian Standke <c.standke@gmail.com>
2018-12-18 22:07:11 +0100

    done!

--- starter_code/routes/index.js
+++ starter_code/routes/index.js
@@ -1,9 +1,25 @@
 const express = require('express');
 const router  = express.Router();
+const Movie = require('../models/movie');
 
 /* GET home page */
 router.get('/', (req, res, next) => {
   res.render('index');
 });
+
+router.get('/movies', (req, res, next) => {
+  Movie.find().then((movies) => {
+    // console.log(movies);
+    res.render('movies',{movie: movies});
+  })
+});
+
+router.get('/movie/:movieId', (req, res, next) => {
+  Movie.findOne({_id:req.params.movieId}).then((movie) => {
+    console.log(movie);
+    res.render('movie',{movie: movie});
+  })
+});
+
 
 module.exports = router;
