df1c0c12f22e72804f0ef9dd385419f4c3eb6771
Luise Schneider <luise.schneider@berlin.de>
2018-12-19 22:43:49 +0100

    added styling

--- starter_code/routes/index.js
+++ starter_code/routes/index.js
@@ -1,9 +1,30 @@
-const express = require('express');
-const router  = express.Router();
+const express = require("express");
+const router = express.Router();
+const Movie = require("../models/Movie.js");
 
 /* GET home page */
-router.get('/', (req, res, next) => {
-  res.render('index');
+router.get("/", (req, res, next) => {
+  res.render("index");
+});
+
+router.get("/movies", (req, res, next) => {
+  Movie.find()
+    .then(movies => {
+      res.render("movies", { movieArray: movies });
+    })
+    .catch(error => {
+      console.log(error);
+    });
+});
+
+router.get("/movies/:movieId", (req, res, next) => {
+  Movie.findOne({ _id: req.params.movieId })
+    .then(document => {
+      res.render("details", { movie: document });
+    })
+    .catch(error => {
+      console.log(error);
+    });
 });
 
 module.exports = router;
