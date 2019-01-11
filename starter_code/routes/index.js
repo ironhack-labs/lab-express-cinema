8a017c917b7c45f2117ec25e333dae320b9ad179
Diego Rodríguez <diegorodriguezgzz@gmail.com>
2019-01-10 23:36:42 -0600

    Avance

--- starter_code/routes/index.js
+++ starter_code/routes/index.js
@@ -1,9 +1,28 @@
 const express = require('express');
-const router  = express.Router();
+const router = express.Router();
+const Movie = require('../models/Movie')
+// const movies = require('../bin/seeds')
 
 /* GET home page */
 router.get('/', (req, res, next) => {
   res.render('index');
+});
+
+router.get("/movies", (req, res) => {
+  Movie.find({})
+    .then(movies => {
+      res.render('movies', { movies });
+    })
+    .catch(err => console.log(err));
+});
+
+router.get("/movie/:id", (req, res) => {
+  let movieId = req.params.id;
+  Movie.findOne({"_id" : movieId})
+    .then(movie => {
+      res.render('movie', { movie });
+    })
+    .catch(err => console.log(err));
 });
 
 module.exports = router;
