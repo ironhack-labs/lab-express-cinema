776baafb7667f2cf6030f830ba2d527a685f0bbf
DanielGoycochea <danielgoycocheardz@gmail.com>
2019-01-12 10:07:24 -0600

    parcial

--- starter_code/routes/index.js
+++ starter_code/routes/index.js
@@ -1,9 +1,31 @@
 const express = require('express');
 const router  = express.Router();
+const Movies = require ('../models/Movies')
 
 /* GET home page */
 router.get('/', (req, res, next) => {
   res.render('index');
 });
+
+router.get('/movies', (req, res)=>{
+  Movies.find()
+  .then(movies=>{
+    res.render('movies',{movies})
+  })
+  .catch(err=>{
+    console.log(err)
+  })
+})
+router.get('/movies/:id', (req, res)=>{
+  let moviesId = req.params.id
+  console.log(moviesId);
+  Movies.findOne({'_id':moviesId})
+  .then((movies)=>{
+    res.render('movies-detalle', {movies})
+  })
+  .catch(err=>{
+    console.log(err)
+  })
+})
 
 module.exports = router;
