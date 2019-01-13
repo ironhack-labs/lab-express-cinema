8726b8b21b682ce59a2207c0984b9519ff09b438
Jose Duarte <duarte@Joses-MacBook-Pro.local>
2019-01-13 08:29:05 -0600

    done

--- starter_code/routes/index.js
+++ starter_code/routes/index.js
@@ -1,9 +1,30 @@
 const express = require('express');
 const router  = express.Router();
+const Movies = require('../models/Movie');
 
 /* GET home page */
 router.get('/', (req, res, next) => {
   res.render('index');
+});
+
+
+router.get('/movies', (req,res) => {
+  Movies.find()
+  .then(movies => {
+    res.render('movies', {movies});
+  })
+  .catch(err => {
+    console.log(err);
+  })
+});
+
+router.get('/movies/:id', (req, res)=> {
+  let movieId = req.params.id;
+  Movies.findOne({'_id': movieId})
+  .then(movie=>
+    {
+      res.render('movie-detail', {movie});
+    })
 });
 
 module.exports = router;
