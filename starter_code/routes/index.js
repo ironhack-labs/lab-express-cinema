9124670d357843eec22eedfb9db5ec7a11c47a2d
Goldziher <nhirschfeld@gmail.com>
2019-01-31 14:38:56 +0100

    finished

--- starter_code/routes/index.js
+++ starter_code/routes/index.js
@@ -1,9 +1,35 @@
 const express = require('express');
 const router  = express.Router();
+const Movie = require("../models/Movie.js")
 
 /* GET home page */
 router.get('/', (req, res, next) => {
   res.render('index');
+});
+
+router.get('/movies', (req, res, next) => {
+  Movie.find()
+    .then(data => {
+      console.log(data)
+      res.render("movies", {
+        movies: data
+      });
+    })
+    .catch(error => {
+      console.log(error)
+    })
+});
+
+router.get('/movieinfo/:id', (req, res, next) => {
+  Movie.findOne({"_id": req.params.id})
+    .then(data => {
+      res.render("movieinfo", {
+        movie: data
+      });
+    })
+    .catch(error => {
+      console.log(error)
+    })
 });
 
 module.exports = router;
