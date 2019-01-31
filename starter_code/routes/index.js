79ca521742f481e15e2816fa0adb57d7409003dc
Rojiblanco182 <robertosalinas1988@gmail.com>
2019-01-31 23:17:10 +0100

    nothing works

--- starter_code/routes/index.js
+++ starter_code/routes/index.js
@@ -1,9 +1,22 @@
 const express = require('express');
-const router  = express.Router();
+const router = express.Router();
+const Movie = require('../models/Movie');
+
 
 /* GET home page */
 router.get('/', (req, res, next) => {
   res.render('index');
+});
+
+router.get("/movies", (req, res) => {
+  Movie.find()
+    .then(movies => {
+      console.log(movies)
+      res.render("movies", { Movie })
+    })
+    .catch(() => {
+      (console.error('Error connecting to mongo', err))
+    })
 });
 
 module.exports = router;
