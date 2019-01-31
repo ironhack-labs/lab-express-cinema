78e46821947d744a5c37cb295adabaeaf5f5600d
Marianna <mddobal@gmail.com>
2019-01-31 20:35:24 -0200

    funcional mas não bonito

--- starter_code/routes/index.js
+++ starter_code/routes/index.js
@@ -1,9 +1,34 @@
 const express = require('express');
-const router  = express.Router();
+const router = express.Router();
+const Movie = require('../models/Movie');
 
 /* GET home page */
 router.get('/', (req, res, next) => {
   res.render('index');
+});
+
+// GET movies page
+router.get('/movies', (req, res, next) => {
+  Movie.find()
+    .then((movies) => {
+      console.log(movies);
+      res.render('movies', { movies });
+    })
+    .catch(err => {
+      console.log(err);
+    });
+});
+
+// GET single movie page
+router.get('/movie/:movieId', (req, res, next) => {
+  Movie.find({_id: req.params.movieId})
+    .then((movie) => {
+      console.log(movie);
+      res.render('movie', { movie });
+    })
+    .catch(err => {
+      console.log(err);
+    });
 });
 
 module.exports = router;
