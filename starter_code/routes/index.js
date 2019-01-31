a587c724af0c27d51c6919b7dcd5b20203c2b7c6
fbath <phoenixa@gmx.de>
2019-01-31 17:55:50 +0100

    done

--- starter_code/routes/index.js
+++ starter_code/routes/index.js
@@ -1,9 +1,32 @@
 const express = require('express');
 const router  = express.Router();
+const Movie = require('../models/Movie.js');
 
 /* GET home page */
 router.get('/', (req, res, next) => {
   res.render('index');
 });
+
+/* GET /movies */
+router.get('/movies', (req, res, next) => {
+  Movie.find()
+  .then(movies => {
+    res.render('movies', {movies});
+  })
+  .catch(error => {
+    console.log(error)
+  })
+})
+
+router.get('/movies/:id',(req,res, next) => {
+  Movie.findOne({_id: req.params.id})
+  .then(movie => {
+    res.render('movies-details', {movie})
+  })
+  .catch((err)=>{
+    console.log(err)
+  })
+})
+
 
 module.exports = router;
