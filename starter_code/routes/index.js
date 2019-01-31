6e02d17db7c7208adafa4b0d500cfb0db56d20bf
Víctor Bañuls <vicbansax@gmail.com>
2019-01-31 22:14:51 +0100

    First Commit

--- starter_code/routes/index.js
+++ starter_code/routes/index.js
@@ -1,9 +1,34 @@
 const express = require('express');
 const router  = express.Router();
+const Movie   = require('../models/Movie.js')
 
 /* GET home page */
 router.get('/', (req, res, next) => {
   res.render('index');
 });
+
+
+router.get("/movies", (req, res, next) => {
+  Movie.find()
+  .then(movies => {
+    console.log(movies)
+    res.render("movies", {movies})
+  })
+  .catch(() => {
+    (console.error('Error connecting to mongo', err))
+  })
+});
+
+router.get("/movies/:id", (req, res, next)=>{
+  Movie.findById(req.params.id)
+  .then(movie => {
+    console.log(movie)
+    res.render("movieDetail", {movie})
+  })
+  .catch(()=> {
+    (console.error('Error connecting to mongo', err))
+  })
+})
+
 
 module.exports = router;
