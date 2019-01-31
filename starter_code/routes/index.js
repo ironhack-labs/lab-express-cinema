811c4423d29f0dc2062cb9fa2534b5138b72349a
Julia Miller <ihre.hexenhafte.hoheit@gmail.com>
2019-01-31 14:59:47 +0100

    done

--- starter_code/routes/index.js
+++ starter_code/routes/index.js
@@ -1,9 +1,31 @@
 const express = require('express');
 const router  = express.Router();
+const Movie = require('../models/Movie.js');
 
 /* GET home page */
 router.get('/', (req, res, next) => {
   res.render('index');
+});
+
+router.get('/movies', (req, res, next) => {
+  Movie.find()
+    .then(movies => {
+      res.render('movies', { movies });
+    })
+    .catch(error => {
+      console.log("Error:", error)
+    })
+});
+
+router.get('/movie/:id', (req, res, next) => {
+  Movie.findById(req.params.id)
+    .then(movie => {
+      res.render('movie-detail', { movie });
+      console.log(movie)
+    })
+    .catch(error => {
+      console.log("Error:", error)
+    })
 });
 
 module.exports = router;
