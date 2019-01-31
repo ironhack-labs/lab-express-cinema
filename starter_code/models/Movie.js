1acf15a5c1c84fdbdeaab29b4793884d3459fcfb
Bruno Dolce <brudolce@gmail.com>
2019-01-31 19:59:46 -0300

    done

---
+++ starter_code/models/Movie.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const MovieSchema = new Schema({
+  title:{type:String, default:"ironMovie"},
+  director:String,
+  stars:Array,
+  image:String,
+  description:String,
+  showtimes:Array
+})
+
+const Movie = mongoose.model("Movie", MovieSchema);
+
+
+module.exports = Movie; 
--- starter_code/routes/index.js
+++ starter_code/routes/index.js
@@ -1,9 +1,27 @@
 const express = require('express');
 const router  = express.Router();
+const Movie = require('../models/Movie');
 
 /* GET home page */
 router.get('/', (req, res, next) => {
   res.render('index');
 });
+
+router.get('/movies', (req, res, next) => {
+  Movie.find().then(movies =>{
+    data = movies;
+    console.log(`paranaue: ${data}`);
+    res.render('movies', {data} );
+  })
+});
+
+router.get('/movies/:id', (req, res, next) => {
+  Movie.findOne({_id: req.params.id}).then(movies =>{
+    data2 = movies;
+    console.log(`capoeira mata um ${data}`);
+    res.render('detail', {data2} );
+  })
+});
+
 
 module.exports = router;
