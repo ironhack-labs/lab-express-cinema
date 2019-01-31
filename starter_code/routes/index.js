8556ecb642fcc618d66e2d660dab3e27810ea00d
Marvin Artioli;` <marvin.artioli@gmail.com>
2019-01-31 14:59:24 +0100

    BER Federico and Marvin done

--- starter_code/routes/index.js
+++ starter_code/routes/index.js
@@ -1,9 +1,33 @@
 const express = require('express');
 const router  = express.Router();
+const Movie = require('../models/Movie.js')
 
 /* GET home page */
 router.get('/', (req, res, next) => {
   res.render('index');
 });
 
+router.get('/movies', (req, res, next)=> {
+  Movie.find() // Find all the movies (no filter)
+  .then(movies => {
+    console.log(movies)
+    res.render('movies', {
+      movies
+    });
+    })
+    .catch(error => {
+      console.log(error)
+    })
+})
+
+router.get('/movie/:id', (req, res, next) => {
+  let movieId = req.params.id;
+  Movie.findOne({'_id': movieId}) //Book.findById(bookId) is the same
+    .then(movie => {
+      res.render("movie-detail", { movie })
+    })
+    .catch(error => {
+      console.log(error)
+    })
+});
 module.exports = router;
