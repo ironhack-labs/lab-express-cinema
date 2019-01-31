6e419a8eabbb77ab475560b0531c1d456f63daf7
Matheus Araujo <abarbierijunior@gmail.com>
2019-01-31 21:09:56 -0200

    [SAO FT2 - DE - W4D4] Matheus Barbieri

---
+++ starter_code/models/Movie.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+
+const { Schema } = mongoose;
+
+const movieSchema = new Schema({
+  title: String,
+  director: String,
+  stars: Array,
+  image: String,
+  description: String,
+  showtimes: Array
+});
+
+const Movie = mongoose.model('Movie', movieSchema);
+
+module.exports = Movie;
--- starter_code/routes/index.js
+++ starter_code/routes/index.js
@@ -1,9 +1,35 @@
 const express = require('express');
-const router  = express.Router();
+const router = express.Router();
 
-/* GET home page */
-router.get('/', (req, res, next) => {
+const Movie = require('../models/Movie.js');
+
+router.get('/', (req, res) => {
   res.render('index');
 });
+
+router.get('/movies', (req, res) => {
+  Movie.find()
+    .then((movies) => {
+      console.log(movies);
+      res.render('movies', { movies });
+      // res.send(movies);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+});
+
+router.get('/movie/:movieId', (req, res) => {
+  Movie.find({ _id: req.params.movieId })
+    .then((movie) => {
+      console.log('filme', movie);
+      res.render('movie', { movie });
+      // res.send(movies);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+});
+
 
 module.exports = router;
