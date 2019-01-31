00d6107cdf9beef7ad8656bf273c6ac5d73cbeb6
André Sebastian <a.sebastian@posteo.de>
2019-01-31 16:08:16 +0100

    First commit. Done

--- starter_code/routes/index.js
+++ starter_code/routes/index.js
@@ -1,9 +1,32 @@
 const express = require('express');
 const router  = express.Router();
+const Movie = require('../models/Movie');
 
 /* GET home page */
 router.get('/', (req, res, next) => {
   res.render('index');
+});
+
+router.get('/movies', (req, res, next) => {
+  Movie.find()
+  .then(movies => {
+    res.render("movies", {movies: movies});
+    console.log(movies);
+  })
+  .catch(error => {
+    console.log(error)
+  });
+});
+
+router.get('/movies/:id', (req, res, next) => {
+  Movie.findById(req.params.id)
+  .then(movie => {
+    res.render("viewmovies", {movie: movie});
+    console.log(movie);
+  })
+  .catch(error => {
+    console.log(error)
+  });
 });
 
 module.exports = router;
