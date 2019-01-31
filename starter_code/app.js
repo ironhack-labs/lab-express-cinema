802477142508490dde74f270ee46672c286f3473
joao pedro <jp_serrat@hotmail.com>
2019-01-31 21:22:56 -0200

    done

--- starter_code/app.js
+++ starter_code/app.js
@@ -50,9 +50,10 @@ app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
 app.locals.title = 'Express - Generated with IronGenerator';
 
 
-
 const index = require('./routes/index');
 app.use('/', index);
+
+
 
 
 module.exports = app;
--- starter_code/routes/index.js
+++ starter_code/routes/index.js
@@ -1,9 +1,26 @@
 const express = require('express');
 const router  = express.Router();
+const Movie = require('../models/Movie.js');
 
 /* GET home page */
 router.get('/', (req, res, next) => {
   res.render('index');
 });
+
+router.get('/movies', (req, res, next) => {
+  Movie.find()
+  .then((movies) => {
+    console.log(movies)
+    res.render('movies', {movies});
+  })
+})
+
+router.get('/movies/:movieId', (req, res, next) => {
+  Movie.find({_id: req.params.movieId})
+  .then((movie) => {
+    console.log(movie)
+    res.render('movie', {movie});
+  })
+})
 
 module.exports = router;
