8c8c224e1d68f698cd1ffc8b53ee88630f23070c
Jamie Egan <j.egan@mac.com>
2019-01-31 14:22:15 +0100

    in progress

---
+++ routes/index.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const router  = express.Router();
+const Movie = require('../models/Movie.js');
+
+// GET home page
+router.get('/', (req, res, next) => {
+  res.render('index');
+});
+
+// GET movies page
+router.get('/movies', (req, res, next) => {
+  Movie.find() // find all the movies (no filter)
+  .then(movies => {
+    res.render("movies", { movies });
+  })
+  .catch(error => {
+    console.log(error);
+  })
+});
+
+// GET movie details
+router.get('/movies/:movieId', (req, res, next) => {
+  Movie.findById(req.params.id)
+    .then(movies => {
+      res.render("movie-details", {movies});
+    })
+    .catch(error => {
+      console.log(error);
+    })
+});
+
+module.exports = router;
