00d14da02a6e6d5dbe31bc883d06890258b69cf3
murillorulkowski@gmail.com <murillorulkowski@gmail.com>
2019-01-31 21:59:17 -0200

    done

--- starter_code/routes/index.js
+++ starter_code/routes/index.js
@@ -1,9 +1,28 @@
 const express = require('express');
-const router  = express.Router();
+const router = express.Router();
+const Movie = require('../models/Movie');
 
 /* GET home page */
 router.get('/', (req, res, next) => {
   res.render('index');
+});
+
+/* GET movies page */
+router.get('/movies', (req, res, next) => {
+  Movie.find()
+    .then((movies) => {
+      console.log(movies);
+      res.render('movies', { movies });
+    });
+});
+
+/* GET movies description */
+router.get('/movie/:id', (req, res, next) => {
+  Movie.find({_id: req.params.id })
+    .then((movie) => {
+      console.log(movie);
+      res.render('description', { movie });
+    });
 });
 
 module.exports = router;
