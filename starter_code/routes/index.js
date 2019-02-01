62f1d82ae25a65e1cbaa60bd3d4bf133ab67fe51
Dani Garcia <danielgarciamartinez1985@gmail.com>
2019-02-01 09:47:59 +0100

    almost done

--- starter_code/routes/index.js
+++ starter_code/routes/index.js
@@ -1,9 +1,28 @@
 const express = require('express');
-const router  = express.Router();
+const router = express.Router();
+const Movies = require('../models/Movie.js');
 
-/* GET home page */
 router.get('/', (req, res, next) => {
   res.render('index');
+});
+
+router.get('/movies', (req, res, next) => {
+  Movies.find()
+    .then((movie) => {
+      console.log('Estas son las pelis' + movie);
+      res.render('movie', { movie });
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+});
+
+router.get('/movie/:title', (req, res, next) => {
+  const moviesName = req.params.title;
+  Movies.findOne({ title: moviesName });
+  then((movie) => {
+    res.render('movie', { movie });
+  }).catch((error) => console.log(error));
 });
 
 module.exports = router;
