3520699a1485fd2a6ba45c102c3acc2cb9930eca
Henrique Mendes <henrique.gmendes@hotmail.com>
2019-02-01 08:57:06 -0200

    [SAO FT2 - DE - W4D5] Henrique Mendes

--- starter_code/app.js
+++ starter_code/app.js
@@ -11,11 +11,11 @@ const path         = require('path');
 
 
 mongoose
-  .connect('mongodb://localhost/starter-code', {useNewUrlParser: true})
-  .then(x => {
+  .connect('mongodb://localhost/express-cinema', { useNewUrlParser: true })
+  .then((x) => {
     console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
   })
-  .catch(err => {
+  .catch((err) => {
     console.error('Error connecting to mongo', err)
   });
 
@@ -33,26 +33,21 @@ app.use(cookieParser());
 // Express View engine setup
 
 app.use(require('node-sass-middleware')({
-  src:  path.join(__dirname, 'public'),
+  src: path.join(__dirname, 'public'),
   dest: path.join(__dirname, 'public'),
   sourceMap: true
 }));
-      
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
 
-
-
 // default value for title local
 app.locals.title = 'Express - Generated with IronGenerator';
 
-
-
 const index = require('./routes/index');
-app.use('/', index);
 
+app.use('/', index);
 
 module.exports = app;
--- starter_code/routes/index.js
+++ starter_code/routes/index.js
@@ -1,9 +1,33 @@
 const express = require('express');
 const router  = express.Router();
+const MovieModel = require('../models/Movie.js');
 
 /* GET home page */
 router.get('/', (req, res, next) => {
   res.render('index');
+});
+
+router.get('/movies', (req, res, next) => {
+  MovieModel.find()
+    .then((returnedMovies) => {
+      console.log(returnedMovies);
+      res.render('moviesList', { returnedMovies });
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+});
+
+router.get('/movie/:id', (req, res, next) => {
+  const movieId = req.params.id;
+  MovieModel.findOne({'_id': movieId})
+    .then((movie) => {
+      console.log(movie);
+      res.render('movie-detail', { movie });
+    })
+    .catch((error) => {
+      console.log(error);
+    });
 });
 
 module.exports = router;
