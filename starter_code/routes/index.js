6c4234b16abd56fc2c085a388121e115d000671c
Jorge Silva <jorgeaferreirasilva@gmail.com>
2019-02-01 09:01:18 -0200

    done

--- starter_code/routes/index.js
+++ starter_code/routes/index.js
@@ -1,9 +1,32 @@
-const express = require('express');
-const router  = express.Router();
+const express = require("express");
+const router = express.Router();
+const Movie = require("../models/Movie.js");
 
 /* GET home page */
-router.get('/', (req, res, next) => {
-  res.render('index');
+router.get("/", (req, res, next) => {
+  res.render("index");
+});
+
+router.get('/movies', (req, res, next) => {
+  Movie.find()
+    .then(returnedMovies => {
+      console.log(returnedMovies);
+      res.render('movies', { returnedMovies });
+      // res.send(returnedMovies);
+    })
+    .catch(error => {
+      console.log(error);
+    });
+});
+router.get('/movies/:id', (req, res, next) => {
+  Movie.find({_id: req.params.id})
+    .then(returnedMovie => {
+      res.render('movies-details', { returnedMovie });
+      // res.send(returnedMovies);
+    })
+    .catch(error => {
+      console.log(error);
+    });
 });
 
 module.exports = router;
