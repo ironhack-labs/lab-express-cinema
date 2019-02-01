e65a7e962d46fae5dfac8bf2f6d6846e1a4d3448
Nira <niralonso@gmail.com>
2019-02-01 09:42:42 +0100

    .

--- starter_code/routes/index.js
+++ starter_code/routes/index.js
@@ -1,9 +1,22 @@
 const express = require('express');
 const router  = express.Router();
+const Movie = require('../models/Movies.js');
 
-/* GET home page */
+
 router.get('/', (req, res, next) => {
   res.render('index');
+});
+
+
+router.get('/movies', (req, res, next) => {
+  Movie.find()
+    .then((movie) => {
+      console.log(movie)
+      res.render("movies",{movie});
+    })
+    .catch(error => {
+      console.log(error)
+    })
 });
 
 module.exports = router;
