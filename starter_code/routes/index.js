ad32cba1a3be21b7538f3a5166f6efb33ae5d9d0
jsanz24 <jesus240492@gmail.com>
2019-02-01 09:10:38 +0100

    Casi completo

--- starter_code/routes/index.js
+++ starter_code/routes/index.js
@@ -1,9 +1,22 @@
 const express = require('express');
 const router  = express.Router();
+const Movie   = require('../models/Movie.js');
 
 /* GET home page */
 router.get('/', (req, res, next) => {
   res.render('index');
+});
+
+router.get("/movies", (req,res) =>{
+  Movie.find({},{image:1,title:1})
+  .then(movies => res.render("movies",{movies}))
+  .catch(err => {console.log(err)})
+});
+
+router.get("/movies/:id", (req,res) => {
+  Movie.findById(req.params.id,{_id:0})
+  .then(movie => res.render("movieDetails",{movie}))
+  .catch(err => {console.log(err)})
 });
 
 module.exports = router;
