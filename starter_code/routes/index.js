bc15c9ece3c1867d227e6ecfb5a6948834eaacbe
Ilara Almeida <ilaraca@gmail.com>
2019-02-01 09:00:10 -0200

    done

--- starter_code/routes/index.js
+++ starter_code/routes/index.js
@@ -1,9 +1,35 @@
 const express = require('express');
-const router  = express.Router();
+
+const router = express.Router();
+
+const Movie = require('../models/Movie.js');
 
 /* GET home page */
-router.get('/', (req, res, next) => {
+router.get('/', (req, res) => {
   res.render('index');
+});
+/* GET movies page */
+router.get('/movies', (req, res) => {
+  Movie.find()
+    .then((movies) => {
+      console.log(movies);
+      res.render('movies', { movies });
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+});
+
+/* GET movie Detail */
+router.get('/movies/:id', (req, res) => {
+  const movieId = req.params.id;
+  Movie.findOne({ _id: movieId })
+    .then((movie) => {
+      res.render('movie-detail', { movie });
+    })
+    .catch((error) => {
+      console.log(error);
+    });
 });
 
 module.exports = router;
