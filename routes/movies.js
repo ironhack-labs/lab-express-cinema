cb340af8f9dceb4ac9cbee388620ff4de4ee090b
Miguelerja <miguelerja@gmail.com>
2019-02-02 22:24:55 +0100

    Add connection to movie info

--- routes/movies.js
+++ routes/movies.js
@@ -2,13 +2,25 @@ const express = require('express');
 const moviesModel = require('../models/Movie');
 const router = express.Router();
 
-/* GET home page */
+/* GET movies page */
 router.get('/', (req, res, next) => {
   moviesModel.find()
     .then((movies) => {
       res.render('movies', { movies });
     })
-    .catch(error => console.log("error loading films", error));
+    .catch(error => console.log('error loading films', error));
+});
+
+/* GET movie info page */
+
+router.get('/:id', (req, res, next) => {
+  const { _id } = req.params;
+  moviesModel.findOne(_id)
+    .then((data) => {
+      console.log(data);
+      res.render('movieInfo', { data });
+    })
+    .catch(error => console.log(error));
 });
 
 module.exports = router;
