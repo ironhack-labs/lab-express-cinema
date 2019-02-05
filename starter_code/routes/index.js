19155e93358f1c821296e80d22023aa000de2036
Jules <jules.badin@gmail.com>
2019-02-05 20:16:06 +0100

    done

--- starter_code/routes/index.js
+++ starter_code/routes/index.js
@@ -1,9 +1,40 @@
-const express = require('express');
-const router  = express.Router();
+const express = require("express");
+const router = express.Router();
+const Movie = require('../models/Movies.js');
+
 
 /* GET home page */
-router.get('/', (req, res, next) => {
-  res.render('index');
+router.get("/", (req, res, next) => {
+  res.render("index");
 });
+
+// GET movies page
+router.get("/movies", (req, res, next) => {
+  Movie.find()
+    // .sort({
+    //   title: 1
+    // })
+    .then(moviesResults => {
+      res.locals.moviesArray = moviesResults
+      console.log(moviesResults)
+      res.render('movies.hbs')
+    })
+    .catch(err => next(err));
+});
+
+// Netflix style of address - PATH PARAMS
+router.get("/movie/:movieId", (req, res, next) => {
+  // res.send(req.params);
+  const {
+    movieId
+  } = req.params;
+
+  Movie.findById(movieId)
+    .then(movieDoc => {
+      res.locals.movieItem = movieDoc;
+      res.render('movie-details.hbs');
+    })
+    .catch(err => next(err))
+})
 
 module.exports = router;
