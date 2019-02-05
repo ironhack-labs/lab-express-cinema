98d58b58139bbf40f940bb12e26aa4a52477487c
unknown <aurelie.benovici@gmail.com>
2019-02-05 23:41:39 +0100

    .

---
+++ starter_code/models/Movie.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const movieSchema = new Schema({
+  title: { type: String, required: true },
+  director: { type: String },
+  image: { type: String },
+  description: { type: String },
+  showtimes: { type: String }
+});
+
+const Movie = mongoose.model("Movie", movieSchema);
+
+module.exports = Movie;
--- starter_code/routes/index.js
+++ starter_code/routes/index.js
@@ -1,9 +1,24 @@
-const express = require('express');
-const router  = express.Router();
+const express = require("express");
+const Movie = require("../models/Movie.js");
+
+const router = express.Router();
 
 /* GET home page */
-router.get('/', (req, res, next) => {
-  res.render('index');
+router.get("/", (req, res, next) => {
+  res.render("index");
+});
+
+router.get("/movies", (req, res, next) => {
+  //Whenever a user visits "/books" find all the books sorted by rating
+  Movie.find()
+    .then(movieResults => {
+      // Send the database query results to the HBS file as "BookArray"
+      // res.json(data.body.items);
+      res.locals.movieArray = movieResults;
+      res.render("movies.hbs");
+    })
+    //Next(err) skips to the error handler in "bin/www" (error.hbs)
+    .catch(err => next(err));
 });
 
 module.exports = router;
