285d3a2faabfc76f51fcec570fd253b2bc4d086b
Anoopdeep Bal <anoopdeep@gmail.com>
2019-02-06 10:07:46 +0100

    done except for css

---
+++ starter_code/models/Movie.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const movieSchema = new Schema(
+  {
+    title: { type: String },
+    director: { type: String },
+    stars: { type: Array },
+    image: { type: String },
+    description: { type: String },
+    showtimes: { type: Array }
+  },
+  {
+    timestamps: true
+  }
+);
+const Movie = mongoose.model("Movie", movieSchema);
+module.exports = Movie;
--- starter_code/routes/index.js
+++ starter_code/routes/index.js
@@ -1,9 +1,30 @@
-const express = require('express');
-const router  = express.Router();
+const express = require("express");
+const router = express.Router();
+const Movie = require("../models/Movie");
 
 /* GET home page */
-router.get('/', (req, res, next) => {
-  res.render('index');
+router.get("/", (req, res, next) => {
+  res.render("index");
+});
+
+router.get("/movies", (req, res, next) => {
+  Movie.find()
+    .then(movieArray => {
+      res.locals.allMovies = movieArray;
+      res.render("movies.hbs");
+    })
+    .catch(err => next(err));
+});
+
+router.get("/movie/:movieId", (req, res, next) => {
+  const { movieId } = req.params;
+
+  Movie.findById(movieId)
+    .then(movieDoc => {
+      res.locals.movieInfo = movieDoc;
+      res.render("movieInfo.hbs");
+    })
+    .catch(err => next(err));
 });
 
 module.exports = router;
