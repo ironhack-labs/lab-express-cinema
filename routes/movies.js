c8083b35063c06b8e143196531cdabc4d53c4736
Isabella Onate <isabellaonate@gmail.com>
2019-02-07 21:32:52 -0600

    /movies is still working. The individual movie-card was sort of working (it was not displaying card info but the id was found by the params request. I modified the h2 in /movies so it would show a title and now the individual movie detail is not working at all. No styles, just tears.

---
+++ routes/movies.js
@@ -0,0 +1,26 @@
+const router = require('express').Router()
+const Movie = require('../models/Movie');
+
+router.get('/', (req, res)=>{
+	Movie.find()
+			.then(movies=>{
+				res.render('movies', {movies})
+			})
+			.catch(e=>{
+				console.log(e)
+				res.render('error')
+			})
+})
+
+router.get('/:id', (req, res)=>{
+	Movie.findById(req.params.id)
+			.then(movie => {
+				res.render('movie-card', movie)
+			})
+			.catch(e=> {
+				console.log(e)
+				res.render('error')
+			})
+})
+
+module.exports = router
