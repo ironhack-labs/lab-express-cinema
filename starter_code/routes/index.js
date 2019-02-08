bcd4069f8195d35bb31efad2be9861bc6f03dc35
Pol Goma <polgoide@gmail.com>
2019-02-07 20:01:26 -0600

    done, no styles

--- starter_code/routes/index.js
+++ starter_code/routes/index.js
@@ -1,9 +1,28 @@
 const express = require('express');
 const router  = express.Router();
+const  Movie = require('../models/Movie')
 
 /* GET home page */
 router.get('/', (req, res, next) => {
   res.render('index');
 });
+
+router.get('/movies', (req, res, next) => {
+  Movie.find({image: {$exists: true}})
+  .then(movies => {
+    res.render('list', {movies});
+  })
+  .catch(e=>res.send(e))
+});
+
+router.get('/movie/:id', (req, res, next) => {
+  const {id} =  req.params
+  Movie.findById(id)
+  .then(movie => {
+    res.render('detail', movie);
+  })
+  .catch(e=>res.send(e))
+});
+
 
 module.exports = router;
