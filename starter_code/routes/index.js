781eb2e87830de4256e142e31e537f833eca271c
Legionario126 <gmsacrpe_diem@hotmail.com>
2019-02-10 13:31:48 -0600

    done

--- starter_code/routes/index.js
+++ starter_code/routes/index.js
@@ -1,9 +1,30 @@
 const express = require('express');
 const router  = express.Router();
+const  Movie = require('../models/Movie')
 
 /* GET home page */
 router.get('/', (req, res, next) => {
   res.render('index');
 });
+
+//pagina de movies no jala
+router.get('/movies', (req, res, next) => {
+  Movie.find({image: {$exists: true}})
+  .then(movies => {
+    res.render('list', {movies});
+  })
+  .catch(e=>res.send(e))
+});
+
+//detalle de cada pelicula mas de lo mismo (no sirve)
+router.get('/movie/:id', (req, res, next) => {
+  const {id} =  req.params
+  Movie.findById(id)
+  .then(movie => {
+    res.render('detail', movie);
+  })
+  .catch(e=>res.send(e))
+});
+
 
 module.exports = router;
