409b0a400d487d51247b941683a02d3d53572450
Matthew Keil <malibuss67@gmail.com>
2019-02-14 10:25:34 -0500

    init commit

--- app.js
+++ app.js
@@ -9,9 +9,16 @@ const mongoose     = require('mongoose');
 const logger       = require('morgan');
 const path         = require('path');
 
+const username = process.env.MONGO_USERNAME || 'DevServer';
+const password = process.env.MONGO_PASSWORD || 'BougieHaus1!';
+const host = process.env.MONGO_HOST || 'ds251588.mlab.com'
+const port = process.env.MONGO_PORT || 51588;
+const dbName = process.env.MONGO_DBNAME || 'dev-bougie-haus'
+
+const uri = `mongodb://${username}:${password}@${host}:${port}/${dbName}`;
 
 mongoose
-  .connect('mongodb://localhost/starter-code', {useNewUrlParser: true})
+  .connect(uri, {useNewUrlParser: true})
   .then(x => {
     console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
   })
