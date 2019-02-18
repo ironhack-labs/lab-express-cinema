397ddb7d4ea9be51e5814414d14ef665a6950e0b
Ric Taglietti <rictaglietti@gmail.com>
2019-02-18 17:25:39 -0500

    done

--- starter_code/routes/index.js
+++ starter_code/routes/index.js
@@ -1,9 +1,30 @@
 const express = require('express');
 const router  = express.Router();
-
+const Movie = require('../models/Movie.js')
 /* GET home page */
 router.get('/', (req, res, next) => {
   res.render('index');
 });
+
+router.get('/movies', (req, res, next) => {
+
+  Movie.find().then(allMovies => {
+    console.log(allMovies)
+    res.render('movies.hbs', {moviesforhbsfile: allMovies});
+  })
+  
+})
+
+//movie/:id Movie findOne({_id:req.params.id}).then(.......)
+
+router.get('/movie/:id', (req, res, next) => {
+  console.log(req.params)
+  Movie.findOne({_id:req.params.id}).then(oneMovie => {
+    console.log(oneMovie)
+    res.render('movie-detail.hbs', { movieDetails: oneMovie});
+  })
+
+})
+
 
 module.exports = router;
