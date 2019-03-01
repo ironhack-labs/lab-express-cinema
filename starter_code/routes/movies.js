db2ff7d3b83a573b1731f0085743ca436fe15187
kikeanaya <luiseanayam@gmail.com>
2019-03-01 19:46:37 +0100

    done

---
+++ starter_code/routes/movies.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const Router = express.Router();
+
+const Movie = require('../models/Movie')
+
+Router.get("/", (req, res) => {
+  Movie.find()
+  .then(movies => {
+    console.log(movies)
+    res.render("movies", {movies});
+  })
+  .catch(err => console.log('Error', err))
+})
+
+Router.get("/:id", (req, res) => {
+  console.log(req.params.id);
+
+  Movie.findById(req.params.id)
+    .then(movie=> {
+      console.log(movie);
+      res.render("info", {movie})
+    })
+})
+
+module.exports = Router;
