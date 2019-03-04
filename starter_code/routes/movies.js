149723f973ada0bd10185df3aa502309004c4daa
Abel Colmenares Napolitano <abelpronet@gmail.com>
2019-03-04 09:06:29 +0100

    done

---
+++ starter_code/routes/movies.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const Router = express.Router();
+
+const Movie = require('../models/Movie')
+
+
+Router.get("/", (req, res) => {
+    Movie.find()
+        .then(movies => {
+            res.render("movies", { movies });
+        })
+        .catch(err => console.log('Error', err))
+})
+
+Router.get("/:id", (req, res) => {
+
+    Movie.findById(req.params.id)
+        .then(movie => {
+            res.render("movie-info", { movie })
+        })
+
+
+})
+
+
+
+module.exports = Router;
