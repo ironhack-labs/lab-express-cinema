f2977eacafa8216d17c5940866a5bac7ac6beec9
lockeas16 <lockeas16@gmail.com>
2019-03-23 17:19:08 -0600

    first iterarion done

---
+++ starter_code/models/Movie.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+movieSchema = new Schema({
+  title:{
+    type: String
+  },
+  director:{
+    type: String
+  },
+  stars:{
+    type: Array
+  },
+  image:{
+    type: String
+  },
+  description:{
+    type: String
+  },
+  showTimes:{
+    type: Array
+  }
+});
+
+module.exports = mongoose.model("Movie",movieSchema);
