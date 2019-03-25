7d743660f5ae7993dad74d989c41cde00d69d7d1
Jelena Jukic <jelena.dj.pg@gmail.com>
2019-03-25 10:47:02 +0100

    done

--- starter_code/routes/index.js
+++ starter_code/routes/index.js
@@ -1,9 +1,26 @@
 const express = require('express');
-const router  = express.Router();
+const router = express.Router();
+const Movie = require('../models/movies.js');
 
 /* GET home page */
 router.get('/', (req, res, next) => {
   res.render('index');
 });
+
+router.get('/movies', (req, res, next) => {
+
+  Movie.find()
+    .then(movies => res.render('movies', {movies}))
+    .catch(error => console.log(error));
+
+})
+
+router.get('/movie/:movieId', (req, res, next) => {
+
+  Movie.findOne({_id:req.params.movieId})
+    .then(movie => res.render('movie', {movie}))
+    .catch(error => console.log(error));
+
+})
 
 module.exports = router;
