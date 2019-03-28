fb01631d2fa6b5ebe6b94f29e75c42a8ca98946f
Isabela Baião Dowsley <isabela.dowsley@Isabelas-MacBook-Pro.local>
2019-03-28 16:24:56 +0100

    almost done

--- starter_code/routes/index.js
+++ starter_code/routes/index.js
@@ -1,9 +1,23 @@
 const express = require('express');
 const router  = express.Router();
 
+const Movie = require('../models/Movie.js');
+
 /* GET home page */
 router.get('/', (req, res, next) => {
   res.render('index');
+});
+
+
+router.get('/movies', (req, res, next) => {
+  Movie.find()
+    .then(moviesDB => {
+      // console.log('Retrieved movies from DB:', moviesDB);
+      res.render('movies' , { movies: moviesDB });
+    })
+    .catch(error => {
+      console.log('Error while getting the movies from the DB: ', error);
+    })
 });
 
 module.exports = router;
