d50d69629b9921331f32c5c7076dde6775e28c73
Raul Contreras <raulc98@gmail.com>
2019-04-11 22:25:06 +0200

    Empty array problem

--- starter_code/routes/index.js
+++ starter_code/routes/index.js
@@ -1,12 +1,17 @@
 const express = require('express');
 const router  = express.Router();
+const Movie = require('../models/Movie.js');
 
 /* GET home page */
 router.get('/', (req, res, next) => {
   res.render('index');
 });
 router.get('/movies', (req, res, next) => {
-  res.render('movies');
+  Movie.find().then(movie => console.log(movie));
+});
+
+router.get('/movie/:id', (req, res, next) => {
+
 });
 
 module.exports = router;
