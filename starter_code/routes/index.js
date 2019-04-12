504a3730fb157f20089c0287b9d69fb021b673eb
JlAviles <jlavilesmolina@gmail.com>
2019-04-12 09:26:32 +0200

    First steps, completed

--- starter_code/routes/index.js
+++ starter_code/routes/index.js
@@ -1,9 +1,18 @@
 const express = require('express');
 const router  = express.Router();
 
+const Movie = require('../models/Movie')
+
 /* GET home page */
 router.get('/', (req, res, next) => {
   res.render('index');
+});
+
+router.get('/movies', (req, res, next) => {
+    Movie.find({});
+    res.json();
+    // .then( => )
+    // .catch( =>)
 });
 
 module.exports = router;
