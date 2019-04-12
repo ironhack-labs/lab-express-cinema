fadc399b1d972ec1e73c928e465b0bdb0a3f6977
Carla <38359702+carlarsmendes@users.noreply.github.com>
2019-04-12 13:06:04 +0100

    carla-mendes-lisbon done

--- starter_code/routes/index.js
+++ starter_code/routes/index.js
@@ -1,9 +1,41 @@
 const express = require('express');
 const router  = express.Router();
+const Movie = require('../models/Movie.js')
+// module.exports = Movie;
 
 /* GET home page */
 router.get('/', (req, res, next) => {
   res.render('index');
 });
 
+router.get('/movies', (req, res, next) => {
+  // res.render('movies');
+  // console.log(data)
+      Movie.find()
+      .then(data => {
+      res.render('movies', {
+          listMovies: data
+      });
+      })
+    .catch(err => {
+      console.log("The error while searching artists occurred: ", err);
+    })
+  //  res.send('artists');
+  })
+
 module.exports = router;
+
+router.get('/movies/:_id', (req, res, next) => {
+  // res.render('movies');
+  // console.log(data)
+      Movie.findById(req.params._id)
+      .then(data => {
+      res.render('see-more', {
+          listMovies: data
+      });
+      })
+    .catch(err => {
+      console.log("The error while searching artists occurred: ", err);
+    })
+  //  res.send('artists');
+  })
