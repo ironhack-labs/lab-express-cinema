ef5e8248e7abd5caa1f4fd4111df58870be0d43a
Fernando Lopez <fnandolopezsanchez@gmail.com>
2019-04-12 09:36:53 +0200

    funciona la base de datos pero nada mas

--- starter_code/app.js
+++ starter_code/app.js
@@ -48,6 +48,8 @@ app.set("view engine", "hbs");
 app.use(express.static(path.join(__dirname, "public")));
 app.use(favicon(path.join(__dirname, "public", "images", "favicon.ico")));
 
+
+
 // default value for title local
 app.locals.title = "Cinema Ironhack";
 
