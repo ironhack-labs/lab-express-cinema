2fd2f5638f0c5b0d23873fbce280d15ecc0fe865
albertofragoso <hola@albertofragoso.com>
2019-04-18 23:47:02 -0500

    Done

---
+++ starter_code/models/Movie.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const movieSchema = new Schema({
+  title: {
+    type: String,
+    required: true
+  },
+  director: {
+    type: String,
+    required: true
+  },
+  stars: [],
+  image: String,
+  description: String,
+  showtimes: []
+},
+{
+  timestamps: true,
+  versionKey: false
+})
+
+module.exports = mongoose.model('Movie', movieSchema)
