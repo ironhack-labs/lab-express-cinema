de6cb2ad5bc16a35beed972b4dd79bb6636479cb
VictoriaPl <victoriapp15@gmail.com>
2019-04-20 23:31:58 -0500

    Done

--- starter_code/routes/index.js
+++ starter_code/routes/index.js
@@ -1,9 +1,29 @@
 const express = require('express');
 const router  = express.Router();
+const Movie = require("../models/Movie")
 
 /* GET home page */
 router.get('/', (req, res, next) => {
   res.render('index');
 });
+/*GET Movies */
+router.get('/movies',(req, res, next)=>{
+  Movie.find()
+  .then(data => {
+    res.render('movies',{ data })
+    console.log(data)
+})
+.catch(err => console.log(err))
+})
 
-module.exports = router;
+router.get('/movies/:movieId', (req, res, next) => {
+  Movie.findById(req.params.movieId)
+    .then(data => {
+      res.render('seeMore', { data : data });
+    })
+    .catch(error => {
+      console.log('Error while retrieving book details: ', error);
+    })
+});
+
+module.exports = router
