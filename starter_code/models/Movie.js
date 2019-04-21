3b6acfd7ac606bc68782fe0a45a571af08cc990f
marypztr <nutrimary.77@gmail.com>
2019-04-20 23:41:27 -0500

    DE MEX MARYJOSE

---
+++ starter_code/models/Movie.js
@@ -0,0 +1,25 @@
+const mongoose =require("mongoose")
+const Schema = mongoose.Schema
+
+const movieSchema = new Schema(
+    {
+        title: {
+          type: String,
+          required: true
+        },
+        director: {
+          type: String,
+          required: true
+        },
+        stars: [],
+        image: String,
+        description: String,
+        showtimes: []
+      },
+      {
+        timestamps: true,
+        versionKey: false
+      },
+      {collection:"movies"});
+      
+module.exports = mongoose.model('Movie', movieSchema);
--- starter_code/routes/index.js
+++ starter_code/routes/index.js
@@ -1,9 +1,30 @@
 const express = require('express');
 const router  = express.Router();
+const Movie = require("../models/Movie")
 
 /* GET home page */
 router.get('/', (req, res, next) => {
   res.render('index');
 });
+/*GET Movies */
+router.get('/movies',(req, res, next)=>{
+  Movie.find()
+  .then(allTheMoviesFromDB => {
+    res.render('movies',{ allTheMoviesFromDB })
 
-module.exports = router;
+})
+.catch(err => console.log(err))
+
+})
+
+router.get('/movies/:id',(req,res,next)=>{
+  const{id} = req.params
+  Movie.findById(id)
+  .then(movie =>{
+    console.log(movie)
+    res.render('movie-detail', movie)
+  })
+
+})
+
+module.exports = router
