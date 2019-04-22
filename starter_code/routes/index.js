63ecf175913c8de803b6900ba1d0ef9ddb815704
Jairo Garcia <jgarcia@indratek.com>
2019-04-21 22:35:48 -0500

    almost done

--- starter_code/routes/index.js
+++ starter_code/routes/index.js
@@ -1,9 +1,25 @@
 const express = require('express');
 const router  = express.Router();
+const Movies = require('../models/Movie')
+
 
 /* GET home page */
 router.get('/', (req, res, next) => {
   res.render('index');
 });
+
+
+router.get('/movies', (req, res) => {
+  Movies.find()
+    .then(movies => res.render('movies', { movies }))
+    .catch(err => console.log(err))
+})
+
+router.get('/movie/:id', (req, res) => {
+  const { id } = req.params
+  Movies.findById(id)
+    .then(movie => res.render('movie', movie))
+    .catch(err => console.log(err))
+})
 
 module.exports = router;
