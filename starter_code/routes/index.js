9dbf787dfa973a3356fb7c0603b727ac6118cf99
Christian Khawam <christiankhawam@outlook.com>
2019-04-29 23:33:30 -0400

    complete

--- starter_code/routes/index.js
+++ starter_code/routes/index.js
@@ -1,9 +1,24 @@
 const express = require('express');
-const router  = express.Router();
+const router = express.Router();
+const Movies = require('../models/movie')
 
 /* GET home page */
 router.get('/', (req, res, next) => {
   res.render('index');
 });
+
+router.get('/movies', (req, res, next) => {
+  Movies.find().then(movies => {
+    res.render("movies", { movies });
+  })
+})
+
+router.get('/movies/:id', (req, res, next) => {
+
+  Movies.findById(req.params.id)
+    .then(movies => {
+      res.render("info", { movies });
+    })
+})
 
 module.exports = router;
