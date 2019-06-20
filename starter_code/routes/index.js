f6577042b3db5a2e747c4a93458486350ecf0df0
Souhair bouallala <souhairbouallala@gmail.com>
2019-06-20 18:00:23 +0200

    done

--- starter_code/routes/index.js
+++ starter_code/routes/index.js
@@ -1,9 +1,45 @@
-const express = require('express');
-const router  = express.Router();
+const express = require("express");
+const router = express.Router();
+const seeds = require("../bin/seeds");
+const mongoose = require("mongoose");
+const Movie = require("../models/Movie");
 
+mongoose
+  .connect("mongodb://localhost/movies", { useNewUrlParser: true })
+  .then(() => {
+    console.log("Connected to Mongo!");
+  })
+  .catch(err => {
+    console.error("Error connecting to mongo", err);
+  });
+
+// Movie.insertMany(seeds, function(err, docs) {
+// console.log(err);
+// });
 /* GET home page */
-router.get('/', (req, res, next) => {
-  res.render('index');
+
+router.get("/movies", (req, res) => {
+  Movie.find()
+    .then(seeds => {
+      res.render("movies", { seeds });
+    })
+    .catch(err => {
+      res.render({ err: "an error occured" });
+    });
+});
+
+router.get("/movies/:id", (req, res) => {
+  Movie.findById(req.params.id)
+    .then(seeds => {
+      res.render("details", { seeds });
+    })
+    .catch(dbErr => {
+      console.log("deb res not ok", dberr);
+    });
+});
+
+router.get("/", (req, res, next) => {
+  res.render("index");
 });
 
 module.exports = router;
