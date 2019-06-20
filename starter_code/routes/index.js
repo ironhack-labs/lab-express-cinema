873288c45df38c0ca82c3621ce6c2e841b07c0f0
Cecile Pham <sheshiru.van@gmail.com>
2019-06-20 20:44:44 +0200

    done

--- starter_code/routes/index.js
+++ starter_code/routes/index.js
@@ -1,9 +1,45 @@
-const express = require('express');
-const router  = express.Router();
+const express = require("express");
+const router = express.Router();
+const MovieModel = require("./../models/Movie");
+const movies = require("./../bin/seeds");
 
 /* GET home page */
-router.get('/', (req, res, next) => {
-  res.render('index');
+router.get("/", (req, res, next) => {
+  const bodyClass = "index";
+  res.render("index", { bodyClass });
 });
+
+router.get("/movies", (req, res) => {
+  const moviePageTitle = "Ironhack Cinema";
+  MovieModel.find({})
+    .then(dbRes => {
+      res.render("movies", { movie: dbRes, moviePageTitle });
+    })
+    .catch(err => {
+      res.render("movies", { err: "an error occured" });
+    });
+});
+
+pushMovies = data => {
+  MovieModel.insertMany(data)
+    .then(dbRes => {
+      console.log("data successfully pushed", dbRes);
+    })
+    .catch(err => {
+      console.log("Oups, an error ocured", err);
+    });
+};
+
+router.get("/movie/:id", (req, res) => {
+  MovieModel.findById(req.params.id)
+    .then(dbRes => {
+      res.render("movieDetails", { movie: dbRes });
+    })
+    .catch(dbErr => {
+      console.log("dbRes not ok", dbErr);
+    });
+});
+
+// pushMovies(movies);
 
 module.exports = router;
