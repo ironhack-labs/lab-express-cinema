b9f1deca5958daa06c7952ab3bf555359441145d
Camille Charpentier <cam.charpentier@live.fr>
2019-06-21 16:22:22 +0200

    done

--- starter_code/app.js
+++ starter_code/app.js
@@ -45,14 +45,15 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
 
 
-
 // default value for title local
-app.locals.title = 'Express - Generated with IronGenerator';
-
+app.locals.title = 'Cinema Ironhack';
 
 
 const index = require('./routes/index');
 app.use('/', index);
 
+app.listen(3000, () => {
+  console.log('server ready @ http://localhost:3000');
+});
 
 module.exports = app;
