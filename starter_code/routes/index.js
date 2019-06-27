1b41f8ffe146184bd2b208436b0329e973ed4277
Ingo Grossmann <ingogrossmann@icloud.com>
2019-06-27 21:36:31 +0100

    logic implemented, final css missing

--- starter_code/routes/index.js
+++ starter_code/routes/index.js
@@ -1,9 +1,27 @@
 const express = require('express');
 const router  = express.Router();
+const Movie = require("../models/Movie");
+
 
 /* GET home page */
 router.get('/', (req, res, next) => {
   res.render('index');
 });
+
+router.get("/movies", (req, res, next) => {
+  Movie.find()
+    .then(moviesFromDb => {
+      res.render("movies", { movies: moviesFromDb });
+    });
+});
+
+router.get("/movie/:movieId", (req, res, next) => {
+  let movieId = req.params.movieId
+  Movie.findById(movieId)
+    .then(moviesFromDb => {
+      res.render("movie-detail", { movie: moviesFromDb });
+    })
+});
+
 
 module.exports = router;
