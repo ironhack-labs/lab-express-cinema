232428868b0b7d0e2bfa67fe3aef923442485188
Miriloper <miriam.mad.ml@gmail.com>
2019-06-27 20:18:08 +0200

    some changes

--- starter_code/routes/index.js
+++ starter_code/routes/index.js
@@ -1,9 +1,18 @@
 const express = require('express');
 const router  = express.Router();
+const Movie = require('../models/Movie')
 
 /* GET home page */
 router.get('/', (req, res, next) => {
   res.render('index');
 });
+
+router.get("/movies", (req, res, next) =>{
+  res.render("movies")
+})
+
+// router.get('/paco', (req, res, next) => {
+//   res.render('vistapaco');
+// });
 
 module.exports = router;
