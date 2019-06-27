8d4a7582d9255c82f690495a9c96e6b583019e1b
Gabriel B <gabelmo92@hotmail.com>
2019-06-27 21:23:20 +0200

    done

--- starter_code/app.js
+++ starter_code/app.js
@@ -11,7 +11,7 @@ const path         = require('path');
 
 
 mongoose
-  .connect('mongodb://localhost/starter-code', {useNewUrlParser: true})
+  .connect('mongodb://localhost/Pelis', {useNewUrlParser: true})
   .then(x => {
     console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
   })
@@ -45,14 +45,10 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
 
 
-
-// default value for title local
-app.locals.title = 'Express - Generated with IronGenerator';
-
-
-
 const index = require('./routes/index');
 app.use('/', index);
 
-
+// app.listen(3000, () => {
+//   console.log('My first app listening!!')
+// });
 module.exports = app;
