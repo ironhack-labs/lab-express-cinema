76676a929455418c8c500446e4358412f315c880
rogerayra <roger.ayra@gmail.com>
2019-06-27 23:52:02 -0500

    done

--- starter_code/app.js
+++ starter_code/app.js
@@ -11,7 +11,7 @@ const path         = require('path');
 
 
 mongoose
-  .connect('mongodb://localhost/starter-code', {useNewUrlParser: true})
+  .connect(process.env.DB, {useNewUrlParser: true})
   .then(x => {
     console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
   })
@@ -41,13 +41,14 @@ app.use(require('node-sass-middleware')({
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
+hbs.registerPartials(__dirname + "/views/partials");
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
 
 
 
 // default value for title local
-app.locals.title = 'Express - Generated with IronGenerator';
+app.locals.title = 'Cinema Ironhack';
 
 
 
--- starter_code/routes/index.js
+++ starter_code/routes/index.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const router  = express.Router();
+const { findMovies, findOneMovie } = require('../controllers/movies.controller')
 
 /* GET home page */
 router.get('/', (req, res, next) => {
   res.render('index');
 });
+
+router.get('/movies', findMovies)
+
+router.get('/movie/:id', findOneMovie)
 
 module.exports = router;
