bf00979df5f63148fdf3ae397bb3aef32a1a2c78
Vanesa Puerta <Vanesa@MacBook-Air-de-Vanesa.local>
2019-06-28 09:16:59 +0200

    done

--- starter_code/routes/index.js
+++ starter_code/routes/index.js
@@ -1,9 +1,28 @@
 const express = require('express');
 const router  = express.Router();
+const Movie = require("../models/Movie")
 
 /* GET home page */
 router.get('/', (req, res, next) => {
   res.render('index');
+});
+
+router.get('/movies', (req, res, next) => {
+  Movie
+    .find()
+    .then((movies)=>{res.render("movies",{movies})
+    }).catch((err) => {
+      console.log(err)
+    })
+});
+
+router.get('/movie/:id', (req, res, next) => {
+  Movie
+    .findOne({_id: req.params.id})
+    .then((movie) => {res.render("movie",{movie})
+    }).catch((err) => {
+      console.log(err)
+    })
 });
 
 module.exports = router;
