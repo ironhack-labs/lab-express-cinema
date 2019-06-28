0e21f006c52509af8f14f5558d0cf9eb36ed0b5f
Iván López Ruiz <ivan@MacBook-Air-de-Ivan.local>
2019-06-28 09:29:05 +0200

    done

--- starter_code/routes/index.js
+++ starter_code/routes/index.js
@@ -1,9 +1,22 @@
 const express = require('express');
 const router  = express.Router();
+const Movie = require('../models/Movie');
 
 /* GET home page */
 router.get('/', (req, res, next) => {
   res.render('index');
+});
+
+router.get("/movies",(req,res,next)=>{
+  res.render("movies");
+});
+
+router.get("/movies", (req, res, next) => {
+  Movie.find().then(movies => {
+    res.render('movies', {movies})
+  }).catch((err) => {
+    console.log(err)
+  })
 });
 
 module.exports = router;
