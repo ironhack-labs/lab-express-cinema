e5f51d4ac3a80b0f20285c2a6c2f59b15fadeb4f
Sergio Festa <sfestatomari@gmail.com>
2019-07-05 17:53:39 -0300

    Backend done

---
+++ starter_code/models/movies.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+const Schema   = mongoose.Schema;
+
+const movieSchema = new Schema({
+  title: String,
+  director: String,
+  stars: Array,
+  image: String,
+  description: String,
+  showtimes: Array,
+}, {
+  timestamps: true,
+});
+
+const Movies = mongoose.model("Movies", movieSchema);
+
+module.exports = Movies;
--- starter_code/routes/index.js
+++ starter_code/routes/index.js
@@ -1,9 +1,27 @@
 const express = require('express');
 const router  = express.Router();
+const Movies  = require('../models/movies');
 
 /* GET home page */
 router.get('/', (req, res, next) => {
   res.render('index');
 });
+
+router.get('/movies', (req, res) => {
+  Movies.find()
+  .then(data => {
+    res.render('movies', { data });
+  })
+  .catch(err => console.log(err))
+});
+
+router.get('/movie-details/:id', (req, res) => {
+  Movies.findById(req.params.id)
+  .then(dataMovie => {
+    console.log(dataMovie);
+    res.render('movie-details', dataMovie);
+  })
+  .catch(err => console.log(err))
+})
 
 module.exports = router;
