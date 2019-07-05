cf29847a76ba722484c3108671ce555a5f026233
Sergio Velikopoljski <sergiovelo17@gmail.com>
2019-07-05 16:35:11 -0400

    lab finished

--- starter_code/routes/index.js
+++ starter_code/routes/index.js
@@ -1,9 +1,30 @@
 const express = require('express');
 const router  = express.Router();
-
-/* GET home page */
+const Movie = require('../models/Movie')
 router.get('/', (req, res, next) => {
   res.render('index');
 });
+router.get('/movies',(req,res,next)=>{
+  Movie.find()
+  .then((theMovies)=>{
+    console.log(theMovies);
+    res.render('movies',{allTheMovies: theMovies});
+
+  })
+  .catch((err)=>{
+     next(err)
+  })
+})
+router.get('/movies/:id',(req,res,next)=>{
+  let movieID = req.params.id;
+  Movie.findById(movieID)
+  .then((oneMovie)=>{
+    res.render('movieDetails', {theMovie:oneMovie})
+  })
+  .catch((err)=>{
+    console.log(err);
+  })
+
+})
 
 module.exports = router;
