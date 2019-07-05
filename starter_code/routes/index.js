c48f8ab49bda0793ee339860230aa0c17c4119d2
Taluama Velludo <taluama@gmail.com>
2019-07-05 17:23:28 -0400

    done--will work on visuals

--- starter_code/routes/index.js
+++ starter_code/routes/index.js
@@ -1,9 +1,40 @@
 const express = require('express');
 const router  = express.Router();
+const Movie = require('../models/Movies')
 
 /* GET home page */
 router.get('/', (req, res, next) => {
   res.render('index');
 });
+
+// router.get('/movies', (req, res, next) => {
+//   res.render('movies');
+// });
+
+router.get('/movies', (req, res, next)=>{
+  Movie.find()
+  .then((theThingWeGetBackFromDB)=>{
+    // res.send(theThingWeGetBackFromDB)
+    console.log(theThingWeGetBackFromDB);
+    res.render('movies', {allMovies: theThingWeGetBackFromDB});
+  })
+  .catch((err)=>{
+    next(err);
+  })
+})
+
+router.get('/movies/:ref', (req, res, next)=>{
+  let theID = req.params.ref;
+  Movie.findById(theID)
+  .then((oneMovie)=>{
+    res.render('movieDetails', {theMovie: oneMovie})
+  })
+  .catch((err)=>{
+    next(err);
+  })
+})
+
+
+
 
 module.exports = router;
