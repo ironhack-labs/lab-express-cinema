08451e64854e6902569914e05c5b32c3c0f766f2
Michael Roth <michael.franklin.roth@gmail.com>
2019-07-05 16:30:28 -0400

    base functionality is done

--- starter_code/routes/index.js
+++ starter_code/routes/index.js
@@ -1,9 +1,32 @@
 const express = require('express');
-const router  = express.Router();
+const router = express.Router();
+const Movie = require('../models/Movie')
 
 /* GET home page */
 router.get('/', (req, res, next) => {
   res.render('index');
 });
+
+router.get('/movies', (req, res, next) => {
+  Movie.find()
+    .then((allTheMovies) => {
+
+      res.render('movies', { theMovies: allTheMovies })
+    })
+    .catch((err) => {
+      next(err)
+    })
+})
+
+router.get('/movie/:movieID', (req, res, next) => {
+  let theID = req.params.movieID
+  Movie.findById(theID)
+    .then((oneSingleMovie) => {
+      res.render('movie', { theMovie: oneSingleMovie })
+    })
+    .catch((err) => {
+      next(err)
+    })
+})
 
 module.exports = router;
