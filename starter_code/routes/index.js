6df868e81b2b6470951e26b26ad76ef2484694e1
Gabriel Holanda <gabriel_ebot@hotmail.com>
2019-07-05 18:37:06 -0300

    project done

--- starter_code/routes/index.js
+++ starter_code/routes/index.js
@@ -1,9 +1,25 @@
 const express = require('express');
-const router  = express.Router();
-
+const router = express.Router();
+const movies = require('../models/movie');
 /* GET home page */
 router.get('/', (req, res, next) => {
   res.render('index');
 });
 
+router.get('/movies', (req, res) => {
+  movies.find()
+    .then(movies => {
+      res.render('movies', { movies });
+    })
+    .catch(err => console.log(err));
+})
+
+router.get('/movies/:movieID', (req, res) => {
+  const Movie = req.params.movieID;
+  movies.findById(Movie)
+    .then(movies => {
+      res.render('movie-details', { movies });
+    })
+    .catch(err => console.log(err));
+})
 module.exports = router;
