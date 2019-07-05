32455e45a8cff44fe7cafdaa16407e4228128653
Jarsory <arsory92@gmail.com>
2019-07-04 21:27:37 -0500

    cinema vue

--- starter_code/app.js
+++ starter_code/app.js
@@ -3,8 +3,8 @@ require('dotenv').config();
 const bodyParser   = require('body-parser');
 const cookieParser = require('cookie-parser');
 const express      = require('express');
-const favicon      = require('serve-favicon');
-const hbs          = require('hbs');
+//st favicon      = require('serve-favicon');
+//const hbs          = require('hbs');
 const mongoose     = require('mongoose');
 const logger       = require('morgan');
 const path         = require('path');
@@ -31,7 +31,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 
 // Express View engine setup
-
+/*
 app.use(require('node-sass-middleware')({
   src:  path.join(__dirname, 'public'),
   dest: path.join(__dirname, 'public'),
@@ -42,9 +42,9 @@ app.use(require('node-sass-middleware')({
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 app.use(express.static(path.join(__dirname, 'public')));
+
 app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
-
-
+*/
 
 // default value for title local
 app.locals.title = 'Express - Generated with IronGenerator';
--- starter_code/routes/movies.js
+++ starter_code/routes/movies.js
@@ -1,21 +1,36 @@
 const express = require ("express")
-const mongoose = require ("mongoose")
 const Movie = require ("../models/Movie")
 
 const router = express.Router();
+
 router.get('/', (req, res, next)=>{
-    console.log('MOVIESSS')
-    Movie.find({})
-        .then(resp =>{
-            console.log(res)
-            res.json( resp )
+    Movie.find({}, 'tittle image')
+        .then(data =>{
+            res.status(200).json(
+                {text: "Error del servidor",
+                data}
+            )
+        })
+        .catch(err => {
+            res.status(500).json({
+                text: "Error del servidor",
+                err: err
+            })
         })
 })
 
+
+
 router.get('/:id', (req, res, next) =>{
     Movie.findById({_id:req.params.id})
-    .then(resultado => {
-        res.json(resultado)
+    .then(theMovie => {
+        res.status(200).json(theMovie)
+    }).catch(err => {
+        res.status(500).json({
+            text: "Error del servidor",
+            err: err
+        })
     })
 })
+
 module.exports = router;
