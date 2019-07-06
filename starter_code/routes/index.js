41a927da9c6a353a45d0c2a6c062c59d4f9b53d9
Gulfiya Isyangulova <gulfiyaisyangulova@Gulfiyas-MacBook-Pro.local>
2019-07-05 21:14:49 -0400

    done

--- starter_code/routes/index.js
+++ starter_code/routes/index.js
@@ -1,9 +1,54 @@
 const express = require('express');
-const router  = express.Router();
+const router = express.Router();
+const Movie = require('../models/Movie')
 
 /* GET home page */
 router.get('/', (req, res, next) => {
   res.render('index');
 });
+
+router.get('/movies', (req, res, next) => {
+  Movie.find()
+    .then((moviesFromBD) => {
+      console.log(moviesFromBD)
+
+
+      res.render('movies', {
+        allMovies:
+          moviesFromBD
+      })
+    })
+    .catch((err) => {
+      next(err);
+    })
+})
+router.get('/movies/:id', (req, res, next) => {
+  
+  Movie.findById(req.params.id)
+    .then((moviesFromBD) => {
+      console.log(moviesFromBD)
+
+
+      res.render('singleMovie', moviesFromBD)
+    })
+    .catch((err) => {
+      next(err);
+    })
+});
+// router.get('/movies', (req, res, next) => {
+//   Movie.find()
+//     .then((moviesFromBD) => {
+//       console.log(moviesFromBD)
+
+
+//       res.render('movies', {
+//         allMovies:
+//           moviesFromBD
+//       })
+//     })
+//     .catch((err) => {
+//       next(err);
+//     })
+// })
 
 module.exports = router;
