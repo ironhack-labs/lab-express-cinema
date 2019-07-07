825dd2b2d867c23a6c3dab0a708025cb0174db63
yi-ting Chiang <tarynchiang@gmail.com>
2019-07-06 21:07:14 -0400

    done

--- starter_code/routes/index.js
+++ starter_code/routes/index.js
@@ -1,9 +1,34 @@
 const express = require('express');
 const router  = express.Router();
+const Movie   = require('../models/Movie');
 
 /* GET home page */
 router.get('/', (req, res, next) => {
   res.render('index');
 });
+
+router.get('/movies', (req, res, next)=>{
+  Movie.find()
+  .then((theThingWeGetBackFromDB)=>{
+    res.render('movies',{allTheMovies: theThingWeGetBackFromDB})
+  })
+  .catch((err)=>{
+    next(err);
+  })
+})
+
+router.get('/movies/:id', (req, res, next)=>{
+  let theID = req.params.id;
+  console.log(theID);
+  Movie.findById(theID)
+  .then((oneSingleMovie)=>{
+    console.log(oneSingleMovie);
+    res.render('movieDetails', {theMovie: oneSingleMovie})
+  })
+  .catch((err)=>{
+    next(err);
+  })
+})
+
 
 module.exports = router;
