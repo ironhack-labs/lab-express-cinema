df738745fb3d949aa44a956fb85a6c25770b1762
Nicolle Gonzalez <nicolle.ngonzalez@gmail.com>
2019-07-07 22:38:49 -0400

    Done

---
+++ starter_code/models/Movie.js
@@ -0,0 +1,24 @@
+// models/Movies.js
+
+const mongoose = require("mongoose");
+const Schema   = mongoose.Schema;
+
+const movieSchema = new Schema({
+  title: String,
+  director: String,
+  stars: [String],
+  image: String,
+  description: String,
+  showtimes: [String]
+}
+// , {
+//   timestamps: {
+//     createdAt: "created_at",
+//     updatedAt: "updated_at"
+//   }
+//}
+);
+
+const MovieModel = mongoose.model("Movie", movieSchema);
+
+module.exports = MovieModel;
--- starter_code/routes/index.js
+++ starter_code/routes/index.js
@@ -1,9 +1,46 @@
 const express = require('express');
 const router  = express.Router();
+const Movie = require('../models/Movie');
 
 /* GET home page */
 router.get('/', (req, res, next) => {
   res.render('index');
 });
+
+
+
+
+/* Get movies page */
+router.get('/movies', (req, res, next)=>{
+
+  Movie.find()
+  .then((allTheMovies)=>{
+    res.render('movies',{allTheMovies:
+      allTheMovies})
+  })
+  .catch((err)=>{
+    next(err);
+  })
+  
+})
+
+
+
+/* Get single movie page */
+router.get('/movie/:id', (req,res,next)=>{
+
+  let theId = req.params.id;
+  Movie.findById(theId)
+  .then((oneSingleMovie)=>{
+    res.render('movie', {theMovie: 
+                              oneSingleMovie})
+  })
+  .catch((err)=>{
+    next(err);
+  })
+})
+
+
+
 
 module.exports = router;
