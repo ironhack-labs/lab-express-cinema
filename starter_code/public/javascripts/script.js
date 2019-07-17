c18c5bbe97fa250e389c8edbc3cafd6ae36a4a92
Mark Trevail <trevail.m@gmail.com>
2019-07-17 12:25:38 +0200

    Added home page and button

--- starter_code/public/javascripts/script.js
+++ starter_code/public/javascripts/script.js
@@ -2,4 +2,8 @@ document.addEventListener('DOMContentLoaded', () => {
 
   console.log('IronGenerator JS imported successfully!');
 
+  document.getElementById('btn-home').addEventListener('click', ()=>{
+    window.location = '/movies'
+  })
+
 }, false);
