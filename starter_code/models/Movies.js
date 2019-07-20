9bf60e19c878ac37d77f4130474ceb229af49f83
Alejandro <alejandro.galvez.rico@gmail.com>
2019-07-20 15:02:25 -0500

    acave

---
+++ starter_code/models/Movies.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+// instanciate the schema
+const movieSchema = new Schema({
+  title: String,
+  director: String,
+  stars: Array,
+  image: String,
+  description: String,
+  showtimes: Array
+});
+
+const movieModel = mongoose.model("Movie", movieSchema);
+
+module.exports = movieModel;
--- starter_code/routes/index.js
+++ starter_code/routes/index.js
@@ -1,9 +1,34 @@
-const express = require('express');
-const router  = express.Router();
+const express = require("express");
+const router = express.Router();
+const movieModel = require("../models/Movie");
 
 /* GET home page */
-router.get('/', (req, res, next) => {
-  res.render('index');
+
+/* GET home page */
+router.get("/", (req, res, next) => {
+  res.render("index");
+});
+
+// lien vers la page movies
+router.get("/movies", (req, res) => {
+  movieModel
+    .find({})
+    .then(movies => {
+      console.log(movies);
+
+      res.render("movies.hbs", { movies });
+    })
+    .catch(err => {
+      res.render();
+    });
+});
+
+// lien vers les infos de chaque film
+router.get("/movie/:id", (req, res) => {
+  movieModel.findById(req.params.id).then(movie => {
+    console.log(movie);
+    res.render("movieDetails", { movie });
+  });
 });
 
 module.exports = router;
