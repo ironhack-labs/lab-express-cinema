5d95018fe60b7648ce4679045f077030b426c848
gramsco <antoningauth@gmail.com>
2019-08-29 17:21:44 +0200

    niraja

--- starter_code/routes/index.js
+++ starter_code/routes/index.js
@@ -1,9 +1,25 @@
 const express = require('express');
-const router  = express.Router();
+const router = express.Router();
+const MovieModel = require("./../models/Movie");
 
 /* GET home page */
 router.get('/', (req, res, next) => {
   res.render('index');
 });
+
+router.get('/movies', (req, res) => {
+  console.log(MovieModel)
+  MovieModel.find()
+    .then(dbRes => {
+      console.log(dbRes)
+    })
+    .catch(dbErr => {
+      console.log(dbErr)
+    })
+  res.render('movies');
+
+});
+
+
 
 module.exports = router;
