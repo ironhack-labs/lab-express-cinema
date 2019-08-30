0f99a06ca2ed345c5cf909b6cbbe17ba52e34c61
Doro Hassan <dorothea.hassan@gmail.com>
2019-08-30 14:09:32 +0200

    done

---
+++ starter_code/routes/base-routes.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const router  = express.Router();
+const MovieModel = require("./../models/Movies");
+
+/* GET home page */
+router.get('/', (req, res, next) => {
+  res.render('index');
+});
+
+router.get('/movies', (req, res, next) => {
+  MovieModel.find().then(dbRes => {
+    // console.log(dbRes)
+    res.render('movies', {movies: dbRes}); // new object {} with key : value pairs {key: value}
+  }).catch(dbErr => {
+    console.log(dbErr);
+  });
+  
+});
+
+router.get('/movies/:id', (req, res, next) => {
+  console.log(req.params.id)
+  MovieModel.findById(req.params.id).then().catch()
+  res.render('movie');
+});
+
+// promiss
+
+
+module.exports = router;
