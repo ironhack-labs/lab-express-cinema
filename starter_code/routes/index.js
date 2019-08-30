7e74a0d5c122e8e7bd024e860bb83321f51bc110
sophieannebourgine <sophieannebourgine@gmail.com>
2019-08-30 15:19:52 +0200

    fini

--- starter_code/routes/index.js
+++ starter_code/routes/index.js
@@ -1,9 +1,29 @@
-const express = require('express');
-const router  = express.Router();
-
+const express = require("express");
+const router = express.Router();
+const MovieModel = require("./../models/Movie");
 /* GET home page */
-router.get('/', (req, res, next) => {
-  res.render('index');
+router.get("/", (req, res, next) => {
+  res.render("index");
+});
+
+router.get("/movies", (req, res, next) => {
+  MovieModel.find()
+    .then(dbRes => {
+      console.log(dbRes);
+      res.render("movies", { movies: dbRes });
+    })
+    .catch(dbErr => console.log(dbErr));
+});
+
+
+router.get("/movie/:id", (req, res, next) => {
+  console.log(req.params)
+  MovieModel.findOne({_id: req.params.id})
+    .then(dbRes => {
+      console.log(dbRes);
+      res.render("movie", { movie: dbRes });
+    })
+    .catch(dbErr => console.log(dbErr));
 });
 
 module.exports = router;
