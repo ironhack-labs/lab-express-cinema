6a05f6f0c338c4cf7fa699f6f76706f1a83ff40c
Juan Vidal <juanvidaldom@gmail.com>
2019-09-05 20:29:55 -0500

    doneeee

--- starter_code/routes/index.js
+++ starter_code/routes/index.js
@@ -1,9 +1,21 @@
 const express = require('express');
 const router  = express.Router();
+const Movie = require('../models/Movie')
 
 /* GET home page */
 router.get('/', (req, res, next) => {
   res.render('index');
+});
+
+router.get('/movies', async (req, res, next) => {
+  const movies = await Movie.find()
+  res.render('movies', {movies});
+});
+
+router.get('/movies/:id', async (req, res, next) => {
+  const {id} = req.params
+  const movie = await Movie.findById(id)
+  res.render('single-movie', movie);
 });
 
 module.exports = router;
