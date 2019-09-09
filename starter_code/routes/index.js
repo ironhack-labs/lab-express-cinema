ce3970834873d3af254c11cf078e3032fe7f0234
Elisa <elisam.esquivel@gmail.com>
2019-09-08 19:16:03 -0500

    Done

--- starter_code/routes/index.js
+++ starter_code/routes/index.js
@@ -1,9 +1,22 @@
 const express = require('express');
 const router  = express.Router();
+const Movie = require('../models/Movie')
+
 
 /* GET home page */
 router.get('/', (req, res, next) => {
   res.render('index');
 });
+
+router.get('/movies',  async (req, res) =>{
+  const movies = await Movie.find()
+  res.render('movies', { movies } );
+})
+
+router.get('/movie-detail/:id', async (req, res) => {
+  const {id} = req.params;
+  const movie = await Movie.findById(id);
+  res.render('movie-detail', movie)
+})
 
 module.exports = router;
