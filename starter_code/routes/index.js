0509d7ccf315b8e91ac602ebb124d7c5638f2723
Nelly-Charlott Schneider <nelly@Nelly-Charlotts-MacBook-Pro.local>
2019-09-09 18:22:00 +0200

    done, no styling

--- starter_code/routes/index.js
+++ starter_code/routes/index.js
@@ -1,9 +1,30 @@
-const express = require('express');
-const router  = express.Router();
+const express = require("express");
+const Movie = require("../models/Movie");
+const router = express.Router();
 
 /* GET home page */
-router.get('/', (req, res, next) => {
-  res.render('index');
+router.get("/", (req, res, next) => {
+  res.render("index");
+});
+
+router.get("/movies", (req, res) => {
+  Movie.find()
+    .then(movies => {
+      // console.log(movies);
+      res.render("movies.hbs", { movieList: movies });
+    })
+    .catch(err => {
+      console.log(err);
+    });
+});
+
+router.get("/movies/:id", (req, res) => {
+  console.log(req.params);
+  const movieId = req.params.id;
+
+  Movie.findById(movieId).then(movie => {
+    res.render("movieDetails.hbs", { thisMovie: movie });
+  });
 });
 
 module.exports = router;
