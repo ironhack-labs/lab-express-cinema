629822d925e3ec6724ec4db0862bc96af4961813
Thuy Le <gitnap@mailbox.org>
2019-09-09 16:21:34 +0200

    Iterations 1-4 done without styling

--- starter_code/app.js
+++ starter_code/app.js
@@ -11,7 +11,7 @@ const path         = require('path');
 
 
 mongoose
-  .connect('mongodb://localhost/starter-code', {useNewUrlParser: true})
+  .connect('mongodb://localhost/lab-express-cinema', {useNewUrlParser: true})
   .then(x => {
     console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
   })
@@ -37,7 +37,7 @@ app.use(require('node-sass-middleware')({
   dest: path.join(__dirname, 'public'),
   sourceMap: true
 }));
-      
+
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
@@ -53,6 +53,5 @@ app.locals.title = 'Express - Generated with IronGenerator';
 
 const index = require('./routes/index');
 app.use('/', index);
-
 
 module.exports = app;
--- starter_code/routes/index.js
+++ starter_code/routes/index.js
@@ -1,9 +1,33 @@
 const express = require('express');
+const Movie = require("../models/Movie");
+
 const router  = express.Router();
 
 /* GET home page */
 router.get('/', (req, res, next) => {
   res.render('index');
 });
+
+// Register movies route
+router.get("/movies", (req, res) => {
+  Movie.find()
+    .then(movies => {
+      console.log(`MOVIES:`, movies);
+      res.render("movies", { moviesList: movies });
+    })
+    .catch(err => console.log(`ERROR: Movies page could not be loaded.`, err));
+});
+
+// Register Single movie page
+router.get("/movies/:movieId", (req, res) => {
+  const movieId = req.params.movieId;
+
+  Movie.findById(movieId).then(movie => {
+    res.render("movieDetails", {
+      movie: movie
+    });
+  });
+});
+
 
 module.exports = router;
