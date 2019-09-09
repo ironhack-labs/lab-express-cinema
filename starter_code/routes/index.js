c0294b68203130fb80e0bc3a0dd9f6af280b3a33
iourisorokine <isorokine@gmail.com>
2019-09-09 18:26:57 +0200

    done, no styling

--- starter_code/routes/index.js
+++ starter_code/routes/index.js
@@ -1,9 +1,29 @@
 const express = require('express');
-const router  = express.Router();
+const router = express.Router();
+const Movie = require('../models/Movie');
 
 /* GET home page */
 router.get('/', (req, res, next) => {
   res.render('index');
+});
+
+router.get('/movies', (req, res, next) => {
+  Movie.find().then(movies => {
+    res.render('movies', {
+      movies
+    })
+  })
+})
+
+router.get('/movie/:movieId', (req, res) => {
+  const movieId = req.params.movieId;
+  console.log(movieId);
+  Movie.findById(movieId).then(movie => {
+    console.log(movie);
+    res.render('movieDetails', {
+      movie: movie
+    })
+  });
 });
 
 module.exports = router;
