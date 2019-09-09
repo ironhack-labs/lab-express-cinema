d38c7759046c58abf4943e6ed5161a68a943f30c
Alfonso García-Miñaur <Alfonso@Alfonso-GMS.local>
2019-09-09 18:11:42 +0200

    got the beans

--- starter_code/routes/index.js
+++ starter_code/routes/index.js
@@ -1,9 +1,24 @@
 const express = require('express');
 const router  = express.Router();
-
+const Movie=require("../models/movies");
 /* GET home page */
 router.get('/', (req, res, next) => {
   res.render('index');
 });
+router.get('/movies', (req, res, next) => {
+  Movie.find().then(movies=>{
+    
+    res.render('movies', {moviesList:movies});
+  })
 
+});
+
+router.get("/movies/:movieID", (req,res,next)=>{
+  let uniqmovie=req.params.movieID;
+console.log(uniqmovie)
+  Movie.findById(uniqmovie).then(movie=>{
+    console.log(movie);
+    res.render('movie.hbs', {oneMovie:movie});
+  })
+})
 module.exports = router;
