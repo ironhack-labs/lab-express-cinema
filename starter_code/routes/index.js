37511004f8968060a94c04909b20d8dee239753d
Cristopher <servicioscuin@gmail.com>
2019-09-08 22:12:06 -0500

    done

--- starter_code/routes/index.js
+++ starter_code/routes/index.js
@@ -1,9 +1,32 @@
 const express = require('express');
-const router  = express.Router();
+const router = express.Router();
+const Movie = require('../models/Movie')
 
 /* GET home page */
 router.get('/', (req, res, next) => {
   res.render('index');
+});
+
+
+router.get('/movies', (req, res, next) => {
+  Movie.find()
+    .then(movies => {
+      res.render('movies', { movies: movies })
+    })
+    .catch(error => {
+      console.log('Error while getting the movies from the DB: ', error);
+    })
+});
+
+router.get('/movies/:MovieId', (req, res, next) => {
+  console.log(req.params.MovieId)
+  Movie.findById({ _id: req.params.MovieId })
+    .then(movie => {
+      res.render('movieDetail', { movie: movie })
+    })
+    .catch(error => {
+      console.log('Error while getting the movie detail from the DB: ', error);
+    })
 });
 
 module.exports = router;
