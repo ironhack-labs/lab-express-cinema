2770c23d75af1dc7a7aa8e427b7b9fb1345ba306
Noriko Takizawa <norikotakizawa@Norikos-MacBook-Pro.local>
2019-09-09 18:17:57 +0200

    done

--- starter_code/routes/index.js
+++ starter_code/routes/index.js
@@ -1,9 +1,32 @@
 const express = require('express');
-const router  = express.Router();
+const router = express.Router();
+const Movie = require("../models/Movie")
 
 /* GET home page */
 router.get('/', (req, res, next) => {
   res.render('index');
 });
+
+
+router.get('/movies', (req, res, next) => {
+  Movie.find().then(movies => {
+    res.render("movies", {
+      moviesList: movies
+    });
+
+  });
+
+})
+
+router.get('/movies/:movieId', (req, res) => {
+  const movieId = req.params.movieId;
+  Movie.findById(movieId).then(movie => {
+    res.render('details', {
+      detailsList: movie
+    });
+  })
+
+
+})
 
 module.exports = router;
