5a0f43d2d2b9498d4becbed26ebcc73211e53ad2
anna-marie-now <am.nauruschat@gmail.com>
2019-09-09 18:44:26 +0200

    logic done, no css

--- starter_code/routes/index.js
+++ starter_code/routes/index.js
@@ -1,9 +1,21 @@
-const express = require('express');
-const router  = express.Router();
+const express = require("express");
+const router = express.Router();
+const Movie = require("../models/Movie");
 
 /* GET home page */
-router.get('/', (req, res, next) => {
-  res.render('index');
+router.get("/", (req, res, next) => {
+  res.render("index");
+});
+
+router.get("/movies", (req, res, next) => {
+  Movie.find().then(movies => {
+    res.render("movies", { movieList: movies });
+  });
+});
+router.get("/movie/:id", (req, res, next) => {
+  Movie.findOne({ _id: req.params.id }).then(movie => {
+    res.render("movieDetail", { movie });
+  });
 });
 
 module.exports = router;
