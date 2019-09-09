17ac7d64e53e7b9b419cdeae16239f6b7c22ca54
Moritz Wright <me@moritzwright.com>
2019-09-09 16:20:11 +0200

    done

--- starter_code/routes/index.js
+++ starter_code/routes/index.js
@@ -1,9 +1,41 @@
 const express = require('express');
-const router  = express.Router();
+const router = express.Router();
+const Movie = require("../models/Movie")
 
 /* GET home page */
 router.get('/', (req, res, next) => {
   res.render('index');
 });
+
+
+
+
+
+router.get('/movies', (req, res, next) => {
+
+  Movie.find().then(movie => {
+    console.log(movie)
+    res.render("movies.hbs", {
+      moviesList: movie
+    })
+  })
+
+  //res.render('movies');
+});
+
+
+router.get("/movies/:movieId", (req, res) => {
+  const movieId = req.params.movieId
+
+  Movie.findById(movieId).then(movie => {
+
+    res.render("movie.hbs", {
+      movie: movie
+    })
+  })
+})
+
+
+
 
 module.exports = router;
