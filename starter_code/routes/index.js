b952f4b25a0465affe25b6e087dd9397cb1cba5a
Steve <stewright88@gmail.com>
2019-09-09 18:42:55 +0200

    done, except the styling

--- starter_code/routes/index.js
+++ starter_code/routes/index.js
@@ -1,9 +1,26 @@
-const express = require('express');
-const router  = express.Router();
+const express = require("express");
+const router = express.Router();
+const Movie = require("../models/Movie");
 
 /* GET home page */
-router.get('/', (req, res, next) => {
-  res.render('index');
+router.get("/", (req, res, next) => {
+    res.render("index");
+});
+
+router.get("/movies", (req, res, next) => {
+    Movie.find().then(movies => {
+        res.render("movies", { movieList: movies });
+    });
+});
+
+router.get("/movies/:movieId", (req, res) => {
+    const movieId = req.params.movieId;
+
+    Movie.findById(movieId).then(movie => {
+        console.log(movie);
+
+        res.render("moviesDetail", { movie: movie });
+    });
 });
 
 module.exports = router;
---
+++ starter_code/routes/movies.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const router = express.Router();
+
+/* GET home page */
+router.get("/movies", (req, res, next) => {
+    res.render("movies");
+});
+
+module.exports = router;
