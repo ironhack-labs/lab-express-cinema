64b734f4d48b98fea01282027052256f3997d3d8
Ali Kucukavci <ali91kavci@outlook.dk>
2019-09-09 18:29:58 +0200

    final submission

--- starter_code/routes/index.js
+++ starter_code/routes/index.js
@@ -1,9 +1,27 @@
 const express = require('express');
-const router  = express.Router();
+const router = express.Router();
+const Movie = require('../models/Movie');
+
+router.get('/', (req, res) => {
+    res.render('index.hbs');
+});
 
 /* GET home page */
-router.get('/', (req, res, next) => {
-  res.render('index');
+router.get('/movies', (req, res) => {
+    // get all the books
+    // render a 'books' view with the books data
+
+    Movie.find().then(data => {
+        res.render('movies.hbs', { moviesList: data });
+    });
+});
+
+router.get('/movies/:movieId', (req, res) => {
+    const movieId = req.params.movieId;
+
+    Movie.findById(movieId).then(data => {
+        res.render('movieDetails.hbs', { moviesList: data });
+    });
 });
 
 module.exports = router;
