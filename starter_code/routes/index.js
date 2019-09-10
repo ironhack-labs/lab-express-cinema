747e22e1ecaf9552fff87f58b56b24c399869872
Harinath Vutla <harinathvutla428@gmail.com>
2019-09-10 11:47:11 +0200

    first commit

--- starter_code/routes/index.js
+++ starter_code/routes/index.js
@@ -1,9 +1,26 @@
-const express = require('express');
-const router  = express.Router();
+const express = require("express");
+const router = express.Router();
+const Movie = require("../models/Movie");
 
 /* GET home page */
-router.get('/', (req, res, next) => {
-  res.render('index');
+router.get("/", (req, res, next) => {
+  res.render("index");
+});
+
+router.get("/movies/", (req, res, next) => {
+  Movie.find().then(mov => {
+    res.render("movies", { movieList: mov });
+  });
+});
+
+router.get("/movies/:movieid", (req, res) => {
+  const movieid = req.params.movieid;
+
+  console.log("movie: " + movieid);
+
+  Movie.findById(movieid).then(mov => {
+    res.render("movie", { movie: mov });
+  });
 });
 
 module.exports = router;
