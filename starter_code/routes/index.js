445c8051a910e304d0e111818303e06f57cf7169
Md Tanjil Shivan <salwadhaka@gmail.com>
2019-09-10 11:42:04 +0200

    done without styling, need comment on iteration 4

--- starter_code/routes/index.js
+++ starter_code/routes/index.js
@@ -1,9 +1,30 @@
 const express = require('express');
 const router  = express.Router();
+const Movie = require ("../models/Movies");
 
 /* GET home page */
 router.get('/', (req, res, next) => {
   res.render('index');
+});
+
+
+/* GET movies on click to the home page */
+router.get('/movies', (req, res, next) => {
+  Movie.find().then(movies  => {
+      res.render("movies.hbs", {movies});
+    console.log (movies);
+  });
+  // res.render("movies.hbs", {movieList:movies});
+});
+
+/* GET movie on clicking See more */
+/* get movie by Id */
+
+router.get('/movies/:movieId', (req, res) => {
+  // req.param.movieId --- request parameter movieId from the object
+  Movie.findById(req.param.movieId).then(movie => {
+    res.render("detail.hbs", {movie});
+  });
 });
 
 module.exports = router;
