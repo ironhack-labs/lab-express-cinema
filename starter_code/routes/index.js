1aa5705e26a6a9745edb8bcaa00edf7412867745
Keighley McFarland <mcfarland.ke@gmail.com>
2019-09-10 09:04:59 +0200

    done

--- starter_code/routes/index.js
+++ starter_code/routes/index.js
@@ -1,9 +1,26 @@
-const express = require('express');
-const router  = express.Router();
+const express = require("express");
+const router = express.Router();
+const Movie = require("../models/Movie.js");
 
 /* GET home page */
-router.get('/', (req, res, next) => {
-  res.render('index');
+router.get("/", (req, res, next) => {
+  res.render("index");
+});
+
+router.get("/movies", (req, res, next) => {
+  // get all movies
+  // render movies view with movies data
+  Movie.find().then(movies => {
+    console.log(movies);
+    res.render("movies.hbs", { moviesList: movies });
+  });
+});
+
+router.get("/movies/:movieId", (req, res) => {
+  const movieId = req.params.movieId;
+  Movie.findById(movieId).then(movie => {
+    res.render("movieDetails.hbs", { movie: movie });
+  });
 });
 
 module.exports = router;
