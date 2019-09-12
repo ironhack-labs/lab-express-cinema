3f92f7f3d088d1b90a3b3e1e96902187bce7dc96
Andres Weber <andresmweber@gmail.com>
2019-09-12 17:24:50 -0400

    done.

--- app.js
+++ app.js
@@ -1,27 +1,28 @@
 require('dotenv').config();
 
-const bodyParser   = require('body-parser');
+const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
-const express      = require('express');
-const favicon      = require('serve-favicon');
-const hbs          = require('hbs');
-const mongoose     = require('mongoose');
-const logger       = require('morgan');
-const path         = require('path');
-
+const express = require('express');
+const favicon = require('serve-favicon');
+const hbs = require('hbs');
+const mongoose = require('mongoose');
+const logger = require('morgan');
+const path = require('path');
+const Movie = require('./models/Movie')
+const dbName = 'ironhack-cinema';
 
 mongoose
-  .connect('mongodb://localhost/starter-code', {useNewUrlParser: true})
-  .then(x => {
-    console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
-  })
-  .catch(err => {
-    console.error('Error connecting to mongo', err)
-  });
+    .connect(`mongodb://localhost/${dbName}`, { useNewUrlParser: true })
+    .then(x => {
+        console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
+    })
+    .catch(err => {
+        console.error('Error connecting to mongo', err)
+    });
 
+hbs.registerPartials(path.join(__dirname, 'views', 'partials'))
 const app_name = require('./package.json').name;
 const debug = require('debug')(`${app_name}:${path.basename(__filename).split('.')[0]}`);
-
 const app = express();
 
 // Middleware Setup
@@ -31,28 +32,45 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 
 // Express View engine setup
-
 app.use(require('node-sass-middleware')({
-  src:  path.join(__dirname, 'public'),
-  dest: path.join(__dirname, 'public'),
-  sourceMap: true
+    src: path.join(__dirname, 'public'),
+    dest: path.join(__dirname, 'public'),
+    sourceMap: true
 }));
-      
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
 
-
-
 // default value for title local
-app.locals.title = 'Express - Generated with IronGenerator';
-
-
+app.locals.title = 'Cinema Ironhack';
 
 const index = require('./routes/index');
 app.use('/', index);
+
+app.get('/movies', (req, res, next) => {
+    Movie.find({})
+        .then(data => {
+            res.render('movies', { movies: data })
+        })
+        .catch(err => {
+            console.log(`Ran into an error getting all movies: ${err}`)
+            res.send(err)
+        })
+})
+
+app.get('/movie/:id', (req, res, next) => {
+    Movie.findById(req.params.id)
+        .then(data => {
+            data.showtimes = data.showtimes.join(' | ')
+            res.render('movie', { movie: data })
+        })
+        .catch(err => {
+            console.log(`Ran into an error getting movie ${req.id}: ${err}`)
+            res.send(err)
+        })
+})
 
 
 module.exports = app;
