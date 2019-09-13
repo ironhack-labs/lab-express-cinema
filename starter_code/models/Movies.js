66dbb903581afdc105a17e8a8ddcdb060eeed13c
guimartinsalmeida <guimartinsalmeida@gmail.com>
2019-09-13 00:31:34 -0500

    done

---
+++ starter_code/models/Movies.js
@@ -0,0 +1,17 @@
+const { Schema, model } = require('mongoose')
+
+const movieSchema = new Schema(
+  {
+    title: String,
+    director: String,
+    stars: [String],
+    image: String,
+    description: String,
+    showtimes: [String]
+  },
+  {
+    timestamps: true,
+    versionKey: false
+  }
+)
+module.exports = model('Movie', movieSchema)
