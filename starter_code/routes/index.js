ce953b9ee0d69600297f6351cddcc97c19c25b56
borgila <elbienesbueno@gmail.com>
2019-09-13 09:31:56 +0200

    first

--- starter_code/routes/index.js
+++ starter_code/routes/index.js
@@ -1,9 +1,17 @@
 const express = require('express');
 const router  = express.Router();
+const Movie = require("../models/Movie");
+// const mongoose = require("mongoose");
 
 /* GET home page */
 router.get('/', (req, res, next) => {
   res.render('index');
+});
+
+router.get("/movies/", (req, res, next) => {
+  Movie.find().then(movieList => {
+    res.render("movies", { movieList});
+  });
 });
 
 module.exports = router;
