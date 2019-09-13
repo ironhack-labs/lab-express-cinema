7b94b0d1beb4010870757fc9efb85b1b1097e6d3
Ester-Martinez <martinez.ester@gmail.com>
2019-09-13 09:31:32 +0200

    not finished

--- starter_code/routes/index.js
+++ starter_code/routes/index.js
@@ -1,9 +1,24 @@
 const express = require('express');
 const router  = express.Router();
+const Movies = require("../models/Movies");
 
 /* GET home page */
 router.get('/', (req, res, next) => {
   res.render('index');
 });
 
+router.get("/movies", (req, res) => {
+    Movies.find({}).select({title: 1, image: 1})
+    .then(movies => {
+      console.log(movies)
+      res.render("movies", {movies})
+    })
+    
+  });
+  app.get("/movie/:id", (req, res) => {
+    Movies.findById(req.params.id).then(oneMovie => {
+      res.render("movie-details", { oneMovie, host: process.env.HOST });
+    });
+  });
+  
 module.exports = router;
