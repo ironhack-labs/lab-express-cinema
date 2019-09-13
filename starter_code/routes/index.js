4303eb8023198c588e2a34868af99ee20f0727bb
Christian Roque <croqu008@fiu.edu>
2019-09-13 12:49:27 -0400

    deployed

--- starter_code/routes/index.js
+++ starter_code/routes/index.js
@@ -1,9 +1,18 @@
 const express = require('express');
-const router  = express.Router();
+const router = express.Router();
+const Movie = require('../models/Movie');
+
+// const { router } = require('./index.js');
 
 /* GET home page */
 router.get('/', (req, res, next) => {
-  res.render('index');
+	res.render('index');
+});
+
+router.get('/movies', async (req, res, next) => {
+	const movieArr = await Movie.find({});
+	console.log(movieArr);
+	res.render('movies', movieArr);
 });
 
 module.exports = router;
