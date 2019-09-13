b2b0daacf37fb4bb37ac95a8a07661b05377ac04
Maximiliano Sussini <maxsus60@gmail.com>
2019-09-13 08:28:16 +0200

    done

--- starter_code/app.js
+++ starter_code/app.js
@@ -9,9 +9,8 @@ const mongoose     = require('mongoose');
 const logger       = require('morgan');
 const path         = require('path');
 
-
 mongoose
-  .connect('mongodb://localhost/starter-code', {useNewUrlParser: true})
+  .connect(`mongodb://localhost/${process.env.DB}`, {useNewUrlParser: true, useUnifiedTopology: true })
   .then(x => {
     console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
   })
@@ -37,22 +36,16 @@ app.use(require('node-sass-middleware')({
   dest: path.join(__dirname, 'public'),
   sourceMap: true
 }));
-      
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
 
-
-
 // default value for title local
-app.locals.title = 'Express - Generated with IronGenerator';
-
-
+app.locals.title = 'Cinema Ironhack';
 
 const index = require('./routes/index');
 app.use('/', index);
-
 
 module.exports = app;
--- starter_code/routes/index.js
+++ starter_code/routes/index.js
@@ -1,9 +1,23 @@
 const express = require('express');
 const router  = express.Router();
+const Movies = require('../models/Movies');
 
 /* GET home page */
 router.get('/', (req, res, next) => {
   res.render('index');
+});
+
+router.get('/movies', (req, res, next) => {
+  Movies.find({}).then(allMovies => {
+    res.render("movies", { allMovies, host: process.env.HOST })
+  })
+});
+
+router.get("/movie/:id", (req, res, next) => {
+  Movies.findById(req.params.id)
+    .then(movie => {
+    res.render("movie-detail", { movie });
+    });
 });
 
 module.exports = router;
