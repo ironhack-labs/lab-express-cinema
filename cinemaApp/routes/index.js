9aed96f7239da21daad9984a92e142700f261df1
chris bolivar <christopherbolivar@hotmail.com>
2019-09-12 21:43:01 -0400

    done

---
+++ cinemaApp/routes/index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const router  = express.Router();
+const Movie = require('../models/movie.js'); // Import of the model Recipe from './models/Recipe'
+const data = require('../bin/seeds.js');
+/* GET home page */
+router.get('/', (req, res, next) => {
+  res.render('index');
+});
+
+router.get('/movies', (req, res, next) => {
+  Movie.find({}).then(films=> {
+    res.render('movies', { films })
+});
+
+})
+router.get('/info/:id', (req, res, next) => {
+  Movie.findById(req.params.id).then(b => {
+    console.log(b)
+    res.render('info', { b })
+});
+
+})
+// Movie.insertMany(data).then(a =>{
+//   console.log(a)
+// })
+module.exports = router;
