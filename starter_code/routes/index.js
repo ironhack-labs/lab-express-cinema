c8b9f895f80ac259064648a50675a7b3dac622c3
Nick Sherman <ogleffeckt2@yahoo.com>
2019-09-12 22:12:13 -0400

    changes

--- starter_code/routes/index.js
+++ starter_code/routes/index.js
@@ -7,18 +7,19 @@ router.get('/', (req, res, next) => {
   res.render('index');
 });
 
-router.get('/movie/:id', (req, res, next) => {
-Movies.find().then(movies => {
-  res.render('movies', {movies})
-}).catch(error => {
-console.log(error)
-})
+router.get('/movies', (req, res, next) => {
+  Movies.find().then(movies => {
+      res.render('movies', {movies})
+  }).catch(error => {
+    console.log(error)
+  })
 })
 
-// Movies.findById(req.params.id).then(movie => {
-//   console.log(movie)
-//   res.render('movie', {movie})
-// }).catch(error => console.log(error))
+router.get('/movie/:id', (req, res, next) => {
+  Movies.findById(req.params.id).then(movie => {
+      console.log(movie)
+      res.render('movie', {movie})
+  }).catch(error => console.log(error))
+})
 
 module.exports = router;
-  
