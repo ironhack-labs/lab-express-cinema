a0bbbe6d44550c56a71da9f80dbd2e362d5290fb
Pablo Giral Alonso <pablo@MacBook-Pro-de-Pablo.local>
2019-09-13 09:33:22 +0200

    [MAD DE] Pablo Giral

--- starter_code/routes/index.js
+++ starter_code/routes/index.js
@@ -1,9 +1,33 @@
-const express = require('express');
-const router  = express.Router();
-
+const express = require("express");
+const router = express.Router();
+const Movie = require("../models/movie");
 /* GET home page */
-router.get('/', (req, res, next) => {
-  res.render('index');
+router.get("/", (req, res) => {
+  res.render("index");
 });
+
+/* Get movies page */
+router.get("/movies", (req, res) => {
+  Movie.find()
+    .then(allTheMovies => {
+      res.render("movies", {allTheMovies});
+    })
+    .catch(err => {
+      next(err);
+    });
+});
+
+
+router.get("/movie/:id", (req,res)=>{
+
+  Movie.findById(req.params.id)
+  .then((oneMovie)=>{
+    res.render("movie", {oneMovie})
+  })
+  .catch((err)=>{
+    next(err);
+  })
+})
+
 
 module.exports = router;
