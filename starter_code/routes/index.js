6c78f7fec7c1bd0cddc7655d91f10e1646634c88
Vitor Tomazzi <vitor.c.tomazzi@gmail.com>
2019-09-13 11:57:18 -0400

    done

--- starter_code/routes/index.js
+++ starter_code/routes/index.js
@@ -1,9 +1,33 @@
 const express = require('express');
-const router  = express.Router();
+const router = express.Router();
+const Movie = require("../models/Movie"); //this needed to be added
 
 /* GET home page */
 router.get('/', (req, res, next) => {
-  res.render('index');
+  res.render('index.hbs');
 });
 
+router.get('/movies', (req, res, next) => {
+  Movie.find()
+    .then(moviesFromDB => {
+      console.log('Retrieved movies from DB:', moviesFromDB);
+      res.render('movies.hbs', { movies : moviesFromDB});
+    })
+    .catch(error => {
+      console.log('Error getting movies from DB', error);
+    })
+  // res.render('movies')
+})
+
+router.get('/info/:id', (req, res, next) => {
+  Movie.findById(req.params.id)
+  .then(singleMovie => {
+    res.render('info.hbs', {info : singleMovie})
+  })
+  .catch(error => {
+    console.log('Error getting movies from DB', error);
+  })
+})
+
 module.exports = router;
+
---
+++ starter_code/routes/movies.js
@@ -0,0 +1,16 @@
+// const express = require('express');
+// const router = express.Router();
+
+// router.get('/movies'), (req, res, next) => {
+//   Movie.find()
+//     .then(moviesFromDB => {
+//       console.log('Retrieved movies from DB:', moviesFromDB);
+//       res.render('movies', { movies : moviesFromDB});
+//     })
+//     .catch(error => {
+//       console.log('Error getting movies from DB', error);
+//     })
+//   // res.render('movies')
+// }
+
+// module.exports = router;
