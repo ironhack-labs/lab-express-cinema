096283ad123e8c2d50a3f4db58a9149d0a20cd3a
= <kaiser@LAPTOP-CO6Q31HQ.localdomain>
2019-09-15 22:28:10 -0500

    Finis Coronatus Opus

---
+++ starter_code/models/Movie.js
@@ -0,0 +1,18 @@
+const { Schema, model } = require('mongoose')
+
+const movieSchema = new Schema(
+  {
+    title: String,
+    director: String,
+    stars: Array,
+    image: String,
+    description: String,
+    showtimes: Array
+  },
+  {
+    timestamps: true,
+    versionKey: false
+  }
+)
+
+module.exports = model('Movie', movieSchema)
--- starter_code/routes/index.js
+++ starter_code/routes/index.js
@@ -1,9 +1,22 @@
 const express = require('express');
-const router  = express.Router();
+const router = express.Router();
+const Movie = require('../models/Movie')
 
 /* GET home page */
 router.get('/', (req, res, next) => {
   res.render('index');
 });
+
+router.get('/movies', async (req, res, next) => {
+  const data = await Movie.find()
+  console.log(data)
+  res.render('movies', { data })
+})
+
+router.get('/movies/:id', async (req, res, next) => {
+  const { id } = req.params
+  const data = await Movie.findById(id)
+  res.render('movie-detail', data)
+})
 
 module.exports = router;
