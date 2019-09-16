33c6ed46ebbf69160d9327ed913dfcfd7b65bf5e
Jose Galindo <jgalindo.ironhack@gmail.com>
2019-09-16 11:05:37 -0400

    final changes

--- starter_code/routes/index.js
+++ starter_code/routes/index.js
@@ -1,9 +1,26 @@
 const express = require('express');
-const router  = express.Router();
-
-/* GET home page */
+const router = express.Router();
+const Movie = require('../models/Movie')
+    /* GET home page */
 router.get('/', (req, res, next) => {
-  res.render('index');
+    res.render('index.hbs');
 });
+
+router.get('/movies', (req, res, next) => {
+    Movie.find({}).then(allTheMoviesFromTheDB => { //found all the movies in the database
+
+        res.render('movies.hbs', { moviesToHBS: allTheMoviesFromTheDB }) //redner 
+
+    }).catch(err => console.error(err))
+})
+
+
+router.get('/info/:id', async(req, res, next) => {
+    let id = req.params.id;
+    let movie = await Movie.findById(id)
+    res.render('movie', { movie })
+
+})
+
 
 module.exports = router;
