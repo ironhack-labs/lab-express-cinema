5a0925a4ca63b79534c9cd1c7b6de9edcb5306a3
Jorge Luis <jorge.luis4212@gmail.com>
2019-09-16 20:05:39 -0400

    first commit

--- starter_code/routes/index.js
+++ starter_code/routes/index.js
@@ -1,9 +1,19 @@
 const express = require('express');
 const router  = express.Router();
-
+const Movie = require("../models/Movie")
 /* GET home page */
 router.get('/', (req, res, next) => {
   res.render('index');
 });
 
+router.get('/movies', (req,res,next) => {
+  Movie.find()
+  .then(data => {
+    console.log(data)
+  res.render('movies', {data})
+  }) 
+  .catch(errorCallBack => {
+      console.log(errorCallBack)
+  })
+})
 module.exports = router;
