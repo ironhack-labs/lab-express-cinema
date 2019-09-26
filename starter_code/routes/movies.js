685a473076b5faac93009c6b72c052e47f723d9c
Nikola du Preez <nikola_dupreez@hotmail.com>
2019-09-26 15:54:08 +0200

    create and delete added

--- starter_code/routes/movies.js
+++ starter_code/routes/movies.js
@@ -14,10 +14,50 @@ router.get("/movies", (req, res)=> {
 })
 
 /* GET one movie */
-router.get('/movie/:id', (req, res, next) => {
-    Movie.findById(req.params.id)
+// router.get('/movie/:id', (req, res, next) => {
+//     Movie.findById(req.params.id)
+//     .then((movie) => {
+//         res.render('movie', {movie})
+//     })
+//     .catch((err) => {
+//         res.send(err)
+//     })
+// });
+
+router.get('/movie', (req, res, next) => {
+    Movie.findById(req.query.id)
     .then((movie) => {
-        res.render('movie', {movie})
+        res.render('movie', {movie});
+    })
+    .catch((err) => {
+        res.send(err);
+    });
+});
+
+
+/* Create a movie */
+router.get('/movies/create', (req, res, next) => { 
+    res.render('create-movie');
+});
+
+router.post('/movies/create', (req, res, next) => { 
+    const { title, director, stars, description, image } = req.body;
+    const newMovie = new Movie({ title, director, stars, description, image });
+    newMovie.save()
+    .then((movie) => {
+      res.redirect('/movies');
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+});
+
+
+/* Delete a movie */
+router.get('/movie/delete', (req, res, next) => {
+    Movie.findByIdAndRemove(req.query.id)
+    .then((movie) => {     
+        res.redirect("/movies");
     })
     .catch((err) => {
         res.send(err)
