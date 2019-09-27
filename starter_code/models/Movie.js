56b948998398273b278374d72f0c28416b97dd70
Ptdavid0 <pedroantonio.dj.pereira@gmail.com>
2019-09-27 15:15:16 -0300

    Done, but without layout

---
+++ starter_code/models/Movie.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const moviesSchema = new Schema(
+  {
+    title: String,
+    director: String,
+    stars: [String],
+    image: String,
+    description: String,
+    showtimes: [String]
+  },
+  {
+    timestamps: true
+  }
+);
+
+const Movie = mongoose.model("Movie", moviesSchema);
+
+module.exports = Movie;
--- starter_code/routes/index.js
+++ starter_code/routes/index.js
@@ -1,9 +1,31 @@
-const express = require('express');
-const router  = express.Router();
+const express = require("express");
+const router = express.Router();
+const Movie = require("../models/Movie");
 
 /* GET home page */
-router.get('/', (req, res, next) => {
-  res.render('index');
+router.get("/", (req, res, next) => {
+  res.render("index");
+});
+
+router.get("/movies", (req, res, next) => {
+  Movie.find()
+    .then(allMyMovies => {
+      res.render("movies", { movies: allMyMovies });
+    })
+    .catch(err => {
+      console.log(err);
+    });
+});
+
+router.get("/movie/:id", (req, res, next) => {
+  Movie.findOne({ _id: req.params.id })
+    .then(theMovie => {
+      res.render("movie", { movie: theMovie });
+      console.log(allMyMovies);
+    })
+    .catch(err => {
+      console.log(err);
+    });
 });
 
 module.exports = router;
