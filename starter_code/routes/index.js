bc1fde9ac1a220f3c7a1ede93d323eacc849a432
marcos <marcosaalacerda@gmail.com>
2019-09-27 17:36:19 -0300

    not done

--- starter_code/routes/index.js
+++ starter_code/routes/index.js
@@ -1,9 +1,21 @@
 const express = require('express');
 const router  = express.Router();
-
+const movies= require('../models/Movie')
 /* GET home page */
 router.get('/', (req, res, next) => {
   res.render('index');
+
 });
+
+router.get('/movies', (req, res, next) => {
+movies.find()
+.then(movieSelected=>{
+  
+  res.render('movies',{movieSelected});
+})
+ 
+});
+
+
 
 module.exports = router;
