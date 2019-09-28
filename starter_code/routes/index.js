d7034d22193510a0232dca10efa8d40d8ef459ca
grazidiandra <grazi.diandra@gmail.com>
2019-09-28 16:39:04 -0300

    done

--- starter_code/routes/index.js
+++ starter_code/routes/index.js
@@ -1,5 +1,25 @@
 const express = require('express');
-const router  = express.Router();
+const router = express.Router();
+const Movie = require('../models/Movie');
+
+router.get('/movies', (req, res, next) => {
+  Movie.find()
+    .then(arrMovie => {
+      res.render('movies', {arrMovie});
+    })
+    .catch(err => console.log(`error: ${err}`));
+});
+
+router.get('/movies/:id', (req, res, next) => {
+  Movie.findById(req.params.id)
+    .then(theMovie => {
+      console.log(theMovie);
+      res.render('movies-details', {theMovie});
+    })
+    .catch(error => {
+      console.log('Error while retrieving book details: ', error);
+    });
+});
 
 /* GET home page */
 router.get('/', (req, res, next) => {
