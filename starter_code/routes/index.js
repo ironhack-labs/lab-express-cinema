dd6d6052646fcff407338a0009b81ef06d84e0a7
Gabriel Escame <gabriel.escame@gmail.com>
2019-09-29 20:29:37 -0300

    Done

--- starter_code/routes/index.js
+++ starter_code/routes/index.js
@@ -1,9 +1,26 @@
 const express = require('express');
-const router  = express.Router();
+const router = express.Router();
+const Movie = require('../models/Movie');
 
 /* GET home page */
 router.get('/', (req, res, next) => {
   res.render('index');
 });
+router.get('/movies', (req, res, next) => {
+  Movie.find()
+    .then(moviesFromDB => {
+      console.log(moviesFromDB);
 
+      res.render('movies', { movies: moviesFromDB })
+    })
+    .catch(err => console.log(err))
+});
+
+router.get('/movies/:MovieId', (req, res, next) => {
+  Movie.findById(req.params.MovieId)
+    .then(theMovie => {
+      console.log(theMovie)
+      res.render('movie-details', { movie: theMovie });
+    })
+});
 module.exports = router;
