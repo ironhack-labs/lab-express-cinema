1602a7cb4317978decdaeebd668b86de60ee61b1
Rafael Dias <rafaels.dias@me.com>
2019-09-30 08:18:37 -0300

    backend complet

--- starter_code/routes/index.js
+++ starter_code/routes/index.js
@@ -1,9 +1,37 @@
 const express = require('express');
-const router  = express.Router();
+const router = express.Router();
+
+const Movie = require('../models/Movie.js')
+// const MovieSeeds = require('../bin/seeds.js')
+
 
 /* GET home page */
 router.get('/', (req, res, next) => {
   res.render('index');
+});
+
+router.get('/movies', (req, res, next) => {
+  Movie.find()
+    .then(data => {
+      // console.log('Retrieved movies from DB:', data);
+      res.render('movies', {data});
+    })
+    .catch(error => {
+      console.log('Error while getting the movies from the DB: ', error);
+    })
+});
+
+router.get('/movies/:_id', (req, res, next) => {
+  const movieId = req.params._id;
+
+  Movie.findById(movieId)
+  .then(data => {
+    // console.log('Retrieved movies from DB:', data);
+    res.render('movie-details',data);
+  })
+  .catch(error => {
+    console.log('Error while getting the books from the DB: ', error);
+  })
 });
 
 module.exports = router;
