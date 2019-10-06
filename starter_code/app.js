905c072bddc1293812025f0440853615adb4c675
maerd19 <revolutions_333@hotmail.com>
2019-10-06 18:13:13 -0500

    first iteration

--- starter_code/app.js
+++ starter_code/app.js
@@ -4,20 +4,20 @@ const bodyParser   = require('body-parser');
 const cookieParser = require('cookie-parser');
 const express      = require('express');
 const favicon      = require('serve-favicon');
-const hbs          = require('hbs');
 const mongoose     = require('mongoose');
 const logger       = require('morgan');
 const path         = require('path');
 
 
 mongoose
-  .connect('mongodb://localhost/starter-code', {useNewUrlParser: true})
+  .connect('mongodb://localhost/movies-lab', {useNewUrlParser: true})
   .then(x => {
     console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
   })
   .catch(err => {
     console.error('Error connecting to mongo', err)
   });
+
 
 const app_name = require('./package.json').name;
 const debug = require('debug')(`${app_name}:${path.basename(__filename).split('.')[0]}`);
@@ -40,7 +40,7 @@ app.use(require('node-sass-middleware')({
       
 
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hbs');
+app.set('view engine', 'pug');
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
 
--- starter_code/routes/index.js
+++ starter_code/routes/index.js
@@ -1,9 +1,19 @@
 const express = require('express');
 const router  = express.Router();
+const Movies = require('../models/Movies.js')
 
 /* GET home page */
 router.get('/', (req, res, next) => {
   res.render('index');
+});
+
+router.get('/movies', (req, res) => {
+  movies.find()
+    .then(movies => {
+      console.log('Movies', movies);
+      res.render('movies', { movies });
+    })
+    .catch(err => console.log(err));
 });
 
 module.exports = router;
