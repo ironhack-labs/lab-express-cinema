c72e145a3c0eaf0754d7eaeb68f2b1b61409565b
camille <camille.grison@gmail.com>
2019-11-07 18:33:32 +0100

    until iteration 2

--- starter_code/routes/index.js
+++ starter_code/routes/index.js
@@ -1,9 +1,19 @@
 const express = require('express');
-const router  = express.Router();
+const router = express.Router();
+const moviesModel = require('../models/Movies'); // besoin de creer cette variable pour faire appel à notre base de données
+
 
 /* GET home page */
 router.get('/', (req, res, next) => {
   res.render('index');
+});
+
+router.get('/movies', (req, res) => {
+  moviesModel.find().then(dbRes => {
+    res.render('movies', {
+      movies: dbRes
+    });
+  });
 });
 
 module.exports = router;
