0d5acb7d9a768ad139b3a3f8b4cb1013496221a8
Hanna Mohamed <hanna@MacBook-Pro-de-Hanna.local>
2019-11-08 09:48:37 +0100

    done

--- starter_code/routes/index.js
+++ starter_code/routes/index.js
@@ -1,9 +1,31 @@
-const express = require('express');
-const router  = express.Router();
+const express = require("express");
+const router = express.Router();
+const moviesModel = require("../models/Movie");
 
 /* GET home page */
-router.get('/', (req, res, next) => {
-  res.render('index');
+router.get("/", (req, res, next) => {
+  res.render("index", {
+    title: "Cinema Ironhack"
+  });
+});
+
+router.get("/movies", (req, res) => {
+  moviesModel
+    .find()
+    .then(dbRes => {
+      res.render("movies", {
+        title: "Ironhack Cinema",
+        movies: dbRes
+      });
+    })
+    .catch(err => console.log(err));
+});
+
+router.get("/movie/:id", (req, res) => {
+  moviesModel.findOne({ _id: req.params.id }).then(dbRes => {
+    console.log(dbRes);
+    res.render("movie", { movie: dbRes, css: ["artists"] });
+  });
 });
 
 module.exports = router;
