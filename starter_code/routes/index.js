5b53017e344618a32f5b2a00637c9c8e2ceda87a
Marie Greiner <marie@MacBook-Air-de-Marie.local>
2019-11-08 15:40:00 +0100

    "Done without finishing the CSS"

--- starter_code/routes/index.js
+++ starter_code/routes/index.js
@@ -1,9 +1,30 @@
 const express = require('express');
 const router  = express.Router();
+const Movie = require("./../models/movies")
+
+// router.get("/movies", (req, res) => {
+//   res.render("form-movie");
+// });
 
 /* GET home page */
 router.get('/', (req, res, next) => {
   res.render('index');
 });
+
+router.get("/Movies", (req, res) => {
+  Movie.find().then(dbRes => {
+      res.render("movies.hbs"
+      , {
+          Movies: dbRes,
+      });
+  }).catch(err => console.log(err))
+  });
+
+  router.get("/movies/:id", (req, res) => {
+    Movie.findOne({_id: req.params.id}).then(dbRes => {
+      res.render("form-movie", { movie: dbRes, css: ["form-movie"] });
+   }) .catch(err => console.log("toto"+ err));   
+  
+  });
 
 module.exports = router;
