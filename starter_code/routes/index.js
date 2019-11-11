1316b6b2f75eaaf693435c161f9bec1018ef4529
Davide Bernocchi <55143818+davideberno@users.noreply.github.com>
2019-11-11 23:55:15 +0100

    done

--- starter_code/routes/index.js
+++ starter_code/routes/index.js
@@ -1,9 +1,31 @@
-const express = require('express');
-const router  = express.Router();
+const express = require("express");
+const router = express.Router();
+const Movie = require("../models/Movie");
 
 /* GET home page */
-router.get('/', (req, res, next) => {
-  res.render('index');
+router.get("/", (req, res, next) => {
+  res.render("index");
+});
+
+router.get("/movies", (req, res) => {
+  Movie.find()
+    .then(movies => {
+      res.render("movies", { moviesList: movies });
+    })
+    .catch(err => {
+      console.log(err);
+    });
+});
+
+router.get("/movies/:title", (req, res) => {
+  const title = req.params.title;
+  Movie.findOne({ title: title })
+    .then(movie => {
+      res.render("movie", { movie });
+    })
+    .catch(err => {
+      console.log(err);
+    });
 });
 
 module.exports = router;
