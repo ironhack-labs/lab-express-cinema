e4eb2dd5c5669aa5040750c55f6e972674a90cf9
Amelia Scarbrough <scaram02@Amelias-MacBook-Air.local>
2019-11-11 20:38:52 +0100

    done

--- starter_code/routes/index.js
+++ starter_code/routes/index.js
@@ -1,9 +1,32 @@
-const express = require('express');
-const router  = express.Router();
+const express = require("express");
+const router = express.Router();
+const Movie = require("../models/Movie");
+//const Movie = require("../models/Movie");
 
 /* GET home page */
-router.get('/', (req, res, next) => {
-  res.render('index');
+router.get("/", (req, res, next) => {
+  res.render("index");
+});
+
+router.get("/movies", (req, res) => {
+  Movie.find({})
+    .then(documents => {
+      // console.log(documents);
+      res.render("movies.hbs", { movies: documents });
+    })
+    .catch(err => {
+      console.log(err);
+    });
+});
+
+router.get("/movies/:movieId", (req, res) => {
+  Movie.findById(req.params.movieId)
+    .then(movie => {
+      res.render("movieDetails", { movie: movie });
+    })
+    .catch(err => {
+      console.log(err);
+    });
 });
 
 module.exports = router;
