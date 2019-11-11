db5de810197caf00386aa51c7f1b29f565a15d15
Kyle Chorley <kylechorley@Kyles-MacBook-Pro.local>
2019-11-11 16:05:24 +0100

    done

--- starter_code/routes/index.js
+++ starter_code/routes/index.js
@@ -1,9 +1,39 @@
 const express = require('express');
-const router  = express.Router();
+const router = express.Router();
+const Movie = require('../models/movie.js')
 
 /* GET home page */
 router.get('/', (req, res, next) => {
   res.render('index');
 });
+
+router.get('/movies', (req, res) => {
+  Movie.find({}).then(documents => {
+    // this function runs WHEN the promise succeeds
+    console.log(documents);
+
+    // res.send(documents);
+    res.render('movies.hbs', {
+      movies: documents
+    });
+
+  }).catch(err => {
+    // this function runs WHEN the promise rejects
+    console.log(err)
+  })
+
+});
+
+
+router.get('/movies/:movieId', (req, res) => {
+  Movie.findById(req.params.movieId).then(movie => {
+    res.render("movieDetails.hbs", {
+      movie: movie
+    });
+  }).catch(er => {
+    console.log(err)
+  });
+});
+
 
 module.exports = router;
