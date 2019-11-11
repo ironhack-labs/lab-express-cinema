4fd3738fcd3f57f921788e5aee0ab8cb37cca65f
Iuliia Rozhnova <Juliarakhmetova@live.ru>
2019-11-11 15:15:06 +0100

    Express Cinema | done

--- starter_code/routes/index.js
+++ starter_code/routes/index.js
@@ -1,9 +1,29 @@
-const express = require('express');
-const router  = express.Router();
+const express = require("express");
+const router = express.Router();
+const Movie = require("../models/Movie");
 
 /* GET home page */
-router.get('/', (req, res, next) => {
-  res.render('index');
+router.get("/", (req, res) => {
+  console.log("GET requerst on `/`");
+  res.render("index");
+});
+
+router.get("/movies", (req, res, next) => {
+  Movie.find({})
+    .then(documents => {
+      //res.send(documents);
+      res.render("movies", { movies: documents });
+    })
+    .catch(err => console.log(`Error: ${err.message}`));
+});
+
+router.get("/movies/:movieId", (req, res, next) => {
+  Movie.findById(req.params.movieId)
+    .then(item => {
+      //res.send(item);
+      res.render("movieDetails", { movie: item });
+    })
+    .catch(err => console.log(`Error: ${err.message}`));
 });
 
 module.exports = router;
