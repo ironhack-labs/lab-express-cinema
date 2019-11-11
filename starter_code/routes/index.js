41fe9906fff78aac528367c2b9b353aa3a273278
Michael Garnreiter <mgarnreiter@aol.com>
2019-11-11 15:59:58 +0100

    done

--- starter_code/routes/index.js
+++ starter_code/routes/index.js
@@ -1,9 +1,30 @@
-const express = require('express');
-const router  = express.Router();
+const express = require("express");
+const router = express.Router();
+const Movie = require("../models/Movie");
 
 /* GET home page */
-router.get('/', (req, res, next) => {
-  res.render('index');
+router.get("/", (req, res, next) => {
+  res.render("index");
+});
+
+router.get("/movies", (req, res) => {
+  Movie.find({})
+    .then(movies => {
+      res.render("movies", { movies });
+    })
+    .catch(err => {
+      console.log(err);
+    });
+});
+
+router.get("/movies/:id", (req, res) => {
+  Movie.findById(req.params.id)
+    .then(movies => {
+      res.render("movie-details.hbs", { movies });
+    })
+    .catch(err => {
+      console.log(err);
+    });
 });
 
 module.exports = router;
