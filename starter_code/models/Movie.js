61f0a4c7ab90f28c0b8f5395203159659c25ea21
Jefferson Moraes <jeff.dev.moraes@gmail.com>
2019-11-11 15:34:07 +0100

    done

---
+++ starter_code/models/Movie.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const movieSchema = new Schema({
+  title: {
+    type: String,
+    required: true
+  },
+  director: String,
+  stars: [],
+  image: String,
+  description: String,
+  showtimes: []
+});
+
+const Movie = mongoose.model("Movie", movieSchema);
+
+module.exports = Movie;
--- starter_code/routes/index.js
+++ starter_code/routes/index.js
@@ -1,9 +1,27 @@
-const express = require('express');
-const router  = express.Router();
+const express = require("express");
+const router = express.Router();
+
+const Movies = require("../models/Movie");
 
 /* GET home page */
-router.get('/', (req, res, next) => {
-  res.render('index');
+router.get("/", (req, res, next) => {
+  res.render("index");
+});
+
+router.get("/movies", (req, res, next) => {
+  Movies.find({})
+    .then(documents => {
+      res.render("movies", { movies: documents });
+    })
+    .catch(err => console.log(err));
+});
+
+router.get("/movies/:movieId", (req, res, next) => {
+  Movies.findById(req.params.movieId)
+    .then(movie => {
+      res.render("movieInfo", { movie });
+    })
+    .catch(err => console.log(err));
 });
 
 module.exports = router;
