209ee8be66f59efde9ca82655d04df6245fbd177
Camilosorio <tech@camilo.fr>
2019-11-12 18:04:10 +0100

    done

--- starter_code/routes/index.js
+++ starter_code/routes/index.js
@@ -1,9 +1,36 @@
 const express = require('express');
-const router  = express.Router();
+const router = express.Router();
+const Movie = require("../models/Movie.js");
 
-/* GET home page */
 router.get('/', (req, res, next) => {
   res.render('index');
+});
+
+router.get('/movies', (req, res, next) => {
+  Movie
+    .find()
+    .then(dbRes => {
+      console.log(dbRes);
+      res.render('movies', {
+        movies: dbRes
+      });
+    })
+    .catch(err => {
+      console.log(err)
+    });
+});
+
+router.get("/movie/:id", (req, res) => {
+  Movie
+    .findOne({
+      _id: req.params.id
+    })
+    .then(dbRes => {
+      console.log(dbRes);
+      res.render('movie', {
+        movie: dbRes
+      });
+    });
 });
 
 module.exports = router;
