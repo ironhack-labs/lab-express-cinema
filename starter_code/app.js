847e8e91c39439312ab59facfa1112299cb7d86d
gabzafra <gab.zafra@gmail.com>
2019-11-14 23:30:20 +0100

    final

--- starter_code/app.js
+++ starter_code/app.js
@@ -11,7 +11,7 @@ const path         = require('path');
 
 
 mongoose
-  .connect('mongodb://localhost/starter-code', {useNewUrlParser: true})
+  .connect('mongodb://localhost/movies', {useNewUrlParser: true})
   .then(x => {
     console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
   })
@@ -54,5 +54,7 @@ app.locals.title = 'Express - Generated with IronGenerator';
 const index = require('./routes/index');
 app.use('/', index);
 
-
+app.listen(3000, () => {
+  console.log(`Listening on port 3000`);
+});
 module.exports = app;
--- starter_code/routes/index.js
+++ starter_code/routes/index.js
@@ -1,9 +1,28 @@
 const express = require('express');
 const router  = express.Router();
+const Movies = require("../models/Movies");
 
 /* GET home page */
 router.get('/', (req, res, next) => {
   res.render('index');
+});
+
+router.get("/movies", (req, res) => {
+  Movies
+    .find()
+    .select({ title: 1, image: 1 })
+    .then(movies => {
+      res.render("movies", {movies});
+    });
+});
+
+router.get("/movies/:id", (req, res) => {
+  console.log(req.params.id);
+  Movies
+    .findById(req.params.id)
+    .then(movie => {
+      res.render("info", movie);
+    });
 });
 
 module.exports = router;
