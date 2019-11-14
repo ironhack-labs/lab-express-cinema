6a5599e6a014c6b7cdbaac0e731514b0f75443a5
María Simó <maria@coponstudio.es>
2019-11-14 21:56:59 +0100

    Done

--- starter_code/routes/index.js
+++ starter_code/routes/index.js
@@ -1,9 +1,27 @@
 const express = require('express');
-const router  = express.Router();
+const router = express.Router();
+const Movies = require("../models/Movie");
+
 
 /* GET home page */
 router.get('/', (req, res, next) => {
   res.render('index');
+});
+
+router.get('/movies', (req, res, next) => {
+  Movies.find()
+    .then(moviesPayload => {
+      res.render("movies", {moviesPayload})
+    })
+});
+
+router.get('/movie/:id', (req, res, next) => {
+  let movieId = req.params.id;
+
+  Movies.find({_id: movieId})
+    .then(moviePayload => {
+      res.render("movie", {moviePayload})
+    })
 });
 
 module.exports = router;
