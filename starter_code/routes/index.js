9ee59fcbb49dafc581058ef0163b89c69044ef1c
Flor Urbinati <flor29600@hotmail.com>
2019-11-14 22:06:12 +0100

    finished brutalist style

--- starter_code/routes/index.js
+++ starter_code/routes/index.js
@@ -16,4 +16,12 @@ router.get('/movies', (req, res, next) => {
         res.render('movies', {movie});
       });
     });
+
+    router.get('/movie/:id', (req, res, next) => {
+      Movies.findById(req.params.id).then(movie => {
+        console.log(movie)
+        res.render('movie', movie);
+      });
+    });
+
       module.exports = router;
