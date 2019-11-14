2907c04fdf1b974f27ab220aca13ab768536b3a2
Flor Urbinati <flor29600@hotmail.com>
2019-11-14 21:00:34 +0100

    it1 seeded

---
+++ starter_code/Models/Movie.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const schemaName = new Schema({
+  title : String,
+    director: String,
+    stars: [String],
+    image: String,
+    description: String,
+    showtimes: [String] 
+});
+
+const Model = mongoose.model("Movies", schemaName);
+module.exports = Model;
