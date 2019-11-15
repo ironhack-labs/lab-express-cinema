224de3a2afe52ede3622f28dc0bedb5721059bbe
USUARIO <usuario@Macs-MacBook-Pro.local>
2019-11-15 09:32:06 +0100

    en construcción

--- starter_code/app.js
+++ starter_code/app.js
@@ -51,8 +51,9 @@ app.locals.title = 'Express - Generated with IronGenerator';
 
 
 
-const index = require('./routes/index');
-app.use('/', index);
-
+// const index = require('./routes/index');
+// app.use('/', index);
+app.use('/', require('./routes/index.routes'))
+app.use('/movies', require('./routes/movies.routes'))
 
 module.exports = app;
