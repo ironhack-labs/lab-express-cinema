5075fcfeebe0a2b0fc67acbba7ade6efe9026a04
EChColorado <edxz7c@gmail.com>
2019-11-15 09:22:28 -0600

    Done without styling

--- starter_code/routes/index.js
+++ starter_code/routes/index.js
@@ -1,9 +1,25 @@
 const express = require('express');
 const router  = express.Router();
-
+const Movie = require("../models/Movie");
 /* GET home page */
 router.get('/', (req, res, next) => {
   res.render('index');
 });
+
+router.get('/movies', async (req, res) => {
+  // const movies = await Movie.find();
+  // res.render("index", { movies });
+  Movie.find().then(movies => res.render("movies", { movies }));
+})
+
+router.get("/movies/:id", async (req, res) => {
+  const { id } = req.params;
+  Movie.findById(id)
+    .then(movie => res.render("detail", movie))
+    .catch(err => res.render("detail", { err: "No existe" }));
+
+}) 
+
+
 
 module.exports = router;
