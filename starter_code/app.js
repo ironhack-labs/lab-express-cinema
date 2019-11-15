f72ff6c9b33cd01e3bb44c00d8082a6f44a0ffe7
Ninoska Cairos <ninocairos@NinoCairos.local>
2019-11-15 09:27:56 +0100

    cjsn

--- starter_code/app.js
+++ starter_code/app.js
@@ -8,16 +8,8 @@ const hbs          = require('hbs');
 const mongoose     = require('mongoose');
 const logger       = require('morgan');
 const path         = require('path');
+require('./configs/mongoose.config')
 
-
-mongoose
-  .connect('mongodb://localhost/starter-code', {useNewUrlParser: true})
-  .then(x => {
-    console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
-  })
-  .catch(err => {
-    console.error('Error connecting to mongo', err)
-  });
 
 const app_name = require('./package.json').name;
 const debug = require('debug')(`${app_name}:${path.basename(__filename).split('.')[0]}`);
@@ -51,8 +43,8 @@ app.locals.title = 'Express - Generated with IronGenerator';
 
 
 
-const index = require('./routes/index');
-app.use('/', index);
+app.use("/", require("./routes/index.routes"))
+// app.use('/movies', require('./movies/movies.routes'));
 
 
 module.exports = app;
