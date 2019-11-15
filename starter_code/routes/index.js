560d4e2f5ba4f1f0436dd461706617b848654bea
Sonia Cáceres Benítez <soniacaceresbenitez@Air-de-Sonia.home>
2019-11-15 01:05:57 +0100

    done

--- starter_code/routes/index.js
+++ starter_code/routes/index.js
@@ -1,9 +1,28 @@
 const express = require('express');
-const router  = express.Router();
+const router = express.Router();
+const Movies = require("../models/Movie");
 
 /* GET home page */
 router.get('/', (req, res, next) => {
-  res.render('index');
+  res.render("index");
 });
+
+router.get("/movies", (req, res, next) => {
+  Movies.find({})
+    .then(movies => {
+      res.render("movies", { movies });
+    })
+    .catch(err => console.log(err))
+})
+
+router.get("/movies/:id", (req, res, next) => {
+  const movie = req.params.id
+  Movies.find({ _id: movie })
+    .then(movie => {
+
+      res.render("movie", { movie });
+    })
+    .catch(err => console.log(err))
+})
 
 module.exports = router;
