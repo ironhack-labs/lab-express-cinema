e7aa156c810aea2e4b10becda49604859aa3a836
Itza Zujuy Blanco <39780663+Zujuy@users.noreply.github.com>
2019-11-17 16:08:14 -0600

    done

--- starter_code/routes/index.js
+++ starter_code/routes/index.js
@@ -1,9 +1,21 @@
 const express = require('express');
 const router  = express.Router();
 
+const {
+  findMovies,
+  findOneMovie
+} = require("../controllers/movies.controllers")
+
+
 /* GET home page */
 router.get('/', (req, res, next) => {
   res.render('index');
 });
 
-module.exports = router;
+/* GET all movies */
+router.get("/movies", findMovies)
+
+/* GET one movie */
+router.get("/movies/:id", findOneMovie)
+
+module.exports = router
