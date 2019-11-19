2f8783f875a7a26ddf2465abf498805b965c9d6d
Robert <robcg1102@gmail.com>
2019-11-18 22:07:54 -0600

    done

--- starter_code/app.js
+++ starter_code/app.js
@@ -9,9 +9,8 @@ const mongoose     = require('mongoose');
 const logger       = require('morgan');
 const path         = require('path');
 
-
 mongoose
-  .connect('mongodb://localhost/starter-code', {useNewUrlParser: true})
+  .connect('mongodb://localhost/starter-code', {useNewUrlParser: true, useUnifiedTopology: true})
   .then(x => {
     console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
   })
@@ -24,12 +23,6 @@ const debug = require('debug')(`${app_name}:${path.basename(__filename).split('.
 
 const app = express();
 
-// Middleware Setup
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-
 // Express View engine setup
 
 app.use(require('node-sass-middleware')({
@@ -38,21 +31,19 @@ app.use(require('node-sass-middleware')({
   sourceMap: true
 }));
       
-
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
 
 
-
-// default value for title local
-app.locals.title = 'Express - Generated with IronGenerator';
-
-
-
 const index = require('./routes/index');
 app.use('/', index);
-
+const movies = require('./routes/index');
+app.use('/movies', movies)
 
 module.exports = app;
+
+app.listen(process.env.PORT, () => {
+  console.log(`Listening on ${process.env.PORT}`);
+});
--- starter_code/routes/index.js
+++ starter_code/routes/index.js
@@ -1,9 +1,21 @@
 const express = require('express');
 const router  = express.Router();
+const Movies = require("../models/Movie");
 
 /* GET home page */
 router.get('/', (req, res, next) => {
   res.render('index');
+});
+
+router.get("/movies", async (req, res) => {
+  const movies = await Movies.find({});
+  res.render("movies", { movies });
+});
+
+router.get("/movie/:_id", async (req, res) => {
+  const { _id } = req.params;
+  const movie = await Movies.findOne({ _id });
+  res.render("movie", { movie });
 });
 
 module.exports = router;
