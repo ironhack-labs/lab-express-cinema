32ac010eadc1dd7cb442fb812017af14557c179d
jcazorla2000 <jose.cazorla00@gmail.com>
2019-11-20 23:52:06 -0600

    DE-WDFT-MEX Jose Cazorla

--- starter_code/routes/index.js
+++ starter_code/routes/index.js
@@ -1,9 +1,22 @@
 const express = require('express');
 const router  = express.Router();
+const Movie = require("../models/Movie")
 
 /* GET home page */
 router.get('/', (req, res, next) => {
   res.render('index');
 });
+
+router.get("/movies", (req, res) => {
+  Movie.find()
+  .then(movies => res.render("movies", {movies}))
+  .catch(err => console.error(err))
+})
+
+router.get("/movie/:id", (req, res) => {
+  const { id } = req.params
+  Movie.findById(id)
+    .then(movie => res.render("movie", {movie}))
+})
 
 module.exports = router;
