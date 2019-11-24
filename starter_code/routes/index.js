3617eb06cfaaa8bf7a6bae77eb2cc565eca222c4
RicardoSalas17 <ricardosalas.1716@gmail.com>
2019-11-24 12:06:32 -0600

    done

--- starter_code/routes/index.js
+++ starter_code/routes/index.js
@@ -1,9 +1,31 @@
 const express = require('express');
 const router  = express.Router();
+const Movie = require('../models/Movie.js')
 
 /* GET home page */
 router.get('/', (req, res, next) => {
   res.render('index');
+});
+
+router.get('/movies', (req, res, next) => {
+  Movie.find()
+  .then(movies =>{
+    res.render('movies', {movies});
+  })
+  .catch(error => {
+    res.render(error)
+  })
+});
+
+router.get('/movies/:id', (req, res, next) => {
+  const {id} =req.params;
+  Movie.findById(id)
+  .then(movie =>{
+    res.render('detail', movie);
+  })
+  .catch(error => {
+    res.render( error)
+  })
 });
 
 module.exports = router;
