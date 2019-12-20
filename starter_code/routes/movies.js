1845977ccf6362fb4d3a95f673f76c052fe3b2b7
Fernando Lima Borrelli <flborrelli@gmail.com>
2019-12-20 18:36:04 -0300

    4th interation Done, remaining CSS

--- starter_code/routes/movies.js
+++ starter_code/routes/movies.js
@@ -2,11 +2,21 @@ const express = require('express');
 const router  = express.Router();
 const Movie = require('../models/Movie');
 
-/* GET moveis page */
+/* GET movies page */
 router.get('/', (req, res, next) => {
   Movie.find()
   .then(movies => {
     res.render('movies', { movies })
+  })
+  .catch(error => console.log(error))
+});
+
+/* GET see more movie page */
+router.get('/:Id', (req, res, next) => {
+  let { Id } = req.params;
+  Movie.findById(Id)
+  .then(movies => {
+    res.render('movie', movies )
   })
   .catch(error => console.log(error))
 });
