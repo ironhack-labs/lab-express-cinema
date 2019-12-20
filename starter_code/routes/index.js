64bd683293b71426acf094304e5a7e1466278c36
Leonardo Polon <lpolon@gmail.com>
2019-12-20 14:59:38 -0300

    Work in progress iteration 3: we need to seed the database and create a Movies model

--- starter_code/routes/index.js
+++ starter_code/routes/index.js
@@ -1,9 +1,16 @@
 const express = require('express');
-const router  = express.Router();
+const router = express.Router();
 
 /* GET home page */
 router.get('/', (req, res, next) => {
-  res.render('index');
+  res.render('index.hbs');
 });
+
+router.get('/movies', (req, res) => {
+  // .send() deveria retornar o conteudo de seeds.js
+  // res.send('queria ser uma lista de movies =(')
+  res.render('movies.hbs', {problema: "queria ser uma lista de movies ='("})
+})
+
 
 module.exports = router;
