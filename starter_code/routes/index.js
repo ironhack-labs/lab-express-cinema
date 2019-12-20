13b29396421675960eeb5ff0aaa6d02b5ac6a793
henricasi <henriquecmandruzato@gmail.com>
2019-12-20 15:05:36 -0300

    Finish exercise

--- starter_code/routes/index.js
+++ starter_code/routes/index.js
@@ -1,9 +1,29 @@
 const express = require('express');
 const router  = express.Router();
+const Movie = require('../models/Movie');
 
 /* GET home page */
 router.get('/', (req, res, next) => {
   res.render('index');
 });
+
+router.get('/movies', (req, res, next) =>{
+  Movie.find()
+  .then(movies => {
+    res.render('movies', {movies});
+  })
+  .catch(error => console.log(error))
+  
+})
+
+router.get('/movies/:id', (req, res, next) =>{
+  let {id} = req.params;
+  Movie.findById(id)
+  .then(movie => {
+    console.log(movie);
+    res.render('movie-details', movie);
+  })
+  .catch(error => console.log(error))
+})
 
 module.exports = router;
