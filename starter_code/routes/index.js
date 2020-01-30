975e7b75820ab75f1ec3bb056ae9f3c7f8b830be
Ashrafzhon Kurbonaliev <alforsii@gmail.com>
2020-01-30 07:52:25 -0500

    add Iteration 3 | List the Movies and Iteration 4 | Display Movie Info

--- starter_code/routes/index.js
+++ starter_code/routes/index.js
@@ -1,13 +1,43 @@
 const express = require('express');
 const router = express.Router();
+//import Movie from '/models'
+const Movie = require('../models/Movie.model');
 
-/* GET home page */
+//********************************************************* */
+//1. '/'home page
+//********************************************************* */
 router.get('/', (req, res, next) => {
   res.render('index', { title: 'Cinema | Home' });
 });
 
+//********************************************************* */
+//2. '/movies' page
+//********************************************************* */
 router.get('/movies', (req, res, next) => {
-  res.render('movies', { title: 'Cinema | Movies' });
+  Movie.find()
+    .then(moviesFromDB => {
+      // console.log('moviesFromDB: ', moviesFromDB);
+      res.render('movies-views/movies', {
+        title: 'Cinema | Movies',
+        movies: moviesFromDB,
+      });
+    })
+    .catch(err => console.log(`Can't find the movie. ${err}`));
+});
+
+//********************************************************* */
+//3. '/movies/movie/:movieId' page
+//********************************************************* */
+router.get('/movies/movie/:movieId', (req, res, next) => {
+  Movie.findById(req.params.movieId)
+    .then(moviesFromDB => {
+      console.log('moviesFromDB: ', moviesFromDB);
+      res.render('movies-views/movie-details', {
+        title: 'Cinema | About',
+        movie: moviesFromDB,
+      });
+    })
+    .catch(err => console.log(`Can't find the movie. ${err}`));
 });
 
 module.exports = router;
