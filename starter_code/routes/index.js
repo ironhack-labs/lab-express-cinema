d5b3b47c122abb8500a2a5d8e00c688b3b18b783
Nina Gautreau <nina.gautreau@gmail.com>
2020-01-30 17:14:59 +0100

    First commit

--- starter_code/routes/index.js
+++ starter_code/routes/index.js
@@ -1,9 +1,30 @@
 const express = require('express');
-const router  = express.Router();
+const router = express.Router();
+const movieModel = require("./../models/Movie");
 
 /* GET home page */
 router.get('/', (req, res, next) => {
   res.render('index');
+});
+
+router.get('/movies', (req, res, next) => {
+  movieModel.find()
+  .then(movies => {
+    res.render("movies", { movies: movies });
+  })
+  .catch(err => {
+    console.log(err);
+  });
+});
+
+router.get("/movie/:id", (req, res) => {
+  movieModel.findById(req.params.id)
+  .then(movie => {
+    res.render("movie", { movie });
+  })
+  .catch(err => {
+    console.log(err);
+  });
 });
 
 module.exports = router;
