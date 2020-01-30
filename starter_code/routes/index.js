0ad33af545c571fcb806cb45ef378f2a12cb384c
Alex Meyer <alexandre.meyer78@gmail.com>
2020-01-30 16:22:48 +0100

    All movies pages

--- starter_code/routes/index.js
+++ starter_code/routes/index.js
@@ -1,9 +1,23 @@
 const express = require('express');
 const router  = express.Router();
 
+const Movie = require('./../models/Movie.js');
+
 /* GET home page */
 router.get('/', (req, res, next) => {
   res.render('index');
 });
+
+/* GET movies page */
+router.get('/movies', (req, res, next) => {
+  Movie.find({})
+    .then(movies => {
+      res.render('movies', { movies });
+    })
+    .catch(error => {
+      console.log("Error getting the movies from DB : ", error);
+    })
+});
+
 
 module.exports = router;
