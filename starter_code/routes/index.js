8cec9b82c743bdd0772c6496656fa7cd189fde3a
Jon <orejajonathan@gmail.com>
2020-01-30 22:55:58 +0100

    done

--- starter_code/routes/index.js
+++ starter_code/routes/index.js
@@ -1,9 +1,37 @@
 const express = require('express');
 const router  = express.Router();
+const MovieModel = require("../models/Movie")
+
 
 /* GET home page */
 router.get('/', (req, res, next) => {
   res.render('index');
 });
+
+
+router.get('/movies', (req, res, next) => {
+  MovieModel
+  .find()
+  .then(movies => {
+    res.render("movies", {
+      movies
+    });
+  })
+  .catch(dbErr => {
+    console.log("OH NO ! Database error", dbErr);
+  });
+});
+
+router.get('/movies/:id', (req, res, next) =>{
+  MovieModel
+  .findById(req.params.id)
+  .then(movie =>{res.render("moviepage", { movie });
+})
+.catch(dbErr => console.error("OH no, db err :", dbErr));
+});
+
+
+
+
 
 module.exports = router;
