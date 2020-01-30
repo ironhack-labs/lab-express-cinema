23edc40abbddbc0e4a0e2f239d0bc770f41161b8
Pierre Etryhard <pierre.etryhard.pro@gmail.com>
2020-01-30 22:00:04 +0100

    done

--- starter_code/routes/index.js
+++ starter_code/routes/index.js
@@ -1,9 +1,31 @@
 const express = require('express');
 const router  = express.Router();
-
+const movieModel = require("../models/Movies")
 /* GET home page */
 router.get('/', (req, res, next) => {
   res.render('index');
 });
+
+router.get("/movies", (req,res) => {
+  movieModel
+  .find()
+  .then(dbRes => {
+    res.render("movies", {
+      dbRes
+    });
+  })
+  .catch(dbErr => {
+    console.error("error", dbErr);
+  });
+});
+
+router.get("/movie-infos/:id", (req,res) => {
+  movieModel
+  .findById(req.params.id)
+  .then(dbRes => {
+    res.render("movie-infos", {movie: dbRes});
+  })
+  .catch(err => console.error(err))
+})
 
 module.exports = router;
