92670941425714725e9a8766373d48eeaad95ee5
Remy Pham <remy.8P@gmail.com>
2020-01-31 10:55:44 +0100

    done

--- starter_code/routes/index.js
+++ starter_code/routes/index.js
@@ -1,9 +1,37 @@
 const express = require('express');
 const router  = express.Router();
+const MovieModel = require("./../models/Movie")
 
 /* GET home page */
 router.get('/', (req, res, next) => {
   res.render('index');
 });
+
+// router.get("/movies", (req,res) => {
+//   res.render("movies")
+// })
+
+router.get("/movies", (req,res) => {
+  MovieModel
+  .find()
+  .then(movies => {
+    res.render("movies", {
+      movies
+    });
+  })
+  .catch(dbErr => {
+    console.log("couldn't get info from database")
+  })
+});
+
+
+router.get("/movie/:id", (req,res) => {
+  MovieModel
+  .findById(req.params.id)
+  .then(movie => {
+    res.render("movie", {movie})
+  })
+  .catch(dbErr => console.log("db error", dbErr))
+})
 
 module.exports = router;
