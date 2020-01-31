ecd61ea27c1a07a37dbb2b83ed462ba8b060d22c
unknown <tayssire@hotmail.com>
2020-01-31 09:42:50 +0100

    done

--- starter_code/routes/index.js
+++ starter_code/routes/index.js
@@ -1,9 +1,32 @@
-const express = require('express');
-const router  = express.Router();
+const express = require("express");
+const router = express.Router();
+const movieModel = require("./../models/Movie.model");
 
 /* GET home page */
-router.get('/', (req, res, next) => {
-  res.render('index');
+router.get("/", (req, res, next) => {
+  res.render("index");
+});
+
+router.get("/movies", (req, res) => {
+  movieModel
+    .find()
+    .then(movies => {
+      res.render("movies", {
+        movies
+      });
+    })
+    .catch(dbErr => {
+      console.log("Error when trying to get the movies from database", dbErr);
+    });
+});
+
+router.get("/movies/:id", (req, res) => {
+  movieModel
+    .findById(req.params.id)
+    .then(movie => {
+      res.render("movie-detailled", { movie });
+    })
+    .catch(dbErr => console.error("OH no, db err :", dbErr));
 });
 
 module.exports = router;
