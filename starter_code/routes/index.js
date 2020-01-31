5a5d2f5a7c5be11829fca820c5787480f4860e01
Carlos Dominguez <carlos.cfdr86@gmail.com>
2020-01-30 21:51:46 -0600

    done

--- starter_code/routes/index.js
+++ starter_code/routes/index.js
@@ -6,9 +6,17 @@ const Movie = require('./../models/Movie')
 router.get('/', (req, res, next) => {
   res.render('index');
 });
+
+
+
 router.get('/movies', async (req, res)=>{
   const movies = await Movie.find()
   res.render('movies',{movies})
 })
 
+
+router.get('/movie/:id', async (req, res)=>{
+  const movie = await Movie.find({_id: req.params.id})
+  res.render('movie',{movie})
+})
 module.exports = router;
