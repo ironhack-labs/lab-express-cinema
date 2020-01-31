cdeeb060974dc3cd38cf8b228e4601ad90458ef7
ina <mgmuniza@gmail.com>
2020-01-31 10:44:45 +0100

    finish

--- starter_code/routes/index.js
+++ starter_code/routes/index.js
@@ -1,9 +1,30 @@
 const express = require('express');
-const router  = express.Router();
+const router  = new express.Router();
+const movieModel = require("../models/Movie")
 
 /* GET home page */
 router.get('/', (req, res, next) => {
   res.render('index');
+});
+
+/* GET MOVIES PAGE */
+
+router.get("/movies", (req, res) => {
+  movieModel
+    .find()
+    .then(movies => {
+      res.render("movies", {movies});
+    })
+    .catch(err => console.log(err));
+});
+
+router.get("/movies/:id", (req, res) => {
+  movieModel
+    .findById(req.params.id)
+    .then(movie => {
+      res.render("preview_movie", {movie});
+    })
+    .catch(err => console.log(err));
 });
 
 module.exports = router;
