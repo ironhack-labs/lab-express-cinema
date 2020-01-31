bc8faeda7a919b808332857cd9144418aef9ad35
Ale Ochoa <ale.ochoafi@gmail.com>
2020-01-30 23:26:30 -0600

    Iteracion 3 falta poner bonito

--- starter_code/routes/index.js
+++ starter_code/routes/index.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const router  = express.Router();
+const Movie = require('./../models/Movie')
 
 /* GET home page */
 router.get('/', (req, res, next) => {
   res.render('index');
-});
+})
+.get('/movies',async (req,res)=>{
+  let movieArr= await Movie.find({})
+  res.render('movies',{movieArr})
+})
 
 module.exports = router;
