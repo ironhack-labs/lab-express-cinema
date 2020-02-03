83a2d18c71b3e0079c22a443d559a1e9f6e40444
root <root@eCiwan.localdomain>
2020-02-03 19:28:31 +0100

    done

--- starter_code/routes/index.js
+++ starter_code/routes/index.js
@@ -1,9 +1,21 @@
 const express = require('express');
-const router  = express.Router();
-
+const router = express.Router();
+const Movie = require('../models/Movie');
 /* GET home page */
 router.get('/', (req, res, next) => {
-  res.render('index');
+  res.render('index.hbs');
+});
+
+router.get('/movies', (req, res, next) => {
+  Movie.find({}).then(movies => {
+    res.render('movies.hbs', { movies });
+  });
+});
+
+router.get('/movies/:id', (req, res, next) => {
+  Movie.findById(req.params.id).then(movie => {
+    res.render('movieCart.hbs', movie);
+  });
 });
 
 module.exports = router;
