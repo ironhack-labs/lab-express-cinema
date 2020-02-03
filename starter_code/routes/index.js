4bcea6186fa2d3012fe31a16a832b331c9b6d610
Mia Lavanti <mialavanti@Mias-MacBook-Air-3.local>
2020-02-03 22:00:50 +0100

    done

--- starter_code/routes/index.js
+++ starter_code/routes/index.js
@@ -1,9 +1,31 @@
-const express = require('express');
-const router  = express.Router();
+const express = require("express");
+const router = express.Router();
+const Movie = require("../models/Movie");
 
 /* GET home page */
-router.get('/', (req, res, next) => {
-  res.render('index');
+router.get("/", (req, res, next) => {
+  res.render("index");
+});
+
+router.get("/movies", (req, res, next) => {
+  Movie.find({})
+    .then(movieDocuments => {
+      // res.send(movieDocuments);
+      res.render("movies.hbs", { moviesList: movieDocuments });
+    })
+    .catch(err => {
+      next(error.hbs);
+    });
+});
+
+router.get("/movies/:id", (req, res, next) => {
+  Movie.findById(req.params.id)
+    .then(movieDocument => {
+      res.render("movieDetails.hbs", movieDocument);
+    })
+    .catch(err => {
+      next(err);
+    });
 });
 
 module.exports = router;
