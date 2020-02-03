b9f634cb10e6448e26910eb8add48aee02bb5462
Marcel <marcel.riek@googlemail.com>
2020-02-03 19:50:50 +0100

    done including styling

--- starter_code/routes/index.js
+++ starter_code/routes/index.js
@@ -1,9 +1,32 @@
 const express = require('express');
 const router  = express.Router();
+const Movie = require("../models/Movie");
 
 /* GET home page */
 router.get('/', (req, res, next) => {
   res.render('index');
+});
+
+router.get("/movies", (req, res, next) => {
+  Movie.find({})
+    .then(movieDocuments => {
+      // res.send(movieDocuments);
+      res.render("movies.hbs", { moviesList: movieDocuments });
+    })
+    .catch(err => {
+      next(err);
+    });
+});
+
+router.get("/movies/:id", (req, res, next) => {
+  Movie.findById(req.params.id)
+    .then(movieDocuments => {
+      // res.send(movieDocuments);
+      res.render("movieDetails.hbs", movieDocuments);
+    })
+    .catch(err => {
+      next(err);
+    });
 });
 
 module.exports = router;
