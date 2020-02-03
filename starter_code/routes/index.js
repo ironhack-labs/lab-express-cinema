0506d5959d2c08c63b500efff3d6020f5704a03b
Victoria Blatkowska <victoriablatkowska@gmail.com>
2020-02-03 22:06:28 +0100

    done

--- starter_code/routes/index.js
+++ starter_code/routes/index.js
@@ -1,9 +1,33 @@
-const express = require('express');
-const router  = express.Router();
+const express = require("express");
+const router = express.Router();
+const Movie = require("../models/Movie");
 
 /* GET home page */
-router.get('/', (req, res, next) => {
-  res.render('index');
+router.get("/", (req, res, next) => {
+  res.render("index");
+});
+console.log(Movie);
+router.get("/movies", (req, res, next) => {
+  Movie.find({})
+    .then(data => {
+      // we need to render in the `.then()` because that is WHEN the bookDocuments are accessible
+      // res.send(data);
+      res.render("movies.hbs", { moviesList: data });
+    })
+    .catch(err => {
+      next(err);
+    });
+});
+router.get("/movie/:id", (req, res, next) => {
+  Movie.findById(req.params.id)
+    .then(movieData => {
+      // we need to render in the `.then()` because that is WHEN the bookDocuments are accessible
+      //res.send(movieData);
+      res.render("movie.hbs", movieData);
+    })
+    .catch(err => {
+      next(err);
+    });
 });
 
 module.exports = router;
