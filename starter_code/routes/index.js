ad0e7cb7509d1b5ef8ca2143436465da0ad2f3a8
stifamajstor <stjepan.crncic@gmail.com>
2020-02-04 08:59:33 +0100

    Done

--- starter_code/routes/index.js
+++ starter_code/routes/index.js
@@ -1,9 +1,33 @@
-const express = require('express');
-const router  = express.Router();
+const express = require("express");
+const router = express.Router();
+const Movie = require("../models/Movie");
 
 /* GET home page */
-router.get('/', (req, res, next) => {
-  res.render('index');
+router.get("/", (req, res, next) => {
+  res.render("index");
+});
+
+//GET ALL BOOKS
+router.get("/movies", (req, res, next) => {
+  Movie.find()
+    .then(movieDocuments => {
+      res.render("movies.hbs", { movieList: movieDocuments });
+    })
+    .catch(err => {
+      next(err);
+    });
+});
+
+//DISPLAY A MOVIE
+//DISPLAY A BOOK
+router.get("/movies/:id", (req, res, next) => {
+  Movie.findById(req.params.id)
+    .then(movie => {
+      res.render("movieDetails.hbs", movie);
+    })
+    .catch(err => {
+      next(err);
+    });
 });
 
 module.exports = router;
