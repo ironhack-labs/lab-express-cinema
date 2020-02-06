622b47e7bbc68b57eb9e5c0911ab6db6dee73f1d
alejandrosz <alejandrosz@gmail.com>
2020-02-06 19:45:30 +0100

    done

--- starter_code/routes/index.js
+++ starter_code/routes/index.js
@@ -1,9 +1,24 @@
-const express = require('express');
-const router  = express.Router();
+const express = require("express");
+const router = express.Router();
+const Movies = require("../models/Movie");
 
 /* GET home page */
-router.get('/', (req, res, next) => {
-  res.render('index');
+router.get("/", (req, res, next) => {
+  res.render("index");
 });
 
+router.get("/movies", (req, res, next) => {
+  Movies.find().then(moviesFound => res.render("movies", { moviesFound }));
+  // .then(moviesFound => res.json({moviesFound}));
+});
+
+router.get("/movies/:Id", (req, res, next) => {
+  let { Id } = req.params;
+  // console.log(Id)
+  // Movies.findById(Id).then(movieFound => res.json(movieFound));
+  Movies.findById(Id).then(movieFound =>
+    res.render("movieDetail", { movieFound })
+  );
+  // .then(moviesFound => res.json({moviesFound}));
+});
 module.exports = router;
