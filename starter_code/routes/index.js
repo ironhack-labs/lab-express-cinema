a3d04e7cd26fcd5b0efc09ea399971d6d4f3740d
Jose Manuel Henche Ruiz <josemanuel.hruiz@gmail.com>
2020-02-06 21:47:08 +0100

    Cinema sin css

--- starter_code/routes/index.js
+++ starter_code/routes/index.js
@@ -1,9 +1,30 @@
 const express = require('express');
-const router  = express.Router();
+const router = express.Router();
+const Movies = require('../models/movies')
 
 /* GET home page */
 router.get('/', (req, res, next) => {
+  console.log(req.query)
   res.render('index');
 });
+router.get("/movies", (req, res, next) => {
+  Movies.find().then(movie => res.render("movieList", {
+    movie
+  }));
+});
+
+router.get('/movie/:id', (req, res, next) => {
+  let {
+    id
+  } = req.params
+  Movies.findById(id).then(movieDetail => {
+    res.render('movie', {
+      movieDetail
+    });
+  });
+});
+
+
+
 
 module.exports = router;
