b213b15b375273a54e55f844d48c7fe019db1077
Luz Divina Delgado Castañeda <luzdivdelg@gmail.com>
2020-02-06 21:32:27 +0100

    done until iteration 3

--- starter_code/routes/index.js
+++ starter_code/routes/index.js
@@ -1,9 +1,21 @@
 const express = require('express');
-const router  = express.Router();
+const router = express.Router();
+
+const Movies = require('../models/movie.model');
 
 /* GET home page */
 router.get('/', (req, res, next) => {
   res.render('index');
 });
+
+router.get("/movies", (req, res, next) => {
+  Movies.find({})
+    .then(movies => {
+      res.render("movies", {
+        movies
+      });
+    })
+    .catch(err => console.log(err))
+})
 
 module.exports = router;
