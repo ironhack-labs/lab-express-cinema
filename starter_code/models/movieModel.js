f40adcd4ad07bbffb570ccee45fbffac3d225442
andrewnunezbibby <andrew.bibby3@gmail.com>
2020-02-06 19:25:14 +0100

    done [PARWDFT Andrew]

---
+++ starter_code/models/movieModel.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const movieSchema = new Schema({
+    title: { type: String },
+    director: { type: String },
+    stars: { type: [String] },
+    image: { type: String },
+    description: { type: String },
+    showtimes: { type: [String] }
+})
+
+const movieModel = mongoose.model("Movie", movieSchema)
+
+module.exports = movieModel;
--- starter_code/routes/index.js
+++ starter_code/routes/index.js
@@ -1,9 +1,24 @@
 const express = require('express');
-const router  = express.Router();
+const router = express.Router();
+const movieModel = require("../models/movieModel");
 
 /* GET home page */
 router.get('/', (req, res, next) => {
   res.render('index');
+});
+
+router.get("/movies", (req, res, next) => {
+  movieModel.find()
+    .then(movies => res.render("movies", { movies }))
+    .catch(err => console.log(err))
+});
+
+router.get("/movies/:id", (req, res, next) => {
+  movieModel.findById(req.params.id)
+    .then(movie => {
+      res.render("movie", { movie })
+    })
+    .catch(err => console.log(err))
 });
 
 module.exports = router;
