3cb1064137269589ec228f56ca8fdf9cf3ad9ca3
Pablo Carceller <pablocarceller@me.com>
2020-02-06 22:43:56 +0100

    done

--- starter_code/routes/index.js
+++ starter_code/routes/index.js
@@ -1,9 +1,32 @@
 const express = require('express');
-const router  = express.Router();
+const router = express.Router();
+const movies = require('../models/Movie')
 
 /* GET home page */
 router.get('/', (req, res, next) => {
   res.render('index');
 });
+
+router.get('/movies', (req, res, next) => {
+  movies.find()
+    .then((movies) => {
+      // res.json(movies)
+      res.render('movies', { movies })
+    }).catch((err) => {
+      console.log("espabila")
+    });
+});
+
+router.get('/movie/:movieId', (req, res, next) => {
+  console.log(req.params.movieId)
+  movies.findById(req.params.movieId)
+    .then((moviesFoundId) => {
+      // res.json(movies)
+      res.render('movieSelect', moviesFoundId)
+    }).catch((err) => {
+      console.log("espabila")
+    });
+});
+
 
 module.exports = router;
