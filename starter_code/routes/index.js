73713a781e0454308503603008004039525a0ca1
CrisSugar <aguacero18@hotmail.com>
2020-02-07 08:50:56 +0100

    done

--- starter_code/routes/index.js
+++ starter_code/routes/index.js
@@ -1,9 +1,26 @@
 const express = require('express');
 const router  = express.Router();
+const Movie = require("../models/Movie.js")
 
 /* GET home page */
 router.get('/', (req, res, next) => {
   res.render('index');
 });
+
+router.get('/movies', (req, res) => {
+  
+  Movie.find()
+  .then(moviesFound =>{
+        res.render('movies',{moviesFound})
+       });
+})
+
+router.get('/movie/:id',(req,res)=>{
+  Movie.findById(req.params.id)
+  .then(movieFound =>{
+        res.render('movie',movieFound)
+       });
+})
+
 
 module.exports = router;
