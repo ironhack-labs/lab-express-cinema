0fba3dabae90cc287dab4d5e3f54e64b06f22462
Sara Garzon <saragarzonburgos@gmail.com>
2020-02-07 09:28:13 +0100

    Done

--- starter_code/routes/index.js
+++ starter_code/routes/index.js
@@ -1,9 +1,12 @@
 const express = require('express');
-const router  = express.Router();
+const router = express.Router();
+const Movies = require('../models/movie')
 
 /* GET home page */
-router.get('/', (req, res, next) => {
-  res.render('index');
+router.get('#', (req, res) => {
+  Movies.find()
+    .then(allMovies => res.render('movies', { movies: allMovies }))
+    .catch(err => console.log("Hubo un error en find", err))
 });
 
 module.exports = router;
