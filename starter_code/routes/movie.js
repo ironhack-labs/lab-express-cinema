a88eab816c65d8f81458fa641c0eb413978292d9
Erick Sicard <esrfree@gmail.com>
2020-02-07 22:25:51 -0500

    Iteration 3 - Styling

--- starter_code/routes/movie.js
+++ starter_code/routes/movie.js
@@ -6,7 +6,7 @@ const Movie = require('../models/Movie.js');
 router.get('/movies', (req, res, next) => {
   Movie.find()
   .then( foundMovies => {
-    console.log(foundMovies);
+    //console.log(foundMovies);
     res.render('movies', {foundMovies})
   })
   .catch( err => console.log('Error while getting movies ', err ) )
