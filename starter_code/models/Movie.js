65c235b18d863e236c7ec5713cefa13480accc9f
Marta Mullor <Marta@MacBook-Air-de-marta.local>
2020-02-29 20:29:43 +0100

    initalize the project

---
+++ starter_code/models/Movie.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+
+const { Schema } = mongoose;
+
+const movieSchema = new Schema({
+  title: String,
+  director: String,
+  stars: Array,
+  image: {type: String, default:'https://images-na.ssl-images-amazon.com/images/M/MV5BMzg3Y2MyNjgtMzk4ZS00OTU3LWEwZmMtN2Y0NTdlZjU0NGFiXkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_UX182_CR0,0,182,268_AL_.jpg'},
+  description: String,
+  showtimes: Array
+});
+
+const Movie = mongoose.model('Movie', movieSchema);
+
+module.exports = Movie;
