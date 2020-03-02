2b9378135f7a28ff15077075467b2c413b70d579
Marc Serra <mserrahidalgo@gmail.com>
2020-03-02 19:52:32 +0100

    add update

--- starter_code/routes/movies.js
+++ starter_code/routes/movies.js
@@ -13,8 +13,7 @@ router.get('/', (req, res, next) => {
 
 });
 
-
-
+// READ
 router.get('/detail/:id', (req, res, next) => {
   const movieId = req.params.id
   Movie.findById(movieId)
@@ -25,6 +24,29 @@ router.get('/detail/:id', (req, res, next) => {
 
 });
 
+
+// UPDATE
+router.get('/update/:id', (req, res, next) => {
+  const movieId = req.params.id
+  Movie.findById(movieId)
+    .then((movie) => {
+      res.render('updateMovie', { movie })
+    })
+    .catch(error => console.log(error))
+});
+router.post('/update/:id', (req, res, next) => {
+  const movieId = req.params.id
+  console.log(movieId)
+  Movie.updateOne({ _id: movieId }, req.body)
+    .then((movie) => {
+      res.redirect(`/movies/detail/${movieId}`)
+    })
+    .catch(error => console.log(error))
+
+});
+
+
+// DELETE
 router.post('/:id/delete', (req, res, next) => {
   const movieId = req.params.id
   Movie.deleteOne({ _id: movieId })
@@ -35,10 +57,10 @@ router.post('/:id/delete', (req, res, next) => {
 });
 
 
+// CREATE
 router.get('/add', (req, res, next) => {
   res.render('addMovie')
 });
-
 router.post('/add', (req, res, next) => {
   const newMovie = new Movie(req.body)
   Movie.insertMany(newMovie)
@@ -48,5 +70,7 @@ router.post('/add', (req, res, next) => {
     })
     .catch(error => console.log(error))
 });
+
+
 
 module.exports = router;
