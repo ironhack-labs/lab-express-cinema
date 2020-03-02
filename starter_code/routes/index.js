21fb2f5972e624ac043c82ad6d7fbe6eb6edc183
javier Varela <javivarela@javiers-MacBook-Pro.local>
2020-03-02 02:04:14 +0100

    trying to get films

--- starter_code/routes/index.js
+++ starter_code/routes/index.js
@@ -1,9 +1,21 @@
 const express = require('express');
 const router  = express.Router();
+const Movie = require('../models/Movie');
 
 /* GET home page */
 router.get('/', (req, res, next) => {
   res.render('index');
+});
+
+router.get('/movies', (req, res, next) => {
+  Movie.find()
+    .then(movies => {
+      console.log('Retrieved movies from DB:', movies);
+      res.render('movies', { movies });
+    })
+    .catch(error => {
+      console.log('Error while getting the movies from the DB: ', error);
+    })
 });
 
 module.exports = router;
