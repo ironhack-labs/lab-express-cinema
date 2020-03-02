15b42d4fbbe90f331f9efc31aeaaf691ff4b40db
javier Varela <javivarela@javiers-MacBook-Pro.local>
2020-03-02 02:39:37 +0100

    iteration 4 completed single movie page

--- starter_code/routes/index.js
+++ starter_code/routes/index.js
@@ -10,11 +10,20 @@ router.get('/', (req, res, next) => {
 router.get('/movies', (req, res, next) => {
   Movie.find()
     .then(movies => {
-      console.log('Retrieved movies from DB:', movies);
       res.render('movies', { movies });
     })
     .catch(error => {
       console.log('Error while getting the movies from the DB: ', error);
+    })
+});
+
+router.get('/movies/:movieId', (req, res, next) => {
+  Movie.findById(req.params.movieId)
+    .then(movie => {
+      res.render('movie', { movie });
+    })
+    .catch(error => {
+      console.log('Error while retrieving movie details: ', error);
     })
 });
 
