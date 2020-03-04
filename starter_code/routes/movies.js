d9a3087ea5afb49882dbe7fa76e7760e2d9467e9
Alex <alex.fernandez.canovas@gmail.com>
2020-03-04 13:06:08 +0100

    the update not show the new Director until refresh the page...

--- starter_code/routes/movies.js
+++ starter_code/routes/movies.js
@@ -10,19 +10,22 @@ router.get('/', (req, res, next) => {
   })
 });
 
+
+router.post('/movie/:id/edit', (req,res,next) =>{
+  const {director} = req.body;  //afecta el nombre que se le da a la variable ya que hace referencia al nombre del dato que le pasamos en el form
+  const {id} = req.params;
+  Movie.findByIdAndUpdate({_id: req.params.id}, {$set: {director}})
+  .then(movie => {
+    res.redirect('/movies/movie/${id}');  //hasta que no actualizo la pág no se ve el cambio de nombre...
+  })
+  .catch(next)
+})
+
 router.get('/movie/:id', (req, res, next) => {
   Movie.findOne({_id: req.params.id})  //hasta que no he mirado como se creaba el id (en este caso _id) no cargaban las movies...
   .then(movie => {
     res.render('movie', {movie});
   })
 });
-router.post('/movie/:id/edit', (req,res) =>{
-  const {newDirector} = req.body;
-  Movie.findByIdAndUpdate({_id: req.params.id}, {$set: {director:{newDirector}}})
-  .then(movie => {
-    res.render('movie', {movie});
-  })
-  .catch(error)
-})
 
 module.exports = router;
