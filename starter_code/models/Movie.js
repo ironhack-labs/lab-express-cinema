4c71aaade64f34d2a6ec8eee5c942fcbd7137d93
msallito510 <msallito510@gmail.com>
2020-03-04 23:01:25 +0100

    fist steps

---
+++ starter_code/models/Movie.js
@@ -0,0 +1,15 @@
+let mongoose = require("mongoose");
+let Schema = mongoose.Schema;
+let MovieSchema = new Schema({
+  title: {
+    type: String,
+    unique: true
+  },
+  director: String,
+  stars: Array,
+  image: String,
+  description: String,
+  showtimes: Array
+});
+
+let Movie = mongoose.model("Movie", MovieSchema);
--- starter_code/routes/index.js
+++ starter_code/routes/index.js
@@ -1,9 +1,11 @@
-const express = require('express');
-const router  = express.Router();
+const express = require("express");
+const router = express.Router();
+const Movies = require("../models/Movie");
+const filemovies = require("../bin/seeds.js");
 
 /* GET home page */
-router.get('/', (req, res, next) => {
-  res.render('index');
+router.get("/", (req, res, next) => {
+  res.render("index");
 });
 
 module.exports = router;
