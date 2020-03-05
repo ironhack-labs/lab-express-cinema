acc36b800bf408cd53b67e5c25eabc483ac494cb
Gerard Sole <gerardsole@MacBook-Pro-de-Gerard-2.local>
2020-03-05 17:47:07 +0100

    CRUD done

--- starter_code/routes/movies.js
+++ starter_code/routes/movies.js
@@ -9,6 +9,28 @@ router.get('/',(req, res, next) => {
   })
   .catch(err => console.log('Error while rendering movies: ', err));
 });
+// ADD MOVIES
+router.get('/create', (req, res) => {
+  res.render('create')
+  
+});
+// POST MOVIES 
+router.post('/', (req, res, next) => {
+	const { title, director, stars, image, description, showtimes } = req.body;
+	Movie.create({
+    title, 
+    director,
+    stars,
+    image, 
+    description,
+    showtimes, 
+	})
+		.then(()=> {
+			res.redirect('movies');
+		})
+		.catch(next);
+});
+// GET MOVIES ID
 router.get('/:id',(req, res) =>{
 const { id } = req.params;
 Movie.findById(id)
@@ -17,5 +39,40 @@ Movie.findById(id)
 })
 .catch(err => console.log('Error while rendering movies: ', err));
 });
+//GET MOVIES EDIT
+router.get('/:id/edit',(req, res) =>{
+  const { id } = req.params;
+  Movie.findById(id)
+  .then(aMovie => {
+    res.render('edit', { aMovie });
+  })
+  .catch(err => console.log('Error while rendering movies: ', err));
+});
+// POST MOVIES EDIT
+router.post('/:id/edit', (req, res, next) => {
+	const { id } = req.params;
+	const { title, director, stars, image, description, showtimes } = req.body;
+	Movie.findByIdAndUpdate(id, {
+    title, 
+    director,
+    stars,
+    image, 
+    description,
+    showtimes, 
+	})
+		.then(movieUpdated=> {
+			res.redirect(`/movies/${id}`);
+		})
+		.catch(next);
+});
+// DELETE MOVIES
+router.post('/:id/delete', (req, res, next) => {
+	const { id } = req.params;
 
+	Movie.findByIdAndDelete(id)
+		.then(() => {
+			res.redirect('/movies');
+		})
+		.catch(next);
+});
 module.exports = router;
