fc7948a8d7221a9baf9dc3d443422c295889f85f
Anastasiia Kozlova <anastasiia.sh.v@gmail.com>
2020-03-12 10:35:40 +0100

    done

--- starter_code/routes/index.js
+++ starter_code/routes/index.js
@@ -1,9 +1,25 @@
 const express = require('express');
 const router  = express.Router();
+const Movie = require('../models/Movies');
+
 
 /* GET home page */
 router.get('/', (req, res, next) => {
   res.render('index');
 });
+
+router.get('/movies', (req,res,next) => {
+  Movie.find({}).then((movies)  => {
+    res.render('movies', {movies})
+  })
+} )
+
+router.get('/movies/:id', (req,res,next) => {
+  console.log()
+  Movie.findById(req.params.id).then((movie)  => {
+    res.render('movie', {movie})
+  })
+  
+} )
 
 module.exports = router;
