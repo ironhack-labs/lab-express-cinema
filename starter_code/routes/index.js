ac5bcf4e38383c61264d2a184b1dfeb049fb3fe4
Friso <fhoman@gmail.com>
2020-03-13 14:18:03 +0100

    done

--- starter_code/routes/index.js
+++ starter_code/routes/index.js
@@ -1,9 +1,36 @@
 const express = require('express');
-const router  = express.Router();
+const router = express.Router();
+const Movie = require('../models/Movie');
 
 /* GET home page */
 router.get('/', (req, res, next) => {
   res.render('index');
 });
+
+// Get movies page 
+
+router.get('/movies', (req, res, next) => {
+  Movie.find().then(movies => {
+    res.render('movies', {
+      movies: movies
+    });
+  })
+});
+
+// Get movie detail page
+
+router.get('/movies/:id', (req, res, next) => {
+  console.log(req.params.id)
+  Movie.findOne({
+      '_id': req.params.id
+    })
+    .then(movie => {
+      console.log(movie)
+      res.render('movie', {
+        movie: movie
+      });
+    })
+});
+
 
 module.exports = router;
