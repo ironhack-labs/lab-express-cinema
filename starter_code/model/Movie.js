e829c00c4e9306127ed52d51f391480947d18da3
Boyuan Shi <sbyshjd@hotmail.com>
2020-03-13 14:11:42 +0100

    done

---
+++ starter_code/model/Movie.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const movieSchema = new Schema(
+    {
+        title: {type:String},
+        director: {type:String},
+        stars:{type:[String]},
+        image:{type:String},
+        description:{type:String},
+        showtimes:{type:[String]},
+    },
+    {
+        timestamps:{
+            createdAt:'created_at',
+            updatedAt:'updated_at'
+        }
+    }
+);
+
+const Movie = mongoose.model('Movie',movieSchema);
+module.exports = Movie;
--- starter_code/routes/index.js
+++ starter_code/routes/index.js
@@ -1,9 +1,32 @@
 const express = require('express');
 const router  = express.Router();
+const Movie = require('../model/Movie')
+// const app = express();
+// app.set('view engine', 'hbs');
+// hbs.registerPartials(__dirname+'/views/partials')
 
 /* GET home page */
 router.get('/', (req, res, next) => {
   res.render('index');
 });
+
+//GET the movies page;
+router.get('/movies',(req,res,next)=> {
+  Movie.find()
+    .then(movies => {
+      res.render('movies',{movies});
+    })
+    .catch(err => console.error(err));
+})
+
+//GET the movie detail page;
+router.get('/movie/:movieId',(req,res,next)=> {
+  Movie.findById(req.params.movieId)
+    .then(movie => {
+      console.log(movie);
+      res.render('movie-info',movie)
+    })
+    .catch(err => console.error(err));
+})
 
 module.exports = router;
