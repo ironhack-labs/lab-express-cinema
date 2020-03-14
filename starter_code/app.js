8dcbda317effbf0b8627a0c129ac64c2ece612d2
Alberto Quiñonero Ayllon <albertoquinoneroayllon@MBPdeAlberto2.home>
2020-03-14 11:12:36 +0100

    funciona sólo falta css

--- starter_code/app.js
+++ starter_code/app.js
@@ -9,15 +9,25 @@ const mongoose     = require('mongoose');
 const logger       = require('morgan');
 const path         = require('path');
 
+const Movies = require('./models/Movies');
+
+
 
 mongoose
-  .connect('mongodb://localhost/starter-code', {useNewUrlParser: true})
+  .connect('mongodb://localhost/cinema-project', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
   .then(x => {
-    console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
+    console.log(
+      `Connected to Mongo! Database name: "${x.connections[0].name}"`
+    );
   })
   .catch(err => {
-    console.error('Error connecting to mongo', err)
+    console.error('Error connecting to mongo', err);
   });
+
+
 
 const app_name = require('./package.json').name;
 const debug = require('debug')(`${app_name}:${path.basename(__filename).split('.')[0]}`);
@@ -43,16 +53,63 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
-
+hbs.registerPartials(__dirname + "/views/partials");
 
 
 // default value for title local
 app.locals.title = 'Express - Generated with IronGenerator';
 
+app.get('/', function(req, res, next) {
+  res.render('index');
+});
 
 
-const index = require('./routes/index');
-app.use('/', index);
+
+
+app.get('/movies/add', (req, res, next) => {
+  res.render("movies-add");
+});
+
+app.post('/movies/add', (req, res, next) => {
+  const { title, director, stars, image, description, showtimes } = req.body;
+  const newMovie = new Movies({ title, director, stars, image, description, showtimes})
+  newMovie.save()
+  .then((movie) => {
+    res.redirect('/movies');
+  })
+  .catch((error) => {
+    console.log(error);
+  })
+});
+
+
+
+app.get('/movies', (req, res, next) => {
+  Movies.find()
+    .then(allTheMoviesFromDB => {
+      // console.log('Retrieved books from DB:', allTheBooksFromDB);
+      res.render('movies', { movies: allTheMoviesFromDB });
+    })
+    .catch(error => {
+      console.log('Error while getting the movies from the DB: ', error);
+    })
+});
+
+app.get('/results', (req, res, next) => {
+  res.render("results");
+});
+
+
+app.get('/movies/:moviesId', (req, res, next) => {
+  console.log(req.params.moviesId)
+  Movies.findById(req.params.moviesId)
+    .then(theMovies => {
+      res.render('results', { movies: theMovies });
+    })
+    .catch(error => {
+      console.log('Error while retrieving book details: ', error);
+    })
+});
 
 
 module.exports = app;
