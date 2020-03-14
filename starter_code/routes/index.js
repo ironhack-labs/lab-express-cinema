8bb2c7c634b33309467fc756d0469531648aeda9
Miguel Dominguez Chamorro <mdchamorro7@gmail.com>
2020-03-14 13:38:56 +0100

    finish without css

--- starter_code/routes/index.js
+++ starter_code/routes/index.js
@@ -7,4 +7,25 @@ router.get('/', (req, res, next) => {
   res.render('index');
 });
 
+router.get("/movies", (req, res) => {
+  Movie.find()
+  .then(allMovies => {
+  res.render("movies", {movies : allMovies})
+})
+.catch(error => {
+  console.log('Error', error);
+})
+});
+
+router.get('/movies/:movieId', (req, res, next) => {
+  Movie.findById(req.params.movieId)
+    .then(amovie => {
+      res.render('detailed-movies', { movies: amovie });
+    })
+    .catch(error => {
+      console.log('Error', error);
+    })
+});
+
 module.exports = router;
+
