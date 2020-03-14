9a1a5be53b0ef9eb5561aefee470ad59ad1c21ef
Víctor Ronda <victor.ronda@gmail.com>
2020-03-14 22:09:46 +0100

    DONE

---
+++ starter_code/models/movie.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const movieSchema = new Schema(
+    {
+        title: String,
+        director: String,
+        stars: Array,
+        image: String,
+        description: String,
+        showtimes: Array
+
+    },
+    {
+      timestamps: {
+        createdAt: 'createdAt',
+        updatedAt: 'updatedAt'
+      }
+    }
+  );
+  
+  const Movie = mongoose.model('Movie', movieSchema);
+  
+  module.exports = Movie
--- starter_code/routes/index.js
+++ starter_code/routes/index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const router  = express.Router();
+const Movie = require('../models/movie.js')
+
 
 /* GET home page */
 router.get('/', (req, res, next) => {
@@ -7,3 +9,27 @@ router.get('/', (req, res, next) => {
 });
 
 module.exports = router;
+
+//GET Movies
+router.get('/movies', (req, res, next) => {
+	Movie.find()
+		.then((allTheMoviesFromDB) => {
+			console.log('Retrieved movies from DB:', allTheMoviesFromDB);
+			res.render('movies', { movies: allTheMoviesFromDB });
+		})
+		.catch((error) => {
+			console.log('Error while getting the movies from the DB: ', error);
+		})
+});
+
+
+router.get('/movies/:id', (req, res, next) => {
+	Movie.findById(req.params.id)
+		.then((theMovie) => {
+			//console.log('The Movie I want to see is:', theMovie.title);
+			res.render('movies-info', { movie: theMovie });
+		})
+		.catch((error) => {
+			console.log('Error while getting the movies from the DB: ', error);
+		})
+});
