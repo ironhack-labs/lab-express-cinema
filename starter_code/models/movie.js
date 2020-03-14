03e3c79eace146188829a8f350c90e34cda0e6fd
Raul Marchan <alountk@gmail.com>
2020-03-14 15:59:24 +0100

    [BCN-WDFT-RAUL]-1

---
+++ starter_code/models/movie.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+const Schema   = mongoose.Schema;
+const moviesSchema = new Schema({
+  title: String,
+  director: String,
+  stars: Array,
+  image: String,
+  description: String,
+  showtimes: Array,
+});
+const Movie = mongoose.model("movie", moviesSchema);
+module.exports = Movie;
