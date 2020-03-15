1a221a4d5e08100af2ecf153b15206580b954a34
Cristina Garcia <cristinagarciagrillo@gmail.com>
2020-03-15 15:40:52 +0100

    Finished Lab-express-cinema

--- starter_code/app.js
+++ starter_code/app.js
@@ -1,23 +1,24 @@
 require('dotenv').config();
 
-const bodyParser   = require('body-parser');
+const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
-const express      = require('express');
-const favicon      = require('serve-favicon');
-const hbs          = require('hbs');
-const mongoose     = require('mongoose');
-const logger       = require('morgan');
-const path         = require('path');
+const express = require('express');
+const favicon = require('serve-favicon');
+const hbs = require('hbs');
+const mongoose = require('mongoose');
+const logger = require('morgan');
+const path = require('path');
+const Movie = require('./models/Movie');
 
 
 mongoose
-  .connect('mongodb://localhost/starter-code', {useNewUrlParser: true})
-  .then(x => {
-    console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
-  })
-  .catch(err => {
-    console.error('Error connecting to mongo', err)
-  });
+	.connect('mongodb://localhost/cinema', {useNewUrlParser: true})
+	.then(x => {
+		console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
+	})
+	.catch(err => {
+		console.error('Error connecting to mongo', err)
+	});
 
 const app_name = require('./package.json').name;
 const debug = require('debug')(`${app_name}:${path.basename(__filename).split('.')[0]}`);
@@ -27,17 +28,17 @@ const app = express();
 // Middleware Setup
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({extended: false}));
 app.use(cookieParser());
 
 // Express View engine setup
 
 app.use(require('node-sass-middleware')({
-  src:  path.join(__dirname, 'public'),
-  dest: path.join(__dirname, 'public'),
-  sourceMap: true
+	src: path.join(__dirname, 'public'),
+	dest: path.join(__dirname, 'public'),
+	sourceMap: true
 }));
-      
+
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
@@ -45,14 +46,38 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
 
 
-
 // default value for title local
 app.locals.title = 'Express - Generated with IronGenerator';
-
 
 
 const index = require('./routes/index');
 app.use('/', index);
 
+
+//LIST MOVIES
+
+app.get('/movies', (req, res) => {
+	Movie.find()
+		.then(movies => {
+			res.render('movies', {movies});
+		})
+		.catch(error => {
+			console.log('Error while getting the movies from the DB: ', error);
+		})
+});
+
+
+//DETAILS BY ID
+
+app.get('/movie/:id', (req, res) => {
+	const id = req.params.id;
+	Movie.findById(id)
+		.then(movie => {
+			res.render('movie', {movie});
+		})
+		.catch(error => {
+			console.log('Error while getting the details ', error);
+		})
+});
 
 module.exports = app;
