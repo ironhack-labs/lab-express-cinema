43c7486fb872147753a5fdddccaae65dbece182f
Angelilla <angelamorealba@gmail.com>
2020-03-15 23:51:31 +0100

    done

--- starter_code/routes/index.js
+++ starter_code/routes/index.js
@@ -1,9 +1,33 @@
 const express = require('express');
 const router  = express.Router();
+const Movie = require('../models/Movie');
 
 /* GET home page */
 router.get('/', (req, res, next) => {
   res.render('index');
 });
+
+router.get('/movies', async (req, res, next) => {
+  const movies = await Movie.find();
+  res.render('movies',{movies: movies})
+});
+
+router.get('/movie/:id', (req, res, next) => {
+   Movie
+    .findById(req.params.id)
+    .then(film => {
+      res.render('movie',film)
+    })
+});
+
+
+
+
+
+
+
+
+
+
 
 module.exports = router;
