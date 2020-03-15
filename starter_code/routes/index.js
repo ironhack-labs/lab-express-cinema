15ae98efb9c3f3e6e48c043516b748fa0c3edefc
M.A.Garcia <m.angel.gf04.com>
2020-03-15 14:17:29 +0100

    ok

--- starter_code/routes/index.js
+++ starter_code/routes/index.js
@@ -1,9 +1,36 @@
 const express = require('express');
 const router  = express.Router();
-
+const Movie = require('../models/Movie.js')
 /* GET home page */
 router.get('/', (req, res, next) => {
   res.render('index');
 });
+
+
+router.get('/movies', (req, res, next) => {
+  Movie.find()
+    .then(allTheMoviesFromDB => {
+      console.log('Retrieved books from DB:', allTheMoviesFromDB);
+      res.render('movies', {allTheMoviesFromDB})
+
+    })
+    .catch(error => {
+      console.log('Error while getting the movies from the DB: ', error);
+    })
+})
+
+
+router.get('/movie/:id', (req, res, next) => {
+  Movie.find({"_id": req.params.id})
+    .then(movie => {
+      console.log('/movie/:id', req.params.id);
+      res.render('movie', movie[0])
+
+    })
+    .catch(error => {
+      console.log('Error while getting the movies from the DB: ', error);
+    })
+})
+
 
 module.exports = router;
