025ac6095163148705466ea5f0ea2f9a1cc8e46a
Lou08 <l.casals86@gmail.com>
2020-03-15 17:53:09 +0100

    done

--- starter_code/routes/index.js
+++ starter_code/routes/index.js
@@ -1,9 +1,29 @@
 const express = require('express');
 const router  = express.Router();
-
+const Movies = require('../models/Movie')
 /* GET home page */
 router.get('/', (req, res, next) => {
   res.render('index');
 });
 
 module.exports = router;
+
+router.get('/movies', (req, res, next) => {
+  Movies.find()
+    .then(moviesFromDB => {
+      res.render('movies', {movies: moviesFromDB});
+    })
+    .catch(error => {
+      console.log('Error while getting the movies from the DB: ', error);
+    })
+  
+}); 
+router.get('/movies/:id', (req, res)=>{
+  Movies.findOne({_id: req.params.id})
+  .then(movie=>{
+    res.render('moviesdetails', movie)})
+
+    .catch(error => {
+      console.log('Error while getting info: ', error);
+    })
+  });
