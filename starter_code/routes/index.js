f6d167f5ac51baa5679e6a1acd18d3c3b4f0d594
Raul Marchan Catala <chan@Rauls-iMac.local>
2020-03-15 13:37:09 +0100

    [BCN-WDFT-RAUL]-done

--- starter_code/routes/index.js
+++ starter_code/routes/index.js
@@ -1,34 +1,34 @@
 const express = require('express');
-const router  = express.Router();
+const router = express.Router();
 const Movie = require('../models/Movie');
 /* GET home page */
 router.get('/', (req, res, next) => {
-  res.render('index');
+    res.render('index');
 });
 router.get('/movies', (req, res, next) => {
-  Movie.find()
-    .then(allMoviesFromDB => {
-      // console.log(allMoviesFromDB);
-      res.render('movies', { movies: allMoviesFromDB });
-    })
-    .catch(error => {
-      console.log('Error while getting the movies from the DB: ', error);
-    })
+    Movie.find()
+        .then(allMoviesFromDB => {
+            // console.log(allMoviesFromDB);
+            res.render('movies', { movies: allMoviesFromDB });
+        })
+        .catch(error => {
+            console.log('Error while getting the movies from the DB: ', error);
+        })
 });
 router.get('/movies/:movieId', (req, res, next) => {
-  let movieId = req.params.movieId;
-  if (!/^[0-9a-fA-F]{24}$/.test(movieId)) { 
-    return res.status(404).render('not-found');
-  }
-  Movie.findOne({'_id': movieId})
-    .then(movie => {
-      console.log(movie.star);
-      if (!movie) {
-          return res.status(404).render('not-found');
-      }
-      res.render("movies-profile", { movie })
-    })
-    .catch(next)
+    let movieId = req.params.movieId;
+    if (!/^[0-9a-fA-F]{24}$/.test(movieId)) {
+        return res.status(404).render('not-found');
+    }
+    Movie.findOne({ '_id': movieId })
+        .then(movie => {
+            console.log(movie.star);
+            if (!movie) {
+                return res.status(404).render('not-found');
+            }
+            res.render("movies-profile", { movie })
+        })
+        .catch(next)
 });
 
 module.exports = router;
