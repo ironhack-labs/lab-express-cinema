266764ddc4eb7332eb959ebc97319b5e5f222fb9
Chelsey Grice <chelseygrice92@gmail.com>
2020-03-21 10:06:49 +0100

    final commit

--- starter_code/app.js
+++ starter_code/app.js
@@ -11,13 +11,15 @@ const path         = require('path');
 
 
 mongoose
-  .connect('mongodb://localhost/starter-code', {useNewUrlParser: true})
+  .connect('mongodb://localhost/cinema', {useNewUrlParser: true})
   .then(x => {
     console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
   })
   .catch(err => {
     console.error('Error connecting to mongo', err)
   });
+
+
 
 const app_name = require('./package.json').name;
 const debug = require('debug')(`${app_name}:${path.basename(__filename).split('.')[0]}`);
@@ -47,12 +49,12 @@ app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
 
 
 // default value for title local
-app.locals.title = 'Express - Generated with IronGenerator';
+app.locals.title = 'Ironhack Movies';
 
 
 
-const index = require('./routes/index');
-app.use('/', index);
+const movieRouter = require('./routes/movies');
+app.use('/movies', movieRouter);
 
 
 module.exports = app;
