04e8b816e22ee5d8e9eec198dbac930b60f04c2a
Daniel Paiva <danielcspaiva@gmail.com>
2020-03-30 18:24:19 -0300

    done

--- starter_code/routes/index.js
+++ starter_code/routes/index.js
@@ -1,9 +1,36 @@
 const express = require('express');
 const router  = express.Router();
+const Movie = require('../models/Movie')
 
 /* GET home page */
 router.get('/', (req, res, next) => {
   res.render('index');
+});
+
+router.get('/movies', (req, res, next) => {
+  Movie
+    .find()
+    .then(data => {
+      // console.log(data)
+      res.render('movies', {
+        data
+      })
+    })
+    .catch((err) => {
+      console.log(err)
+    });
+});
+
+router.get('/movie/:id', (req, res, next) => {
+  Movie
+    .findById(req.params.id)
+    .then(data => {
+      console.log(data)
+      res.render('movie', data)
+    })
+    .catch((err) => {
+      console.log(err)
+    });
 });
 
 module.exports = router;
