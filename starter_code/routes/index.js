3b067333a34ca30f9d99f3ce1318f464183b465b
Julia Foresti <juliajforesti@gmail.com>
2020-03-30 17:56:09 -0300

    done

--- starter_code/routes/index.js
+++ starter_code/routes/index.js
@@ -1,9 +1,35 @@
 const express = require('express');
 const router  = express.Router();
+const Movie = require('../models/movies');
 
 /* GET home page */
 router.get('/', (req, res, next) => {
   res.render('index');
 });
+
+// get /movies
+router.get('/movies', (req, res, next) => {
+  Movie
+    .find()
+    .then(movies => {
+      console.log(movies)
+      res.render('movies', {
+        movies
+      });
+    })
+    .catch(error => console.log(error));
+});
+
+// get movie details
+router.get('/movie/:movieId', (req, res, next) => {
+  Movie
+    .findById(req.params.movieId)
+    .then(movies => {
+      res.render('movie-details', {
+        movies
+      })
+    })
+    .catch(error => console.log(error));
+})
 
 module.exports = router;
