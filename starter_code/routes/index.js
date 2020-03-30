60de8dbd14485bde76d6ae40e18ecf23e8ef8488
“Marcela <marcelavieirafreire@gmail.com>
2020-03-30 18:07:27 -0300

    Done

--- starter_code/routes/index.js
+++ starter_code/routes/index.js
@@ -1,9 +1,29 @@
 const express = require('express');
 const router  = express.Router();
+const Movie = require('../models/Movie');
 
 /* GET home page */
 router.get('/', (req, res, next) => {
   res.render('index');
 });
+
+router.get('/movies', (req, res, next) => {
+  Movie
+  .find()
+  .then(movies => { console.log(movies)
+    res.render('movies', {movies})} ) 
+  .catch(error => console.log(error))
+});
+
+
+router.get('/movie/:movieId', (req, res ) => {
+  Movie
+  .findById(req.params.movieId)
+  .then(movie => {
+   res.render('movies-info', {movie})
+  })
+  .catch(error => console.log(error));
+})
+
 
 module.exports = router;
