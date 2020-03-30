d07bead1b7b3bad0ecaa8a00339f31259d4080c6
Fabio Menozzi <f.menozzi@yahoo.com>
2020-03-30 17:22:59 -0300

    Done, just need to do CSS

--- starter_code/routes/index.js
+++ starter_code/routes/index.js
@@ -1,9 +1,44 @@
 const express = require('express');
-const router  = express.Router();
+const router = express.Router(); 
+const Movie = require('../models/Movie')
 
 /* GET home page */
 router.get('/', (req, res, next) => {
   res.render('index');
 });
+
+/* GET Movies */
+router.get('/movies', (req, res, next) => {
+  Movie
+    .find()
+    .then(moviesFromMongoDB => {
+      console.log(moviesFromMongoDB)
+      res.render('movies', {
+        movies: moviesFromMongoDB
+      });
+    
+    }) // closing .then
+
+    .catch(error => console.log(error));
+  
+});
+
+/* GET by ID  */
+
+router.get('/movies/:movieId',(req, res, next) => {
+  
+  Movie
+    .findById(req.params.movieId)
+
+    .then(theMovie => {
+
+      res.render('movies-details', { movie: theMovie });
+    
+    }) // closing .then 
+
+    .catch(error => console.log(error));
+
+});
+
 
 module.exports = router;
