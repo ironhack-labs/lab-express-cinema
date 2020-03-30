81b7c81b297edba07a72417b31761b412c1aa8a2
sebagalindo <cmsgalindo@icloud.com>
2020-03-30 17:41:58 -0300

    done without css

--- starter_code/routes/index.js
+++ starter_code/routes/index.js
@@ -1,9 +1,28 @@
 const express = require('express');
 const router  = express.Router();
+const Movie = require('../models/movie');
 
 /* GET home page */
 router.get('/', (req, res, next) => {
   res.render('index');
 });
+
+router.get('/movies', (req, res, next) => {
+  Movie
+    .find()
+    .then(allMovies => {
+      res.render('movies', {movie: allMovies});
+    })
+    .catch(error => console.log(error));
+});
+
+router.get('/movies/:movieId', (req, res, next) => {
+  Movie
+    .findById(req.params.movieId)
+    .then(theMovie => {
+      res.render('infomovie', {movie: theMovie})
+    })
+    .catch(error => console.log(error));
+})
 
 module.exports = router;
