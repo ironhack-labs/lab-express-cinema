31d43d728bc8db8e9929e542f71ead41f478f2c1
Gabriela Aguiar <aguiar.gabi95@gmail.com>
2020-03-30 19:38:20 -0300

    done

--- starter_code/routes/index.js
+++ starter_code/routes/index.js
@@ -1,9 +1,32 @@
 const express = require('express');
 const router  = express.Router();
+const Movie = require('../models/Movies');
 
 /* GET home page */
 router.get('/', (req, res, next) => {
   res.render('index');
+});
+
+router.get('/movies', (req, res, next) => {
+  Movie
+  .find()
+  .then(movies => {
+    res.render('movies', {
+      movies
+    });
+  })
+  .catch(error => console.log(error))
+});
+
+router.get('/movies/:id', (req, res, next) => {
+  Movie
+  .findById(req.params.id)
+  .then(movies => {
+    res.render('movie-info', {
+      movies
+    });
+  })
+  .catch(error => console.log(error))
 });
 
 module.exports = router;
