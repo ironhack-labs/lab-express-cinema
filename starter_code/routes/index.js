29cde881a0b23f4a57e72fb88e75b9dc9a8218aa
Felipe Massao <massao.felipe@gmail.com>
2020-03-30 18:59:35 -0300

    css and page changes in movie details

--- starter_code/routes/index.js
+++ starter_code/routes/index.js
@@ -1,44 +1,33 @@
 const express = require('express');
-const router = express.Router(); 
-const Movie = require('../models/Movie')
+const router = express.Router();
+const Movie = require('../models/Movie');
 
 /* GET home page */
 router.get('/', (req, res, next) => {
-  res.render('index');
+	res.render('index');
 });
 
 /* GET Movies */
 router.get('/movies', (req, res, next) => {
-  Movie
-    .find()
-    .then(moviesFromMongoDB => {
-      console.log(moviesFromMongoDB)
-      res.render('movies', {
-        movies: moviesFromMongoDB
-      });
-    
-    }) // closing .then
-
-    .catch(error => console.log(error));
-  
+	Movie.find()
+		.then((moviesFromMongoDB) => {
+			console.log(moviesFromMongoDB);
+			res.render('movies', {
+				movies: moviesFromMongoDB
+			});
+		}) // closing .then
+		.catch((error) => console.log(error));
 });
 
 /* GET by ID  */
 
-router.get('/movies/:movieId',(req, res, next) => {
-  
-  Movie
-    .findById(req.params.movieId)
-
-    .then(theMovie => {
-
-      res.render('movies-details', { movie: theMovie });
-    
-    }) // closing .then 
-
-    .catch(error => console.log(error));
-
+router.get('/movies/:movieId', (req, res, next) => {
+	Movie.findById(req.params.movieId)
+		.then((theMovie) => {
+			console.log(theMovie);
+			res.render('movies-details', { movie: theMovie });
+		}) // closing .then
+		.catch((error) => console.log(error));
 });
-
 
 module.exports = router;
