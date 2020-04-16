d4995287c4e37d2e789e6b5851c627d25b8e2aba
Yai <myd.rio@hotmail.com>
2020-04-16 20:25:59 +0200

    un poco hecho

--- starter_code/routes/index.js
+++ starter_code/routes/index.js
@@ -1,9 +1,21 @@
 const express = require('express');
-const router  = express.Router();
+const router = express.Router();
+const Movie = require('../views/models/Movies')
 
 /* GET home page */
 router.get('/', (req, res, next) => {
   res.render('index');
+});
+
+router.get('/movies', (req, res, next) => {
+
+  Movie.find()
+    .then(allMovies => res.render('movies', {
+      allMovies
+    }))
+    .catch(err => console.log('ha habido un error cogiendo las movies', err))
+
+
 });
 
 module.exports = router;
