bd51c9a212e1b1da1050fe03e71320ed1387bb3b
William Hermozo <william.hermozo@protonmail.com>
2020-04-16 18:41:29 +0200

    done

---
+++ starter_code/models/Movie.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+
+const movieSchema = new mongoose.Schema({
+  title: String,
+  director: String,
+  stars: [String],
+  image: String,
+  description: String,
+  showtimes: [String],
+});
+
+const Movie = mongoose.model('Movie', movieSchema);
+
+module.exports = Movie;
--- starter_code/routes/index.js
+++ starter_code/routes/index.js
@@ -1,9 +1,34 @@
 const express = require('express');
-const router  = express.Router();
+const router = express.Router();
+const Movie = require('../models/Movie');
 
 /* GET home page */
 router.get('/', (req, res, next) => {
-  res.render('index');
+  res.render('index.hbs');
+});
+
+router.get('/movies', (req, res, next) => {
+  Movie.find({})
+    .then((dbRes) => {
+      res.render('movies.hbs', {
+        movies: dbRes,
+      });
+    })
+    .catch((dbErr) => console.log(dbErr));
+});
+
+router.get('/:id', (req, res) => {
+  Movie.findById(req.params.id)
+    .then((dbRes) => {
+      res.render('oneMovie.hbs', {
+        movie: dbRes,
+      });
+    })
+    .catch((err) => {
+      res.render('error.hbs', {
+        message: err.message,
+      });
+    });
 });
 
 module.exports = router;
