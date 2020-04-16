44cb9c9e6d1ec8b41058a3a105b18a4ab2a5ce60
Mathias Gautier <mths.gautier@gmail.com>
2020-04-16 18:45:52 +0200

    done

--- starter_code/routes/index.js
+++ starter_code/routes/index.js
@@ -1,9 +1,43 @@
 const express = require('express');
-const router  = express.Router();
+const router = express.Router();
+const Movie = require("../models/movie");
 
 /* GET home page */
 router.get('/', (req, res, next) => {
   res.render('index');
 });
+
+
+/* GET movies page */
+
+router.get('/movies', (req, res) => {
+  Movie
+  .find({})
+    .then((dbResult) => {
+      res.render("movies.hbs", {
+        movies: dbResult,
+      });
+    })
+    .catch((err) => console.log(err));
+});
+
+/* GET one movie infos */
+router.get("/:id", (req, res)=> {
+  Movie
+  .findById(req.params.id)
+  .then((dbResult)=> {
+    res.render("this-movie.hbs", {
+      movie: dbResult,
+    });
+  })
+  .catch((err) => {
+    res.render("error.hbs", {
+      message: err.message,
+    });
+  });
+});
+
+
+
 
 module.exports = router;
