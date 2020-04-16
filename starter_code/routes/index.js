4edaf1fa93d23d3e7e65ea29f07dc6190a266996
Maxime Paolone <paolonemax@gmail.com>
2020-04-16 15:47:50 +0200

    TODO: CSS

--- starter_code/routes/index.js
+++ starter_code/routes/index.js
@@ -1,9 +1,31 @@
-const express = require('express');
-const router  = express.Router();
-
+const express = require("express");
+const router = express.Router();
+const Movie = require("../models/Movie");
 /* GET home page */
-router.get('/', (req, res, next) => {
-  res.render('index');
+router.get("/", (req, res, next) => {
+  res.render("index");
+});
+
+router.get("/movies", (req, res) => {
+  Movie.find({}).then((dbResult) => {
+    res.render("movies.hbs", {
+      movies: dbResult,
+    });
+  });
+});
+
+router.get("/movie/:id", (req, res) => {
+  Movie.findById(req.params.id)
+    .then((dbResult) => {
+      res.render("oneMovie.hbs", {
+        movie: dbResult,
+      });
+    })
+    .catch((err) => {
+      res.render("not-found.hbs", {
+        message: err.message,
+      });
+    });
 });
 
 module.exports = router;
