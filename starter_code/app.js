df785b77197b96b9dcc41923ccc6eefc0546459e
Florian Jomain <florian.jomain@gmail.com>
2020-04-16 17:20:33 +0200

    Committing lab

--- starter_code/app.js
+++ starter_code/app.js
@@ -9,7 +9,6 @@ const mongoose     = require('mongoose');
 const logger       = require('morgan');
 const path         = require('path');
 
-
 mongoose
   .connect('mongodb://localhost/starter-code', {useNewUrlParser: true})
   .then(x => {
@@ -38,12 +37,12 @@ app.use(require('node-sass-middleware')({
   sourceMap: true
 }));
       
+hbs.registerPartials(path.join(__dirname, "views/partials"));
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
-
 
 
 // default value for title local
--- starter_code/routes/index.js
+++ starter_code/routes/index.js
@@ -1,9 +1,36 @@
 const express = require('express');
 const router  = express.Router();
+const Movies = require('../models/movie')
 
 /* GET home page */
-router.get('/', (req, res, next) => {
-  res.render('index');
+router.get('/', (req, res) => {
+  res.render(('index.hbs'), {
+    layout: false,
+  });
+});
+
+router.get('/movies', (req, res) => {
+  Movies.find()
+    .then((dbResponse) => {
+      res.render(("movies.hbs"), {
+        movies: dbResponse,
+      })
+    })
+    .catch((dbErr) => {
+      console.log(dbErr);
+    })
+});
+
+router.get('/movies/:id', (req, res) => {
+  Movies.findById(req.params.id)
+    .then((dbResult) => {
+      res.render(("oneMovie.hbs"), {
+        movie: dbResult,
+      });
+    })
+    .catch((dbErr) => {
+      console.log(dbErr);
+    })
 });
 
 module.exports = router;
