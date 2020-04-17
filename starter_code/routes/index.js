0bdb46b1f3dd8e79224c75a4e6ccab8ccf620ba4
Julian Kizeno <julianquiceno20@gmail.com>
2020-04-17 09:05:28 +0200

    unfinshed

--- starter_code/routes/index.js
+++ starter_code/routes/index.js
@@ -1,9 +1,18 @@
-const express = require('express');
-const router  = express.Router();
+const express = require("express");
+const router = express.Router();
+const Movie = require('../models/Movie')
 
 /* GET home page */
-router.get('/', (req, res, next) => {
-  res.render('index');
+router.get("/", (req, res, next) => {
+    res.render("index");
+});
+
+router.get('/movies', (req, res, next) => {
+    Movie.find({})
+    .then(moviesFound => res.render('movies', {moviesFound}))
+    .catch(error =>{
+        console.log('Ha habido error', error);
+    })
 });
 
 module.exports = router;
