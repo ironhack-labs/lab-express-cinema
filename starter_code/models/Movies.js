03971021ce11b8c34aa04e16edb742e878af2ad3
FranciscoEP <francisco.eugp@gmail.com>
2020-04-17 09:27:00 -0500

    unfinished

---
+++ starter_code/models/Movies.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const movieSchema = new Schema({
+  title: {
+    type: String
+  },
+  director: {
+    type: String
+  },
+  stars: {
+    type: []
+  },
+  image: {
+    type: String
+  },
+  description: {
+    type: String
+  },
+  showtimes: {
+    type: []
+  }
+});
+
+const Movie = mongoose.model('Movie', movieSchema);
+module.exports = Movie;
--- starter_code/routes/index.js
+++ starter_code/routes/index.js
@@ -1,9 +1,20 @@
-const express = require('express');
-const router  = express.Router();
+const express = require('express')
+const router = express.Router()
 
 /* GET home page */
 router.get('/', (req, res, next) => {
-  res.render('index');
-});
+  res.render('index')
+})
 
-module.exports = router;
+router.get('/movies', async (req, res, next) => {
+  const movies = await Movie.find({})
+  res.render('movies', { movies })
+})
+
+router.get('/movie/:id', async (req, res, next) => {
+  const [movie] = await Movie.find({ _id: req.params.id })
+  console.log(movie)
+  res.render('movie', { movie, title: 'test' })
+})
+
+module.exports = router
