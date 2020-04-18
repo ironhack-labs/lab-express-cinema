4be5fc3595c720946e07a150d924f2f7e5aeed45
YuvalBu <Yuval.Bu@gmail.com>
2020-04-18 19:28:40 +0200

    Completed

--- starter_code/routes/index.js
+++ starter_code/routes/index.js
@@ -1,9 +1,38 @@
 const express = require('express');
-const router  = express.Router();
+const router = express.Router();
+const movie = require('../models/Movie');
 
 /* GET home page */
 router.get('/', (req, res, next) => {
-  res.render('index');
+	res.render('index.hbs');
+});
+
+router.get('/movies', (req, res, next) => {
+	movie
+		.find()
+		.then((dbResult) => {
+			res.render('movies.hbs', {
+				movies: dbResult
+			});
+		})
+		.catch((err) => {
+			console.log(err);
+		});
+});
+
+router.get('/:id', (req, res, next) => {
+	movie
+		.findById(req.params.id)
+		.then((dbResult) => {
+			res.render('oneMovie.hbs', {
+				movie: dbResult
+			});
+		})
+		.catch((err) => {
+			res.render('not-found.hbs', {
+				message: err.message
+			});
+		});
 });
 
 module.exports = router;
