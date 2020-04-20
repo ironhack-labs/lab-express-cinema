84f3326a972fbdb81631439411d0c1bf125a1e2e
Teddy ROGERS <teddy.rogers.pro@outlook.com>
2020-04-20 23:49:37 +0200

    PAR WDFT 0320 Teddy ROGERS
    iteration 0-1-2 done...

--- starter_code/models/Movie.js
+++ starter_code/models/Movie.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const movieSchema = new Schema({
-    title : String,
+    title: String,
     director: String,
     stars: [String],
     image: String,
