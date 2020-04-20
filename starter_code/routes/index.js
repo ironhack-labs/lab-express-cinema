d1f82061aa7ff9ea589f7ecfeb583d9f5c3ffb53
OBxel <olivier.a.brule@gmail.com>
2020-04-20 23:25:45 +0200

    Cinema Done but only a few css made

--- starter_code/routes/index.js
+++ starter_code/routes/index.js
@@ -1,9 +1,35 @@
-const express = require('express');
-const router  = express.Router();
-
+const express = require("express");
+const router = express.Router();
+const Movie = require("../models/Movie");
 /* GET home page */
-router.get('/', (req, res, next) => {
-  res.render('index');
+router.get("/", (req, res, next) => {
+  res.render("index");
+});
+
+router.get("/movies", (req, res) => {
+  Movie.find({})
+    .then((dbSuccess) => {
+      console.log(dbSuccess);
+      res.render("movies", {
+        myMovies: dbSuccess,
+      });
+    })
+    .catch((err) => {
+      console.error(err);
+    });
+});
+
+router.get("/movies/:id", (req, res) => {
+  Movie.findById(req.params.id)
+    .then((dbSuccess) => {
+      res.render("show", {
+        myMovie: dbSuccess,
+      })
+      console.log(dbSuccess)
+    })
+    .catch((err) => {
+      console.log(err)
+    })
 });
 
 module.exports = router;
