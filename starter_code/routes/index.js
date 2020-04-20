cce5dfb5530dc7771ff6544821faff06590ce7a2
Avtandil <a.shaburishvili@gmail.com>
2020-04-20 20:11:02 +0400

    button styling left

--- starter_code/routes/index.js
+++ starter_code/routes/index.js
@@ -1,9 +1,24 @@
-const express = require('express');
-const router  = express.Router();
+const express = require("express");
+const router = express.Router();
+
+const Movie = require("../models/Movie");
 
 /* GET home page */
-router.get('/', (req, res, next) => {
-  res.render('index');
+router.get("/", (req, res, next) => {
+  res.render("index");
+});
+
+router.get("/movies", (req, res) => {
+  Movie.find().then((movies) => {
+    res.render("movies", { allMovies: movies });
+  });
+});
+
+router.get("/movies/:id", (req, res) => {
+  const id = req.params.id;
+  Movie.findById(id).then((movie) => {
+    res.render("moviedetails", { movie: movie });
+  });
 });
 
 module.exports = router;
