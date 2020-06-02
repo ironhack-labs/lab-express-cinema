a8df21dd1bebd4456a072c24738aeb17ba85704a
Antoine Rolland <antoine-rolland@hotmail.fr>
2020-06-02 21:08:06 +0200

    Iterations 1 - 4

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,27 @@
 const express = require('express');
 const router = express.Router();
 
+const Movie = require("../models/movie")
+
 /* GET home page */
 router.get('/', (req, res, next) => res.render('index'));
+
+router.get('/movies', async (req, res, next) => {
+    try {
+        const movies = await Movie.find();
+        res.render('movies', {movies})
+    } catch (error) {
+        console.log(error)
+    }
+})
+
+router.get('/movies/:id', async (req, res, next) => {
+    try {
+        const movie = await Movie.findById(req.params.id);
+        res.render('movie-details', {movie})
+    } catch (error) {
+        console.log(error)
+    }
+})
 
 module.exports = router;
