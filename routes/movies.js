4db97207b9294b887d09051534104b4c4dfbe860
Marcela Vilas Boas <marcela.vilasboas@gmail.com>
2020-06-02 20:44:43 +0200

    done

--- routes/movies.js
+++ routes/movies.js
@@ -10,6 +10,15 @@ router.get('/', (req, res, next) => {
         res.render('movies', {movies: movies});
     })
     .catch(err => console.log('Error while looking for movies: ', err));
-})
+});
+
+router.get('/:id', (req, res, next) => {
+    console.log(req.params.id);
+    Movie.findById(req.params.id)
+    .then(movie => {
+        res.render('details', {movie: movie});
+    })
+    .catch(err => console.log(`Error while searching for movie detalis: `, err))
+});
 
 module.exports= router;
