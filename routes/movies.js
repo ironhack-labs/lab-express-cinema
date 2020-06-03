e250f36440e3bdb43ecc349bee89a44c9bda7159
Claudi Sanchez <claudi.sanchezserrano@gmail.com>
2020-06-03 16:31:14 +0200

    env file

--- routes/movies.js
+++ routes/movies.js
@@ -5,11 +5,10 @@ const Movie = require('../models/Movie.model.js');
 router.get("/", (req, res, next) => {
     Movie.find()
     .then(movies => res.render('movies/movies', { movies }))
-    .catch(error => console.log('Error while getting the celebrities from the DB: ', error))
+    .catch(error => console.log('Error while getting the celebrities from the DB: ', error));
   });
 
 router.get("/:id",(req, res, next) => {
-    console.log(req.params.id)
     Movie.findById(req.params.id)
         .then(theMovie => {
             res.render('movies/show', {
