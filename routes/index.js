ef5c25b9f1e638ccdf543b3b5efc6fcdd5cb7d0b
Laura Ramirez <lau.ramirez.encinas@gmail.com>
2020-06-04 15:01:41 +0200

    done

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,36 @@
 const express = require('express');
 const router = express.Router();
+const Movie = require("../models/Movie.model")
 
 /* GET home page */
 router.get('/', (req, res, next) => res.render('index'));
+
+// Get movies page
+router.get('/movies', (req, res, next) => {
+    Movie.find()
+        .then(allMovies => {
+            console.log(allMovies)
+            res.render('movies/movies', { movies: allMovies });
+
+        })
+        .catch(error => {
+            console.log('No encuentra pelicula', error);
+        })
+
+});
+
+router.get('/movies/show', (req, res, next) => {
+    Movie.findById(req.query._id)
+        .then(movie => {
+            res.render('movies/details', { movie: movie });
+
+        })
+        .catch(error => {
+            console.log('No encuentra datos de la pelicula', error);
+        })
+
+});
+
+
 
 module.exports = router;
