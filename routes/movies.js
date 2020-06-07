5af60eafe4a8d7b52bd7612111aeafa5398e4d95
Robert Ledezma <Robert@MacBook-Pro-de-Robert-2.local>
2020-06-07 19:19:42 +0200

    Done

--- routes/movies.js
+++ routes/movies.js
@@ -6,13 +6,25 @@ const Movie = require('../models/Movie.model.js')
 //Render all movies
 router.get('/movies', (req, res, next) => {
     Movie.find()
-        .then(allMovies => {
+        .then(movie => {
             res.render('movies', {
-                movies: allMovies
+                allMovies: movie
             })
         })
         .catch(error => {
             console.log('error al renderizas la página', error);
+        })
+});
+
+router.get('/movies/:id', (req, res, next) => {
+    Movie.findById(req.params.id)
+        .then(movieDetails => {
+            res.render('movieDetails', {
+                movie: movieDetails
+            })
+        })
+        .catch(error => {
+            console.log('Error al acceder a los detalles de la movie', error);
         })
 });
 
