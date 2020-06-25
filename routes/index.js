be247c665bd0bd3013f5ec0112f1317114e1c1b2
SoonWoo Kwon <soonk@outlook.com>
2020-06-25 23:03:35 +0200

    done, falta diseño

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,29 @@
 const express = require('express');
 const router = express.Router();
 
+const Movie = require('../models/Movie.model')
+
 /* GET home page */
 router.get('/', (req, res, next) => res.render('index'));
+
+router.get('/movies', (req, res) => {
+    Movie.find()
+        .then(allMovies => {
+            console.log(allMovies)
+            res.render('movies', {
+                allMovies
+            })
+        })
+        .catch(err => console.log("Error en la BBDD", err))
+})
+
+router.get('/details/:id', (req, res) => {
+
+    Movie.findById(req.params.id)
+        .then(theMovie => res.render('details', {theMovie}))
+        .catch(err => console.log("Error en la BBDD", err))
+})
+
+
 
 module.exports = router;
