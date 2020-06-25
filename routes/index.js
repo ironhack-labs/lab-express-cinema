e89ddf057b92b44c41f833f6d3e0f330a0d0f84f
Diego Ortiz <diegortiz988@gmail.com>
2020-06-25 20:58:59 +0200

    iteraciones 1,2,3 y casi 4

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,26 @@
 const express = require('express');
+
 const router = express.Router();
 
 /* GET home page */
-router.get('/', (req, res, next) => res.render('index'));
+
+
+Movie = require('../models/Movie.model.js')
+
+router.get('/', (req,res,next) => res.render('index'));
+
+router.get('/movies', (req,res,next) => {
+
+    Movie.find()
+        .then(allMovies => res.render('movies', {allMovies}))
+        .catch(err => console.log("Error en la BBDD", err))
+})
+
+router.get('/movies-details/:id', (req,res,next) => {
+
+    Movie.findById(req.params.id)
+        .then(details => res.render('movies-details', details))
+        .catch(err => console.log("Error en la BBDD", err))
+})
 
 module.exports = router;
