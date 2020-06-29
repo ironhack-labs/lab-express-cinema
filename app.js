c3b129973c293409c09992b969a642f024f6195e
Ivan Mickovski <ivan_mickovski@hotmail.com>
2020-06-29 16:20:15 +0200

    Not quite working yet

--- app.js
+++ app.js
@@ -38,4 +38,8 @@ app.locals.title = 'Express - Generated with IronGenerator';
 const index = require('./routes/index');
 app.use('/', index);
 
+// const movies = require('./')
+const movies = require('./routes/index');
+app.use('/movies', movies);
+
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,18 @@
 const express = require('express');
 const router = express.Router();
+const Movie = require('../models/Movie.model')
 
 /* GET home page */
 router.get('/', (req, res, next) => res.render('index'));
+
+router.get('/movies', (req, res, next) => {
+    Movie.find()
+    .then(movies => {
+        // console.log(moviesFromDataBase);
+        res.render('movies', { movies })
+    }).catch(err => {
+        console.log(err);
+    })
+});
 
 module.exports = router;
