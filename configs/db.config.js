1afe423e7bc8ce46eba4f90474c96bd4cb59cb6c
Old <onelifedesigning@gmail.com>
2020-07-01 20:42:26 +0200

    first iteration

--- configs/db.config.js
+++ configs/db.config.js
@@ -1,12 +1,23 @@
 const mongoose = require('mongoose');
+const process = require('process');
 
+// Connection to the database "recipe-app"
 mongoose
-  .connect('mongodb://localhost/express-cinema-dev', {
+  .connect('mongodb://localhost:27017/express-cinema-dev', {
     useCreateIndex: true,
     useNewUrlParser: true,
     useUnifiedTopology: true
   })
-  .then(x =>
-    console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
-  )
+  .then(self => {
+    // Before adding any documents to the database, let's delete all previous entries
+    console.log(`Connected to Mongo! Database name: "${self.connections[0].name}"`)
+    // return self.connection.dropDatabase();
+  })
   .catch(err => console.error('Error connecting to mongo', err));
+
+  process.on('SIGINT', () => {
+    mongoose.connection.close(() => {
+      console.log('Mongoose default connection disconnected through app termination');
+      process.exit(0);
+    });
+  });
