1434efe2a96d417a96a36d0fd1abc4e06010a0f9
Pumpel <romanbaos@gmail.com>
2020-07-01 19:53:10 +0200

    Iteration 1 complete

---
+++ models/Movie.model.js
@@ -0,0 +1,21 @@
+const { Schema } = require("mongoose");
+
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const movieSchema = new Schema({
+    "title": {
+        type: String,
+        required: true
+    },
+    "director": String,
+    "stars": [String],
+    "image": String,
+    "description": String,
+    "showtimes": [String]
+});
+
+const Movie = mongoose.model("Movie",movieSchema);
+
+module.exports = Movie;
+
