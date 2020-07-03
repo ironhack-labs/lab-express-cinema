f6dde5f466dfd7ceeb4ca90fa5fbef6bd7709d21
miguelbachi5a <miguelbachi5a@gmail.com>
2020-07-03 16:59:24 +0200

    Exercise solved without styling

--- routes/index.js
+++ routes/index.js
@@ -4,11 +4,19 @@ const Movie = require('../models/Movie.model.js')
 
 /* GET home page */
 router.get('/', (req, res, next) => res.render('index'));
+
 router.get('/movies', (req, res) => {
 
     Movie.find({})
         .then(movies => res.render('movies', { movies: movies }))
         .catch(err => console.log('Error retrieving movies', err))
+})
+
+router.get('/movies/:id', (req, res) => {
+    console.log(req.params.id);
+    Movie.findById(req.params.id)
+        .then(movie => res.render('info', { movie: movie }))
+        .catch(err => console.log('Error retrieving the movie', err))
 })
 
 module.exports = router;
