3ae73d0ac7ee239ed2a5794b370e4252e7ef606d
Alice Prigent <aliprigent@gmail.com>
2020-07-06 18:06:45 +0200

    [PAR-FT-062020] Alice Prigent

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,23 @@
-const express = require('express');
+const express = require("express");
+const MovieModel = require("../models/Movie.model");
 const router = express.Router();
 
 /* GET home page */
-router.get('/', (req, res, next) => res.render('index'));
+router.get("/", (req, res, next) => res.render("index"));
+
+router.get("/movies", (req, res) => {
+  MovieModel.find()
+    .then((dbRes) => res.render("movies.hbs", { movies: dbRes }))
+    .catch((err) => console.error(err));
+});
+
+router.get("/movies/:id", (req, res) => {
+  MovieModel.findById(req.params.id)
+    .then((movie) => {
+        console.log(movie)
+        res.render("movie.hbs", { movie })
+    })
+    .catch((dbErr) => console.error(dbErr));
+});
 
 module.exports = router;
