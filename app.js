70ede2e385125b8001f5cb93c1929ae6bac0b574
MarieAmp <marymary09@hotmail.com>
2020-07-06 19:46:30 +0200

    ok avant css

--- app.js
+++ app.js
@@ -31,6 +31,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
+hbs.registerPartials(path.join(__dirname, "views/partials")); // where are the tiny chunks of views ?
 
 // default value for title local
 app.locals.title = 'Express - Generated with IronGenerator';
--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,26 @@
 const express = require('express');
+const movieModel = require('../bin/models/Movie.model');
 const router = express.Router();
 
 /* GET home page */
 router.get('/', (req, res, next) => res.render('index'));
+
+router.get("/movies", async(req,res)=>{
+    const allMovies=await movieModel.find();
+    res.render("movies.hbs", {allMovies})
+  });
+
+  router.get("/movie/:id", async(req,res)=>{
+   try{
+       const movie= await movieModel.findById(req.params.id);    
+    res.render("oneMovie.hbs",{movie})
+    }
+
+catch(err){
+    console.log(err)
+}
+  });
+
+
 
 module.exports = router;
