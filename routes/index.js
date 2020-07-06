9547eda5e750763b8a24ad6511b202622e849a0e
Axel Chauvin <axelchauvin@gmail.com>
2020-07-06 17:50:05 +0200

    Completed lab

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,24 @@
 const express = require('express');
 const router = express.Router();
+const MovieModel = require("./../models/Movie");
 
 /* GET home page */
 router.get('/', (req, res, next) => res.render('index'));
+
+router.get("/movies", async (req, res) => {
+    try {
+        res.render("movies", { movies: await MovieModel.find() });
+    } catch(err) {
+        console.log(err);
+    }
+})
+
+router.get("/movie/:id", async (req, res) => {
+    try{
+        res.render("detailed-movie", await MovieModel.findById(req.params.id));
+    } catch(err) {
+        console.log(err);
+    }
+})
 
 module.exports = router;
