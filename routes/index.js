beea6053968c8f46509d699998b1ac21615da560
Aude Richon <auderichon@gmail.com>
2020-07-06 18:10:44 +0200

    iterations 2&3

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,18 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
+const movieModel = require("./../models/Movie.model.js");
+
 /* GET home page */
-router.get('/', (req, res, next) => res.render('index'));
+router.get("/", (req, res, next) => res.render("index"));
+
+router.get("/movies", (req, res, next) => {
+  movieModel
+    .find()
+    .then((movies) => {
+      res.render("movies", { movies });
+    })
+    .catch(next);
+});
 
 module.exports = router;
