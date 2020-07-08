1c556df03eb6687b5a2a79156828935251b56be4
mateomp <mateomadariaga96@gmail.com>
2020-07-08 18:03:02 +0200

    All iterations completed

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,22 @@
 const express = require('express');
 const router = express.Router();
+const Movie = require('../models/Movie.model.js');
 
 /* GET home page */
 router.get('/', (req, res, next) => res.render('index'));
+
+router.get('/movies', (req, res) => {
+
+    Movie.find({})
+        .then(movies => res.render('movies', { movies: movies }))
+        .catch(err => console.log('Error when showing movies', err))
+})
+
+router.get('/movies/:id', (req, res) => {
+    console.log(req.params.id);
+    Movie.findById(req.params.id)
+        .then(movie => res.render('movie-info', { movie: movie }))
+        .catch(err => console.log('Error when showing movie', err))
+})
 
 module.exports = router;
