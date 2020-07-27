467f18542b08a6f141f2ea86d75002baa84328e6
BielQuerol <bielbes@hotmail.com>
2020-07-28 00:04:18 +0200

    hasta la 4

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,30 @@
 const express = require('express');
 const router = express.Router();
-
+const Movies = require('../models/Movie');
 /* GET home page */
 router.get('/', (req, res, next) => res.render('index'));
+router.get('/movies', (req, res, next) => {
+    Movies.find()
+    .then(allTheMoviesFromDB => {
+        console.log('retrieved movies: ', allTheMoviesFromDB);
+    res.render('movies', {
+        movies: allTheMoviesFromDB
+    })
+})
+.catch(error => {
+    console.log('Error getting the movies', error)
+})
+});
+
+
+  router.get('/movies/:movieId', (req, res, next) => {
+    Movie.findById(req.params.movieId)
+      .then(theMovie => {
+        res.render('/movie-details', { movie: theMovie });
+      })
+      .catch(error => {
+        console.log('Error while retrieving movie details: ', error);
+      })
+  });
 
 module.exports = router;
