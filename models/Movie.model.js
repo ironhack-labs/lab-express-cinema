b70a34f8e87e953b1fd9ff02f48745f2033c4280
Jean-Baptiste Castillo <castillo.jbaptiste@gmail.com>
2020-07-27 18:05:44 +0200

    Iteracion 2 & 3

--- models/Movie.model.js
+++ models/Movie.model.js
@@ -3,11 +3,30 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const cinemaSchema = new Schema({
-    title: String,
-    director: String,
-    stars: [],
-    image: String,
-    description: String
+    title: {
+        type: String,
+        required: true
+    },
+    director: {
+        type: String,
+        required: true
+    },
+    stars: {
+        type: [String],
+        required: true
+    },
+    image: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    showtimes: {
+        type: [String],
+        required: true
+    }
 });
 
 const Cinema = mongoose.model('Cinema', cinemaSchema);
--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,21 @@
 const express = require('express');
 const router = express.Router();
+const Movies = require('../models/Movie.model');
 
 /* GET home page */
 router.get('/', (req, res, next) => res.render('index'));
+
+router.get('/movies', (req, res, next) => {
+    Movies.find()
+        .then(allTheMoviesFromDB => {
+            console.log('Retrieved movies: ', allTheMoviesFromDB);
+            res.render('movies', {
+                movies: allTheMoviesFromDB
+            });
+        })
+        .catch(error => {
+            console.log('Error getting the movies from the DB ', error);
+        });
+})
 
 module.exports = router;
