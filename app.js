9c63b4f3c8b4177bc06ec58d6f8c85c05bf53819
Luis De Gouveia <degouveiasousa@gmail.com>
2020-07-27 18:58:39 +0200

    Iteration 1, 2 and 3 done, the 4 almost ready, I have problems, It doesn't show me the information of each movie. It doesn't have any CSS.

--- app.js
+++ app.js
@@ -9,6 +9,15 @@ const mongoose = require('mongoose');
 const logger = require('morgan');
 const path = require('path');
 
+mongoose
+.connect('mongodb://localhost/expresscinema', { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(data => {
+    console.log(`Connected to Mongo! Database name: "${data.connections[0].name}"`)
+  })
+  .catch(err => {
+    console.error('Error connecting to mongo', err)
+  })
+
 const app_name = require('./package.json').name;
 const debug = require('debug')(
   `${app_name}:${path.basename(__filename).split('.')[0]}`
@@ -32,10 +41,16 @@ app.set('view engine', 'hbs');
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
 
+hbs.registerPartials(path.join(__dirname, 'views', 'partials'))
+
 // default value for title local
 app.locals.title = 'Express - Generated with IronGenerator';
 
 const index = require('./routes/index');
 app.use('/', index);
+
+const theMovies = require('./routes/movies');
+app.use('/movies', theMovies);
+
 
 module.exports = app;
---
+++ routes/movies.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const router = express.Router();
+const hbs = require('hbs');
+const path = require('path');
+
+const Movie = require('../models/Movie.models');
+
+router.get('/', (req, res, next) => {
+    Movie.find()
+    .then(allTheMoviesFromDB => {
+        console.log('Retrieved movies from DB:', allTheMoviesFromDB);
+        res.render('movies', { movies: allTheMoviesFromDB } );
+    })
+    .catch(error => {
+        console.log('Error while getting the celebrities from the DB: ', error);
+    })
+})
+
+router.get('/details/:movieId', (req, res, next) => {
+    Movie.findById(req.params.id)
+    .then( (movieDetails) => {
+    console.log(movieDetails)
+    res.render('movie-details', { movieDetails })
+    })
+    .catch( (err) => {
+    next(err)
+    })
+});
+
+module.exports = router 
