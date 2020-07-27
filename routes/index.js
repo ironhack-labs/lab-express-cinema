b40af322539286597b9a33af7f0dd8e15d377a7b
Jean-Baptiste Castillo <castillo.jbaptiste@gmail.com>
2020-07-27 18:52:33 +0200

    Iteracion 3

--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,7 @@ const Movies = require('../models/Movie.model');
 /* GET home page */
 router.get('/', (req, res, next) => res.render('index'));
 
+/*GET movies page */
 router.get('/movies', (req, res, next) => {
     Movies.find()
         .then(allTheMoviesFromDB => {
@@ -15,6 +16,21 @@ router.get('/movies', (req, res, next) => {
         })
         .catch(error => {
             console.log('Error getting the movies from the DB ', error);
+        });
+})
+
+/* GET movie details page */
+router.get('/movies/:id', (req, res, next) => {
+    const movieId = req.params.id;
+    Movies.findById(movieId)
+        .then(allTheMoviesFromDB => {
+            console.log('ID: ', allTheMoviesFromDB);
+            res.render('movie-info', {
+                movieinfo: allTheMoviesFromDB
+            });
+        })
+        .catch(error => {
+            console.log('Error getting the movies info from the DB ', error);
         });
 })
 
