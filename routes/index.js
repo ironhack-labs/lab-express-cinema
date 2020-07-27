ccb12233cbaf37b0ebd453b3340456d6643faa05
Angelilla <angelamorealba@gmail.com>
2020-07-27 21:47:04 +0200

    done

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,28 @@
 const express = require('express');
 const router = express.Router();
+const Movie = require('../models/Movie');
 
 /* GET home page */
 router.get('/', (req, res, next) => res.render('index'));
+
+router.get('/movies', (req, res, next) => {
+    Movie
+        .find()
+        .then(moviesFromDB => {
+            //console.log('Movies fron DB: ', moviesFromDB);
+            res.render('movies', {movies: moviesFromDB})
+        })
+        .catch(error => {
+            console.log(error);
+        })
+});
+
+router.get('/movie/:id', (req, res, next) => {
+    Movie
+        .findById(req.params.id)
+        .then(movie => {
+            res.render('movie', {movie: movie})
+        })
+})
 
 module.exports = router;
