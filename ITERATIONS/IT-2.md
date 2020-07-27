641f030cb3a2cebb2a2d6ad24db7ef02b73b78c8
Noiret <sergio.ko@icloud.com>
2020-07-27 18:56:28 +0200

    Iteration 3 done

--- ITERATIONS/IT-2.md
+++ ITERATIONS/IT-2.md
@@ -1,1 +1,2 @@
 1. Relleno el HTML para la home
+2. Añado link /movies al botón
--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,35 @@
 const express = require('express');
 const router = express.Router();
+//IT-3
+const Movie = require('../models/Movie.model.js')
 
 /* GET home page */
 router.get('/', (req, res, next) => res.render('index'));
+
+//IT-3
+router.get('/movies', (req, res, next) => {
+    Movie.find()
+        .then(allMoviesFromDB => {
+            res.render('movies.hbs', {
+                movies: allMoviesFromDB
+            })
+        })
+        .catch((err) => {
+            console.log('Error while displaying the movies', err)
+            next(err)
+        })
+})
+
+router.get('/movies/:id', (req, res, next) => {
+    Movie.findById(req.params.id)
+        .then(thisMovieDB => {
+            res.render('show.hbs', {
+                thisMovie: thisMovieDB
+            })
+        })
+        .catch((err) => {
+            console.log('Error while displaying the specific movie', err)
+        })
+})
 
 module.exports = router;
