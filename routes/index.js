09b1d5b36a6527d99b2c40657ffd6eafcc90d3c6
Jordi TM <jorditorrasm@gmail.com>
2020-07-27 23:07:49 +0200

    falta css

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,37 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
+const hbs = require("hbs");
+const path = require("path");
 
+const Movies = require("../models/Movie.models");
 /* GET home page */
-router.get('/', (req, res, next) => res.render('index'));
+router.get("/", (req, res, next) => res.render("index"));
+
+router.get("/movies", (req, res, next) => {
+  Movies.find()
+    .then((allTheMoviesFromDB) => {
+      console.log("Retrieved movies: ", allTheMoviesFromDB);
+      res.render("movies", {
+        movies: allTheMoviesFromDB,
+      });
+    })
+    .catch((error) => {
+      console.log("Error getting the movies from the DB ", error);
+    });
+});
+
+router.get("/movies/:id", (req, res, next) => {
+  const movieId = req.params.id;
+  Movies.findById(movieId)
+    .then((allTheMoviesFromDB) => {
+      console.log("ID: ", allTheMoviesFromDB);
+      res.render("movie-page", {
+        movieinfo: allTheMoviesFromDB,
+      });
+    })
+    .catch((error) => {
+      console.log("Error getting the movies info from the DB ", error);
+    });
+});
 
 module.exports = router;
