cf7af69eabc93617071378418f6081405ae3e959
alejandrocolorado <alejandrocolo13@gmail.com>
2020-07-28 02:03:14 -0500

    Completed all iterations - no style

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,33 @@
 const express = require('express');
+
 const router = express.Router();
+
+const Movie = require('../models/Movie.model');
 
 /* GET home page */
 router.get('/', (req, res, next) => res.render('index'));
+
+router.get('/movies', (req, res, next) => {
+    Movie.find()
+        .then(allMovies => {
+            console.log('All movies:', allMovies._id)
+            res.render('movies', {moviesArr: allMovies})
+        })
+        .catch((err) => {
+            console.log(err);
+        })
+
+});
+
+router.get('/movies/:id', (req, res, next) =>{
+    Movie.findById(req.params.id)
+        .then (oneMovie => {
+            console.log(oneMovie)
+            res.render('movie', {movie: oneMovie})
+        })
+        .catch ((err) => {
+            console.log(err)
+        })
+})
 
 module.exports = router;
