10cee3ebf7972bf6941a5168f7317558687633a9
Rodrigohernandezsan <rodrigoherandezsan@gamil.com>
2020-08-05 01:50:29 +0200

    iteractions 1-2

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,26 @@
 const express = require('express');
 const router = express.Router();
+const Movies = require("../models/Movie.model.js");
 
 /* GET home page */
-router.get('/', (req, res, next) => res.render('index'));
+router.get('/', (req, res) => res.render('index'));
+
+router.get('/movies', (req, res) => {
+    Movies.find((err, data) => {
+        res.render('movies', {
+            movies: data
+        })
+    });
+});
+
+router.get('/movies/:id', (req, res) => {
+    const idMovie = req.params.id;
+    Movies.findById(idMovie, (err, data) => {
+        res.render('movie', { 
+            movie: data
+        });
+    });
+});
+
 
 module.exports = router;
