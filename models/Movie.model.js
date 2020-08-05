972c4c8cb7728355149ae78b0a950ef9df87b837
Rodrigo Hernández <58347463+Rodrigohernandezsan@users.noreply.github.com>
2020-08-05 23:04:35 +0200

    Update Movie.model.js

--- models/Movie.model.js
+++ models/Movie.model.js
@@ -1,18 +1,3 @@
-// const mongoose = require('mongoose');
-
-// mongoose
-//   .connect('mongodb://localhost/express-cinema-dev', {
-//     useCreateIndex: true,
-//     useNewUrlParser: true,
-//     useUnifiedTopology: true
-//   })
-//   .then(x =>
-//     console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
-//   )
-//   .catch(err => console.error('Error connecting to mongo', err));
-
-
-
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
