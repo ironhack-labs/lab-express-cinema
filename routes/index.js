2053a9d25d5c1c66bb44d106d6be6c348fa14935
Pedro Magalhães Lamy <pedrolamy@Air-de-Pedro.lan>
2020-08-06 14:07:38 +0100

    finished

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,25 @@
 const express = require('express');
 const router = express.Router();
-
-/* GET home page */
+const Movies = require("../models/Movie.model.js");
 router.get('/', (req, res, next) => res.render('index'));
+router.get('/', (req, res) => res.render('index'));
+
+module.exports = router;
+router.get('/movies', (req, res) => {
+    Movies.find((err, data) => {
+        res.render('movies', {
+            movies: data
+        })
+    });
+});
+
+router.get('/movies/:id', (req, res) => {
+    const idMovie = req.params.id;
+    Movies.findById(idMovie, (err, data) => {
+        res.render('movie', { 
+            movie: data
+        });
+    });
+});
 
 module.exports = router;
