2e8bf1b80aeec92c976d333b352e6a4b5ee0b280
Lynn W-B <l.welshboucard@yahoo.co.uk>
2020-08-06 14:22:04 +0200

    Done

--- app.js
+++ app.js
@@ -11,8 +11,7 @@ const path = require('path');
 
 const app_name = require('./package.json').name;
 const debug = require('debug')(
-  `${app_name}:${path.basename(__filename).split('.')[0]}`
-);
+  `${app_name}:${path.basename(__filename).split('.')[0]}`);
 
 const app = express();
 
--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,31 @@
 const express = require('express');
 const router = express.Router();
+const Movies = require('../models/Movie.model');
 
 /* GET home page */
 router.get('/', (req, res, next) => res.render('index'));
+
+router.get('/movies', (req,res, next) => {
+    Movies.find() 
+    .then((data) => {
+        res.render('movies', 
+        { movies:data});
+    })
+    .catch ((error) => {
+    console.log(`Error: ${error}`)
+    });
+});
+
+router.get('/movies:id', (req, res, next) => {
+    const {movieId} = req.params.id;
+    Movies.findOne(movieId)
+    .then((data) =>
+        res.render('movie', 
+        { movie: data})
+    )
+    .catch ((error) => {
+        console.log(`Error:${error}`)
+    });   
+});
 
 module.exports = router;
