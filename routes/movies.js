06859f5433d80d6ae1578301a196b407bfb65a9d
Andrew <andre.mastercode@gmail.com>
2020-08-06 15:10:03 +0000

    done

---
+++ routes/movies.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const router = express.Router();
+const Movies = require("../models/Movie-model");
+
+/* GET movies page */
+
+router.get("/movies", (req, res, next) => {
+    Movies.find()
+      .then((allMoviesFromDB) => {
+        res.render("movies", {
+          movies: allMoviesFromDB,
+        });
+      })
+      .catch((error) =>
+        console.log(`Error while getting the movies from DB, ${error}`)
+      );
+  });
+
+router.get("/movies/:id", (req, res, next) => {
+    Movies.findById(req.params.id)
+    .then((movie) => {
+      res.render("movie-details", {
+        movie: movie,
+      });
+    })
+    .catch((error) =>
+      console.log(`Error while getting the movies from DB, ${error}`)
+    );
+})
+
+
+module.exports = router;
