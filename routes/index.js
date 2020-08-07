a3ed62f5128fca43c67694137a9c0c487f779f8e
Arianna Valle <ariannamarbella@gmail.com>
2020-08-06 21:25:22 -0400

    Complete lab

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,27 @@
 const express = require('express');
 const router = express.Router();
+const Movie = require("../models/Movie.model");
 
 /* GET home page */
 router.get('/', (req, res, next) => res.render('index'));
 
+router.get('/browse', (req,res,next) => {
+    Movie.find()
+    .then(moviesFromDb => {
+        console.log("Movie list: ", moviesFromDb)
+        res.render('movie-views/browseMovies', {movies: moviesFromDb}) })
+    .catch((err) => {
+        console.log("Something went wrong retrieving all movies: ", err)
+    })
+})
+
+router.get('/details/:id', (req,res,next) => {
+    Movie.findById(req.params.id)
+    .then(movieFromDb => {
+        console.log("Movie details: ", movieFromDb)
+        res.render('movie-views/movieDetails', {movie: movieFromDb}) })
+    .catch((err) => {
+        console.log("Something went wrong retrieving all movies: ", err)
+    })
+})
 module.exports = router;
