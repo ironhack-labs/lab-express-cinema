598b7343092d8507a984743c87cf56c11b711cf2
Alex Anthopoulos <zephoxcodese@gmail.com>
2020-08-24 12:54:06 +0200

    iteration 4 done - before styling

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,7 @@ const Movies = require('../models/Movie.model');
 /* GET home page */
 router.get('/', (req, res, next) => res.render('index'));
 //Adding here the router for /movies
-router.get('/movies', (req, res, next) => {
+router.get('/movies', (req, res) => {
     Movies.find()
         .then(allTheMoviesFromDB => {
             console.log('Retrieved movies: ', allTheMoviesFromDB);
@@ -15,6 +15,21 @@ router.get('/movies', (req, res, next) => {
         })
         .catch(error => {
             console.log('Error getting the movies from the DB ', error);
+        });
+})
+
+/* GET movie details page */
+router.get('/movies/:id', (req, res) => {
+    const movieId = req.params.id;
+    Movies.findById(movieId)
+        .then(allTheMoviesFromDB => {
+            console.log('ID: ', allTheMoviesFromDB);
+            res.render('movie-info', {
+                movieinfo: allTheMoviesFromDB
+            });
+        })
+        .catch(error => {
+            console.log('Error getting the movies info from the DB ', error);
         });
 })
 module.exports = router;
