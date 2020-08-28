f8c3e3d1393f45ed5e2ae0136a1fb0fb74b29427
Hugo Suastegui <hugo.suastegui.pineda@gamil.com>
2020-08-27 20:22:11 -0500

    99% Completado

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 require('dotenv').config();
-
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const express = require('express');
@@ -8,6 +7,8 @@ const hbs = require('hbs');
 const mongoose = require('mongoose');
 const logger = require('morgan');
 const path = require('path');
+const Movie = require('./models/Movie.model')
+
 
 const app_name = require('./package.json').name;
 const debug = require('debug')(
@@ -29,6 +30,7 @@ app.use(cookieParser());
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
+hbs.registerPartials(`${__dirname}/views/partials`)
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
 
@@ -39,3 +41,15 @@ const index = require('./routes/index');
 app.use('/', index);
 
 module.exports = app;
+
+// CONTROLLERS 
+app.get('/movies/:movieId', async (req, res, next) => {
+  const {movieId} = req.params
+  const movie = await Movie.findById(movieId)
+  res.render("movieDetail", movie)
+})
+
+app.get('/movies', async (req, res) => {
+  const movies = await Movie.find()
+  res.render('movies.hbs', { movies })
+})
