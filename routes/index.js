54a238e49d99c7cd761d73fc501a8447d1dd1f85
edvicaty <edvicaty@hotmail.com>
2020-08-27 19:07:52 -0500

    done

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,26 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
+const Movie = require("../models/Movie");
 
 /* GET home page */
-router.get('/', (req, res, next) => res.render('index'));
+router.get("/", (req, res, next) => res.render("index"));
+
+router.get("/movies", async (req, res) => {
+  const movies = await Movie.find();
+  res.render("movies", { movies });
+});
+
+// router.get("/movie/:movieID", async (req, res) => {
+//   const { movieID } = req.params;
+//   const movie = await Movie.find({ _id: `${movieID}` });
+//   console.log(movieTitle);
+//   res.render("movieDetails", movie);
+// });
+
+router.get("/movie/:movieID", async (req, res) => {
+  const movieId = req.params.movieID;
+  const data = await Movie.findOne({ _id: movieId });
+  res.render("movieDetails", data);
+});
 
 module.exports = router;
