4151b3583b0060e438439c43643580cbdd565fe2
anna dodo <anna.dorenskaya@gmail.com>
2020-09-03 21:50:53 +0200

    3rd iteration

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,24 @@
-const express = require('express');
+/* const express = require('express');
 const router = express.Router();
 
-/* GET home page */
 router.get('/', (req, res, next) => res.render('index'));
+
+module.exports = router; */
+
+
+const express = require('express');
+const router = express.Router();
+const Cinemas = require('../models/Movie.model')
+
+router.get('/', (req, res, next) => res.render('index'));
+
+router.get('/movies', (req, res, next) => {
+    console.log('16')
+    Cinemas.find()
+        .then(data => {
+            console.log(data)
+            res.render('movies', {data})
+        })
+});
 
 module.exports = router;
---
+++ routes/movies.js
@@ -0,0 +1,10 @@
+/* const express = require('express');
+const router = express.Router();
+const Cinemas = require('../models/Movie.model')
+
+
+
+
+
+module.exports = router;
+ */
