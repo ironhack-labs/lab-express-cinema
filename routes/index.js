74a942beac8ac18ea15a9cb88563ebc579f3b290
Gilad Tsabar <gilad@tsabar.net>
2020-09-09 08:12:21 +0200

    Iteration 1 + 2 - Seed and homepage

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,9 @@ const express = require('express');
 const router = express.Router();
 
 /* GET home page */
-router.get('/', (req, res, next) => res.render('index'));
+router.get('/', (req, res, next) => {
+  const buttonMovies = 'Check the movies';
+  res.render('index', {buttonMovies});
+});
 
 module.exports = router;
