3af41904b9eecd36f16c7fa4e438b81027ddf677
Tami Pipoca <tamisakita@hotmail.com>
2020-09-09 22:56:35 -0300

    finished iteration 1

--- app.js
+++ app.js
@@ -5,13 +5,12 @@ const cookieParser = require('cookie-parser');
 const express = require('express');
 const favicon = require('serve-favicon');
 const hbs = require('hbs');
-const mongoose = require('mongoose');
 const logger = require('morgan');
 const path = require('path');
 
 const app_name = require('./package.json').name;
 const debug = require('debug')(
-  `${app_name}:${path.basename(__filename).split('.')[0]}`
+  `${app_name}:${path.basename(__filename).split('.')[0]}`,
 );
 
 const app = express();
@@ -29,6 +28,7 @@ app.use(cookieParser());
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
+hbs.registerPartials(path.join(__dirname, 'views', 'partials'));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
 
--- configs/db.config.js
+++ configs/db.config.js
@@ -1,12 +1,11 @@
 const mongoose = require('mongoose');
 
 mongoose
-  .connect('mongodb://localhost/express-cinema-dev', {
+  .connect(process.env.MONGODB_URI, {
     useCreateIndex: true,
     useNewUrlParser: true,
-    useUnifiedTopology: true
+    useUnifiedTopology: true,
   })
   .then(x =>
-    console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
-  )
+    console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`))
   .catch(err => console.error('Error connecting to mongo', err));
