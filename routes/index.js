40d254649f11a2ce420aed9145cd87f7d0fcb587
Tami Pipoca <tamisakita@hotmail.com>
2020-09-13 21:37:12 -0300

    finish iteration 2

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,19 @@
 const express = require('express');
+const Movies = require('../models/Movies');
+
 const router = express.Router();
 
 /* GET home page */
 router.get('/', (req, res, next) => res.render('index'));
+
+router.get('/movies', async (req, res) => {
+  try {
+    const movies = await Movies.find();
+
+    res.render('movies', { movies });
+  } catch (error) {
+    console.log(error);
+  }
+});
 
 module.exports = router;
