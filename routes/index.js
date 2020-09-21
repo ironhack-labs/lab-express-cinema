6a6df372f54cb10083de522b30b0629ef78d115f
Bouziane <bey.bouziane@live.fr>
2020-09-21 17:02:09 +0200

    Iteration 3 done

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,21 @@
-const express = require('express');
+const express = require("express");
+const Movies = require("../models/Movie.model");
 const router = express.Router();
 
 /* GET home page */
-router.get('/', (req, res, next) => res.render('index'));
+router.get("/", (req, res, next) => res.render("index"));
+
+//GET movies page
+router.get("/movies", async (req, res, next) => {
+    console.log("je suis bien lq")
+  try {
+    const movies = await Movies.find();
+    console.log(movies);
+    res.render("movies", { movies });
+  } catch (error) {
+    next(error);
+    return error;
+  }
+});
 
 module.exports = router;
