fff7153e9b2467155c55d66eac13c6a11709d024
Laura d'Aboville <lauradaboville@hotmail.com>
2020-09-21 17:59:01 +0200

    iteration 3 done and modif on iteration 2

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,14 @@
 const express = require('express');
 const router = express.Router();
+const MovieModel = require("../models/Movie.models")
 
 /* GET home page */
 router.get('/', (req, res, next) => res.render('index'));
+
+router.get("/movies", async (req, res) => {
+    const movie = await MovieModel.find(); 
+    console.log(movie);
+    res.render("movies", { movie });
+});
 
 module.exports = router;
