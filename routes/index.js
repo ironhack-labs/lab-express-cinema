fd00463569338654c4582950042aea75883e511a
JorellLangmar <jorell.langmar@gmail.com>
2020-09-21 17:22:42 +0200

    Iteration 2 CSS done, Iteration 3 done without CSS

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,13 @@
 const express = require('express');
 const router = express.Router();
+const MoviesModel = require("../models/Movies.models")
 
 /* GET home page */
 router.get('/', (req, res, next) => res.render('index'));
+
+router.get("/movies", async (req, res) => {
+    const movie = await MoviesModel.find(); // wait
+    res.render("movies", { movie });
+});
 
 module.exports = router;
