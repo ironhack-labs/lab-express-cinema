acde6a0a4115a3e9205bf5b96714a48e74fb5ae2
Ailie McCorkindale <ailiem134@hotmail.com>
2020-09-21 17:25:59 +0200

    Iteration 3 done

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,21 @@
 const express = require('express');
 const router = express.Router();
+const Movie = require("../models/Movie.model")
 
 /* GET home page */
 router.get('/', (req, res, next) => {
     res.render('index', {css : ["homepage"]});
 });
+
+router.get('/movies', async(req, res, next) => {
+    try {
+        const movieList = await Movie.find();
+        console.log(movieList);
+        res.render("movies", {movie: movieList});
+    }
+    catch(err) {
+        next(err);
+    }
+})
 
 module.exports = router;
