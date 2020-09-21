0558afc4bdbed38313973a61924b2ed559e165de
Bouziane <bey.bouziane@live.fr>
2020-09-21 17:39:18 +0200

    Iteration 4 done

--- routes/index.js
+++ routes/index.js
@@ -7,7 +7,6 @@ router.get("/", (req, res, next) => res.render("index"));
 
 //GET movies page
 router.get("/movies", async (req, res, next) => {
-    console.log("je suis bien lq")
   try {
     const movies = await Movies.find();
     console.log(movies);
@@ -15,6 +14,16 @@ router.get("/movies", async (req, res, next) => {
   } catch (error) {
     next(error);
     return error;
+  }
+});
+
+router.get("/movie/:id", async (req, res, next) => {
+  try {
+    const movieId = req.params.id;
+    const dbResult = await Movies.findById(movieId);
+    res.render("moviesDetail.hbs", { movies: dbResult });
+  } catch (error) {
+    next(error);
   }
 });
 
