ca1c85f3908c85e40bacbcc73b604ef9a1f66bc3
Laura d'Aboville <lauradaboville@hotmail.com>
2020-09-21 19:45:53 +0200

    iteration 4 done some CSS left to be done

--- routes/index.js
+++ routes/index.js
@@ -11,4 +11,12 @@ router.get("/movies", async (req, res) => {
     res.render("movies", { movie });
 });
 
+router.get("/movie-details/:id", async (req, res) => {
+    MovieModel.findById(req.params.id)
+    .then((dbRes) => {
+        res.render("movieDetails", { movie: dbRes });
+      })
+      .catch(next);
+});
+
 module.exports = router;
