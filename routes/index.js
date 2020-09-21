f616b73706a4c84fee623f1f0ab81af3b97d822a
pheno <nicolas.enault@gmail.com>
2020-09-21 17:07:03 +0200

    All iterations done without any CSS

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,27 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
+const Movie = require("../models/Movie.model");
 
 /* GET home page */
-router.get('/', (req, res, next) => res.render('index'));
+router.get("/", (req, res, next) => res.render("index"));
+
+router.get("/movies", async (req, res, next) => {
+  try {
+    res.render("movies.hbs", {
+      movies: await Movie.find(),
+    });
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.get("/movie/:id", async (req, res, next) => {
+  try {
+    const movie = await Movie.findById(req.params.id);
+    res.render("movieDetails", {movie});
+  } catch (err) {
+   next(err);
+  }
+});
 
 module.exports = router;
