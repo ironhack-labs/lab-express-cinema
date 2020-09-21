64e21c2ee0989ab6b72ddf5164b254137c473801
Lcdevi <caillauxlaura@gmail.com>
2020-09-21 22:12:46 +0200

    iteration 3 : done

--- routes/index.js
+++ routes/index.js
@@ -3,13 +3,32 @@ const router = express.Router();
 const Movie = require("../models/Movie.model");
 
 /* GET home page */
-router.get('/', (req, res, next) => res.render('index'));
+// router.get('/', (req, res, next) => res.render('index'));
 
+
+
+router.get("/", (req, res, next) => {
+
+    res.render("index", {
+      css: ["style"],
+    });
+  });
 
 
 /* GET movie page */
-router.get("/movies", (req, res) => res.render("movies"));
+
+router.get("/movies", (req, res, next) => {
+    console.log(req.body, "this is body");
+    console.log(req.params, "this is req params-----");
 
 
+   Movie.find({}) // --- ^
+     .then((dbResult) => {
+       res.render("movies", { movies: dbResult, css: ["stylemovies"] });
+     })
+     .catch((error) => {
+       next(error); // Sends us to the error handler middleware in app.js if an error occurs
+     });
+ });
 
 module.exports = router;
--- routes/movies.js
+++ routes/movies.js
@@ -1,13 +1,27 @@
-const express = require('express');
-const router = express.Router();
-const Movie = require("../models/Movie.model");
+// const express = require('express');
+// const router = express.Router();
+// const Movie = require("../models/Movie.model");
 
 
 
 
-/* GET movie page */
-router.get("/movies", (req, res) => res.render("movies"));
+// /* GET movie page */
+// // router.get("/movies", (req, res) => res.render("movies"));
 
 
+// // router.get("/movies", (req, res, next) => {
+// //     console.log(req.body, "this is body");
+// //     console.log(req.params, "this is req params-----");
 
-module.exports = router;
+
+// //    Movie.find({}) // --- ^
+// //      .then((dbResult) => {
+// //        res.render("movies", { movies: dbResult });
+// //      })
+// //      .catch((error) => {
+// //        next(error); // Sends us to the error handler middleware in app.js if an error occurs
+// //      });
+// //  });
+
+
+// module.exports = router;
