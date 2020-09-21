c2b54441b2669e00622d403b93ed84152f687fea
FanyMarin <nicte-ha.marin@hotmail.com>
2020-09-20 22:13:21 -0500

    primer commit, movie Model, tengo problemas para hacer las views porque en mi clase no ense;aron hbs

---
+++ Models/Movie.model.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const movieSchema = new Schema({
+    title: {
+        type: String,
+    },
+    director: {
+        type: String,
+    },
+    stars: {
+        type: [String],
+    },
+    image: {
+        type: String,
+    },
+    description: {
+        type: String,
+    },
+    showtimes: {
+        type: [String],
+    }
+});
+
+const movieModel = mongoose.model("movie", movieSchema);
+module.exports = movieModel;
