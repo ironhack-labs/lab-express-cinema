a7926ec526255921db6c0fea984af50d084231a5
laurent vuillaume <figura.graphik@gmail.com>
2020-09-22 08:53:03 +0200

    add iteration 2,3, 4, done

--- configs/db.config.js
+++ configs/db.config.js
@@ -1,12 +1,13 @@
 const mongoose = require('mongoose');
 
 mongoose
-  .connect('mongodb://localhost/express-cinema-dev', {
-    useCreateIndex: true,
+  .connect(process.env.MONGODB_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true
   })
-  .then(x =>
-    console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
-  )
-  .catch(err => console.error('Error connecting to mongo', err));
+  .then((self) => {
+    console.log(`Connected to Mongo! Database name: "${self.connections[0].name}"`)
+  })
+  .catch((err) => {
+    console.error('Error connecting to mongo', err)
+  });
--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,46 @@
 const express = require('express');
 const router = express.Router();
+const Movie = require("../models/Movie.model");
 
 /* GET home page */
-router.get('/', (req, res, next) => res.render('index'));
+router.get('/', (req, res, next) => res.render('index', {
+    title: "Cinema Ironhack",
+    labelbtn: 'Check the movies'
+}));
+
+router.get('/movies', async (req, res, next) => {
+
+    try {
+        const moviesDb = await Movie.find({});
+        console.log('-----', moviesDb);
+
+
+        res.render('movies.hbs', {
+            movies: moviesDb,
+            title: "Cinema Ironhack",
+            subtitle: "Click on the movie to check the showtime",
+        });
+    } catch (errDb) {
+        console.log(errDb);
+        next(errDb);
+    }
+
+});
+
+router.get('/movies/:id', async (req, res, next) => {
+
+    try {
+        const movieId = req.params.id;
+        console.log(movieId);
+        const movie = await Movie.findById(movieId);
+        console.log(movie);
+
+        res.render("single_movie", {
+            movie: movie,
+        });
+    } catch (errDb) {
+        next(errDb);
+    }
+});
 
 module.exports = router;
