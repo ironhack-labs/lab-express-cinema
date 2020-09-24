4708e16aa057e53b1d3c0940c13adbe9c7b74cce
AyloSrd <aylo.srd@gmail.com>
2020-09-24 13:21:54 +0200

    iteration-3 done

---
+++ routes/movies.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const router = express.Router()
+const MovieModel = require('../models/Movie.model')
+
+router.get('/', async(req, res, next) => {
+	try {
+		const movies = await MovieModel.find()
+		res.render('movies', { movies })		
+	} catch (error) {
+		console.error(error)
+	}
+
+})
+
+module.exports = router
