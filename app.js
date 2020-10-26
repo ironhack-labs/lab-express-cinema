6cf8ca64b8baba21e5582ff82a5ec63db67003e5
Xavier Caparros Gelabert <xcaparros@hotmail.com>
2020-10-26 18:42:39 +0100

    done

--- app.js
+++ app.js
@@ -31,7 +31,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
-
+app.listen(3000);
 // default value for title local
 app.locals.title = 'Express - Generated with IronGenerator';
 
--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,21 @@
 const express = require('express');
 const router = express.Router();
+var Movie = require('../models/Movie.model.js')
 
 /* GET home page */
 router.get('/', (req, res, next) => res.render('index'));
+
+router.get('/movies', async (req, res, next) => {
+    const movies = await Movie.find();
+    await console.log('movies', movies)
+    res.render('movies', {movies})
+});
+
+router.get('/movie/:id', async function(req, res, next){
+    console.log(req.params.id)
+    let movie = await Movie.findById(req.params.id);
+    console.log(movie)
+    res.render('movie', {movie})
+  })
 
 module.exports = router;
