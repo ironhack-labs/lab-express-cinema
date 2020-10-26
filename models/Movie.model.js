4b5416bc4cfdd7e64500dd3b1ea616e67ffd0864
paniaguaadrian <paniaguasanchezadrian@gmail.com>
2020-10-26 21:22:24 +0100

    Done

---
+++ models/Movie.model.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose");
+const schema = new mongoose.Schema({
+  title: { type: String },
+  director: { type: String },
+  stars: [String],
+  image: { type: String },
+  description: { type: String },
+  showtimes: [String],
+});
+const Movie = mongoose.model("movie", schema);
+module.exports = Movie;
