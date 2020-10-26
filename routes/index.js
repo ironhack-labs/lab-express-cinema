d50acc4443753a679c7d9c3594cde04952f8d3a7
Claudia D <claudia.do.gallego@gmail.com>
2020-10-26 16:43:07 +0100

    sin css

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,19 @@
 const express = require('express');
+const Movie = require('../models/Movie');
 const router = express.Router();
 
 /* GET home page */
 router.get('/', (req, res, next) => res.render('index'));
+
+router.get('/movies', async (req, res, next)=>{
+    const movies= await Movie.find();
+    res.status(200).render('movies', {movies});
+});
+
+router.get('/movie/:id', async(req,res,next)=>{
+    const selectedMovie= await Movie.findById(req.params.id);
+    console.log(selectedMovie);
+    res.status(200).render('movie', {selectedMovie})
+})
 
 module.exports = router;
