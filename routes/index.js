8b98e3b480896d3050877cd39c1b18944eef982f
Cristina Castro <cristinacastro87@gmail.com>
2020-10-26 19:56:13 +0100

    done

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,32 @@
-const express = require('express');
-const router = express.Router();
+
+var express = require("express");
+const Movie = require("../models/Movie.model");
+var router = express.Router();
+
 
 /* GET home page */
 router.get('/', (req, res, next) => res.render('index'));
 
+
+//mostrar
+router.get('/movies', async (req, res, next) => {
+    try{
+      let movie = await Movie.find()
+        res.render('movies', {movie});
+    }catch(err){
+        console.log('Error while getting the movies from the DB: ', err);
+    }
+  });
+
+  router.get('/movies/:id', async (req, res, next) => {
+    try{
+      let movie = await Movie.findById(req.params.id)
+        res.render('movie-detail', {movie});
+    }catch(err){
+        console.log('Error while getting the movies from the DB: ', err);
+    }
+  });
+
+
 module.exports = router;
+
