35c7a24178763c54f6f4eff3ca9bad48944e410b
Marta Camacho <martacamachovargas@gmail.com>
2020-10-26 23:59:38 +0100

    did not figure out how to list the movies

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,33 @@
 const express = require('express');
+const { update } = require('../models/Movie.model');
 const router = express.Router();
+const Movie = require("../models/Movie.model");
 
 /* GET home page */
 router.get('/', (req, res, next) => res.render('index'));
+
+router.get('/movies', async (req, res, next) => {
+     Movie.find()
+      .then( allTheMoviesFromDB => {
+        console.log('Retrieved movies from DB:', allTheMoviesFromDB);
+        res.render('movies', { movies: allTheMoviesFromDB });
+      })
+      .catch(error => {
+        next(error);
+      });
+  });
+
+  router.get('/:_id', async (req, res, next) =>  {
+     Movie.findById(req.params.id)
+    .then( theMovie => {
+      res.render('movie/:_id', { movie: theMovie });
+    })
+    .catch(error => {
+      next()
+      console.log('Error while retrieving movie details: ', error);
+    })
+});
+
+
 
 module.exports = router;
