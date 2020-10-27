9949eb802bad6343c0dce5097cd01e88cfbb84db
Andreu Martínez <martinez.andreu@gmail.com>
2020-10-27 08:53:22 +0100

    [WDFT-BCN-0920] Andreu Mtez - not 100% finished

--- app.js
+++ app.js
@@ -38,4 +38,7 @@ app.locals.title = 'Express - Generated with IronGenerator';
 const index = require('./routes/index');
 app.use('/', index);
 
+const movies = require('./routes/index');
+app.use('/movies', movies);
+
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,38 @@
 const express = require('express');
 const router = express.Router();
 
+const Movie = require('../models/Movie.model');
+
 /* GET home page */
-router.get('/', (req, res, next) => res.render('index'));
+
+// Iteration 2 - The home page
+
+console.log("I'm in index.js")
+router.get('/', function (req, res, next) {
+    console.log("I'm within router.get");
+    res.render('index');
+});
+
+// Iteration 3 - List the movies
+
+router.get('/movies', function(req, res, next) {
+    console.log("I'm within movies...");
+    Movie.find()
+    .then(movies => res.render('movies', {movies: movies}))
+    .catch(error => {
+      console.log('Error while getting the movies list from the DB: ', error);
+    });
+});
+
+// Iteration 4 - Display movie details
+
+router.get('/movies/:id', function (req, res, next) {
+  console.log("I'm within movies details");
+  Movie.findById(req.params.id)
+  .then(movie => res.render('moviedetails', {movie: movie}))
+  .catch(error => {
+    console.log('Error while getting the movie Id from the DB: ', error);
+  });
+}); 
 
 module.exports = router;
