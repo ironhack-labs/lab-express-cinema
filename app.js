3e7a5b2330e59d441accff21a1f0dc9f66b30a8f
Luizzmy <luizzmy@example.com>
2020-11-05 12:58:37 -0600

    iteracion 1 y 2

--- app.js
+++ app.js
@@ -33,9 +33,13 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
 
 // default value for title local
-app.locals.title = 'Express - Generated with IronGenerator';
+app.locals.title = 'Cinema Ironhack';
 
 const index = require('./routes/index');
 app.use('/', index);
 
 module.exports = app;
+
+app.listen(3000, () => {
+  console.log("listening on: http://localhost:3000")
+})
---
+++ models/Movie.model.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const movieSchema = new Schema({
+    title:{
+        type:String,
+        required:true,
+    },
+    director:{
+        type:String
+    },
+    stars:{
+        type:[String]
+    },
+    image:{
+        type:String
+    },
+    description:{
+        type:String
+    },
+    showtimes:{
+        type:[String]
+    }
+})
+
+const Movie = mongoose.model('Movie', movieSchema);
+
+module.exports = Movie;
