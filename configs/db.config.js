eeab90b297a46c24daf4d7e9321e1b8097e8570f
ahernandez <albertohernandez_91@yahoo.com>
2020-11-05 14:39:14 -0600

    done

--- configs/db.config.js
+++ configs/db.config.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose');
+const data = require('../bin/seeds');
+const Movie = require('../models/Movie.model');
 
 mongoose
   .connect('mongodb://localhost/express-cinema-dev', {
@@ -6,7 +8,19 @@ mongoose
     useNewUrlParser: true,
     useUnifiedTopology: true
   })
-  .then(x =>
-    console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
-  )
-  .catch(err => console.error('Error connecting to mongo', err));
+  .then(self => {
+    console.log(`Connected to the database: "${self.connection.name}"`);
+    // Before adding any documents to the database, let's delete all previous entries
+    return self.connection.dropDatabase();
+  })
+  .then(() => {
+    // Run your code here, after you have insured that the connection was made
+    Movie.insertMany(data)
+      .then(movies => {
+        console.log("connected to database 🔥")
+      })
+      .catch(error => console.log(error))
+  })
+  .catch(error => {
+    console.error('Error connecting to the database', error);
+  });
--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,20 @@
 const express = require('express');
 const router = express.Router();
+const Movie = require('../models/Movie.model');
 
 /* GET home page */
-router.get('/', (req, res, next) => res.render('index'));
+router.get('/', (req, res) => res.render('index'));
+
+router.get('/movies', async (req, res) => {
+  const movies = await Movie.find({}, {image: 1, title: 1})
+  console.log(movies)
+  res.render('movies', {movies})
+});
+
+router.get('/movies/:id', async (req, res) => {
+  const movie = await Movie.findById(req.params.id)
+  console.log(movie)
+  res.render('movie', movie)
+})
 
 module.exports = router;
