e811dd7e5ea407654ae2b7af9a8e3d4722e9460c
Ivan Rubio <ivanrubioangeles@gmail.comn>
2020-11-05 11:20:06 -0600

    added schema

--- models/Movie.model.js
+++ models/Movie.model.js
@@ -1,1 +1,20 @@
 // Create a folder models and in it add the Movie.model.js file for the Movie model;
+const mangoose = require('mongoose');
+const Schema = mangoose.Schema
+
+
+const movieSchema = new Schema(
+  {
+    title: String,
+    director: String,
+    stars: [ String ],
+    image: String,
+    description: String,
+    showtimes: [ String ],
+  },
+
+);
+
+const Movie = mongoose.model("Movie", movieSchema);
+
+module.exports = Movie;
