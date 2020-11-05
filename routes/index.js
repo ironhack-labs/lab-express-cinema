16f53dfe4c30f41957cab3c7a0e6f82fd866466a
Ivan Rubio <ivanrubioangeles@gmail.comn>
2020-11-05 12:54:19 -0600

    render aync movies

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,28 @@
 const express = require('express');
 const router = express.Router();
-const movie = require('../models/Movie.model')
+const Movie = require('../models/Movie.model')
 
 /* GET home page */
 router.get('/', (req, res, next) => res.render('index'));
-router.get("/movies", (req, res) => res.render("movies"))
+
+
+router.get("/movies", (req, res, next) => {
+
+  async function getMovies() {
+    const response = await Movie.find().catch(err => {
+      console.log('Error in fetching data');
+    });
+
+    if (response) {
+      console.log("encottradi");
+      console.log(response)
+      res.render("movies", { response });
+    }
+  }
+
+  getMovies()
+});
+
+
 
 module.exports = router;
