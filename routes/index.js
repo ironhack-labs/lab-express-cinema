827e0030171b4c33d27897b0e551e3a2c9b2e015
Gabriela Polanco <gabrielapolancoferreyra@gmail.com>
2020-11-08 10:58:36 -0500

    done

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,23 @@
 const express = require('express');
 const router = express.Router();
+const Movie = require("../models/Movie")
+
 
 /* GET home page */
 router.get('/', (req, res, next) => res.render('index'));
+
+
+router.get('/movies', async (req, res, next) => {
+    const movies = await Movie.find();
+    res.render('movies', {movies})
+})
+
+
+router.get("/movies/:id", async (req, res) => {
+const { id } = req.params
+const movies = await Movie.findById(id)
+ res.render("moviesDetail", {movies})
+})
+
 
 module.exports = router;
