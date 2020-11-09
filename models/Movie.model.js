7a0f156519311db534a4bfa18bfb95cfa45be5f0
Rich Alossi <alossix@gmail.com>
2020-11-09 17:24:40 +0100

    Done with all iterations

--- models/Movie.model.js
+++ models/Movie.model.js
@@ -3,11 +3,11 @@ const { Schema, model } = mongoose;
 
 const movieSchema = new Schema({
   title: { type: String, required: true },
-  director: { type: String, required: true },
-  stars: { type: Array, required: true },
+  director: { type: [String], required: true },
+  stars: { type: [String], required: true },
   image: { type: String, required: true },
   description: { type: String, required: true },
-  showtimes: { type: Array, required: true },
+  showtimes: { type: [String], required: true },
 });
 
 module.exports = model("Movie", movieSchema);
--- routes/index.js
+++ routes/index.js
@@ -7,8 +7,17 @@ router.get("/", (req, res, next) => res.render("index"));
 
 router.get("/movies", (req, res, next) => {
   MovieModel.find().then((moviesfromDB) => {
-    console.log(moviesfromDB);
     res.render("movies", { moviesfromDB });
   });
+});
+
+router.get("/movies/:id", (req, res, next) => {
+  const movieId = req.params.id;
+  MovieModel.findById({ _id: movieId })
+    .then((movie) => {
+      console.log(movie);
+      res.render("moviePage", { movie });
+    })
+    .catch((err) => console.error(err));
 });
 module.exports = router;
