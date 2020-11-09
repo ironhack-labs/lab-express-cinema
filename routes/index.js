324d17b19cd0560d291f0b489b0637f1b6a34288
Irene <ihatepineapple@gmail.com>
2020-11-09 17:07:27 +0100

    Assignment done

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,21 @@
 const express = require('express');
 const router = express.Router();
 
+const Movie = require("../models/Movie.model")
+
 /* GET home page */
 router.get('/', (req, res, next) => res.render('index'));
 
+router.get('/movies', (req, res) => {
+    Movie.find({})
+    .then((moviesFromDB) => res.render('movies', { moviesFromDB } ))
+    .catch((error) => `Error fetching movies: ${error}`)
+});
+
+router.get('/movies/:id', (req, res, next) => {
+
+    Movie.findById(req.params.id)
+    .then((movieDetails) => res.render('movies-detail',  movieDetails  ))
+    .catch((error) => `Error fetching movies: ${error}`)
+});
 module.exports = router;
