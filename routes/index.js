713007b695063e1ce77152a8f1ff6049f5594d0b
Emad Ghnim <emad.ghnim@outlook.com>
2020-11-09 19:49:39 +0100

    done

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,23 @@
 const express = require('express');
 const router = express.Router();
+const Movie = require("../models/Movie.model");
 
-/* GET home page */
-router.get('/', (req, res, next) => res.render('index'));
+router.get('/', (req, res, next) => {
+    res.render('index', {title: "IronHack Cinema"});
+})
 
+router.get("/movies", (req, res, next) => {
+    Movie.find({})
+    .then(allMoviesFromDB => {
+        console.log(allMoviesFromDB)
+        res.render("movies", {movies: allMoviesFromDB})
+    })
+})
+
+router.get("/movies/:id", (req, res, next) => {
+    Movie.findById(req.params.id)
+    .then((movieFromDB) => {
+        res.render('movie-detail', movieFromDB)
+      })
+})
 module.exports = router;
