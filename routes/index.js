d5f6fb2ff5ccf37885b945021f7c06bb5ceaca35
Elia Chiarucci <elia.chiarucci@gmail.com>
2020-11-09 16:32:58 +0100

    All iterations done

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,21 @@
-const express = require('express');
+const express = require("express");
+const MovieModel = require("../models/Movie.model");
 const router = express.Router();
 
 /* GET home page */
-router.get('/', (req, res, next) => res.render('index'));
+router.get("/", (req, res, next) => res.render("index"));
+
+router.get("/movies", (req, res) => {
+  MovieModel.find({})
+    .then((movies) => res.render("movies", { movies }))
+    .catch((err) => console.error(err));
+});
+
+router.get("/movie/:id", (req, res) => {
+  const { id } = req.params;
+  MovieModel.findOne({ _id: id })
+    .then((movie) => res.render("movie", { movie }))
+    .catch((err) => console.log(err));
+});
 
 module.exports = router;
