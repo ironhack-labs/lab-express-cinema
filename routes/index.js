919c7aab4df4e173d9daf2ade02d4c0918070844
Emma Northridge <emmanorthridge1@gmail.com>
2020-11-09 16:43:21 +0100

    All iterations done. Need to style still

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,25 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
+const Movie = require("../models/Movie.model");
 
 /* GET home page */
-router.get('/', (req, res, next) => res.render('index'));
+router.get("/", (req, res, next) => res.render("index"));
+
+router.get("/movies", (req, res, next) => {
+  Movie.find()
+    .then((moviesFromDB) => {
+      res.render("movies", { movies: moviesFromDB });
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+});
+
+router.get("/movies/:id", (req, res, next) => {
+  Movie.findById(req.params.id).then((displayMovie) => {
+      console.log(displayMovie)
+    res.render("movie-details", { display: displayMovie });
+  });
+});
 
 module.exports = router;
