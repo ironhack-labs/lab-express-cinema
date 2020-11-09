6f81f8e800bd1bc1bb096e03e97caefa47894e4e
sophiepa <sophiepa@hotmail.com>
2020-11-09 17:07:07 +0100

    all iterations done, no styling

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,28 @@
 const express = require('express');
+const Movie = require ("../models/movie.models.js")
 const router = express.Router();
+
+module.exports = router;
 
 /* GET home page */
 router.get('/', (req, res, next) => res.render('index'));
 
-module.exports = router;
+router.get('/movies', (req, res) => {
+    Movie.find()
+    .then((allMoviesFromDB) => {
+        res.render("movies", { allMoviesFromDB });
+    })
+    .catch(error => console.log("Error while getting the movies from the DB:", error))
+});
+
+router.get('/movies/:id', (req, res) => {
+    const {id} = req.params;
+
+    Movie.findOne({_id: id})
+    .then((theMovie) => {
+        res.render("movie-details", { theMovie });
+    })
+    .catch(error => console.log("Error while getting the movies from the DB:", error))
+});
+
+
