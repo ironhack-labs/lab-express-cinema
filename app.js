350edadf1bb60334e3779146e4b2d5b121206aa6
Ceyda Ulubas <ceyda.ulubas@gmail.com>
2020-11-10 15:07:21 +0300

    completed all iterations but can't seem on the browsers and MongoDB Compass

--- app.js
+++ app.js
@@ -9,6 +9,22 @@ const mongoose = require('mongoose');
 const logger = require('morgan');
 const path = require('path');
 
+mongoose
+.connect("mongodb://localhost/express-cinema-project", {
+  useCreateIndex: true,
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+.then((x) => {
+  console.log(
+    `Connected to Mongo! Database name: "${x.connections[0].name}"`
+  );
+})
+.catch((err) => {
+  console.error("Error connecting to mongo", err);
+});
+
+
 const app_name = require('./package.json').name;
 const debug = require('debug')(
   `${app_name}:${path.basename(__filename).split('.')[0]}`
--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,29 @@
 const express = require('express');
 const router = express.Router();
 
+const Movie= require("../models/Movie.model");
+ 
 /* GET home page */
 router.get('/', (req, res, next) => res.render('index'));
 
+//created routes
+router.get('/movies', (req, res, next) => {
+    Movie.findOne({})
+    .then((allTheMoviesFromDB) => {
+        res.render('movies', { allTheMoviesFromDB });
+    })
+    .catch(error => console.log('Error while retrieving movie details: ', error));
+});
+
+router.get('movie/:id', (req, res, next) => {
+    const { id } =req.params;
+
+    Movie.findById(id)
+    .then((foundMovie) => {
+        res.render('movies', foundMovie);
+    })
+    .catch(error => console.log('Error while retrieving movie details: ', error));
+});
+
 module.exports = router;
+
