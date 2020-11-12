9aad8d41b34eefa1f7f3be1fcac6404d1e5be3eb
Lara_Morgana <laraanjiru@gmail.com>
2020-11-12 23:19:15 +0100

    Done

---
+++ routes/movie-routes.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const router = express.Router()
+
+const Movie = require('./../models/movie.model')
+
+router.get('/', (req, res) => {
+
+    Movie
+        .find()
+        .then(allMovies => res.render('movies/movies-list', { allMovies }))  
+        .catch(err => console.log(err))
+})
+
+
+router.get('/details/:movie_id', (req, res) => {
+
+    const movieId = req.params.movie_id
+
+    Movie
+        .findById(movieId)
+        .then(theMovie => res.render('movies/details', theMovie))
+        .catch(err => console.log(err))
+})
+
+
+module.exports = router
