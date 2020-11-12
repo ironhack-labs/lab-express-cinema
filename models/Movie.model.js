614b8497619f1832063407a872e5b81cf7d25608
Cristy Lopez Piñeiro <aclopezp93@gmail.com>
2020-11-13 00:11:46 +0100

    done

---
+++ models/Movie.model.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose")
+const Schema = mongoose.Schema
+
+const movieSchema = new Schema({
+    title: String,
+    director: String,
+    Stars: [String],
+    image: String,
+    description: String,
+    showtimes: [String]
+}, {
+    timestamps: true
+})
+
+const Movie = mongoose.model("Movie", movieSchema);
+module.exports = Movie;
--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,16 @@
 const express = require('express');
 const router = express.Router();
 
+const Movie = require('../models/Movie.model')
+
 /* GET home page */
-router.get('/', (req, res, next) => res.render('index'));
+router.get('/', (req, res) => res.render('index'));
+router.get('/movies', (req, res) => {
+    Movie.find({})
+        .then((printAllMoviesCreated) => {
+            res.render('movies', { printAllMoviesCreated })
+        })
+        .catch((error) => `Error viewing ${error}`)
+})
 
 module.exports = router;
