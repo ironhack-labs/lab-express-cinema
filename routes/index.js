7ec1d775f7fe6847ac67bf00d79f721de982c892
RaquelRB <raquelrobles95@gmail.com>
2020-11-13 14:28:37 +0100

    Exercise completed

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,34 @@
 const express = require('express');
 const router = express.Router();
 
+const Movie = require('../models/Movie.model')
+
 /* GET home page */
 router.get('/', (req, res, next) => res.render('index'));
+
+router.get('/movies', (req,res,next)=>{
+    Movie.find({}, {title:1, image:1, _id:1})
+    .then((movie) => {
+      res.render('movies', {movie})
+    })
+    .catch((err) => { 
+        console.log(err) 
+        res.send(err)
+    })
+})
+
+router.get('/movies/:id', (req,res,next)=>{
+    const movieId = req.params.id
+
+    Movie.findById(movieId)
+    .then((result)=>{
+        res.render('singleMovie', result)
+    })
+    .catch((err)=>{
+        console.log(err)
+        res.send(err)
+    })
+})
+
 
 module.exports = router;
