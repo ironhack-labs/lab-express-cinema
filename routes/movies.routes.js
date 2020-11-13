a08b3d8f64d482bcf99c5467e17059207c4115f1
dreyanfranco <dreyanfranco@gmail.com>
2020-11-13 01:04:06 +0100

    done

---
+++ routes/movies.routes.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const router = express.Router();
+
+const Movie = require('../models/Movie.model');
+
+router.get('/', (req, res) => {
+    Movie
+        .find()
+        .then(allMovies => res.render('movies', { allMovies: allMovies }))
+        .catch((error) => console.log(`It's not getting the movies: ${error}`))
+})
+
+// detalle
+router.get('/detalle/:movie_id', (req, res) => {
+    Movie
+        .findById(req.params.movie_id)
+        .then(theMovie => res.render('details', theMovie))
+        .catch(err => console.log(err))
+})
+
+module.exports = router;
