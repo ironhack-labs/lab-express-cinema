f17ad59fa26af6da121e66f5f52157a07b09ec88
sarastackhouse <sarapalacios7@gmail.com>
2020-11-13 20:06:50 +0100

    Done with Iron Express

--- app.js
+++ app.js
@@ -1,79 +1,30 @@
 require('dotenv').config();
 
-const bodyParser    = require('body-parser');
-const cookieParser  = require('cookie-parser');
-const express       = require('express');
-const favicon       = require('serve-favicon');
+const express       = require('express');;
 const hbs           = require('hbs');
 const mongoose      = require('mongoose');
-const logger        = require('morgan');
 const path          = require('path');
 const chalk         = require('chalk');
-
-
-const app_name = require('./package.json').name;
-const debug = require('debug')(
-  `${app_name}:${path.basename(__filename).split('.')[0]}`
-);
 
 const app = express();
 
 // require database configuration
+
 require('./configs/db.config');
-
-// Middleware Setup
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-
-// Express View engine setup
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
 
 // default value for title local
 app.locals.title = 'Express - Generated with IronGenerator';
 
 const index = require('./routes/index');
-const Movie = require('./models/Movie.model');
-
-
-
 app.use('/', index);
+const movies = require('./routes/movies');
+app.use('/movies', movies);
 
 module.exports = app;
-
-app.get('/', (req, res, next) => {
-  res.render('index');
-})
-
-app.get('/movies', (req, res, next) => {
-  Movie.find({}, {title: 1, image: 1, description: 1})
-  .then((movie) => {
-    console.log(movie)
-    res.render('movies', {movie});
-    
-  })
-  .catch((err) => {
-    console.log(err)
-    res.send(err)
-  })
-  
-})
-
-app.get('/movie/:id', (req, res, next)=>{
-  const movieID = req.params.id;
-  Movie.findById(movieID)
-  .then((result)=>{
-      res.render('singleMovie', result);
-  })
-  .catch((error)=>{
-      res.send(error);
-  });
-});
 
 app.listen(process.env.PORT, ()=>{
   console.log(chalk.green.inverse.bold(`Conectado al puerto ${process.env.PORT}`));
---
+++ routes/movies.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const router = express.Router();
+const Movie = require('../models/Movie.model');
+
+module.exports = router;
+
+
+/* GET movies page */
+router.get('/', (req, res, next) => {
+    Movie.find({}, {title: 1, image: 1, description: 1})
+    .then((movie) => {
+      console.log(movie)
+      res.render('movies', {movie});
+      
+    })
+    .catch((err) => {
+      console.log(err)
+      res.send(err)
+    })
+    
+  })
+
+  router.get('/details/:id', (req, res, next)=>{
+    const movieID = req.params.id;
+    Movie.findById(movieID)
+    .then((result)=>{
+        res.render('singleMovie', result);
+    })
+    .catch((error)=>{
+        res.send(error);
+    });
+  });
+  
+
+
+  
+module.exports = router;
