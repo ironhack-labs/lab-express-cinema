fbc1e4d7ced5f43114140d169ef15a0efdfbecbf
Sergio <srosduran@gmail.com>
2020-11-14 17:17:51 +0100

    three itinerations done

--- configs/db.config.js
+++ configs/db.config.js
@@ -1,12 +1,14 @@
+const chalk = require('chalk');
 const mongoose = require('mongoose');
 
 mongoose
   .connect('mongodb://localhost/express-cinema-dev', {
     useCreateIndex: true,
     useNewUrlParser: true,
-    useUnifiedTopology: true
+    useUnifiedTopology: true,
+    useFindAndModify: true
   })
   .then(x =>
-    console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
+    console.log(chalk.yellow.inverse.bold(`Connected to Mongo! Database name: "${x.connections[0].name}"`))
   )
   .catch(err => console.error('Error connecting to mongo', err));
