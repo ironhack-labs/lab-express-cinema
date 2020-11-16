62d94f7867052e3cf58c25d42c8ed13b975ac160
Mark <mark.sanjuan.sole@gmail.com>
2020-11-16 09:11:33 +0100

    fixed some mistakes

--- routes/index.js
+++ routes/index.js
@@ -8,21 +8,5 @@ router.get('/', (req, res, next) => {
     res.render('index')
 })
 
-router.get('/movies', (req, res, next) => {
-    Movie.find({}, {title: 1, image: 1})
-    .then(movies => {
-        res.render('movies', {movies})
-    })
-    .catch(err => console.error(err))
-})
-
-router.get('/movie/:id', (req, res, next) => {
-    Movie.findById(req.params.id)
-    .then(movie => {
-        console.log(movie)
-        res.render('one-movie', movie)
-    })
-    .catch(err => console.error(err))
-})
 
 module.exports = router;
