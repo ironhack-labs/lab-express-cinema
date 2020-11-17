051968fc88f12c8c2800d56ac8529d817bfbb511
Maxim Jurgandere <maxim.jurgander@gmail.com>
2020-11-17 13:05:04 +0100

    almost done

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,25 @@
 const express = require('express');
 const router = express.Router();
+const Movie = require('../models/Movie.model');
 
 /* GET home page */
 router.get('/', (req, res, next) => res.render('index'));
+
+router.get('/movies', (req, res, next) => {
+   Movie.find().then(movies => {
+     console.log(movies);
+    res.render('movies', { movies });
+  }).catch((err) => {
+    console.log('Error', err);
+  })
+});
+
+router.get('/:id', (req, res, next) => {
+  const { id } = req.params;
+    Movie.findById(id).then(id => {
+      console.log(id)
+      res.render('moviedetails', {id});
+  })
+})
 
 module.exports = router;
