02500346e7e9545acffbe2849c857d8a8d9604ab
Ashley Vermaas <ashleyvermaas1@gmail.com>
2020-11-20 12:56:03 +0100

    complete iteration 3

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,16 @@
 const express = require('express');
 const router = express.Router();
+const Movie = require('../models/Movie.model');
 
 /* GET home page */
 router.get('/', (req, res, next) => res.render('index'));
+
+router.get('/movies', (req, res, next) => {
+    Movie.find()
+    .then((movies) => {
+        res.render('movies', {movies});
+    })
+    .catch((error) => console.log(`The movies can't be shown due to an error`, error));
+});
 
 module.exports = router;
