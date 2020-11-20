66848bd2a0f0e45f50389bb80fb34ae2841abc2c
Ashley Vermaas <ashleyvermaas1@gmail.com>
2020-11-20 13:43:41 +0100

    done, complete iteration 4

--- routes/index.js
+++ routes/index.js
@@ -7,10 +7,18 @@ router.get('/', (req, res, next) => res.render('index'));
 
 router.get('/movies', (req, res, next) => {
     Movie.find()
-    .then((movies) => {
-        res.render('movies', {movies});
-    })
-    .catch((error) => console.log(`The movies can't be shown due to an error`, error));
+        .then((movies) => {
+            res.render('movies', {movies});
+        })
+        .catch((error) => console.log(`The movies can't be shown due to an error`, error));
+});
+
+router.get('/movies/:movieId', (req, res, next) => {
+    Movie.findById(req.params.movieId)
+        .then((movie) => {
+            res.render('details', {movie});
+        })
+        .catch((error) => console.log(`This movie can't be shown due to an error`, error));
 });
 
 module.exports = router;
