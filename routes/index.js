fe22740d12d5bcb5eee2b685fa8d2906357ac0fc
MADFutcher <mikalob247@gmail.com>
2020-11-20 14:17:53 +0100

    Finished Express Movies

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,20 @@
 const express = require('express');
 const router = express.Router();
+const Movies = require('../models/Movie.model')
 
 /* GET home page */
 router.get('/', (req, res, next) => res.render('index'));
+
+router.get('/movies', (req,res,next)=>{
+    Movies.find().then((movies)=>{
+        res.render('movies',{movies})
+    }).catch(err=>console.log(err))
+})
+
+router.get('/movies/:id',(req,res,next)=>{
+    Movies.findById(req.params.id).then((movie)=>{
+        res.render('movieInfo',{movie})
+    }).catch(err=>console.log(err))
+})
 
 module.exports = router;
