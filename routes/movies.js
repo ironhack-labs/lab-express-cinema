a22223ad5d5086d1eadd68f3af5342df428571c8
Zhenni Qian <zhenni.qian913@gmail.com>
2020-11-21 19:08:22 +0100

    lab solved

--- routes/movies.js
+++ routes/movies.js
@@ -10,4 +10,10 @@ router.get('/movies', (req, res, next) => {
     .catch(err => console.log(`Error while getting the movies from the DB: ${err}`));   
 });
 
+router.get('/movies/:id', (req, res, next) => {
+    Movie.findById(req.params.id)
+    .then(movie => res.render('movie-details', movie))
+    .catch(err => console.log(`Error while getting the movie from the DB: ${err}`));   
+});
+
 module.exports = router;
