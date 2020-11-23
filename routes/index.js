a6a2fc19944160e1101b54fb532c12aff8b72e26
Samantha van der Put <samantha.vdput@gmail.com>
2020-11-23 20:51:31 +0100

    done except styling

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,25 @@
 const express = require('express');
 const router = express.Router();
+const Movie = require('../models/Movie.model.js');
 
 /* GET home page */
 router.get('/', (req, res, next) => res.render('index'));
+
+router.get('/movies', (req, res) => {
+  Movie.find()
+    .then(moviesFromDB => {
+      console.log('Retrieved movies from DB:', moviesFromDB);
+      res.render('movies-list', { movies: moviesFromDB });
+    })
+    .catch(error => console.log('Error while getting the movies from the DB: ', error));
+});
+  
+  router.get('/movie/:movieId', (req, res) => {
+    const { movieId } = req.params;
+   
+    Movie.findById(movieId)
+      .then(theMovie => res.render('movies-details', { movie: theMovie }))
+      .catch(error => console.log('Error while retrieving movie details: ', error));
+  });
 
 module.exports = router;
