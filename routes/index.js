01ac76d25b183d96999c8d000b256835b2d9acec
Dennis Deuling <dennis.deuling@gmail.com>
2020-11-27 16:41:20 +0100

    all done

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,36 @@
 const express = require('express');
 const router = express.Router();
+const Movie = require('../models/Movie.model');
 
 /* GET home page */
-router.get('/', (req, res, next) => res.render('index'));
+router.get('/', (req, res, next) => {
+    res.render('index');
+});
+
+router.get('/movies', (req, res, next) => {
+    Movie.find()
+        .then(movies => {
+            console.log(`The Movies are: ${movies}`);
+            res.render('movies', { movies });
+        })
+        .catch(error => {
+            console.log(`Oh no, we are having an error: ${error}`);
+        });
+});
+
+
+router.get('/movies/:id', (req, res, next) => {
+    Movie.findById(req.params.id)
+        .then(movie => {
+            console.log(`The movie is: ${movie}`);
+            res.render('movie-detail', { movie });
+        })
+        .catch(error => {
+            console.log(`Oh no, we are having an error: ${error}`);
+        });
+});
+
+
+
 
 module.exports = router;
