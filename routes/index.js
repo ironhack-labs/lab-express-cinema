e8a9410917aab1860b55170b44e0830f4662a89e
Edouard Pfauwadel <edouard.pfauwadel@gmail.com>
2020-12-07 16:36:32 +0100

    done with seeds routes and pages need css now

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,28 @@
 const express = require('express');
 const router = express.Router();
 
+const MovieModel = require("./../models/Movie.model")
+
 /* GET home page */
 router.get('/', (req, res, next) => res.render('index'));
+
+
+router.get("/movies", async (req, res) => {
+
+    try {
+    const movies = await MovieModel.find();
+    res.render("movies", { movies });
+    } catch (err) {
+    console.log(err)
+    }
+})
+
+
+router.get("/movies/:id", async (req, res) => {
+const movie = await MovieModel.findById(req.params.id);
+res.render("movieDetail", movie)
+
+})
+
 
 module.exports = router;
