b98daf906b3999e9a4dbd0a5e46a34e948e8241b
Katalin Stolcz <katikastolcz@hotmail.hu>
2020-12-09 21:00:44 +0100

    lab done without styling

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,28 @@
 const express = require('express');
 const router = express.Router();
+const mongoose = require('mongoose');
+
+const MovieModel = require("./../models/Movie.model");
 
 /* GET home page */
 router.get('/', (req, res, next) => res.render('index'));
+
+router.get("/movies", async (req, res, next) => {
+    try {
+        const movies = await MovieModel.find();
+        res.render("movies", { movies }); 
+    } catch (err) {
+        next(err);
+    }
+});
+
+router.get("/movies/:id", async (req, res, next) => {
+    try {
+        const movieDetails = await MovieModel.findById(req.params.id);
+        res.render("movieDetail", movieDetails); 
+    } catch (err) {
+        next(err);
+    }
+});
 
 module.exports = router;
