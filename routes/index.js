e8fabc86982f8830285bbda2f7826c87ea121371
Ida <so.kida@hotmail.com>
2021-01-09 16:46:15 -0500

    done

--- routes/index.js
+++ routes/index.js
@@ -1,30 +1,29 @@
-const express = require('express');
-const router  = express.Router();
+const express = require("express");
+const router = express.Router();
 
-const Movie = require('../models/Movie.model.js')
+const Movie = require("../models/Movie.model.js");
 
 /* GET home page */
-router.get('/', (req, res, next) => {
-  res.render('index');
+router.get("/", (req, res, next) => {
+  res.render("index");
 });
 
-router.get('/movies', (req,res, next) => {
+router.get("/movies", (req, res, next) => {
   Movie.find()
-  .then(allMoviesFromDB => {
-    
-    res.render('movies-list', {movies: allMoviesFromDB});
-  })
-  .catch(error => console.log('Error while getting the movies from the DB: ', error))
+    .then((allMoviesFromDB) => {
+      res.render("movies-list", { movies: allMoviesFromDB });
+    })
+    .catch((error) =>
+      console.log("Error while getting the movies from the DB: ", error)
+    );
 });
 
-router.get('/movies/:movieId', (req, res)=>{
-
+router.get("/movies/:movieId", (req, res) => {
   Movie.findOne(req.params.id)
-  .then(theMovie => res.render ('movie-details', {movie: theMovie}))
-  .catch(error=> console.log('Error while retrieving movie details: ', error))
-    
+    .then((theMovie) => res.render("movie-details", { movie: theMovie }))
+    .catch((error) =>
+      console.log("Error while retrieving movie details: ", error)
+    );
 });
-
-
 
 module.exports = router;
