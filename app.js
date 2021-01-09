799041c44bea5e968e76647bf1aea39bed80d687
Sebastian Montoya <seb246@gmail.com>
2021-01-08 21:47:43 -0500

    iteration 3

--- app.js
+++ app.js
@@ -9,6 +9,16 @@ const mongoose = require('mongoose');
 const logger = require('morgan');
 const path = require('path');
 
+// mongoose
+//   .connect('mongodb://localhost/express-cinema-dev', {useNewUrlParser: true})
+//   .then(x => {
+//     console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
+//   })
+//   .catch(err => {
+//     console.error('Error connecting to mongo', err)
+//   });
+
+
 const app_name = require('./package.json').name;
 const debug = require('debug')(
   `${app_name}:${path.basename(__filename).split('.')[0]}`
@@ -37,5 +47,6 @@ app.locals.title = 'Express - Generated with IronGenerator';
 
 const index = require('./routes/index');
 app.use('/', index);
+app.use(`/movies`, require(`./routes/index`));
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,18 @@
 const express = require('express');
+const Movie = require('../models/Movie.model.js');
 const router = express.Router();
 
 /* GET home page */
 router.get('/', (req, res, next) => res.render('index'));
+
+router.get('/movies', (req,res,next) => {
+    Movie.find()
+    .then(allMovies => {
+        res.render(`movies`, {movies: allMovies});
+    })
+    .catch(err => {
+        console.log(`error getting movies due to ${err}`);
+    })
+});
 
 module.exports = router;
