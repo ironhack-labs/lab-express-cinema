89f256e805d4a8900409578ac6e8a56f09629dfa
Ida <so.kida@hotmail.com>
2021-01-08 23:52:19 -0500

    3 iterations

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,31 @@
 const express = require('express');
-const router = express.Router();
+const router  = express.Router();
+
+const Movie = require('../models/Movie.model.js')
 
 /* GET home page */
-router.get('/', (req, res, next) => res.render('index'));
+router.get('/', (req, res, next) => {
+  res.render('index');
+});
+
+router.get('/movies', (req,res, next) => {
+  Movie.find()
+  .then(allMoviesFromDB => {
+    console.log('Retrieve:', allMoviesFromDB)
+    res.render('movies-list', {movies: allMoviesFromDB});
+  })
+  .catch(error => console.log('Error while getting the movies from the DB: ', error))
+});
+
+router.get('/movies/:movieId', (req, res)=>{
+  const {movieId} = req.params;
+
+  Movie.findOne(movieId)
+  .then(theMovie => res.render ('movie-details', {movie: theMovie}))
+  .catch(error=> console.log('Error while retrieving movie details: ', error))
+    
+});
+
+
 
 module.exports = router;
