765dea3aed1c6686303229256e63c6a7e4a89d0d
Ángela Herrador <angelaherrador1991@gmail.com>
2021-01-29 15:07:56 +0100

    Iteration 1 and 2 done

---
+++ bin/seeds.js
@@ -0,0 +1,13 @@
+require("dotenv").config()
+require("../configs/db.config")
+const data = require('../data')
+const Movie = require("../models/Movie.model")
+
+
+Movie.deleteMany()
+    .then(() => {
+        Movie.insertMany(data)
+        }).then((movie) => {
+        console.log (movie)
+    })
+    .catch((e) => console.log("Error", e))
