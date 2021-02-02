1fa6754067f94cdced244ee052976d2e6e732011
Pedro Frias <plfriasf@gmail.com>
2021-02-02 13:42:52 -0800

    Iteration #4 | Display Movie Details done

--- configs/routes.js
+++ configs/routes.js
@@ -7,9 +7,18 @@ const Movie = require("../models/Movie.model");
 router.get("/", (req, res, next) => res.render("index"));
 
 router.get("/movies", (req, res, next) => {
-    Movie.find()
-      .then((movies) => res.render("movies", { movies }))
-      .catch((e) => next(e));
+    Movie
+        .find()
+            .then((movies) => res.render("movies", { movies }))
+            .catch((e) => next(e));
 })
+
+router.get("/movies/:id", (req, res, next) => {
+    const id = req.params.id
+    Movie
+        .findById(id)
+            .then((movie) => res.render("movieDetail", movie))
+            .catch((e) => next(e));
+})                                                                                          
 
 module.exports = router;
