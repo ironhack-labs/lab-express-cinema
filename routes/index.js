63f412e80e779fea7b15b2466f5bf78d8733bcca
andrea morales <amoralesp88@gmail.com>
2021-02-03 19:16:34 -0600

    in process

--- routes/index.js
+++ routes/index.js
@@ -4,4 +4,24 @@ const router = express.Router();
 /* GET home page */
 router.get('/', (req, res, next) => res.render('index'));
 
+router.get('/movies', (req, res, next) => {
+    console.log(movies.find)
+    Movies
+        .find()
+        .then(data => {
+            res.render('movies')
+        })
+        .catch((e) => next(e));
+})
+
+router.get('/movies/:id', (req, res, next) => {
+
+    Movies
+        .findById()
+        .then(data => {
+            res.render('movies')
+        })
+        .catch((e) => next(e));
+})
+
 module.exports = router;
