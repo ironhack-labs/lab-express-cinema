f27b6cb04f845b0e8c69fb99496f4f5e0195a7c8
Sebastian Septien <sebsepdus@hotmail.com>
2021-02-08 17:38:03 -0600

    it is done

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,34 @@
 const express = require('express');
 const router = express.Router();
 
+const Movie = require('../models/Movie.model');
+
 /* GET home page */
 router.get('/', (req, res, next) => res.render('index'));
+
+
+router.get('/movies', (req, res,next) => {
+    Movie.find()
+      // -> allTheBooksFromDB is a placeholder, it can be any word
+      // |
+      .then(allTheMoviesFromDB => {
+        res.render("movies",{movie:allTheMoviesFromDB});
+    })
+      .catch(error => {
+        console.log('Error while getting the books from the DB: ', error);
+   
+        // Call the error-middleware to display the error page to the user
+        next(error);
+      });
+  });
+
+  router.get('/movies/:idMovie',(req,res,next)=>{
+    const idMovie = req.params.idMovie;
+    Movie.findOne({_id:idMovie}).then(theMovie=>{
+      console.log(theMovie);
+      res.render("movie-detail",{movie:theMovie});
+    }).catch(err=>console.log(err));
+
+  });
 
 module.exports = router;
