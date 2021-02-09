7a4e4d2e9231bab52b712afb290a4baf14281b8b
fernando <fernom95@gmail.com>
2021-02-08 18:17:41 -0600

    se finit

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,28 @@
 const express = require('express');
 const router = express.Router();
+const Movie = require('../models/Movie.model.js')
 
 /* GET home page */
 router.get('/', (req, res, next) => res.render('index'));
 
+router.get('/movies', async (req, res, next) => {
+    Movie.find({})
+    .then((movies) => {
+        console.log(movies)
+        res.render('movies' , {peliculas : movies}) 
+    })
+});
+
+router.get('/movies/:id', async (req, res, next) => {
+    const movieId =  req.params.id
+    Movie.findById(movieId)
+    .then((movies) => {
+        console.log(movies)
+        res.render("movie-info", {info : movies})
+    })
+
+});
+
+
 module.exports = router;
+
