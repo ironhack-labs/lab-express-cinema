c9a39493e9bfc15ac026f391b2b2ab0b5adb0b21
Sotiria Dialeti <soti.dialeti@gmail.com>
2021-02-09 19:35:21 +0100

    Finished

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,36 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
+const Movie = require("./../models/Movie.model");
 
 /* GET home page */
+
 router.get('/', (req, res, next) => res.render('index'));
+
+
+
+router.get("/movies", (req, res, next) => {
+  Movie.find()
+    .then((allMovies) => {
+      const data = {
+        movies: allMovies,
+      };
+      res.render("movies", data);
+    })
+    .catch((error) => {
+      console.log(error);
+      next(error);
+    });
+});
+
+router.get("/:id", (req, res, next) => {
+  Movie.findById(req.params.id)
+    .then((foundMovie) => {
+      res.render("details", {foundMovie});
+    })
+    .catch((error) => {
+      console.log(error);
+      next(error);
+    });
+});
 
 module.exports = router;
