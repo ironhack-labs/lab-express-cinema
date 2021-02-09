9c1d3357f5f36600e385a25a770f00ed17f7802a
Diego Barrera <di_barrera@hotmail.com>
2021-02-09 08:23:53 -0600

    not finished yet

--- app.js
+++ app.js
@@ -38,4 +38,5 @@ app.locals.title = 'Express - Generated with IronGenerator';
 const index = require('./routes/index');
 app.use('/', index);
 
+// app.listen(3000, () => console.log('Conected to port 3000'))
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,28 @@
 const express = require('express');
 const router = express.Router();
+const Movie = require('../models/Movie.model');
 
 /* GET home page */
-router.get('/', (req, res, next) => res.render('index'));
+router.get('/', (req, res, next) => {
+    res.render('index');
+})
+
+router.get('/movies', (req, res, next) => {
+    Movie
+        .find()
+        .then(allMovies => {
+            res.render('movies', { movies: allMovies })
+        })
+        .catch(error => console.log(error))
+})
+
+router.get('/movies/:movieID', (req, res, next) => {
+    Movie
+        .findById(req.params.movieID)
+        .then(oneMovie => {
+            res.render('movie-details', { oneMovie })
+        })
+        .catch(error => console.log(error))
+})
 
 module.exports = router;
