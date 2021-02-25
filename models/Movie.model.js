02aa08d694ed7fc29bc12bbe9e0a25c265da5508
Paul Parant <paul.parant@gmail.com>
2021-02-25 18:20:40 +0100

    iteration 2

---
+++ models/Movie.model.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+
+const movies = new Schema({
+  title: String,
+  director: String,
+  stars: Array,
+  image: String,
+  description: String,
+  showtimes: Array,
+}
+);
+
+
+
+const MovieModel = mongoose.model("Movie.model", Schema);
+
+module.exports = MovieModel;
