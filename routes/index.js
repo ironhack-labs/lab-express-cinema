af2f2c21abe21a68883b7b54cde20bf950fc6100
Mackenzie Minch <madameminch@gmail.com>
2021-02-25 22:22:14 +0100

    first draft

--- routes/index.js
+++ routes/index.js
@@ -5,3 +5,6 @@ const router = express.Router();
 router.get('/', (req, res, next) => res.render('index'));
 
 module.exports = router;
+
+
+
---
+++ routes/movies.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const router = express.Router();
+const MovieModel = require("./../model/Movie.model");
+
+router.get("/", (req, res, next) => {
+    
+    MovieModel.find()
+      .then((movieDatabase) => {
+        res.render("movies", {
+          list: movieDatabase,
+        });
+      })
+      .catch((dbError) => {
+        next(dbError);
+      });
+  });
+  
+  
+   router.get("/:id", (req, res, next) => {
+    console.log(req.params.id); 
+    
+    MovieModel.findById(req.params.id)
+      .then((movieDatabase) => {
+        res.render("movies", {
+          movies: movieDatabase,
+        });
+      })
+      .catch((dbError) => {
+        next(dbError);
+      });
+  });
+  
+  router.get("/as-json", async (req, res, next) => {
+    try {
+      const movies = await MovieModel.find();
+      res.json(movies);
+    } catch (dbError) {
+      next(dbError); 
+    }
+  });
+  
+  module.exports = router;
