c7c80ec59887979c691b6dcfe4786af90ec9dba0
maudfabrello <ma.fabrello@gmail.com>
2021-02-25 19:29:27 +0100

    maudfabrello lab-express-cinema

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,19 @@
 const express = require('express');
 const router = express.Router();
 
+const MovieModel=require("../models/Movie.model")
+
 /* GET home page */
 router.get('/', (req, res, next) => res.render('index'));
+/* router.get('/views/movies.hbs', (req, res, next) => res.render('movies.hbs')); */
+
+router.get("/movies/:id", (request, response, next) => {
+moviesDocument.findById(request.params.id)
+.then((moviesDocument => {
+    res.render("movies.hbs", { Movie: moviesDocument });
+})
+.catch((error) => {
+    console.log(error);
+});
 
 module.exports = router;
