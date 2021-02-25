e17a333645f8cdb0dc7b9500af7385dc3005e4b8
Anna Lefour <anna.lefour@laposte.net>
2021-02-25 23:50:57 +0100

    It 3 can't connect the movies route

--- routes/movies.js
+++ routes/movies.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-const movies = require('../bin/seeds') 
+const movies = require('../models/movie.model') 
 
 
 router.get('/movies', (req, res) =>  res.render("movies"));
