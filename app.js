4216b0eabec5f96fe6e7f0ff1ed39dffc081b458
Rowan Williams <rmtwilliams@hotmail.co.uk>
2021-02-25 22:44:50 +0100

    done

--- app.js
+++ app.js
@@ -33,9 +33,14 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
 
 // default value for title local
-app.locals.title = 'Express - Generated with IronGenerator';
+// app.locals.title = 'Express - Generated with IronGenerator';
+
+
+
 
 const index = require('./routes/index');
 app.use('/', index);
+app.use('/movies', index);
+app.use('/moviesDetails', index);
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,37 @@
 const express = require('express');
 const router = express.Router();
+const Movies = require('../models/Movie.model.js');
+
 
 /* GET home page */
 router.get('/', (req, res, next) => res.render('index'));
 
+// get the movies page
+
+router.get('/movies', (req, res, next) =>{
+   
+    Movies.find()
+    .then(movies => {
+        res.render("movies", {
+            list: movies
+        })
+    })
+    .catch(error=> {console.log(error)})
+
+})
+
+//  get the links id page
+
+router.get('/movies/:id', (req, res, next) =>{
+    Movies.findById(req.params.id)
+    .then((movie) => {
+        res.render("movieDetails", {details: movie})
+    })
+    .catch((error) => {
+        console.log(error)
+    })
+})
+
 module.exports = router;
+
+
