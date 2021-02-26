64d072cbf3a10f49d6fa9171d6661a93281b746b
Anna Lefour <anna.lefour@laposte.net>
2021-02-26 11:02:36 +0100

    CSS correction

--- routes/movies.js
+++ routes/movies.js
@@ -14,4 +14,14 @@ router.get('/movies', function (req, res, next) {
     .catch((err) => console.log(err))
 });
 
+router.get('/movies', function (req, res, next) {
+    console.log(req.params);
+    MoviesModel.find()
+    .then((dbRes) => {
+        console.log(dbRes);
+        res.render("movies.hbs", {movie: dbRes})
+    })
+    .catch((err) => console.log(err))
+});
+
 module.exports = router;
