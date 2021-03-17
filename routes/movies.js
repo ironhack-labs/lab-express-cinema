eec55342f81678eef49b150e081431e00cbcb622
Paula Nicioli <paulanicioli@gmail.com>
2021-03-16 22:24:27 -0300

    Iteration 4

--- routes/movies.js
+++ routes/movies.js
@@ -11,4 +11,12 @@ router.get('/', (req, res, next) => {
   });
 });
 
+/* GET movies detail page */
+router.get('/:movieId', (req, res, next) => {
+  Movie.findById(req.params.movieId).then((movie) => {
+    console.log('Detail movie successfully extracted');
+    res.render('movie-detail', { movie });
+  });
+});
+
 module.exports = router;
