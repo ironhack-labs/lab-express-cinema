fded33e6e837ad5a1ed53b519924dd057d38547c
Paula Nicioli <paulanicioli@gmail.com>
2021-03-16 21:35:19 -0300

    Iteration 2

--- app.js
+++ app.js
@@ -35,6 +35,11 @@ app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
 // default value for title local
 app.locals.title = 'Express - Generated with IronGenerator';
 
+// Listening to a locals port
+app.listen(3000, () => {
+  console.log('Connected to port 3000');
+});
+
 const index = require('./routes/index');
 app.use('/', index);
 
