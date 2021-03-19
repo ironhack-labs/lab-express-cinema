2aa3011bdcd84142d60322af7d42ac0083be45e6
michaloumen <michelle.lmendonca@gmail.com>
2021-03-18 22:22:53 -0300

    [WD-PT-SAO-01-2021] Michelle Mendonça

---
+++ models/Movie.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+
+const { Schema } = mongoose;
+const moviesSchema = new Schema({
+  title: { type: String, required: true },
+  director: { type: String, required: true },
+  stars: [{ type: String }],
+  image: { type: String, required: true },
+  description: { type: String },
+  showtimes: [{ type: String }],
+});
+
+const Movies = mongoose.model("movie", moviesSchema);
+
+module.exports = Movies;
--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,29 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
+const Movies = require("../models/Movie");
 
 /* GET home page */
-router.get('/', (req, res, next) => res.render('index'));
+router.get("/", (req, res, next) => res.render("index"));
+
+router.get("/movies", async (req, res) => {
+  try {
+    const movies = await Movies.find();
+    res.render("movies", { movies });
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+router.get("/movie/:movieId", async (req, res) => {
+  try {
+    const { movieId } = req.params;
+
+    const movie = await Movies.findById(movieId);
+
+    res.render("movie-detail", movie);
+  } catch (error) {
+    console.log(error);
+  }
+});
 
 module.exports = router;
