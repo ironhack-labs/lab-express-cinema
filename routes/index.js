9041b1acc77950c5a96e6d5b51b48d07bfb2ae07
Tomsylva <Tomsylvanz@gmail.com>
2021-03-19 13:35:55 +0100

    godamn it

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,15 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
+const Movie = require("../models/Movie.model");
 
 /* GET home page */
-router.get('/', (req, res, next) => res.render('index'));
+router.get("/", (req, res, next) => res.render("index"));
+
+router.get("/movies", (req, res) => {
+  Movie.find({}).then((allMovies) => {
+    console.log("Movies found: ", allMovies);
+    res.render("movies", { allMovies: allMovies });
+  });
+});
 
 module.exports = router;
