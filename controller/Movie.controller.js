37824da22010709e615d3723321f75943b78f98c
mmanso <mayara.manso@mercadolivre.com>
2021-03-20 11:58:44 -0300

    refactor error console, creates get and post newMovie

--- controller/Movie.controller.js
+++ controller/Movie.controller.js
@@ -1,14 +1,13 @@
 const MovieModels = require('../Model/Movie.model');
 
-
 class MovieController {
   static async getAllMovies(req, res) {
     try {
       const results = await MovieModels.find();
 
-      res.send(results);
+      res.render('movies', { movies: results });
     } catch (error) {
-      console.log({ error })
+      console.log({ errorGetAllMovies: error })
     }
   }
 
@@ -20,9 +19,39 @@ class MovieController {
       console.log(results);
       res.send(results);
     } catch (error) {
-      console.log({ error })
+      console.log({ errorGetInfoMovieById: error })
+    }
+  }
+
+  static async getNewMovie(req, res) {
+    res.render('formMovie')
+  }
+  static async postNewMovie(req, res) {
+    const {
+      movieTitle,
+      movieDirector,
+      movieStar,
+      movieImage,
+      movieDescription,
+      movieShowTimes
+    } = req.body;
+
+    const newMovie = {
+      title: movieTitle,
+      director: movieDirector,
+      start: movieStar,
+      image: movieImage,
+      description: movieDescription,
+      showtimes: movieShowTimes
     }
 
+    try {
+      await MovieModels.create(newMovie);
+
+      res.redirect('/movies')
+    } catch (error) {
+      console.log({ errorPostNewMovie: error })
+    }
   }
 }
 
