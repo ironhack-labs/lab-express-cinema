2685b31d25f3a93951c0c46b5e1dae4f0f61c07d
mmanso <mayara.manso@mercadolivre.com>
2021-03-20 11:58:01 -0300

    creates routs to get form and post new movie

--- routes/movie.routes.js
+++ routes/movie.routes.js
@@ -6,7 +6,9 @@ const MovieController = require('../controller/Movie.controller');
 /* GET home page */
 router.get('/', (req, res, next) => res.render('index'));
 
-router.get('/movies', MovieController.getAllMovies)
-router.get('/movie/:movieId', MovieController.getInfoMovieById)
+router.get('/movies', MovieController.getAllMovies);
+router.get('/movie/:movieId', MovieController.getInfoMovieById);
+router.get('/newMovie', MovieController.getNewMovie);
+router.post('/newMovie', MovieController.postNewMovie);
 
 module.exports = router;
