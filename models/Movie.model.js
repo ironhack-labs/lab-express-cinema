5cd6e1971999061e47a892fa9d7a1e80e21b60cf
jahfunk <xknottykidx@gmail.com>
2021-03-21 20:48:44 +0100

    done

---
+++ models/Movie.model.js
@@ -0,0 +1,16 @@
+// const Movies = require("../bin/seeds");
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const movieSchema = new Schema({
+  title: String,
+  director: String,
+  stars: [String],
+  image: String,
+  description: String,
+  showtimes: [String],
+});
+
+const Movies = mongoose.model("Movies", movieSchema);
+
+module.exports = Movies;
--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,24 @@
-const express = require('express');
+const express = require("express");
+const Movies = require("../models/Movie.model");
+const info = require("../bin/seeds");
 const router = express.Router();
 
 /* GET home page */
-router.get('/', (req, res, next) => res.render('index'));
+router.get("/", (req, res, next) => res.render("index"));
 
+router.get("/movies", (req, res) => {
+  Movies.find({}).then((allMovies) => {
+    res.render("movies", { movies: allMovies });
+  });
+  router.get("/info/:id", (req, res) => {
+    Movies.findOne({ _id: req.params.id }).then((info) => {
+      console.log();
+
+      res.render("info", { info });
+    });
+  });
+  // .catch((err) => {
+  //   console.log("error while getting the movies", err);
+  // });
+});
 module.exports = router;
