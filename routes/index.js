ed0161a4ec89bbe7391736ae7d7cb76557497346
Keith <keithrichardmcgowan@gmail.com>
2021-03-22 18:06:12 -0400

    The web page and routes load, and I can transfer info from my code to MongoDB.  However, I can't get the info from MongoDB to display on the website.

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,32 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
+const Movie = require("../models/Movie.model");
+
 /* GET home page */
-router.get('/', (req, res, next) => res.render('index'));
+router.get("/", (req, res, next) => res.render("index"));
+
+router.get("/movies", (req, res, next) => {
+  Movie.find()
+    .then((allTheMoviesFromDB) => {
+      //   console.log('Retrieved movies from DB: ', allTheMoviesFromDB);
+      res.render("movies", { movies: allTheMoviesFromDB });
+    })
+    .catch((error) => {
+      console.log("Error while getting the movies from the DB: ", error);
+      next(error);
+    });
+});
+
+router.get("/movies/:movieId", (req, res) => {
+  const { movieId } = req.params;
+
+  Movie.findById(movieId)
+    .then((theMovie) => res.render("movie-details", { movie: theMovie }))
+    .catch((error) => {
+      console.log("Error while retrieving movie details: ", error);
+      next(error);
+    });
+});
 
 module.exports = router;
