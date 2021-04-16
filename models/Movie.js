6d470761313d60ef68cab575cd633465b980f321
German DG <germandg6@gmail.com>
2021-04-16 19:52:13 +0100

    all completed, styles are missing

---
+++ models/Movie.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const movieSchema = new Schema({
+    title:{type:String},
+    director:{type:String},
+    stars: [String],
+    image: {type:String},
+    description: {type:String},
+    showtimes: [String],
+})
+
+
+const Movie = mongoose.model('Movie',movieSchema)
+
+module.exports = Movie
--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,32 @@
 const express = require('express');
 const router = express.Router();
 
+const Movie = require('../models/Movie.js')
+
 /* GET home page */
 router.get('/', (req, res, next) => res.render('index'));
+
+router.get('/movies',(req,res)=>{
+    Movie.find({})
+    .then((result)=>{
+    res.render('movies',{movie:result})
+    })
+    .catch((error)=>{
+    console.log(error)
+    })
+})
+
+// router.get('/movie/:_id',(req,res)=>{
+//     console.log(req.params._id)
+// })
+router.get(`/movieInfo/:_id`,(req,res)=>{
+    Movie.findById(req.params)
+      .then((result)=>{
+        res.render('movieInfo', {movie:result})
+      })
+      .catch((err)=>{
+        console.log(err)
+      })
+  })
 
 module.exports = router;
