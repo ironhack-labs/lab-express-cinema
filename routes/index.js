93633c9603941eafc2cd08983f3c9a20250da02c
silvia rodriguez <silrodcar@gmail.com>
2021-04-19 18:38:40 +0200

    iteration 4 done but not loading the see-more page

--- routes/index.js
+++ routes/index.js
@@ -8,6 +8,7 @@ const movie = require(`../models/Movie.model`)
 
 /* GET home page */
 router.get('/', (req, res, next) => res.render('index'));
+
 router.get('/movies', (req, res) => {
     movie.find({})
     .then((result) => {
@@ -17,5 +18,21 @@ router.get('/movies', (req, res) => {
         console.log(err); 
     }); 
     })
+
+ 
+
+router.get('/movies/:movieId', (req, res) => {
+    const { movieId } = req.params;
+   
+    Book.findOne(movieId)
+      .then(theMovie => res.render('see-more', { movie: theMovie }))
+      .catch(error => {
+        console.log('Error while retrieving movie details: ', error);
+   
+        next(error);
+      });
+  });
+
+
 
 module.exports = router;
