a5b1cd3ac7eda0f29165727c71941649de1ced69
Silinde87 <paurodriguezmolina@gmail.com>
2021-04-21 10:08:19 +0200

    Done Iteration 4

--- routes/index.js
+++ routes/index.js
@@ -13,4 +13,14 @@ router.get('/movies', (req, res, next) => {
         .catch((err) => console.error(err));
 })
 
+router.get('/movies/:id', (req, res, next) => {
+    const { id } = req.params;
+    Movie.findById(id)
+        .then((movie) => {
+            console.log(movie);
+            res.render('movies/show-movie', movie);
+        })
+        .catch((err) => console.error(err));
+})
+
 module.exports = router;
