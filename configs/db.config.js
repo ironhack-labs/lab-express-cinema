3d52e1db2d4efb43723a11e6bb68d1684b98e14c
Luciano Colabraro <luciano.colabraro@gmail.com>
2021-05-01 17:04:28 +0200

    done without style

--- configs/db.config.js
+++ configs/db.config.js
@@ -1,12 +1,12 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 mongoose
-  .connect('mongodb://localhost/express-cinema-dev', {
+  .connect("mongodb://localhost/express-cinema-dev", {
     useCreateIndex: true,
     useNewUrlParser: true,
-    useUnifiedTopology: true
+    useUnifiedTopology: true,
   })
-  .then(x =>
+  .then((x) =>
     console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
   )
-  .catch(err => console.error('Error connecting to mongo', err));
+  .catch((err) => console.error("Error connecting to mongo", err));
--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,30 @@
-const express = require('express');
+const express = require("express");
+const Movie = require("../models/Movie.model");
 const router = express.Router();
 
 /* GET home page */
-router.get('/', (req, res, next) => res.render('index'));
+router.get("/", (req, res, next) => res.render("index"));
 
+router.get("/movies", (req, res, next) => {
+  Movie.find({})
+    .then((moviesToShow) => {
+      console.log("Movies Showed", moviesToShow);
+      res.render("movies", { movies: moviesToShow });
+    })
+    .catch((error) => {
+      console.log("No movies found", error);
+    });
+});
+
+router.get("/movie/:id", (req, res, next) => {
+  const { id } = req.params;
+  Movie.findById(id)
+    .then((moviebyId) => {
+      console.log("Movie Chosen", moviebyId);
+      res.render("movie-details", { movieDeatails: moviebyId });
+    })
+    .catch((error) => {
+      console.log("Error while showing movie details", error);
+    });
+});
 module.exports = router;
