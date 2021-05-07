316ef939358869604793d58bdf56bb2be9da8428
EstherMS93 <esthermartinez@LAPTOP-BKUFCM5Q.localdomain>
2021-05-07 14:27:38 +0200

    done

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,33 @@
 const express = require('express');
 const router = express.Router();
 
+const movieModel = require("./../models/Movie.model")
 /* GET home page */
 router.get('/', (req, res, next) => res.render('index'));
+
+router.get("/movies", (req, res, next) => {
+    movieModel.find()
+    .then((dbResult) => {
+        res.render('movies', {
+            movies: dbResult,
+        });
+    }).catch(error => next(error));
+});
+
+router.get("/movies/:id", (req, res, next) => {
+movieModel.findById(req.params.id)
+    .then((dbResult) => {
+        res.render("details", {
+            oneMovie: dbResult,
+    });
+}).catch(error => next(error));
+});
+
+router.get("/movies/delete/:id", (req, res, next) => {
+    movieModel.findByIdAndDelete(req.params.id)
+        .then((dbResult) => {
+            res.redirect("/movies");
+    }).catch(error => next(error));
+});
 
 module.exports = router;
