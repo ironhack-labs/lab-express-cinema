d452474f6e829dff6b5ff25b977cc01c6e9dad7c
Brian Brenes <bbrenes14@gmail.com>
2021-05-11 00:11:09 -0500

    Done

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,40 @@
 const express = require('express');
 const router = express.Router();
+const Movie = require('../models/Movie.model.js'); 
 
 /* GET home page */
 router.get('/', (req, res, next) => res.render('index'));
 
+/* GET MOVIES */
+router.get('/movies', (req, res, next) => {
+    Movie.find()
+      .then(allTheMoviesFromDB => {
+        // console.log('Retrieved books from DB:', allTheBooksFromDB);
+   
+        // res.render('books-list.hbs', {books: allTheBooksFromDB}) // extension .hbs is not mandatory so we can omit it
+        res.render('movies', { movies: allTheMoviesFromDB }); // pass `allTheBooksFromDB` to the view (as `books`)
+      })
+      .catch(error => {
+        console.log('Error while getting the movies from the DB: ', error);
+   
+        // Call the error-middleware to display the error page to the user
+        next(error);
+      });
+  });
+
+router.get('/movies/:movieId', (req, res) => {
+  const { movieId } = req.params;
+ 
+  Movie.findById(movieId)
+    .then(theMovie => res.render('movie-details', { movie: theMovie }))
+    .catch(error => {
+      console.log('Error while retrieving movie details: ', error);
+ 
+      // Call the error-middleware to display the error page to the user
+      next(error);
+    });
+});
+
 module.exports = router;
+
+
