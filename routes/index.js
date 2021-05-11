35a58c327a24e3ed046b6057c20c874204369c79
fereber1294 <freyesber@gmail.com>
2021-05-11 00:13:20 -0500

    lab done

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,36 @@
 const express = require('express');
+const Movie = require('../models/Movie.model');
 const router = express.Router();
 
 /* GET home page */
 router.get('/', (req, res, next) => res.render('index'));
+
+
+//BUSCAR TODAS LAS PELÍCULAS
+router.get('/movies', (req, res) => {
+  Movie.find()
+      .then(moviesFromDb => {
+          console.log('movies:', moviesFromDb)
+          res.render('movies',{ movies: moviesFromDb})
+      })
+      .catch(e => {
+          console.log('Error while getting the books from DB',e)
+          next(e)
+      })
+})
+
+//BUSCAR POR ID 
+router.get('/movies/:movieId', (req,res) => {
+  const {movieId} = req.params;
+  Movie.findById(movieId)
+    .then(theMovie => res.render('movie-details', {movie: theMovie}))
+    .catch(e => {
+      
+      console.log('Error while retrieving movie details: ',e)
+      next(e)
+    })
+
+})
+
 
 module.exports = router;
