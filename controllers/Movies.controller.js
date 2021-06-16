625a18df3e31d3f8dc5b7e74152b878c9d856fd5
Israel Zambrana <zambrana90@gmail.com>
2021-06-16 22:33:41 +0200

    till iteration 3

---
+++ controllers/Movies.controller.js
@@ -0,0 +1,19 @@
+const Movie = require("../models/Movie.model");
+
+module.exports.home = (req, res, next) => {
+  res.render("index").catch((e) => console.error(e));
+};
+
+module.exports.listMovies = (req, res, next) => {
+  Movie.find()
+    .then((movies) => {
+      res.render("movies", { movies });
+    })
+    .catch((e) => console.error(e));
+};
+
+module.exports.getMovie = (req, res, next) => {
+  Movie.findById(req.params.id)
+    .then((movie) => res.render("movie", { movie }))
+    .catch((e) => console.error(e));
+};
--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,10 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
+const moviesController = require('../controllers/movies.controller')
 
 /* GET home page */
-router.get('/', (req, res, next) => res.render('index'));
+router.get('/', moviesController.home);
+router.get("/movies", moviesController.listMovies);
+router.get("/movies/:id", moviesController.getMovie);
 
 module.exports = router;
