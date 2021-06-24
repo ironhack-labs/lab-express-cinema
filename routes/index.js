f42b2a986c9a6e6736327d2174681eb459b2846e
CarolinaVitale <vitale.carola@gmail.com>
2021-06-25 00:06:08 +0200

    done

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,18 @@
-const express = require('express');
-const router = express.Router();
+const express = require('express')
+const router = express.Router()
 
-/* GET home page */
-router.get('/', (req, res, next) => res.render('index'));
+router.get('/', (req, res, ) => res.render('index'))
 
-module.exports = router;
+router.get('/movies', (req, res, next) => {
+    Movie.find()
+       
+        .then(allTheMoviesFromDB => {
+            console.log('Retrieved movies from DB:', allTheMoviesFromDB)
+            console.log(allTheMoviesFromDB)
+            res.render('movies', {movies : allTheMoviesFromDB})
+        })
+        .catch(error => console.log('Error while getting the movies from the DB: ', error))
+    
+});
+const Movie = require('../models/Movie.model');
+module.exports = router
