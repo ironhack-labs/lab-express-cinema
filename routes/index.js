0c78f85e847af153a431ad6afc1280df6ea5f371
Nicholas Zanotto <nicholas.zanotto@hotmail.com>
2021-06-24 20:18:28 +0200

    [WEB-MAD DE] Nicholas Zanotto

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,16 @@
 const express = require('express');
 const router = express.Router();
+const Movie = require('../model/movie.model')
 
 /* GET home page */
 router.get('/', (req, res, next) => res.render('index'));
 
+router.get('/movies', (req, res,) => {
+	Movie
+        .find()
+		.then((movies) => res.render('movies', {movies : movies }))
+		.catch((err) => console.log(err));
+});
+
 module.exports = router;
+
