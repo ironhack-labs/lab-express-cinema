1400cb62ba38e2a85e3d9c502b39b5a1c76117b9
Enrique VOZA <enriquevozmediano@yahoo.es>
2021-06-24 23:52:25 +0200

    WDFT0521 ENRIQUE VOZMEDIANO

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,18 @@
 const express = require('express');
 const router = express.Router();
 
+
+const Movie = require('../models/Movie.model')
 /* GET home page */
 router.get('/', (req, res, next) => res.render('index'));
+
+router.get('/movies/listado', (req, res) => {
+
+    Movie
+        .find()
+        .select('title')
+        .then(movies => res.render('movies-list', { movies }))
+        .catch(err => console.log(err))
+})
 
 module.exports = router;
