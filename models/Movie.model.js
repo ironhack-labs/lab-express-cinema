b1a2333658eeaf54c8531fa20e83c74568209a3e
Pablo Molleda <pmolledadelrio@gmail.com>
2021-06-24 21:13:41 +0200

    lab done

---
+++ models/Movie.model.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose")
+
+const moviesSchema = new mongoose.Schema({
+    title: String,
+    director: String,
+    stars: [String],
+    image: String,
+    description: String,
+    showtimes: [String]
+}, {
+    timestamps: true
+})
+
+const Movie = mongoose.model("Movie", moviesSchema)
+
+module.exports = Movie
--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,31 @@
 const express = require('express');
 const router = express.Router();
 
+require("dotenv/config")
+
+
+const Movie = require('./../models/Movie.model')
 /* GET home page */
 router.get('/', (req, res, next) => res.render('index'));
+
+router.get('/movies', (req, res) => {
+
+  Movie
+    .find()
+    .then(movies => res.render('movies-list', { movies }))
+    .catch(err => console.log('UPS, hay un error cargando la lista de pelis', err))
+})
+
+router.get('/movies/details/:movies_id', (req, res) => {
+
+  const { movies_id } = req.params
+
+  Movie
+    .findById(movies_id)
+    .then(movie => res.render('movies-details', movie))
+    .catch(err => console.log(err))
+})
+
+
 
 module.exports = router;
