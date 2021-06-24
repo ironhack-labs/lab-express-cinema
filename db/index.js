066a014289549175ed18d1bed7f4b70163213b96
juanfuerteshdz@gmail.com <juanfuerteshdz@gmail.com>
2021-06-25 01:23:48 +0200

    m

--- db/index.js
+++ db/index.js
@@ -16,6 +16,7 @@ mongoose
   })
   .then((x) => {
     console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`);
+
   })
   .catch((err) => {
     console.error("Error connecting to mongo: ", err);
