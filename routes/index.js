26a4847e791c9fc7c2f10ba598cba7f4b8d72334
dechilds1@gmail.com <dechilds1@gmail.com>
2021-06-28 23:29:23 +0200

    Completed Lab Express CInema

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,25 @@
 const express = require('express');
 const router = express.Router();
 
-/* GET home page */
+const Movies = require('../models/Movie.model');
+
+
 router.get('/', (req, res, next) => res.render('index'));
+
+
+router.get('/movies', (req, res, next) => {
+    Movies.find()
+    .then((moviesList) => res.render('movies', {moviesList}))
+    .catch(err => console.log(err))
+});
+
+router.get('/movie/:id', (req, res, next) => {
+    const id = req.params.id
+
+    Movies.findById(id)
+    .then((selectedMovie) => res.render('movie-details', selectedMovie))
+    .catch(err => console.log(err))
+});
+
 
 module.exports = router;
