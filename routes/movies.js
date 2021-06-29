7406d7506dd272d5f2d18d9dfc4c81401740b52c
Irina <ifalko@protonmail.com>
2021-06-29 15:00:19 +0200

    last iteration done

--- routes/movies.js
+++ routes/movies.js
@@ -5,11 +5,24 @@ const Movies = require("../models/Movie.model");
 router.get("/", (req, res, next) => {
   Movies.find()
     .then((movies) => {
-      console.log(movies);
       res.render("movies", { movies });
     })
     .catch((err) => {
       next(err);
+    });
+});
+
+router.get("/:id", (req, res, next) => {
+  console.log(req.params.id);
+  const movieId = req.params.id;
+
+  Movies.findById(movieId)
+    .then((movieFromDB) => {
+      console.log(movieFromDB);
+      res.render("movie", { movieFromDB });
+    })
+    .catch((err) => {
+      console.log(err);
     });
 });
 
