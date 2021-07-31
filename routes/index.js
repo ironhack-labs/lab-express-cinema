b6e1616a68303cdf73c810a8608902b5545d24c0
Hannes Polte <hannes.polte@gmx.de>
2021-07-31 15:38:14 +0200

    added all iterations

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,24 @@
 const express = require('express');
 const router = express.Router();
+const Movies= require('../models/Movie.model.js')
 
 /* GET home page */
 router.get('/', (req, res, next) => res.render('index'));
+
+router.get ('/movies', (req,res)=>{
+
+    Movies.find().then(allMoviesFromDb=>{
+        res.render ('movies',{movies:allMoviesFromDb})
+    })
+})
+
+router.get('/movies/:id',(req,res)=>{
+    const movieId = req.params.id;
+    Movies.findById(movieId).then(resMovie=>{
+        console.log(resMovie)
+        res.render ('movie-details',{movie:resMovie})
+        // {resMovie} === { resMovie: resMovie}
+    })
+})
 
 module.exports = router;
