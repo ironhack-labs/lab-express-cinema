d773915b67ec433c142ac060a384fe109e77aa0c
vloparevich <vadim.loparevich@gmail.com>
2021-08-19 20:44:15 -0700

    Implement func part of the Ironhack Cinema

--- app.js
+++ app.js
@@ -5,12 +5,7 @@ require('dotenv/config');
 // ℹ️ Connects to the database
 require('./db');
 
-// Handles http requests (express is node js framework)
-// https://www.npmjs.com/package/express
 const express = require('express');
-
-// Handles the handlebars
-// https://www.npmjs.com/package/hbs
 const hbs = require('hbs');
 
 const app = express();
@@ -20,13 +15,16 @@ require('./config')(app);
 
 // default value for title local
 const projectName = 'lab-express-cinema';
-const capitalized = string => string[0].toUpperCase() + string.slice(1).toLowerCase();
+const capitalized = (string) =>
+  string[0].toUpperCase() + string.slice(1).toLowerCase();
 
 app.locals.title = `${capitalized(projectName)}- Generated with Ironlauncher`;
-
 // 👇 Start handling routes here
 const index = require('./routes/index');
 app.use('/', index);
+
+const movies = require('./routes/movies.routes');
+app.use('/', movies);
 
 // ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
 require('./error-handling')(app);
---
+++ routes/movies.routes.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const router = express.Router();
+const Movie = require('../models/Movie.model');
+
+router.get('/movies', (req, res, next) => {
+  Movie.find()
+    .then((allMoviesFromDB) => {
+      // console.log(JSON.stringify(allMoviesFromDB, null, '\t'))
+      res.render('movies', { movies: allMoviesFromDB });
+    })
+    .catch((err) => {
+      console.log('Movies were not retrieved from the DB', err);
+    });
+});
+
+router.get('/movies/:movieId', (req, res, next) => {
+  console.log('Movie id', req.params.movieId);
+  Movie.findById(req.params.movieId)
+    .then((movieFromDB) => {
+      // console.log(JSON.stringify(movieFromDB, null, '\t'));
+      res.render('movie-details', movieFromDB);
+    })
+    .catch((err) => {
+      console.log(`Movie was not found or smth went wrong: ${err}`);
+    });
+});
+
+module.exports = router;
