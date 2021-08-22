a56a7fa1d629230b2248c9c218d555fcddfc6de9
kpla001 <kpla001@fiu.edu>
2021-08-22 17:36:47 -0400

    complete iterations 1-4

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,24 @@
 const express = require('express');
 const router = express.Router();
+const Movie = require("../models/Movie.model");
 
 /* GET home page */
 router.get('/', (req, res, next) => res.render('index'));
+
+router.get('/movies', (req, res, next) => {
+    Movie.find()
+    .then((allMoviesFromDB) => {
+        res.render('movies', { movies: allMoviesFromDB })
+    })
+    .catch(err => console.log("An error occurred while retrieving your movies", err))
+});
+
+router.get('/movies/:id', (req, res, next) => {
+    Movie.findById(req.params.id)
+    .then(movieFromDB => {
+        res.render('movie-details', movieFromDB);
+    })
+    .catch(err => console.log("An error occurred while retrieving movie details", err))
+})
 
 module.exports = router;
