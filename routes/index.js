5fd903af7c021c4fa157a750ea09981efb14859f
Fede <fedemarinasruiz@gmail.com>
2021-09-02 21:30:04 +0200

    [MAD DE] Fede Mariñas

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,27 @@
 const express = require('express');
 const router = express.Router();
+const Movies = require('./../models/movie.model')
+
 
 /* GET home page */
 router.get('/', (req, res, next) => res.render('index'));
 
+router.get('/movies', (req, res) => {
+
+    Movies
+        .find()
+        .then(movie => res.render('movies', { movie }))
+        .catch(err => console.log(err))
+
+})
+
+
+router.get('/movies/details/:movie_id', (req, res) => {
+
+    const { movie_id } = req.params
+    Movies
+        .findById(movie_id)
+        .then(theMovie => res.render('details', theMovie))
+        .catch(err => console.log(err))
+})
 module.exports = router;
