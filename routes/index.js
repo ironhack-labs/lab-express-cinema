5190759a59f8e0566451d9cacb3f452cc0a6e0bd
Ivan Espinoza <icaro_19@hotmail.com>
2021-09-02 23:56:43 +0200

    finish work

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,28 @@
 const express = require('express');
 const router = express.Router();
+require('./../seed/movies.seed')
 
+const Movie = require('./../models/Movie.model')
 /* GET home page */
 router.get('/', (req, res, next) => res.render('index'));
 
+router.get('/movies', (req, res, next) => {
+    Movie
+        .find()
+        .then(movies => {
+            console.log(movies)
+            res.render('movies', { movies })
+        }).catch()
+});
+
+router.get('/movie/:id', (req, res, next) => {
+    let id = req.params
+    Movie
+        .findById(id.id)
+        .then((movie) => {
+            console.log(movie)
+            res.render('movie', { movie })
+        })
+        .catch()
+})
 module.exports = router;
