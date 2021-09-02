60650caf6b17ea6ab39af576b7a9c9cd7a444956
dany <dacr63@gmail.com>
2021-09-02 19:40:11 +0200

    lab done

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,30 @@
 const express = require('express');
 const router = express.Router();
 
+const Movie = require('../models/movie.model')
+
 /* GET home page */
 router.get('/', (req, res, next) => res.render('index'));
+
+
+router.get('/movies', (req, res) => {
+
+    Movie
+      .find()
+      .then(movies => res.render('movies', { movies }))
+      .catch(err => console.log(err))
+  })
+
+router.get('/movies/:id', (req, res) => {
+
+    const {id} = req.params;
+    console.log("LA ID ES", id);
+
+    Movie
+    .findById(id)
+    .then(theMovie => res.render('moviedetails', {movies: theMovie}))
+    .catch(err => console.log(err))
+})
+
 
 module.exports = router;
