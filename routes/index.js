ede12e625c46795b98c2ba6acd216fd0d9e3f07c
David Martinez <tinezillo_94@hotmail.com>
2021-09-02 20:59:55 +0200

    Iteration finished, start with styling

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,34 @@
 const express = require('express');
 const router = express.Router();
 
+const Movie = require('./../models/Movies.model')
+
+
 /* GET home page */
 router.get('/', (req, res, next) => res.render('index'));
+
+//listado de movies
+router.get('/movies', (req, res) => {
+    
+    Movie
+    .find()
+    .then(movies => res.render('movies', { movies }))
+    .catch(err => console.log(err))
+
+})
+
+//
+router.get('/movie/:movie_id', (req, res) => {
+
+    const { movie_id } = req.params
+
+    console.log("este es el id", movie_id)
+
+    Movie
+    .findById(movie_id)
+    .then(theMovie => res.render('movie-details', theMovie ))
+    .catch(err => console.log(err))
+    
+})
 
 module.exports = router;
