8019fffcac0971483e95a6f2f03d625b083cceb4
Alberto Massa <massa.alberto@outlook.com>
2021-09-02 21:36:28 +0200

    done

---
+++ models/Movie.model.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+
+const movieSchema = new mongoose.Schema({
+    title: String,
+    director: String,
+    stars: Array,
+    image: String,
+    description: String,
+    showtimes: Array
+}, {
+    timestamps: true
+})
+
+const Movie = mongoose.model("Movie", movieSchema);
+
+module.exports = Movie
--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,22 @@
 const express = require('express');
 const router = express.Router();
+const Movie = require('./../models/Movie.model')
 
-/* GET home page */
-router.get('/', (req, res, next) => res.render('index'));
+router.get('/', (req, res) => res.render('index'));
+
+router.use('/movies', (req, res) =>{
+    Movie
+        .find()
+        .then(movies => res.render('movies', {movies}) )
+        .catch(err => console.log(err))
+})
+
+router.use('/movie/:id', (req, res) => {
+    const {id} = req.params
+    Movie
+        .findById(id)
+        .then(theMovie => res.render('movie', theMovie))
+        .catch(err => console.log(err))
+})
 
 module.exports = router;
