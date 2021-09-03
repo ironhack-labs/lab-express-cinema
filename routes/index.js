c30ca086340be5c569b483691da9521c58e0e1e9
Juan De Santis <juanbds66@gmail.com>
2021-09-03 09:16:44 +0200

    [MAD DE] JUAN DE SANTIS

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,33 @@
 const express = require('express');
 const router = express.Router();
+require('./seed/movies.seed')
+
+const allMovies = require('../routes/models/movie.model')
+
+
+
+
+
+
+
+
 
 /* GET home page */
 router.get('/', (req, res, next) => res.render('index'));
+
+router.get('/movies', (req, res) => {
+
+  allMovies
+    .find()
+    .then(movie =>{
+        console.log('pelicuak')
+         res.render('movies', { movie })
+
+    })
+    .catch(err => console.log(err))
+
+})
+
+
 
 module.exports = router;
