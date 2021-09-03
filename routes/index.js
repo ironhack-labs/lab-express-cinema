6399ff1ed2f7b0b74747f18c6cb32a63262f03da
unknown <martarrubio@telefonica.net>
2021-09-03 09:09:25 +0200

    done

--- routes/index.js
+++ routes/index.js
@@ -4,4 +4,14 @@ const router = express.Router();
 /* GET home page */
 router.get('/', (req, res, next) => res.render('index'));
 
+
+
+Movie
+    .find()
+    .then(movies => res.render('movies/list', { movies }))
+    .catch(err => console.log(err))
+
+    const { movie_id } = req.params
+    console.log('NO ARRIESGO =====>', movie_id)
+
 module.exports = router;
