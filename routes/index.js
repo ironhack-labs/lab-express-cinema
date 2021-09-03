b9629b5aff66a6674f10238a58bd961686d4de01
Jesus-Navas <jesusnavas@outlook.com>
2021-09-03 09:19:45 +0200

    [MAD DE] JESUS NAVAS

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,22 @@
-const express = require('express');
+const express = require("express");
+const Movie = require("../models/Movie.model");
 const router = express.Router();
 
 /* GET home page */
-router.get('/', (req, res, next) => res.render('index'));
+router.get("/", (req, res, next) => res.render("index"));
+
+router.get("/movies", (req, res) => {
+  Movie.find().then((movies) => res.render("movies", { movies }));
+});
+
+router.get("/movies/:movies_id", (req, res) => {
+  const { movies_id } = req.params;
+
+  Movie.findById(movies_id)
+    .then((theMovies) => {
+      res.render("details", theMovies);
+    })
+    .catch((err) => console.log(err));
+});
 
 module.exports = router;
