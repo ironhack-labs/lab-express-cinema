dd3e7a71aa960fe8d6907af2cd8e4bdea35ccf78
AitorSantaeugenia <aitor.j.s.m@gmail.com>
2021-09-06 01:30:18 +0200

    iteration 3 completed

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,23 @@
 const express = require('express');
 const router = express.Router();
+const Movie = require('../models/Movie.model');
 
 /* GET home page */
 router.get('/', (req, res, next) => res.render('index'));
+
+router.use('/movies', (req, res) => {
+	Movie.find()
+		//.count()
+		.then((movies) => {
+			//console.log('Movies from DB:', JSON.stringify(movies));
+			//console.log('Movies from DB:', movies);
+			//console.log('Lenght of movies from the DB', movies);
+			res.render('movies', { movies });
+			//res.send(JSON.stringify(movies));
+		})
+		.catch((err) => {
+			console.log(err);
+		});
+});
 
 module.exports = router;
