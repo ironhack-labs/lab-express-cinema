30ef752fbf5ec81f8481a3b8f45238afc7bfb5f9
alejandro <ftvalex@hotmail.com>
2021-09-06 23:32:05 +0100

    commit 2
    estableciendo la ruta para movies
    ya añadidas las pelis a la BBDD

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,23 @@
 const express = require('express');
 const router = express.Router();
+const Movie = require('../models/Movie.model')
 
 /* GET home page */
 router.get('/', (req, res, next) => res.render('index'));
 
+router.get('/movies', (req, res, next) => {
+    Movie
+       .find()
+       .then(theMovies => {
+           console.log('Retrieved movies from DB:', theMovies);
 
+           res.render('movies.hbs', { movie: theMovies }); 
+         })
+         .catch(error => {
+           console.log('Error while getting the movies from the DB: ', error);
+
+           next(error);
+         });
+     });
 
 module.exports = router;
