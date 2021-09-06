8c06d72d7cf92d9a3fb778197d8e540ed7fbd4fd
Jaume Ballester <86907672+jaumebt90@users.noreply.github.com>
2021-09-06 20:45:03 +0200

    It. 1-2-3

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,19 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
+const Movie = require("../models/Movie.model.js");
 
 /* GET home page */
-router.get('/', (req, res, next) => res.render('index'));
+router.get("/", (req, res, next) => res.render("index"));
+
+router.get("/movies", (req, res, next) => {
+  Movie.find()
+    .then((movies) => {
+      //console.log("hola");
+      res.render("movies", { movies });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
 
 module.exports = router;
