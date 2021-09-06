d5f5e0d6e6ab6ba0e777b1260236a0eb175e3409
Adrian Fernandez Carmona <adrianfdezbcn@gmail.com>
2021-09-06 20:20:18 +0200

    WD082021-Adrian Fernandez
    Done, me faltan los stylings

---
+++ routes/movies.routes.js
@@ -0,0 +1,30 @@
+const router = require('express').Router();
+const Movie = require('../models/Movie.model');
+
+//Everytime we call the server with /movies
+router.get('/movies', (req, res, next)=>{
+    Movie.find()
+        .then(allTheMoviesFromDB => {
+            console.log('Movies from the DB:', allTheMoviesFromDB);
+            res.render('../views/movies.hbs', {movies: allTheMoviesFromDB})
+        })
+        .catch(error => {
+            console.log('Error while getting the movies from the DB: ', error);
+       
+            // Call the error-middleware to display the error page to the user
+            next(error);
+          });
+});
+router.get('/movies/:movieId', (req, res) =>{
+    const {movieId} = req.params;
+
+    Movie.findById(movieId)
+        .then(theMovie => res.render ('../views/movie-details.hbs', {movies: theMovie}))
+        .catch(error =>{
+            console.log('Error while retrieving movie details: ', error);
+        
+        next(error);
+        });
+});
+
+module.exports = router;
