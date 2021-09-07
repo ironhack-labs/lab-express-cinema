add36e24c528afbfc487472d464f84f3942e2903
simplePac <rober_alicante@hotmail.es>
2021-09-07 17:57:19 +0200

    WD08201
    Lab Finalizado. Falta CSS

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,35 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
+const Movie = require("../models/Movie.model");
+
 /* GET home page */
-router.get('/', (req, res, next) => res.render('index'));
+router.get("/", (req, res, next) => res.render("index"));
+
+router.get("/movies", (req, res) => {
+  Movie.find()
+    .then((movies) => {
+      // console.log("Películas recibidos desde la DB:", movies);
+
+      res.render("movies", { movies });
+    })
+
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
+router.get("/details/:movieId", (req, res) => {
+  const { movieId } = req.params;
+
+  Movie.findById(movieId)
+    .then((movie) => {
+      res.render("details", { movie });
+    })
+
+    .catch((err) => {
+      console.log(err);
+    });
+});
 
 module.exports = router;
