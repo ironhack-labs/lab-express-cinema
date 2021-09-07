c78297b0380c5ca4d88c7b7935618ee59c18f817
jdavesa <86001040+jdavesa@users.noreply.github.com>
2021-09-07 10:50:02 +0200

    Lab Completado

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,35 @@
 const express = require('express');
 const router = express.Router();
+const Movie = require('../models/Movie.model');
+
 
 /* GET home page */
 router.get('/', (req, res, next) => res.render('index'));
+
+router.get('/movies',(req, res, next)=> {
+    Movie.find()
+        .then(data =>{
+            /* console.log('the movies!!!!!!',data) */
+            const moviesInfo = data
+            res.render('movies',{moviesInfo})
+        })
+      
+
+        .catch(err => console.error(err))})
+  
+
+router.get('/movie/:movieId',(req,res)=>{
+    const movieID = req.params.movieId
+    Movie.findById(movieID)
+    .then(data=>{
+        console.log(data)
+        const movieInfo = data
+        res.render('movie',{movieInfo})
+
+    })
+
+    .catch(err => console.error(err))
+
+})
 
 module.exports = router;
