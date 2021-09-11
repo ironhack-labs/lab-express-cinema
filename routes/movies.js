258b558e01ec56cf578784fcfe642d69a63b4f65
Chris <christaylor269@gmail.com>
2021-09-11 16:24:33 +0200

    Finished without styles

---
+++ routes/movies.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const router = express.Router();
+const Movie = require("../models/Movie.model");
+
+router.get("/", (req, res, next) => {
+  Movie.find()
+    .then((moviesFromTheDB) => {
+      res.render("movies", { movies: moviesFromTheDB });
+    })
+    .catch((error) => {
+      console.log("Error, no movies for you ", error);
+
+      next(error);
+    });
+});
+
+router.get("/:id", (req, res, next) => {
+  Movie.findById(req.params.id)
+    .then((thisMovie) => {
+      res.render("movie", { movie: thisMovie });
+    })
+    .catch((error) => {
+      console.log("Error, no movies for you ", error);
+
+      // Call the error-middleware to display the error page to the user
+      next(error);
+    });
+});
+
+module.exports = router;
