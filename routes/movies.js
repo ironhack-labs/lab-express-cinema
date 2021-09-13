892e131d6c4612c82cbca14a14342803c01584e8
Noemi Radvan <radvanno@gmail.com>
2021-09-13 10:00:51 +0200

    Done, no styling

--- routes/movies.js
+++ routes/movies.js
@@ -1,13 +1,25 @@
 const express = require("express");
 const router = express.Router();
 const Movie = require("../models/Movie.model.js");
+
 router.get("/", (req, res, next) => {
   Movie.find()
     .then((allMoviesFromDataBase) => {
       res.render("movies", { movies: allMoviesFromDataBase });
     })
     .catch((error) => {
-      console.log("Error while getting the books from the DB:", error);
+      console.log("Error while getting the movies from the DB:", error);
+      next(error);
+    });
+});
+
+router.get("/:id", (req, res, next) => {
+  Movie.findById(req.params.id)
+    .then((oneMovie) => {
+      res.render("movie-details", { movies: oneMovie });
+    })
+    .catch((error) => {
+      console.log("Error while getting the movies from DB:", error);
       next(error);
     });
 });
