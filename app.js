bcd9bee1942879e839654066bcb220a4f108f6fd
Pablo Alonso Lorente <pablochux@me.com>
2021-09-13 20:45:31 +0200

    Iteration 4 done

--- app.js
+++ app.js
@@ -33,44 +33,6 @@ app.use("/", index);
 const movieRouter = require("./routes/movies");
 app.use("/movies", movieRouter);
 
-// 1. Make sure the users can arrive DONE
-//     - Create the route (usually on app.js), add a console.log() DONE
-// 2. Display something DONE
-//     - Create the view that will render (hbs file) DONE
-//     - Add some content to the view DONE
-// 3. Display the movies
-//     - Get movies data from the DB
-//     - Display them in hbs
-
-// //     - Get movies data from the DB
-// We have all of them in a mongo DB
-// -> We don't care the seed file, we need to look for the data on the DB
-// Query the DB
-// -> We have the data
-//
-// We display the data, movies.hbs
-
-// Seed file is only used once, then you don't use (from the moment that it runs, it's inmediately updated said god)
-
-// Since you are creating the router object, you only need to specify the URL when you use app.use, inside the movies.js folder, you don't need to specify any router on the get since it will be called when you use the app.use. In the movies.js you add all the urls that start from /movies, like /movies/matrix.
-
-// How does Routes work, we have a specific folder for them, for each parent route, we ne
-
-// When you create a router object with express.Router(), all the routes start from the app.use that you used to call that router
-
-// app.get("/movies", (req, res, next) => {
-//   console.log("VIEWING MOVIES");
-
-//   // Take all the data from the DB
-
-//   const movies = {};
-
-//   // Pass all the data into a view
-//   res.render("movies", {
-//     movies: movies,
-//   });
-// });
-
 // ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
 require("./error-handling")(app);
 
--- routes/movies.js
+++ routes/movies.js
@@ -29,4 +29,28 @@ router.get("/", (req, res, next) => {
   // We display the data, movies.hbs
 });
 
+// /movies
+router.get("/:id", (req, res, next) => {
+  console.log(req.params.id);
+
+  // We need to call the db (the db may fail)
+
+  // Query the DB
+  // -> We have the data
+
+  // Every time we query the database, is it SYNC OR ASYNC????
+  Movie.findById(req.params.id)
+    .then((resultFromTheDB) => {
+      res.render("movie-details", {
+        movie: resultFromTheDB,
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+
+  // mongoose doesn't need {} if you want to query the all the database
+  // We display the data, movies.hbs
+});
+
 module.exports = router;
