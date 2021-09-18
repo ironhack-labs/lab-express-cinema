4d39cc0ed964d1e6253bf533f560b765f892d07d
ErickRoo <erick.rod.ag@outlook.com>
2021-09-17 21:48:47 -0500

    done

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,13 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
+const movieController = require("../controllers/movieController");
+
 /* GET home page */
-router.get('/', (req, res, next) => res.render('index'));
+router.get("/", movieController.home);
+
+router.get("/movies", movieController.listMovies);
+
+router.get("/:movieID", movieController.movieById);
 
 module.exports = router;
