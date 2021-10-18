fa0619fe013bfff1648f8e7b5975c66f91b3a246
Nekruz Avgani <nekruz.avgani@gmail.com>
2021-10-18 23:40:37 +0100

    Itiration 4

--- routes/movieRoutes.js
+++ routes/movieRoutes.js
@@ -21,4 +21,17 @@ router.get('/movies', (req, res, next)=> {
 })
 
 
+router.get('/movies/:movieId', (req, res) => {
+   Movie.findById(req.params.movieId)
+   .then((details) => {
+      console.log('------->' + req.params);
+      res.render('movieDetails', details)
+   })
+   .catch((error) => {
+      console.log('An error occured, could not load movie details', error);
+      next(error);
+   });
+})
+
+
 module.exports = router;
