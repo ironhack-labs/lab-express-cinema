5d9a23af5629ec96be428117eac8e2f64b7b2166
Aintzane Morales <aintzanemorales1992@gmail.com>
2021-10-19 20:41:23 +0200

    Funcionality for DELETE film

--- routes/moviesRoute.js
+++ routes/moviesRoute.js
@@ -47,7 +47,6 @@ router.post("/movies/create", (req, res, next) => {
 router.get("/movies/:movieId",  (req, res, next)=>{
     Movie.findById(req.params.movieId)
     .then((movieFromDB) => {
-        // console.log(movieFromDB)
         res.render("movies/movies-details", movieFromDB)
     })
     .catch((err) => {
@@ -59,14 +58,13 @@ router.get("/movies/:movieId",  (req, res, next)=>{
 router.get("/movies/:movieId/edit", (req, res, next)=>{
     Movie.findById(req.params.movieId)
     .then((movieFromDBtoEdit) => {
-        //console.log(movieFromDBtoEdit)
         res.render("movies/movies-edit" , movieFromDBtoEdit)
     })
     .catch((err) => {
         console.log("ups, an error has been detected getting the details of a singel movie", err)
         next (err);
     })
-    
+ 
 });
 
 
@@ -74,19 +72,26 @@ router.post("/movies/:movieId/edit", (req, res, next) => {
 
     const {title, director, description, stars} = req.body;
 
-    
-    
     Movie.findByIdAndUpdate(req.params.movieId, req.body, {new : true})
-        .then( (movieFromDBtoEdit) => {
-            res.redirect("/movies/" + movieFromDBtoEdit._id);  
-        })
+    .then( (movieFromDBtoEdit) => {
+        res.redirect("/movies/" + movieFromDBtoEdit._id);  
+    })
 
-        .catch((err) => {
-            console.log("ups, an error has been detected displaying the details of a singel movie", err);
-            next (err);
+    .catch((err) => {
+        console.log("ups, an error has been detected displaying the details of a singel movie", err);
+        next (err);
+    })
+});
 
-        })
 
+router.post("/movies/:movieId/delete" , (req, res, next) => {
+    Movie.findByIdAndDelete(req.params.movieId)
+    .then (() => {
+        res.redirect("/movies/");
+    })
+    .catch((err) => {
+        console.log("ups, an error has been detected deleting that movie", err);
+        next (err);
+    });    
 
-    
 });
