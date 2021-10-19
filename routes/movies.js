7894a5811e78a0e971d8dc53e60e072a57e8eebe
Alice <alice.jost@outlook.de>
2021-10-19 20:54:44 +0200

    Bonus: add functionality to delete and edit

--- routes/movies.js
+++ routes/movies.js
@@ -22,7 +22,7 @@ router.get('/movies/:id', (req, res, next) => {
 
 router.get('/new-movie', (req, res, next) => {
     res.render('new-movie')
-})
+});
 
 router.post('/new-movie', (req, res, next) => {
     const {title, director, stars, image, description, showtimes} = req.body;
@@ -30,6 +30,45 @@ router.post('/new-movie', (req, res, next) => {
         .then( () => {
             res.redirect('/movies')
         })
+});
+
+router.get('/movies/:id/edit', (req, res, next) => {
+    Movie.findById(req.params.id)
+        .then(data => {
+            res.render('edit', data)
+        })
+        .catch (err => {
+            console.log(`An error occured: ${err}`)
+        })
+});
+
+router.post('/movies/:id/edit', (req, res, next) => {
+    const  {title, director, stars, description} = req.body
+    const updatedMovie = {
+        title,
+        director,
+        stars,
+        description,
+    }
+    Movie.findByIdAndUpdate(req.params.id, updatedMovie)
+        .then((data) => {
+            res.redirect(`/movies/${data.id}`)
+        })
+        .catch(err => {
+            console.log(`An error occured: ${err}`)
+        })
+});
+
+router.post('/movies/:id/delete', (req, res, next) => {
+    Movie.findByIdAndDelete(req.params.id, (err) => {
+        if (err) {
+            throw console.error(err);
+        } else {
+            res.redirect('/movies')
+        }
+    })
 })
+
+
 
 module.exports = router;
