3462c005793954bd7b30c822514d165dc90e3ff2
karlajaramillo <karlav.jaramillo@gmail.com>
2021-11-10 11:20:42 +0100

    Iteration 0 and Iteration 1 - Schema, Model and Seed the DB

---
+++ models/Movie.model.js
@@ -0,0 +1,22 @@
+
+const { Schema, model } = require("mongoose");
+
+// TODO: Please make sure you edit the model based on the properties that will received
+// Define schema
+const movieSchema = new Schema(
+  {
+    title: String,
+    director: String,
+    stars: [String],
+    image: String,
+    description: String,
+    showtimes: [String],
+  }
+);
+
+// export the model
+// model receives 2 arguments 
+// --> name of the new Collection, Mongoose is going to make it plural
+// --> name of the Schema created
+
+module.exports = model("Movie", movieSchema);
