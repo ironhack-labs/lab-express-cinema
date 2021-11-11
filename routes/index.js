3cdc1d3d82fabc5f45d5048d47b4d1813428a497
Eunice Santiago <eunicevsantiago@gmail.com>
2021-11-11 23:26:57 +0100

    Falta parte visual

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,28 @@
 const express = require('express');
 const router = express.Router();
+const Movie = require('../models/Movie.model');
 
 /* GET home page */
 router.get('/', (req, res, next) => res.render('index'));
+
+router.get("/movies", (req, res, next) => {
+
+    Movie.find()
+      .then(allTheMovies => res.render("movies", { allTheMovies }))
+      .catch(err => console.log(err))
+  
+  });
+
+  router.get("/movie/:id", (req, res, next) => {
+
+    //Se puede acceder a los params mediante req.params
+    // const idParam = req.params.id
+    const { id } = req.params
+  
+    Movie.findById(id)
+      .then(movie => res.render("movie", movie))
+      .catch(err => console.log(err))
+  
+  });
 
 module.exports = router;
