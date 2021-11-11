40d511100cdd59a60ca9d8712d85a3a6149e8805
Miguel Munoz Estelles <mmestelles@gmail.com>
2021-11-11 19:28:54 +0100

    done

---
+++ models/Movie.model.js
@@ -0,0 +1,17 @@
+const { Schema, model } = require("mongoose");
+
+// TODO: Please make sure you edit the user model to whatever makes sense in this case
+const movieSchema = new Schema(
+  {
+    title: String,
+    director: String,
+    stars: [String],
+    image: String,
+    description: String,
+    showtimes: [String]
+  }
+);
+
+const Movie = model("Movie", movieSchema);
+
+module.exports = Movie;
--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,31 @@
 const express = require('express');
 const router = express.Router();
+const Movie = require('../models/Movie.model');
 
 /* GET home page */
 router.get('/', (req, res, next) => res.render('index'));
+
+router.get("/movies", (req, res) => {
+
+    Movie.find()
+      .then(allTheMovies => {
+        console.log(allTheMovies);
+        res.render("movies", { allTheMovies })
+        })
+      .catch(err => console.log(err))
+  
+  });
+
+router.get('/details/:moviesId', (req, res) => {
+    const clickedMovie = req.params.moviesId
+    console.log(clickedMovie);
+    Movie
+        .findById(clickedMovie)
+        .then((movieResult) => {
+            console.log(movieResult);
+            res.render('details', movieResult)
+        })
+        .catch(err => console.log('The error while searching artists occurred: ', err));
+})
 
 module.exports = router;
