fe87343ee8b8f8238e4070ec729c2f6d990c24ea
Analia Lopez <analialopezrodriguez@gmail.com>
2021-11-11 21:57:23 +0100

    lab

---
+++ model/Movie.model.js
@@ -0,0 +1,18 @@
+const { Schema, model } = require("mongoose");
+
+
+const movieSchema = new Schema(
+    {
+        title: String,
+        director: String,
+        stars: Array,
+        image: String,
+        description:String,
+        showtimes: Array
+    }
+)
+
+const Movie = model("Movie", movieSchema);
+
+module.exports = Movie;
+   
--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,20 @@
 const express = require('express');
 const router = express.Router();
+const Movie = require("../model/Movie.model")
+
 
 /* GET home page */
 router.get('/', (req, res, next) => res.render('index'));
+
+router.get("/movies", (req, res, next) => {
+    console.log("movies");
+    Movie.find()
+      .then(allTheMovies => {
+          console.log(allTheMovies);
+          res.render("movies", { allTheMovies })
+        })
+      .catch(err => console.log(err))
+  
+  });
 
 module.exports = router;
