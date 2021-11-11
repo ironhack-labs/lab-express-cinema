c7c4b922039200338785f9fa7086fd7b59849add
Alfredo Menarguez <amenarguezcruz@gmail.com>
2021-11-11 23:46:59 +0100

    ...

---
+++ models/Movie.model.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const movieSchema = new Schema({
+  title: String,
+  director: String,
+  stars: Array,
+  img: String,
+  description: String,
+  showtimes: Array,
+  }
+);
+
+
+
+const Movie = mongoose.model('Movie', movieSchema);
+
+module.exports = Movie;
--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,14 @@
 const express = require('express');
+const Movie = require('../models/Movie.model.js');
 const router = express.Router();
 
 /* GET home page */
 router.get('/', (req, res, next) => res.render('index'));
+
+router.get('/movies', (req, res, next) => {
+Movie.find().then(listOfMovies => res.render("movies", {listOfMovies: listOfMovies }))
+});
+
+
 
 module.exports = router;
