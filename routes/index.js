56f579753f5ad458a91b0d83e666307908367cbf
Miguel Angel Abad <miguel.abad86@gmail.com>
2021-11-11 19:57:19 +0100

    Complete iterations 1 to 3. Restart PC due to DB freezing

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,15 @@
-const express = require('express');
-const router = express.Router();
+const express = require('express')
+const router = express.Router()
+
+const Movie = require('../models/Movie.model')
 
 /* GET home page */
-router.get('/', (req, res, next) => res.render('index'));
+router.get('/', (req, res, next) => res.render('index'))
 
-module.exports = router;
+router.get('/movies', (req, res, next) => {
+  Movie.find()
+    .then((movies) => res.render('movies', { movies }))
+    .catch((err) => console.log(err))
+})
+
+module.exports = router
