a51ed458d31a24b2103c5f944fa83f963984974e
Carlos Fung <carlos.fung@me.com>
2021-11-14 01:28:53 +0100

    Iteration 1 - 3

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,29 @@
-const express = require('express');
+const express = require("express");
+const Movie = require("../models/Movie.model");
+const { db } = require("../models/Movie.model");
 const router = express.Router();
 
+
 /* GET home page */
-router.get('/', (req, res, next) => res.render('index'));
+router.get("/", (req, res, next) => res.render("index"));
+
+// router.get("/movies", (req, res, next) => res.render("movies"));
+
+
+
+router.get("/movies", (req,res) => {
+     
+        // res.sendFile(__dirname + '/index.html');
+        var cursor = db.collection('movies').find().toArray(function(err, result){
+            
+            if (err) { return console.log(err) }
+            
+            res.render('movies', result)
+        
+            return result
+        })
+})
+
+
 
 module.exports = router;
