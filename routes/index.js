47f59e13eb38f7621431d7ecced71feb6c3b1084
Amy Davies <amyloudav@gmail.com>
2021-11-14 22:05:17 +0100

    lab done, css not complete

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,23 @@
 const express = require('express');
 const router = express.Router();
+const Movie = require("../models/Movie.model")
 
 /* GET home page */
 router.get('/', (req, res, next) => res.render('index'));
+
+router.get('/movies', (req, res) => {
+    Movie.find()
+    .then((movies) => {
+        res.render('movies', {movies})
+    })
+})
+
+router.get('/movies/:id', (req, res) => {
+    const { id } = req.params;
+    Movie.findById(id)
+    .then((movie) => {
+        res.render("movie-info", { movie });
+    })
+})
 
 module.exports = router;
