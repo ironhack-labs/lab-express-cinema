9babc8e8a9473fea293f49c5a9895034d4ee4aa7
Carlos Fung <carlos.fung@me.com>
2021-11-14 12:36:06 +0100

    Final!!!

--- routes/index.js
+++ routes/index.js
@@ -7,21 +7,30 @@ const router = express.Router();
 /* GET home page */
 router.get("/", (req, res, next) => res.render("index"));
 
-// router.get("/movies", (req, res, next) => res.render("movies"));
 
 
 
 router.get("/movies", (req,res) => {
      
         // res.sendFile(__dirname + '/index.html');
-        var cursor = db.collection('movies').find().toArray(function(err, result){
+        const reqfromdb = db.collection('movies').find().toArray(function(err, result){
             
             if (err) { return console.log(err) }
             
             res.render('movies', result)
-        
-            return result
         })
+})
+
+router.get( "/display/:id", (req, res) => {
+
+    Movie.findById(req.params.id, function (err, resultofid){
+
+        res.render('display', resultofid)
+
+    }) 
+
+
+
 })
 
 
