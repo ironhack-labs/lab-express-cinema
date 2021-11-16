0a4ff3308ee911d9fc947b4d0a0e7d7a7166b057
Federico Altava Redondo <fedealtava@gmail.com>
2021-11-16 20:14:46 +0100

    lab done

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,32 @@
 const express = require('express');
 const router = express.Router();
-
+const Movie = require("../models/Movie.model")
 /* GET home page */
 router.get('/', (req, res, next) => res.render('index'));
+
+
+
+router.get('/movies', async (req, res) => {
+    try{
+          const movies = await  Movie.find()
+            res.render('movies', {movies})
+    }catch{
+        console.log('The error while searching movie occurred: ', err)
+    }
+    
+})
+
+router.get('/movies/:id', async (req, res) => {
+    const idMovie  = req.params.id;
+    try{
+const moviesWhitId = await Movie.findById(idMovie)
+
+res.render("movies-info",  moviesWhitId);
+    }catch{
+        console.log('The error while searching moviesId occurred: ', err)
+    }
+    
+        
+})
 
 module.exports = router;
