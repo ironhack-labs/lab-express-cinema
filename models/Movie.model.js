304c85e9bd08625bcab9ad2b2e989bedf6e9ae80
Guillem Ferrer <guillemfd@gmail.com>
2021-11-16 17:25:52 +0100

    Iterationt 1 --> 2

---
+++ models/Movie.model.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const movieSchema = new Schema({
+  title: {type: String, required: true},
+  director: {type: String, required: true},
+  stars: [{type: String}],
+  image: {type: String},
+  description: {type: String},
+  showtime: [{type: String}],
+}, { versionKey: false, timestamps: true  })
+
+module.exports = mongoose.model('Movie', movieSchema)
