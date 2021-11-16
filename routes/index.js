299f8e0b260452040028836b92bf296bf5bd751c
Aarón López <n_aaron5@hotmail.com>
2021-11-16 17:41:18 +0100

    ilaboratory done

--- routes/index.js
+++ routes/index.js
@@ -7,9 +7,25 @@ const Movie = require('../models/Movie.model')
 router.get('/', (req, res, next) => res.render('index'));
 
 router.get('/movies', async (req, res) => {
-    const movies = await Movie.find()
-    console.log(movies)
-    res.render('movies', {movies})
+    try{
+        const movies = await Movie.find()
+        console.log(movies)
+        res.render('movies', {movies})
+    } catch(err){
+        console.log(chalk.bgRed(err))
+    }
+    
+})
+
+router.get('/movies/:id', async (req, res) => {
+    try{
+        const movie = await Movie.findById(req.params.id)
+        res.render('moviesInfo',{movie})
+    } catch(err){
+        console.log(chalk.bgRed(err))
+    }
+    
+
 })
 
 module.exports = router;
