3642b77e84e4b8f263916433f8c7e2a2b1011157
Jose Gutierrez <josgutcej@gmail.com>
2021-11-16 23:27:46 +0100

    no me sale

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const Movie = require('../models/Movie.model');
 
 /* GET home page */
 router.get('/', (req, res, next) => res.render('index'));
--- seeds/movies.seed.js
+++ seeds/movies.seed.js
@@ -1,3 +1,7 @@
+const  Movie = require('../models/Movie.model')
+const  mongoose = require('mongoose')
+
+
 const movies = [
     {
       title: "A Wrinkle in Time",
@@ -81,6 +85,28 @@ const movies = [
     }
   ];
 
-  // Add here the script that will be run to actually seed the database (feel free to refer to the previous lesson)
+  const MONGO_URI = process.env.MONGODB_URI || "mongodb://localhost/lab-express-cinema";
 
-// ... your code here
+  mongoose
+    .connect(MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useFindAndModify: false,
+      useCreateIndex: true
+    })
+    .then((x) => {
+      console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`);
+    })
+    .catch((err) => {
+      console.error("Error connecting to mongo: ", err);
+    });
+  
+  const moviesCreate = async () => {
+    try {
+      await Movie.create(movies)
+      await mongoose.connection.close();
+    } catch (err) {
+      console.log('Error:', err)
+    }
+  }
+  moviesCreate();
