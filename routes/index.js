a70a5c3ce6a1badfa8d2ed3c46c8f0c777f5fd03
Guillem Ferrer <guillemfd@gmail.com>
2021-11-16 18:23:11 +0100

    One more step

--- routes/index.js
+++ routes/index.js
@@ -4,4 +4,20 @@ const router = express.Router();
 /* GET home page */
 router.get('/', (req, res, next) => res.render('index'));
 
+
+
+
+router.get('/movies', (req, res, next) => res.render('movies'));
+
+
+
 module.exports = router;
+
+// app.get('/movies', (req, res) => {
+//     res.render("movies.hbs");
+// });
+
+
+// app.get("/", (req, res) => {
+//     res.render("home.hbs");
+//   });
