b50ce474f0426e028c001b33c0dddc87af096faa
Aarón López <n_aaron5@hotmail.com>
2021-11-16 15:52:15 +0100

    iteration 3 done

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const router = express.Router();
 const chalk = require('chalk')
-
+const Movie = require('../models/Movie.model')
 
 /* GET home page */
 router.get('/', (req, res, next) => res.render('index'));
+
+router.get('/movies', async (req, res) => {
+    const movies = await Movie.find()
+    console.log(movies)
+    res.render('movies', {movies})
+})
 
 module.exports = router;
