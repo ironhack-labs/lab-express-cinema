8436c59deaedf71bb5f38de6a536a0d86d3f0ea2
Hector Biminchumo <hectorbiminchumo@gmail.com>
2021-11-22 18:01:51 -0500

    interation 1 & 2

---
+++ models/Movie.model.js
@@ -0,0 +1,23 @@
+// IMPORTACIONES
+const mongoose  = require("mongoose")
+
+// SCHEMA
+//REQUISITOS PARA CREAR UNA MOVIE
+const movieSchema = mongoose.Schema({
+    title: String,
+    director: String,
+    stars: [String],
+    image: String,
+    description: String,
+    showtimes:[String]
+
+},
+{
+    timestamps: true 
+})
+
+// MODELO
+const Movie = mongoose.model("Movie",movieSchema)
+
+// EXPORTACION
+module.exports = Movie
