bad786a0359539df7f5e0d1749f26989b5e6cf9e
Dulce Fabiola <dulcefabiola.rm@gmail.com>
2021-11-22 20:15:32 -0600

    done

---
+++ controllers/movieController.js
@@ -0,0 +1,17 @@
+const Movie = require("./../models/Movie.model");
+
+exports.getAllMovies = async (req, res) => {
+  const allMovies = await Movie.find({});
+  console.log(allMovies);
+  res.render("movies", {
+    data: allMovies,
+  });
+};
+
+exports.getMovie = async (req, res) => {
+  const movieID = req.params.movieID;
+  const getTheMovie = await Movie.findById(movieID);
+  res.render("details", {
+    data: getTheMovie,
+  });
+};
--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,13 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
+const movieController = require("./../controllers/movieController");
 
+// 2. RUTAS DE LA URL BASE
 /* GET home page */
-router.get('/', (req, res, next) => res.render('index'));
+router.get("/", (req, res, next) => res.render("index"));
+router.get("/movies", movieController.getAllMovies);
+//crear
+router.get("/:movieID", movieController.getMovie);
 
+//4. EXPORTACION
 module.exports = router;
