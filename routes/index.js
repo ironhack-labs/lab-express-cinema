1f7f5ab4de294f13bfa7d5053e2baafff38c4683
SerchPro <proyects_serch@hotmail.com>
2021-12-02 01:28:10 -0600

    done

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,37 @@
 const express = require('express');
 const router = express.Router();
+const app = express();
+const Movie = require("../models/Movie.model");
+
 
 /* GET home page */
-router.get('/', (req, res, next) => res.render('index'));
+router.get('/', (req, res, next) => {
+    res.render('index')
+});
+
+router.get('/movies', (req, res, next) => {
+    Movie.find().then(movies => {
+        res.render('movies', { movies })
+    }).
+    catch(err => {
+        console.log("Error", err)
+    })
+
+});
+
+router.get('/movie/:movieTitle', (req, res, next) => {
+    const { movieTitle } = req.params
+    Movie.find({ title: movieTitle }).then(movie => {
+        const movieData = movie[0]
+        console.log(movieData)
+        res.render('movie', { movieData });
+    }).
+    catch(err => {
+        console.log("Error", err)
+    })
+
+});
+
+
 
 module.exports = router;
