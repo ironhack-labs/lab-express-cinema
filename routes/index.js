ad577d349bcaad87a29f00ffc28368c708f6618b
Rox <you@example.com>
2022-01-12 13:03:57 +0100

    no styles

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,32 @@
 const express = require('express');
 const router = express.Router();
+const Movie = require('../models/Movie.model');
 
 /* GET home page */
-router.get('/', (req, res, next) => res.render('index'));
+router.get('/', (req, res, next) => {
+  res.render('index');
+});
+
+// GET movies 
+router.get('/movies', async (req, res, next) => {
+  try {
+    const movies = await Movie.find();
+    res.render('movies.hbs', {
+      movies: movies,
+    });
+  } catch (err) {
+    console.log('Error:', err);
+  }
+});
+
+//GET movie details
+router.get('/movie-details/:id', async (req, res, next) => {
+  try {
+    const movieDetails = await Movie.findById(req.params.id)
+    res.render('movie-details.hbs', movieDetails);
+  } catch (err) {
+    console.logÇ('Error:', err);
+  }
+});
 
 module.exports = router;
