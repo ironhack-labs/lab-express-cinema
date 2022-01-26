04a3f3613d315970d5278d7f8daffb4481438e3d
Vitor Afonso <vittokm@hotmail.com>
2022-01-26 12:31:51 +0000

    done

--- routes/index.js
+++ routes/index.js
@@ -9,11 +9,22 @@ router.get('/', (req, res, next) => res.render('index'));
 router.get('/movies', (req, res, next) => {
 
     Movie.find()
-        .then((movies) => {
-            //console.log(`Got ${movies.length} movies from DB.`);
+        .then(movies => {
+            //console.log(`Got ${movies} movies from DB.`);
             res.render('movies', {movies});
-        }).catch((err) => console.log('Something went wrong while trying to get movies from DB.',err));
+        })
+        .catch((err) => console.log('Something went wrong while trying to get movies from DB.',err));
     
+});
+router.get('/movie/:id', (req, res, next) => {
+    const movieId = req.params.id;
+    
+    Movie.findById(movieId)
+        .then(movie => {
+            //console.log(movie);
+            res.render('movie-details', {movie});
+        })
+        .catch((err) => console.log('Something went wrong while trying to get movie from DB.',err));
 });
 
 module.exports = router;
