b736192ed87dc7c9f274aac30fd0f7e7e514d916
Maria Gallego <mariagestionredes@gmail.com>
2022-01-27 23:47:19 +0100

    need help to fix routes

--- routes/movies.js
+++ routes/movies.js
@@ -2,12 +2,22 @@ const express = require("express");
 const router = express.Router();
 const Movie = require("../models/Movie.model");
 
+
+
 router.get('../views/movies.hbs', (req, res, next) => {
     Movie.find()
-    .then((moviesList) => {
+    .then((movies) => {
         console.log("movies")
-        res.render("movies", { movies: moviesList })
+        res.render("movies", { movies })
     })
+});
+
+router.get('/movies/:id', (req, res, next) => {
+    Movie.findById(req.params.id)
+    .then((movie) => {
+        res.render("movie", { movie })
+    })
+    .catch((e) => next(e));
 })
 
 module.exports = router;
