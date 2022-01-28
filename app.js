0266e3f3bc5dbe7f780f146540aabef5230f6fb0
Diogo Carneiro <diogoascarneiro@hotmail.com>
2022-01-28 17:23:25 +0000

    completed the exercise save for styling

--- app.js
+++ app.js
@@ -22,11 +22,15 @@ require('./config')(app);
 const projectName = 'lab-express-cinema';
 const capitalized = string => string[0].toUpperCase() + string.slice(1).toLowerCase();
 
-app.locals.title = `${capitalized(projectName)}- Generated with Ironlauncher`;
+// app.locals.title = `${capitalized(projectName)}- Generated with Ironlauncher`;
+app.locals.title = `Cinema Ironhack`;
 
 // 👇 Start handling routes here
 const index = require('./routes/index');
 app.use('/', index);
+
+const movieRoutes = require('./routes/movies.route');
+app.use('/', movieRoutes);
 
 // ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
 require('./error-handling')(app);
