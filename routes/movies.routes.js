18f4894588df4a34b49a30c433a06a62d88b2393
joaonacho <joaonacho@gmail.com>
2022-01-28 17:40:25 +0000

    all iterations done, including bonus

--- routes/movies.routes.js
+++ routes/movies.routes.js
@@ -1,17 +1,29 @@
 const router = require("express").Router();
 
-// const res = require("express/lib/response");
-
 const Movie = require("../models/Movie.model.js");
 
+//movies route
 router.get("/movies", (req, res, next) => {
   Movie.find()
     .then((allMoviesFromDB) => {
-      //   console.log(allMoviesFromDB);
       res.render("movies", { allmovies: allMoviesFromDB });
     })
     .catch((error) => {
       console.log(`Error while retrieving movies from DB ${error}`);
+      next(error);
+    });
+});
+
+//movie details route
+router.get("/movies/:id", (req, res, next) => {
+  let movieId = req.params.id;
+
+  Movie.findById(movieId)
+    .then((MovieFromDB) => {
+      res.render("movie-details", { movie: MovieFromDB });
+    })
+    .catch((error) => {
+      console.log("Error while getting movie detail:", error);
       next(error);
     });
 });
