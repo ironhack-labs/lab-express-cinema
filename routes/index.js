ebe1668398e43c3dc47a9cf3a61febc4c2fe16a6
betilla x <aclara_77@yahoo.com>
2022-01-31 21:13:23 +0000

    iteration 3

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,19 @@
 const express = require('express');
 const router = express.Router();
+const Movies = require("../models/movies.model")
 
 /* GET home page */
 router.get('/', (req, res, next) => res.render('index'));
+
+router.get("/movies", (req, res, next) => {
+    Movies.find()
+    .then((allMovies) =>{
+        res.render('movies', {movies: allMovies})
+    })
+    .catch((error)=>{
+        console.log(error);
+        next(error);
+    })
+});
 
 module.exports = router;
