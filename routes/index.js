b9f2fbecdb1b6e3a5088ba238822760dc2540577
Isa Gutierrez <isagr_kio@hotmail.com>
2022-02-01 19:00:38 +0100

    Done. Needs styling though.

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,26 @@
 const express = require('express');
 const router = express.Router();
+const Movie = require('../models/Movie.model')
 
 /* GET home page */
 router.get('/', (req, res, next) => res.render('index'));
 
+router.get('/movies', (req, res) => {
+    Movie.find()
+    .then(movies => {
+        res.render('movies', {movies: movies})
+    })
+    .catch(error => console.log('There was an error while retrieving the movies:', error))
+})
+
+router.get('/movies/:id', (req, res, next) => {
+    Movie.findById(req.params.id)
+    .then(movieFound => {
+        res.render('movie-details', {movie: movieFound})
+    })
+    .catch(error => console.log('Error while retrieving movie details: ', error))
+})
+
+
 module.exports = router;
+
