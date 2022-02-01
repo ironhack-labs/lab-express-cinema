ec3a1bdea827cb038e5fc3e14f4a5870e49452ce
Oscar Sangrador <oscarsangrador@gmail.com>
2022-02-01 20:30:52 +0100

    iteration 2-3 done

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,30 @@
 const express = require('express');
+const res = require('express/lib/response');
 const router = express.Router();
+const Movie = require("../models/Movie.model")
 
 /* GET home page */
 router.get('/', (req, res, next) => res.render('index'));
+
+
+router.get('/movies', (req, res, next) => {
+    Movie.find()
+    .then(movies => {
+        res.render('movies', {movies: movies})
+    })
+    .catch(err => console.log(err))
+})
+
+
+router.get('/movies/:id',(req, res,next)=> {
+    const movieId = req.params.id;
+
+    Movie.findById(movieId)
+        then(movie => { res.render('movie-details', {movie})
+
+})
+    .catch((err) => console.log(err));
+
+})
 
 module.exports = router;
