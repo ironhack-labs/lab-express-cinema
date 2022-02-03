7e851cc815e703a27fe6d7f058173c3b4299c202
Alexis Ocampo <al3xisoc4mpo@gmail.com>
2022-02-03 17:09:50 -0600

    Iteration 3 completed

---
+++ controllers/movieController.js
@@ -0,0 +1,22 @@
+// ./controllers/bookController.js
+
+const Movie = require("../models/Movie.model")
+
+exports.getMovies = async (req, res) => {
+
+    try {
+
+        const foundMovies = await Movie.find({})
+        console.log(foundMovies)
+
+        res.render("movies", {
+            data: foundMovies
+        })
+
+    } catch (error) {
+
+        console.log(error)
+
+    }
+
+}
