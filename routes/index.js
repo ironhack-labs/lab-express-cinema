c10d445f99195af4b657193119a904b9beb204ac
Gonzalo Otero <gonzalo.chalo@gmail.com>
2022-02-03 21:06:20 +0100

    logica hecha sin estética

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,40 @@
 const express = require('express');
 const router = express.Router();
 
+const Movies = require("../models/Movie.model")
+
 /* GET home page */
 router.get('/', (req, res, next) => res.render('index'));
 
+
+router.get("/movies", (req, res, next) => {
+    Movies.find()
+        .then((allMovies) => {
+            console.log(allMovies)
+            res.render('movies', { movies: allMovies })
+        })
+        .catch((error) => {
+            console.log(error);
+            next(error);
+        })
+});
+
+router.get("/movies/:id", (req, res) => {
+
+    Movies.findById(req.params.id)
+        .then((movie) => {
+            console.log("qqqqq" + movie)
+            res.render('detailsMovie', { movies: movie });
+
+        })
+        .catch((error) => {
+            console.log(error);
+            next(error);
+        })
+
+})
+
 module.exports = router;
+
+
+
