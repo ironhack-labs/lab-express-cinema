0a53bb390e4ad15f722cb25d8eb8075e7309ad98
Shirley Gianina <shirley.cd853f@gmail.com>
2022-02-03 21:48:40 +0100

    Lab solved

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,23 @@
-const express = require('express');
+const express = require("express");
+const Movie = require("../models/Movie.model");
 const router = express.Router();
 
 /* GET home page */
-router.get('/', (req, res, next) => res.render('index'));
+router.get("/", (req, res, next) => res.render("index"));
+
+/* GET movies page */
+router.get("/movies", (req, res, next) => {
+  Movie.find().then((movies) => {
+    res.render("movies-page", { movies });
+  });
+});
+
+router.get("/movies/:id", (req, res, next) => {
+  const { id } = req.params;
+
+  Movie.findById(id).then((movie) => {
+    res.render("movie-page-detail", { movie });
+  });
+});
 
 module.exports = router;
