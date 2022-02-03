fba501399517fe385229f83e2aa069f57485b1b3
Diego V <egovaldel@gmail.com>
2022-02-03 22:22:56 +0100

    Empecé con los estilos pero no me dio tiempo

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,23 @@
-const express = require('express');
-const router = express.Router();
+const express = require('express')
+const req = require('express/lib/request')
+const router = express.Router()
+const Movies = require('../models/Movie.models')
 
 /* GET home page */
-router.get('/', (req, res, next) => res.render('index'));
+router.get('/', (req, res, next) => res.render('index'))
 
-module.exports = router;
+router.get('/movies', (req, res) => {
+  Movies.find().then((allmovies) => {
+    // console.log(allmovies)
+
+    res.render('movies-page', { allmovies })
+  })
+})
+router.get('/movie/:id', (req, res) => {
+  Movies.findById(req.params.id).then((theMovie) => {
+    //console.log(theMovie)
+
+    res.render('movie-page', { theMovie })
+  })
+})
+module.exports = router
