6be512fa342863f20df99e3e31d1e0170a01bb05
Gustavo del Llano <gustavodelllano@hotmail.es>
2022-02-03 21:05:53 +0100

    done but little style

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,24 @@
 const express = require('express');
 const router = express.Router();
+const Movie = require("../models/Movie.model")
 
 /* GET home page */
 router.get('/', (req, res, next) => res.render('index'));
+
+router.get("/movies", (req, res)=> {
+    Movie
+    .find()
+    .then(movies => res.render("movies", {movies}))
+    .catch(err => console.log(err))
+})
+
+router.get("/movie/:movieId", (req,res)=>{
+    const {movieId} = req.params
+   Movie
+   .findById(movieId)
+   .then(movie => res.render("detail", movie))
+   .catch(err => console.log(err))
+  
+})
 
 module.exports = router;
