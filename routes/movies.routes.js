866c7cdc49f5783029bf5f492626b4224fe7f776
Ernesto Espinoza M <emenocal07@gmail.com>
2022-02-03 22:13:05 +0100

    ??¿

---
+++ routes/movies.routes.js
@@ -0,0 +1,15 @@
+// const router = require("express").Router()
+
+// const Movie = require('../models/Movie.model')
+
+// router.get('/movies', (req, res, next) => {
+//     Movie
+//         .find()
+//         .Select('title')
+//         .then(movies => res.render('movies.routes', { movies }))
+//         .catch(err => console.log(err))
+
+
+// })
+
+// module.exports = router
