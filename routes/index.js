40e7b985b437c7a925bd7e5d4cbcb6662c70958d
angelita15 <95506994+angelita15@users.noreply.github.com>
2022-02-03 22:39:13 +0100

    [mad de] ángela rueda

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,39 @@
 const express = require('express');
 const router = express.Router();
+const Movie = require('../models/Movie.model')
 
 /* GET home page */
 router.get('/', (req, res, next) => res.render('index'));
 
+router.get('/movies', (req, res, next) => {
+
+    Movie
+    .find() //me trae un array de ob con todos los registros de la base de datos 
+    .then(movies => res.render('movies-page',{movies}))
+    .catch(err => cosole.log(err))
+})
+
+
+router.get('/movies/:movie_id', (req, res) => {
+
+  const { movie_id } = req.params
+
+  Movie
+    .findById(movie_id)
+    .then(movie => res.render('movi-page', {movie})) 
+    .catch(err => console.log(err))
+})
+
+
 module.exports = router;
+
+
+
+ 
+// router.get("/listado", (req, res, next) => {
+//   Book
+//     .find()  
+//     .select('title')
+//     .then(books => res.render('books/list', { books }))
+//     .catch(err => console.log(err))
+// })
