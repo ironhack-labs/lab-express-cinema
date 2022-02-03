a24ec498f0f44debbf959e0ccc9f95e43b2203a5
Cristian Perdomo <ferperdomo14@gmail.com>
2022-02-03 20:52:50 +0100

    cinema

--- app.js
+++ app.js
@@ -19,14 +19,13 @@ const app = express();
 require('./config')(app);
 
 // default value for title local
-const projectName = 'lab-express-cinema';
-const capitalized = string => string[0].toUpperCase() + string.slice(1).toLowerCase();
 
-app.locals.title = `${capitalized(projectName)}- Generated with Ironlauncher`;
+app.locals.title = `ironmovies`;
 
 // 👇 Start handling routes here
 const index = require('./routes/index');
 app.use('/', index);
+
 
 // ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
 require('./error-handling')(app);
