d094024367b4990374065bdc6a4b8c273e9dd2e6
Alexis Ocampo <al3xisoc4mpo@gmail.com>
2022-02-03 17:52:34 -0600

    Iteration 4 completed

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,36 @@
 const express = require('express');
 const router = express.Router();
-const movieController = require("../controllers/movieController")
+
+const Movie = require("../models/Movie.model")
 
 /* GET home page */
 router.get('/', (req, res, next) => res.render('index'));
 
-router.get('/movies',movieController.getMovies);
+/* GET movies page */
+router.get('/movies', async (req, res) => {
+    try {
+        const foundMovies = await Movie.find({})
+        // console.log(foundMovies)
+        res.render("movies", {
+            data: foundMovies
+        })
+    } catch (error) {
+        console.log(error)
+    }
+});
+
+router.get('/movies/:id', async (req, res) => {
+    try {
+        const movieTitle = req.params.id
+        console.log(movieTitle)
+        const foundMovie = await Movie.findOne({ title: movieTitle })
+        console.log(foundMovie)
+        res.render("detail", {
+            data: foundMovie
+        })
+    } catch (error) {
+        console.log(error)
+    }
+});
 
 module.exports = router;
