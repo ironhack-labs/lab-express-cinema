20c37c9c69963758252e000b5380ac5ee66736df
riiGalaxy <riimolpeceres@gmail.com>
2022-02-04 08:44:38 +0100

    done

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,17 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-
+const Movie = require("../models/Movie.model");
 /* GET home page */
-router.get('/', (req, res, next) => res.render('index'));
+router.get("/", (req, res, next) => res.render("index"));
 
+router.get("/movies", (req, res, next) => {
+  //   console.log("buena");
+
+  Movie.find().then((movies) => res.render("movies", { movies }));
+});
 module.exports = router;
+
+// Book.find()
+//   .select("title")
+//   .then((books) => res.render("books/list", { books }))
+//   .catch((err) => console.log(err));
