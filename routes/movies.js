0fe7420e017bdfc1323f45c2d36773b6dea55744
denisesenguel <denisesenguel@gmail.com>
2022-02-07 16:29:43 +0100

    add iteration 4

--- routes/movies.js
+++ routes/movies.js
@@ -13,8 +13,19 @@ router.get('/', (req, res, next) => {
     })
     .catch((err) => {
         console.log('Error getting movies from DB: ', err);
-    })
+    });
 
 });
+
+router.get('/:movieId', (req, res, next) => {
+
+    Movie.findById(req.params.movieId)
+    .then((oneMovie) => {
+        res.render("movie-details", {movie: oneMovie});
+    })
+    .catch((err) => {
+        console.log("Error getting movie details from DB: ", err);
+    });
+})
 
 module.exports = router;
