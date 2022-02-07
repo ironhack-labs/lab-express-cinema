bfaf5d9f2e5f40653b1fc958d93c02cccb0f936e
Zena Bos <zta.bos@gmail.com>
2022-02-07 19:45:28 +0100

    reading part done, without styling

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,32 @@
-const express = require('express');
+const express = require("express");
+const Movie = require("../models/Movie.model");
 const router = express.Router();
 
 /* GET home page */
-router.get('/', (req, res, next) => res.render('index'));
+router.get("/", (req, res, next) => res.render("index"));
+
+router.get("/movies", (req, res, next) => {
+  Movie.find()
+    .then((moviesFromDB) => {
+      //console.log(movies);
+      res.render("movies", { movies: moviesFromDB });
+    })
+    .catch((err) => {
+      console.log("Error", err);
+    });
+});
+
+router.get("/movies/:movieId", (req, res, next) => {
+  Movie.findById(req.params.movieId)
+    .then((movie) => {
+      res.render("movie-details", movie);
+    })
+    .catch((err) => {
+      console.log("Error", err);
+    });
+});
+
+
+
 
 module.exports = router;
