9c756d7107f46f0c831263eae71dedd4f16f109f
Bart Huiskamp <bartolameus.h@gmail.com>
2022-02-07 17:44:13 +0100

    tobias and ana

---
+++ model/movies.model.js
@@ -0,0 +1,18 @@
+const { Schema, model } = require('mongoose');
+ 
+const movieSchema = new Schema(
+  {
+    title: String,
+    director: String,
+    stars: Array,
+    image: String,
+    description: String,
+    showtimes: Array,
+    
+  },
+  {
+    timestamps: true
+  }
+);
+ 
+module.exports = model('Movie', movieSchema);
