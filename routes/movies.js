f9fe9d9e99e43751697991222b693ada03e6d632
dorrrn <doan7tran@gmail.com>
2022-02-07 20:36:42 +0100

    created movie create page

--- routes/movies.js
+++ routes/movies.js
@@ -12,6 +12,25 @@ router.get("/", (req, res, next) => {
     });
 });
 
+//===== Create GET-route /movies/create
+router.get("/create", (req, res, next) => {
+  res.render("movies/movie-create");
+});
+
+// ===== Create POST-route movies/create submit page
+router.post("/create", (req, res, next) => {
+  const { title, director, stars, description, image, showtimes } = req.body;
+  const newDetails = req.body;
+
+  Movie.create(newDetails)
+    .then(() => {
+      res.redirect("/movies");
+    })
+    .catch((err) => {
+      console.log("Error creating new movie", err);
+    });
+});
+
 //== Create route to /movies/movie-details
 router.get("/:movieId", (req, res, next) => {
   const movieId = req.params.movieId;
@@ -50,6 +69,19 @@ router.post("/:movieId/edit", (req, res, next) => {
     })
     .catch((err) => {
       console.log("Error updating movie details", err);
+    });
+});
+
+//====== Create route to delete movie
+router.post("/:movieId/delete", (req, res, next) => {
+  const movieId = req.params.bookId;
+
+  Book.findByIdAndDelete(movieId)
+    .then(() => {
+      res.redirect("/movies");
+    })
+    .catch((err) => {
+      console.log("Error deleting movie", err);
     });
 });
 
