5ba1a0b8226ea0099b7a2f54f933b415b48039d4
dorrrn <doan7tran@gmail.com>
2022-02-07 18:46:44 +0100

    created movies edit page

--- routes/movies.js
+++ routes/movies.js
@@ -25,4 +25,32 @@ router.get("/:movieId", (req, res, next) => {
     });
 });
 
+// Create route to update movie details
+router.get("/:movieId/edit", (req, res, next) => {
+  const movieId = req.params.movieId;
+
+  Movie.findById(movieId)
+    .then((movieDetails) => {
+      res.render("movies/movie-edit", movieDetails);
+    })
+    .catch((err) => {
+      console.log("Error getting movies details", err);
+    });
+});
+
+// Create route for edit movie details
+router.post("/:movieId/edit", (req, res, next) => {
+  const movieId = req.params.bookId;
+  const { title, director, stars, description, image, showtimes } = req.body;
+  const newDetails = req.body;
+
+  Movie.findByIdAndUpdate(movieId, newDetails)
+    .then(() => {
+      res.redirect(`/movies/${movieId}`);
+    })
+    .catch((err) => {
+      console.log("Error updating movie details", err);
+    });
+});
+
 module.exports = router;
