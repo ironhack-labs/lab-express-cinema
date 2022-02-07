f6f85ad8497d3f06d530bfa976ff2a74ec33be79
Marten Zand-Vakili <martenzv@hotmail.de>
2022-02-07 18:34:18 +0100

    Iteration 4

--- routes/movies.js
+++ routes/movies.js
@@ -11,4 +11,24 @@ router.get("/", (req, res, next) => {
     });
 });
 
+router.get("/:movieId", (req, res, next) => {
+  Movie.findById(req.params.movieId)
+    .then(movieId => {
+      res.render("movies/movies-details", movieId)
+    })
+    .catch("Can't find it sorry");
+})
+
+
+router.post("/:movieId/delete", (req, res, next) => {
+  Movie.findByIdAndDelete(req.params.movieId)
+    .then(() => {
+      res.redirect("/movies");
+    })
+    .catch(err => {
+      console.log("Error deleting movie...", err);
+    });
+
+});
+
 module.exports = router;
