8ed9e5a1e55fe96bef4d9f06e9ce98e17b4f49e0
Bart Huiskamp <bartolameus.h@gmail.com>
2022-02-07 19:49:55 +0100

    for tobias

--- routes/movies.js
+++ routes/movies.js
@@ -10,6 +10,13 @@ router.get("/", (req, res, next) => {
         res.render("movies", {movies: resultsFromDB})
     })
         .catch();
-})
+});
 
+router.get("/:movieId", (req, res, next) => {
+    Book.findById(req.params.bookId)
+      .then( book => {
+        res.render("books/book-id", book);
+      })
+      .catch();
+  });
 module.exports = router;
