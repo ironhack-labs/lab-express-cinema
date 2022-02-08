225df44b07a14960a4be448b8a4032008c3b64c6
Stephan Geyer <stephangeyer@Stephans-MacBook-Pro.local>
2022-02-08 10:31:38 +0100

    Restored from local data with Update and Delete Iteration

--- routes/movies.js
+++ routes/movies.js
@@ -17,5 +17,31 @@ router.get("/:movieId", (req, res, next) => {
         .catch();
 });
 
+router.get('/:movieId/edit', (req, res, next) => {
+    Movie.findById(req.params.movieId)
+      .then(movieToEdit => {
+        res.render("movie-edit", { movie: movieToEdit });
+      })
+      .catch(error => next(error));
+  });
+
+  router.post('/:movieId/edit', (req, res, next) => {
+    const { movieId } = req.params;
+    const { title, director, stars, description, showtimes } = req.body;
+    Movie.findByIdAndUpdate(movieId, { title, director, stars, description, showtimes }, { new: true })
+      .then(updatedMovie => res.redirect(`/movies/${updatedMovie.id}`))
+      .catch(err => {
+        console.log('Error updating movie...', err);
+      })
+  });
+  
+  router.post('/:movieId/delete', (req, res, next) => {
+    const { movieId } = req.params;
+    Movie.findByIdAndDelete(movieId)
+      .then(() => res.redirect('/movies'))
+      .catch(err => {
+        console.log('Error deleting movie...', err);
+      })
+  });
 
 module.exports = router;
