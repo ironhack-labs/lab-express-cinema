9fbe739a17b0e161a24c7263519b8829c6ed87a9
Chris Castle <chrisjcastle93@gmail.com>
2022-02-10 20:23:10 +0100

    done

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,25 @@
-const express = require('express');
+const express = require("express");
+const mongoose = require("mongoose");
 const router = express.Router();
+const Movie = require("../models/Movie.model");
 
 /* GET home page */
-router.get('/', (req, res, next) => res.render('index'));
+router.get("/", (req, res, next) => res.render("index"));
+
+router.get("/movies", (req, res, next) => {
+  //   Movie.find().then((result) => res.render("movies", { result })).catch(err => console.log(err));
+  Movie.find()
+    .then((result) => res.render("movies", { result }))
+    // .then(() => mongoose.connection.close())
+    .catch((err) => console.log(err));
+});
+
+router.get("/movies/:id", (req, res, next) => {
+  const { id } = req.params;
+  Movie.findById(id).then((result) => {
+      const { title, image, director, stars, description, showtimes } = result
+      res.render("movie-details", { title, image, director, stars, description, showtimes })
+  });
+});
 
 module.exports = router;
