450ff4064d7f804e0dbe6c57755ee7912a563def
María Fdez de la Rosa <7mariadlrosa@gmail.com>
2022-04-14 21:26:01 +0200

    done

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,36 @@
 const express = require('express');
 const router = express.Router();
 
-/* GET home page */
+// const Book = require('./../models/Book.model')
+const Movie = require('../models/Movie.model');
+
+/* GET HOME PAGE */
 router.get('/', (req, res, next) => res.render('index'));
+
+/* GET MOVIES */
+router.get("/movies", (req, res) => {
+
+    Movie
+        .find()
+        .then(movies => {
+            res.render("movies", {movies})
+            // console.log(movies)
+        })
+        .catch(err => console.log(err))
+
+})
+
+/* GET DETAILS */
+router.get("/moviedetails/:id", (req, res) => {
+
+    const { id } = req.params
+    Movie
+        .findById(id)
+        .then(movies => {
+            res.render('moviedetails', movies )
+        })
+        .catch(err => console.log(err))
+})
+
 
 module.exports = router;
