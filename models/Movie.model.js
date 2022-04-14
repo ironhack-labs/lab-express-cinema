6e4464c199a1e95eda7b8f94014a981d3ab8a3c7
“Fernando <electromedussa@gmail.com>
2022-04-14 23:48:43 +0200

    done

---
+++ models/Movie.model.js
@@ -0,0 +1,21 @@
+const { Schema, model } = require('mongoose');
+
+const movieSchema = new Schema(
+    {
+        title: { type: String },
+        director: { type: String },
+        stars: {
+            type: [String]
+        },
+        image: { type: String },
+        description: { type: String },
+        showtimes: {
+            type: [String]
+        }
+    },
+
+    { timestamps: true }
+)
+
+module.exports = model('Movie', movieSchema)
+
--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,34 @@
 const express = require('express');
 const router = express.Router();
 
+const Movie = require('./../models/Movie.model')
+
 /* GET home page */
 router.get('/', (req, res, next) => res.render('index'));
+
+
+router.get('/movies', (req, res) => {
+
+
+    Movie
+        .find()
+        .then(movies => {
+            res.render('movies', { movies })
+            console.log('MOVIES', movies)
+        })
+        .catch(err => console.log(err))
+})
+
+router.get('/movie-:movie_id', (req, res) => {
+
+
+    const { movie_id } = req.params
+    Movie
+        .findById(movie_id)
+        .then(movie => {
+            res.render('movie-detail', movie)
+        })
+        .catch(err => console.log(err))
+})
 
 module.exports = router;
