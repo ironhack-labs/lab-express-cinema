60be8594a93e0899c09324f3b1a3eebe8de3a387
Jose Pereira suarez <johndoe@example.com>
2022-04-15 01:25:18 +0200

    [WEB MAD GUILLE PEREIRA]

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,25 @@
 const express = require('express');
 const router = express.Router();
 
+const movies = require('./../models/Movie.model')
+
 /* GET home page */
-router.get('/', (req, res, next) => res.render('index'));
+router.get('/', (req, res, next) => {
+    res.render('index')
+});
+
+router.get('/movies', (req, res) => {
+    movies
+        .find()
+        .then(movies => {
+            console.log('Disfruta de nuestra selección', movies)
+            res.render('movies', { movies })
+        })
+});
+
+router.get('movies/movie_id', (req, res) => {
+    const movie_id = req.params.id;
+    res.send("holi")
+})
 
 module.exports = router;
