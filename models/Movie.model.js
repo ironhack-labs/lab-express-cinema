4b31ff509cfec199f21fecbaef8f9f63387ab445
mdelgado333 <101524023+mdelgado333@users.noreply.github.com>
2022-04-15 00:12:27 +0200

    done

---
+++ models/Movie.model.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const movieSchema = new mongoose.Schema({
+    title:{
+        type:String
+    },
+    director:{
+        type:String
+    },
+    stars:{
+        type:[String]
+    },
+    image:{
+        type:String
+    },
+    description:{
+        type:String
+    },
+    showtimes:{
+        type:[String]
+    },
+})
+
+const Movie = mongoose.model('Movie', movieSchema)
+
+module.exports = Movie
--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,28 @@
 const express = require('express');
 const router = express.Router();
 
+const Movie = require('./../models/Movie.model')
+
 /* GET home page */
 router.get('/', (req, res, next) => res.render('index'));
+
+router.get('/movies',(req,res) => {
+    Movie
+        .find()
+        .then(movies => {
+            console.log('Estas son las pelis', movies)
+            res.render('movies', {movies})
+        })
+})
+
+router.get('/movie/:id',(req,res,next) => {
+    const {id} = req.params
+    Movie
+        .findById(id)
+        .then(selectedMovie => {
+            console.log('la peli es', selectedMovie)
+            res.render('movie', {selectedMovie})
+        })
+})
 
 module.exports = router;
