0c79f92b6310e1912c94c1007c0329ebd81468ef
Isaac Miralles <isaacmiralles@gmail.com>
2022-04-14 21:24:57 +0200

    done

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,36 @@ const express = require('express');
 const router = express.Router();
 
 /* GET home page */
-router.get('/', (req, res, next) => res.render('index'));
+
+const Movie = require('./../models/Movie.model')
+
+router.get('/', (req, res) => res.render('index'));
+
+router.get('/peliculas/listado', (req, res) => {
+    // res.send('index')
+
+    Movie
+        .find()
+        .then(movies => {
+            res.render('movies', { movies })
+        })
+        .catch(err => console.log(err))
+
+});
+
+router.get('/peliculas/detalles/:id', (req, res) => {
+
+// res.render('movie-details')
+
+    const { id } = req.params
+    Movie
+        .findById(id)
+        .then(movie => {
+            res.render('movie-details', movie)
+        })
+        .catch(err => console.log(err))
+})
+
 
 module.exports = router;
+
