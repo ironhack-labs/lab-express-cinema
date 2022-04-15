2ef47bbe93cbe6881b3c70adf2be8f330de5334c
Paula Moratalla <paulamrodriguez723@gmail.com>
2022-04-15 09:19:58 +0200

    done

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,29 @@
 const express = require('express');
 const router = express.Router();
 
-/* GET home page */
+const Movie = require('./../models/Movie.model.js')
+
 router.get('/', (req, res, next) => res.render('index'));
+
+router.get('/movies', (req, res) => {
+
+    Movie
+        .find()
+        .then(movies => {
+            res.render("movies", { movies })
+        })
+        .catch(err => console.log(err))
+})
+
+router.get('/movie-details/:movieID', (req, res) => {
+    const { movieID } = req.params
+
+    Movie
+        .findById(movieID)
+        .then(movies => {
+            res.render("movie-details", { movies })
+        })
+        .catch(err => console.log(err))
+})
 
 module.exports = router;
