2c60a9bd9771059b193824cbb80cca5787fd0301
Gabriela <s.gabriela.bv96@gmail.com>
2022-04-15 09:17:28 +0200

    done

---
+++ models/Movie.model.js
@@ -0,0 +1,14 @@
+const { Schema, model } = require("mongoose");
+
+const movieSchema = new Schema(
+    {
+        title: String,
+        director: String,
+        stars: [String],
+        image: String,
+        showtimes: [String],
+    },
+    { timestamps: true }
+);
+
+module.exports = model("movie", movieSchema);
--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,31 @@
-const express = require('express');
+const express = require("express");
+const { render } = require("express/lib/response");
 const router = express.Router();
 
+const Movie = require("./../models/Movie.model");
+
 /* GET home page */
-router.get('/', (req, res, next) => res.render('index'));
+router.get("/", (req, res, next) => res.render("index"));
+
+//pagina de movies
+
+router.get("/movies", (req, res, next) => {
+    Movie.find()
+        .then((movies) => {
+            res.render("movies", { movies });
+        })
+        .catch((err) => console.log(err));
+});
+
+router.get("/movies/:movies_id", (req, res, next) => {
+    const id = req.params.movies_id;
+
+    Movie.findById(id)
+
+        .then((singlemovie) => {
+            res.render("single-movie", { singlemovie });
+        })
+        .catch((err) => console.log(err));
+});
 
 module.exports = router;
