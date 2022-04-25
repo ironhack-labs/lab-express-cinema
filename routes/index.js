6fcaf280e63fcdfac1bb870b93a026600b303bd5
Esteban Munoz <esmunozdev@gmail.com>
2022-04-25 18:19:18 -0500

    :tada:

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,25 @@
-const express = require('express');
+const express = require("express");
+const MovieModel = require("../models/Movie.model");
 const router = express.Router();
 
 /* GET home page */
-router.get('/', (req, res, next) => res.render('index'));
+router.get("/", (req, res, next) => res.render("index"));
+
+router.get("/movies", (req, res, next) => {
+  MovieModel.find()
+    .then((findedMovies) => {
+      res.render("movies", { data: findedMovies });
+    })
+    .catch((err) => console.log(Err));
+});
+
+router.get("/movies/:movieDetails", (req, res, next) => {
+  const { movieDetails } = req.params;
+  MovieModel.findById(movieDetails)
+    .then((resultadoMovie) => {
+      res.render("movieDetails", { movie: resultadoMovie });
+    })
+    .catch((err) => console.log(err));
+});
 
 module.exports = router;
