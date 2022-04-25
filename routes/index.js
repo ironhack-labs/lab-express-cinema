38713d34b9874b3170184d61fd0e13ef20897291
mafaldao21 <mafalda.oliveira.21@icloud.com>
2022-04-25 17:35:30 +0100

    complete 4 iterations

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,34 @@
 const express = require('express');
 const router = express.Router();
+const Movie = require("../models/Movie.model")
 
 /* GET home page */
 router.get('/', (req, res, next) => res.render('index'));
+
+
+
+
+
+router.get("/movies", (req, res, next) => {
+    Movie.find()
+        .then((moviesFromDB) => {
+            console.log(moviesFromDB);
+            res.render("movies", {
+                movies: moviesFromDB
+            })
+        })
+        .catch(err => console.log("oops, there was an error" + err))
+
+})
+
+router.get("/movie/:id", (req, res, next) => {
+    const id = req.params.id
+
+    Movie.findById(id)
+    .then((movieDetails) => {
+        res.render("movie-details", movieDetails)
+    })
+    .catch(err => console.log("oops, there was an error" + err))
+})
 
 module.exports = router;
