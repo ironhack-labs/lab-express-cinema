21d07da5ea2bb928c6dd827e145207d6c347e090
Pauline Schnoebelen <paulineschnoebelen@paulines.home>
2022-04-25 22:31:39 +0200

    iteration 3 solved

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,23 @@
 const express = require('express');
 const router = express.Router();
+const Movie = require('../models/Movie.model.js');
 
 /* GET home page */
 router.get('/', (req, res, next) => res.render('index'));
+console.log ("home works")
 
 module.exports = router;
+
+//READ
+router.get("/movies", (req, res, next) => {
+
+    Movie.find() //get all movies
+    .then ( (moviesArr) => {
+        console.log(moviesArr);
+        res.render("movies", {movies: moviesArr})
+    })
+    .catch((err) => {
+      console.log("error getting movies from DB", err)
+      next(err);  
+    });
+});
