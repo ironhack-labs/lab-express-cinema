1f9a891292ff44dffa75f712c05959c68f056a53
Sepideh Attar <sepideh.att@gmail.com>
2022-04-25 18:26:18 +0200

    Solve the lab

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,38 @@
 const express = require('express');
+const Movie = require('../models/Movie.model');
 const router = express.Router();
 
 /* GET home page */
 router.get('/', (req, res, next) => res.render('index'));
+
+
+
+router.get('/movies', (req, res, next) => {
+  Movie.find()
+    .then((moviesArr) => {
+      res.render('movies', { movies: moviesArr })
+    })
+    .catch((e) => {
+      console.log('an error here!', e)
+    })
+});
+
+
+
+router.get('/movies/:movieId', (req, res, next) => {
+  const id = req.params.movieId;
+  Movie.findById(id)
+    .then((movieDetails) => {
+      res.render('movie-details', { movie: movieDetails })
+    })
+    .catch((e) => {
+      console.log('an error here!', e)
+    })
+});
+
+
+
+
+
 
 module.exports = router;
