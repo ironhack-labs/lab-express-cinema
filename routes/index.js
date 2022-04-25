ec6701ec67b64ab7d00cb149c1b5ec0e532db26b
Oscar Gonzalez <oscargonzalezpena@gmail.com>
2022-04-25 19:55:49 +0200

    add funcionality to create, read and update

--- routes/index.js
+++ routes/index.js
@@ -17,6 +17,30 @@ router.get('/movies', (req, res, next) => {
         }) 
 });
 
+// CREATE: display a form to add movies
+router.get("/movies/create", (req, res, next) => {
+    res.render("movie-create");
+})
+
+// CREATE: process form
+router.post("/movies/create", (req, res, next) => {
+
+    const newMovie = {
+        title: req.body.title,
+        director: req.body.director,
+    }
+
+    Movie.create(newMovie)
+        .then((movieFromDB) => {
+            res.redirect("/movies");
+        })
+        .catch(err => {
+            console.log("error creating movie on DB", err)
+            next(err);
+        });
+
+})
+
 // READ: display movie details
 router.get("/movies/:movieId", (req, res, next) => {
     const id = req.params.movieId;
@@ -33,6 +57,37 @@ router.get("/movies/:movieId", (req, res, next) => {
 })
 
 
-
+// UPDATE: display form
+router.get("/movies/:movieId/edit", (req, res, next) => {
+    const id = req.params.movieId;
+    Movie.findById(id)
+        .then((movieDetails) => {
+            res.render("movie-edit", movieDetails);
+        })
+        .catch(err => {
+            console.log("error getting movie details from DB", err)
+            next(err);
+        });
+});
 
 module.exports = router;
+
+// UPDATE: process form
+router.post("/movies/:movieId/edit", (req, res, next) => {
+
+    const id = req.params.movieId;
+
+    const newDetails = {
+        title: req.body.title,
+        director: req.body.director,
+    };
+
+    Movie.findByIdAndUpdate(id, newDetails)
+        .then((movieFromDB) => {
+            res.redirect(`/movies/${movieFromDB._id}`);
+        })
+        .catch(err => {
+            console.log("error updating movie in DB", err)
+            next(err);
+        });
+});
