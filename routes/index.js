8fc21e41bc55dfc4bef59e6eb19c20d4cba4261e
PJPalma <pt_palma@hotmail.com>
2022-04-25 18:58:36 +0100

    copied

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,24 @@
 const express = require('express');
 const router = express.Router();
 
+const Movie = require('../models/Movie.model.js');
+
+
 /* GET home page */
 router.get('/', (req, res, next) => res.render('index'));
+
+
+router.get('/movies', (req, res, next) => {
+Movie.find()
+.then(moviesFromDB => {
+    console.log(`Reading the movies:`, moviesFromDB);
+    res.render('movies.hbs', { movies: moviesFromDB });
+})
+.catch(error => {
+    console.log('Error getting the movies from DB: ', error);
+    next(error);
+});
+});
+
 
 module.exports = router;
