164c37320190f533290fbc1ddc7221826865c3ec
jidekickpush <jeandenisdrane@gmail.com>
2022-04-25 20:49:20 +0200

    Create the routes GET and POST to update details for a single movie and render updated one

--- routes/movies.routes.js
+++ routes/movies.routes.js
@@ -1,6 +1,34 @@
 const Movie = require("../models/Movie.model");
 const router = require("express").Router();
 
+//Get details for one movies for update
+router.get("/movies/:movieId/edit", (req,res,next)=>{
+    Movie.findById(req.params.movieId)
+        .then(movieDetails =>{
+            res.render("movies/movie-edit", {movie: movieDetails})
+        })
+        .catch(err=>{
+            console.log("Error getting movie details from DB", err)
+            next(err)
+        })
+})
+
+//Updating data from the form
+router.post("/movies/:movieId/edit", (req, res, next)=>{
+    const { title, director, stars, image, description, showtimes} = req.body;
+    Movie.findByIdAndUpdate(req.params.movieId, { title, director, stars, image, description, showtimes }, { new: true })
+        .then(updateMovie=>{
+            console.log(updateMovie.id)
+            res.render("movies/movie-details", {movie:updateMovie})
+        }) // go to the details page to see the updates
+        .catch(err=>{
+            console.log("Error updating movie details in the DB")
+            next(err)
+        })
+})
+
+
+//GET list of movies
 router.get("/movies", (req,res,next)=>{
     Movie.find()
         .then(moviesArr=>{
@@ -12,6 +40,8 @@ router.get("/movies", (req,res,next)=>{
         })
 })
 
+
+//Get details for one movies using id
 router.get("/movies/:movieId", (req,res,next)=>{
 
     Movie.findById(req.params.movieId)
@@ -25,11 +55,5 @@ router.get("/movies/:movieId", (req,res,next)=>{
             next(err);
         })
 })
-
-
-
-
-
-
 
 module.exports = router;
