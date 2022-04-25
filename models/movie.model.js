4418e9f6ffe4d1105f1d1a645334b98fe1eea51d
alejandro blanco <alejandro@alejandros-MacBook-Air-2.local>
2022-04-25 17:57:19 +0200

    it 1 done

---
+++ models/movie.model.js
@@ -0,0 +1,20 @@
+const {Schema, model} = require("mongoose");
+
+const movieSchema = new Schema(
+    {
+        title: String,
+        director: String,
+        stars: [String],
+        image: String,
+        description: String,
+        showtimes: [String]
+
+    },
+    {
+        timestamps: true,
+    }
+)
+
+const Movie = model("Movie", movieSchema);
+
+module.exports = Movie;
