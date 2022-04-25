b001101f6494005a519832d2d8b2532578c74f02
Eric Aguilar <ar_eric@hotmail.com>
2022-04-25 18:47:24 -0500

    Done until Iteration 4

---
+++ models/Movie.model.js
@@ -0,0 +1,12 @@
+const { Schema, model } = require("mongoose");
+
+const MovieSchema = new Schema({
+  title: String,
+  director: String,
+  stars: [String],
+  image: String,
+  description: String,
+  showtimes: [String],
+});
+
+module.exports = model("Movie", MovieSchema);
--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,28 @@
-const express = require('express');
+const express = require("express");
+const MovieModel = require("../models/Movie.model");
 const router = express.Router();
 
 /* GET home page */
-router.get('/', (req, res, next) => res.render('index'));
+router.get("/", (req, res, next) => res.render("index"));
+
+router.get("/movies", (req, res, next) => {
+  MovieModel.find()
+    .then((findedMovies) => {
+      res.render("movies", { data: findedMovies });
+    })
+    .catch((err) => console.log(Err));
+});
+
+router.get("/movies/:id", (req, res, next) => {
+
+    const {id} = req.params;
+
+    MovieModel.findById(id)
+      .then((movieId) => {
+        res.render("movie-details", { movie: movieId });
+      })
+      .catch((err) => console.log(Err));
+  });
+
 
 module.exports = router;
