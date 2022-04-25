83c6fdc799dfff22b45e6da1118f965351bcb710
mafaldao21 <mafalda.oliveira.21@icloud.com>
2022-04-25 18:09:33 +0100

    C + U functionality added

--- routes/index.js
+++ routes/index.js
@@ -31,4 +31,56 @@ router.get("/movie/:id", (req, res, next) => {
     .catch(err => console.log("oops, there was an error" + err))
 })
 
+router.get("/movies/create", (req, res, post) => {
+    res.render("new-movie")
+})
+
+router.post("/movies/create", (req, res, post) => {
+    const newMovie = {
+        title: req.body.title,
+        director: req.body.director,
+        stars: req.body.stars,
+        image: req.body.image,
+        description: req.body.description,
+        showtimes: req.body.showtimes,
+        
+    }
+
+    Movie.create(newMovie)
+    .then( (movieFromDB) => {
+        res.redirect("/movies")
+    })
+    .catch(err => console.log("oops, there was an error" + err))
+});
+
+router.get("/movies/:id/edit", (req, res, post) => {
+    const id = req.params.id
+
+    Movie.findById(id)
+    .then( (movieDetails) => {
+        res.render("movie-edit", movieDetails)
+    })
+    .catch(err => console.log("oops, there was an error" + err))
+    
+})
+
+router.post("/movies/:id/edit", (req, res, post) => {
+    const id = req.params.id
+
+    const newDetails = {
+        title: req.body.title,
+        director: req.body.director,
+
+    }
+
+    Movie.findByIdAndUpdate(id, newDetails)
+        .then((movieFromDB) => {
+            res.redirect(`/movies`);
+        })
+        .catch(err => console.log("oops, there was an error" + err))
+});
+
+
+
+
 module.exports = router;
