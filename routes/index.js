dd881e01f53be5437d15192e30577370c6db4c5a
Alfonso Cano <alfonsoheartagram@gmail.com>
2022-04-25 21:29:54 -0500

    Done

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,26 @@
 const express = require('express');
 const router = express.Router();
+const pelicula = require("../models/Movie.model");
 
 /* GET home page */
 router.get('/', (req, res, next) => res.render('index'));
+
+router.get('/movies', (req, res, next) => {
+    pelicula.find()
+    .then((totalPelis) => {
+        console.log("Total pelis:", totalPelis)
+        res.render("movies", {totalPelis})
+    })
+    .catch(err => console.log(err))
+})
+
+router.get('/movies:id', (req, res, next) => {
+    pelicula.findById(req.params.id)
+    .then((pel) => {
+        console.log("Pelis:", pel)
+        res.render("movies-details", {pel})
+    })
+    .catch(err => console.log(err))
+})
 
 module.exports = router;
