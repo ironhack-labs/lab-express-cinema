4857e653c0459fb0f7d23c6dce41d6d302cf24e8
Georgina Aceves <aceves.georgina@hotmail.com>
2022-04-26 02:25:42 +0200

    done

---
+++ models/Movie.model.js
@@ -0,0 +1,11 @@
+const {Schema, model} = require("mongoose")
+
+const Movie = new Schema ({
+    title: String,
+    director: String,
+    stars:[String],
+    image: String,
+    description: String,
+    showtimes: [String]
+})
+module.exports = model("Movie", Movie)
--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,28 @@
 const express = require('express');
 const router = express.Router();
+const moviesModel = require("../models/Movie.model")
 
 /* GET home page */
 router.get('/', (req, res, next) => res.render('index'));
+router.get("/movies",(req, res)=>{
+    moviesModel.find()
+    .then(movies=>{
+        console.log(movies)
+        res.render("movies",{movies})
+    })
+    .catch(error=>console.log("error"))
+    
+})
+
+router.get('/movie/:id',(req,res)=>{
+    console.log(req.params.id)
+    moviesModel.findById(req.params.id)
+    .then(movie=>{
+        console.log(movie)
+        res.render("movie",{movie})
+    })
+    .catch(error=>console.log("error"))
+})
+
 
 module.exports = router;
