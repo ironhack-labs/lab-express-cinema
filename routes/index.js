2a306dd4d525d1fbf11904f37b93182db20a20af
luthiwd <annattar@gmail.com>
2022-05-06 22:54:31 +0200

    Terminadas iteraciones inicio de  Html para configurar el CSS

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const async = require('hbs/lib/async');
 const router = express.Router();
 const MovieModel = require("../models/Movie.model.js")
 
@@ -12,13 +13,28 @@ router.get('/movies', async (req, res, next) => {
       
 		const allMovies = await MovieModel.find()	
 		res.render("movies.hbs", {
-			allMovies: allMovies
+			allMovies
 		})
-
-  }
+	}
    catch(err){
-
+		console.log(err)
   }
+})
+
+router.get('/movies/:movieId', async (req, res, next) => {
+	
+	const { movieId } = req.params;
+	try{
+		
+		const detailMovie = await MovieModel.findById(movieId)
+		
+		res.render("details-movie.hbs", {
+			detailMovie
+		})
+	}
+	catch(err){
+		console.log(err)
+	}
 })
 
 module.exports = router;
