f49e0414272bad0c55a018e0a4d628f4578fb8ed
EugeBr <eugebordenave@gmail.com>
2022-05-06 15:24:10 +0200

    Done

--- routes/movie.routes.js
+++ routes/movie.routes.js
@@ -12,5 +12,15 @@ router.get("/movies", async (req, res) => {
 	}
 });
 
+router.get("/movies/:id", async (req, res) => {
+	try {
+		const { id } = req.params;
+		const movie = await Movie.findById(id);
+		res.render('movie-details', movie);
+	} catch(error) {
+		console.log(error);
+	}
+});
+
 //!don't forget to export router
 module.exports = router;
