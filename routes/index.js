7aad0b208561d0f81836c22f16124b38b03bfb46
luthiwd <annattar@gmail.com>
2022-05-06 19:03:51 +0200

    Iteracion 2 y 3 e inicio de la 4

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,24 @@
 const express = require('express');
 const router = express.Router();
+const MovieModel = require("../models/Movie.model.js")
 
 /* GET home page */
 router.get('/', (req, res, next) => res.render('index'));
+
+// Go to List Movies
+
+router.get('/movies', async (req, res, next) => {
+   try {
+      
+		const allMovies = await MovieModel.find()	
+		res.render("movies.hbs", {
+			allMovies: allMovies
+		})
+
+  }
+   catch(err){
+
+  }
+})
 
 module.exports = router;
