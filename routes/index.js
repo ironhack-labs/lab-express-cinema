f146f0fe0959b3b1872c5d21ed6cfd5452d0b040
Jorge <jhermotorrado@gmail.com>
2022-06-23 22:26:22 +0200

    muchas horas, no veo el fallo en el findByID

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,32 @@
 const express = require('express');
 const router = express.Router();
-
-/* GET home page */
+const Movie = require('../models/Movie.model.js')
+    /* GET home page */
 router.get('/', (req, res, next) => res.render('index'));
+
+router.get('/movies', (req, res) => {
+
+    //console.log(Movie)
+    Movie
+        .find()
+        .select({ title: 1, image: 1 })
+        .then(data => {
+            res.render('movies', { data })
+        })
+        .catch(err => console.log(err))
+})
+
+
+router.get('/movieDetails/:movieId', (req, res) => {
+    const { movieId } = req.params
+    console.log(movieId)
+    Movie
+        .findById(movieId)
+        .then(dataId => {
+            console.log(dataId)
+            res.render('movieDetails', { dataId })
+        })
+        .catch(err => console.log(err))
+})
 
 module.exports = router;
