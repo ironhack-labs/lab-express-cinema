56558853f0617dd41dafc01c8c17ec3991ac387d
Ovidiu <ovidiu.catalin.stanescu@gmail.com>
2022-06-24 01:06:56 +0200

    done

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,28 @@
 const express = require('express');
 const router = express.Router();
+const Movie = require('../models/Movie.model')
 
 /* GET home page */
 router.get('/', (req, res, next) => res.render('index'));
+
+router.get('/movies', (req, res) => {
+    Movie
+        .find()
+        .select({ title: 1, image: 1 })
+        .then(movies => {
+            res.render('movies', { movies })
+        })
+        .catch(err => console.log(err))
+})
+
+router.get('/details/:movie_id', (req, res) => {
+    const {movie_id} = req.params
+    Movie
+        .findById(movie_id)
+        .then(movie => {
+            res.render('details', movie)
+        })
+        .catch(err => console.log(err))
+})
 
 module.exports = router;
