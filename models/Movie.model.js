8287d808e14dabd64654f23a18185af3f4c3380d
Javier Ruiz <javierdeveloper1@gmail.com>
2022-06-23 21:14:03 +0200

    done

---
+++ models/Movie.model.js
@@ -0,0 +1,19 @@
+const { Schema, model } = require('mongoose')
+
+const movieSchema = new Schema(
+    {
+        title: String,
+        director: String,
+        stars: Array,
+        image: String,
+        description: String,
+        showtimes: Array,
+
+
+    },
+    {
+        timestamps: true
+    }
+)
+
+module.exports = model('Movie', movieSchema)
