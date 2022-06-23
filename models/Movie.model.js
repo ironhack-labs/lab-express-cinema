158cffac965de45ca24ed6a97c140c89ecab7109
CarolinaArlone <carolinaarlone@gmail.com>
2022-06-23 21:07:32 +0200

    [MAD DE 0522] Carolina Arlone

---
+++ models/Movie.model.js
@@ -0,0 +1,18 @@
+const { Schema, model } = require('mongoose');
+
+const movieSchema = new Schema(
+    {
+        
+        title: String,
+        director: String,
+        stars: [String],
+        image: String,
+        description: String,
+        showtimes: [String]
+    },
+    {
+        timestamps: true
+    }
+);
+
+module.exports = model('Movie', movieSchema);
--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,35 @@
 const express = require('express');
 const router = express.Router();
+const Movie = require('./../models/Movie.model.js')
+
 
 /* GET home page */
 router.get('/', (req, res, next) => res.render('index'));
+
+router.get('/movies', (req, res) => {
+
+    Movie
+        .find()
+        .select({ title: 1, image:1 })
+        .then(movies => {
+            res.render('movies', { movies })
+        })
+        .catch(err => console.log(err))
+})
+
+router.get('/details/:movie_id', (req, res) => {
+
+
+    const { movie_id } = req.params
+
+    Movie.findById(movie_id)
+        //.select({ title: 1, image: 1, description:1, director:1 })
+        .then(movies => {
+
+           res.render('details', movies)
+        })
+        .catch(err => console.log(err))
+})
+
 
 module.exports = router;
