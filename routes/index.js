1b067957657d747e89e327e3f94d3ef0b1f32ae5
Santiago Moncada <santiago.m.a@hotmail.com>
2022-06-23 19:23:35 +0200

    Done

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,28 @@
 const express = require('express');
 const router = express.Router();
 
+const Movie = require('../models/Movie.model.js');
+
 /* GET home page */
 router.get('/', (req, res, next) => res.render('index'));
 
+router.get('/movies', (req, res) => {
+
+    Movie.find()
+        .then(data => {
+            res.render('movies', { data: data });
+        })
+        .catch(err => console.log(err))
+
+})
+
+router.get('/movie/:id', (req, res) => {
+    const { id } = req.params;
+    Movie.findById(id)
+        .then(data => {
+            console.log(data)
+            res.render('movie', data);
+        })
+        .catch(err => console.log(err));
+});
 module.exports = router;
