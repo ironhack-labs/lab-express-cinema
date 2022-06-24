98d6889999b20ab29dfe6ad22fb28b024d9504cc
monica <monicasastudy@gmail.com>
2022-06-24 10:10:38 +0200

    [MAD DE 0522] Mónica Sánchez

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,36 @@
 const express = require('express');
+const { find } = require('../models/Movie.model');
 const router = express.Router();
+
+const Movie = require('../models/Movie.model');
+
 
 /* GET home page */
 router.get('/', (req, res, next) => res.render('index'));
+
+router.get('/movies', (req, res, next) => {
+
+    Movie
+        .find()
+        .select({ title: 1 })
+        .then(movies => {
+            res.render('movies', { movies })
+        })
+        .catch(err => console.log(err))
+})
+
+router.get('/libros/detalles/:book_id', (req, res) => {
+
+    const { book_id } = req.params
+
+    Book
+        .findById(book_id)
+        .then(book => {
+            res.render('books/details', book)
+        })
+        .catch(err => console.log(err))
+})
+
+
 
 module.exports = router;
