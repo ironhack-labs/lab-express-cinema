97a87e9ac54e53cd545145ae9d01ccf4dfaa3b3c
Jorge Garcia <jorge.garcia.camp@outlook,com>
2022-06-24 09:01:05 +0200

    [MAD DD 0522]Jorge Garcia

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,40 @@
 const express = require('express');
 const router = express.Router();
 
+
+const Movie = require('./../models/Movie.model.js')
+
 /* GET home page */
-router.get('/', (req, res, next) => res.render('index'));
+router.get("/", (req, res, next) => {
+    res.render("index");
+});
+
+router.get("/movies", (req, res, next) => {
+
+    Movie
+        .find()
+        .select({ title: 1, image: 1, director: 1, stars: 1, })
+        .then(movies => {
+            res.render('movies', { movies })
+        })
+        .catch(err => console.log(err))
+})
+
+router.get("/details/:movies_id", (req, res, next) => {
+
+    const { movies_id } = req.params
+    Movie
+        .findById(movies_id)
+        .then(moviesData => {
+
+            res.render('details', { moviesData })
+
+        })
+        .catch(err => console.log(err))
+})
+
+
+
+
 
 module.exports = router;
