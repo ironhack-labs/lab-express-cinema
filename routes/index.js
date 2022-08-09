672a9d295a5443e93c460e04c9c1298a5f57b5b1
tiffany_chang <changyunhsuan.fr@gmail.com>
2022-08-09 21:16:04 +0200

    finish iteration 4 and add styling

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,36 @@
 const express = require("express");
 const router = express.Router();
-const movies = require("../seeds/movies.seed");
+const Movie = require("../models/Movie.model");
 
 /* GET home page */
 router.get("/", (req, res, next) => res.render("index"));
 
+/* GET movies page */
 router.get("/movies", (req, res) => {
-  res.render("movies", { movies });
+  Movie.find()
+    .then((allMovies) => {
+      res.render("movies", { movies: allMovies });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
+/* GET movie detail page */
+router.get("/movies/:id", (req, res) => {
+  Movie.findById(req.params.id)
+    .then((movie) => {
+      const { title, director, starts, image, description, showtimes } = movie;
+      res.render("movie-detail", {
+        title,
+        director,
+        starts,
+        image,
+        description,
+        showtimes,
+      });
+    })
+    .catch((err) => console.log(err));
 });
 
 module.exports = router;
