13b4520c894576ec1adb5ec64f58a29bf6b3af83
Tony5300 <tony_tolen@ziggo.nl>
2022-08-14 15:46:02 +0200

    Iteration 3 done

--- routes/index.js
+++ routes/index.js
@@ -7,8 +7,28 @@ const movies = require("../seeds/movies.seeds"); // call the seeds file into the
 router.get("/", (req, res, next) => res.render("index"));
 
 /*GET all movies page */
+
 router.get("/movies", (req, res) => {
   res.render("movies", { movies });
+});
+
+/* GET the movie details page */
+router.get("/movies/:id", (req, res) => {
+  MovieModel.findById(req.params.id)
+    .then((movie) => {
+      const { title, director, starts, image, description, showTimes } = movie;
+      res.render("movie-details", {
+        title,
+        director,
+        starts,
+        image,
+        description,
+        showTimes,
+      });
+    })
+    .catch((err) => {
+      console.log(`oopsie, there went something wrong with the id ${err}`);
+    });
 });
 
 module.exports = router;
