db0bb34647c393061e5448dd581926619b778833
Jaime Martin <martinjaime1119@gmail.com>
2022-09-02 11:00:15 +0200

    Error handled

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,34 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
+const MovieModel = require("../models/Movie.model");
 
 /* GET home page */
-router.get('/', (req, res, next) => res.render('index'));
+router.get("/", (req, res, next) => res.render("index"));
+
+router.get("/movies", (req, res, next) => {
+  MovieModel.find()
+    .then((movies) => {
+      const filteredMovies = movies.map((movie) => {
+        const { title, image, id } = movie;
+        return { title, image, id };
+      });
+      res.render("movies", { filteredMovies });
+    })
+    .catch((err) => {
+      next(err);
+    });
+});
+
+router.get("/movie/:movieId", (req, res, next) => {
+  const { movieId } = req.params;
+  MovieModel.findById(movieId)
+    .then((movie) => {
+      //   console.log(movie);
+      res.render("movie", movie);
+    })
+    .catch((err) => {
+      next(err);
+    });
+});
 
 module.exports = router;
