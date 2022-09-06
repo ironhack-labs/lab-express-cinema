b1d07de96d0dfaef5344506ff51f553493d45c32
Guzman <guzpdl@gmail.com>
2022-09-06 13:42:45 +0200

    finished

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,30 @@
 const express = require('express');
 const router = express.Router();
+const moviesModel = require('../models/movie.model');
 
 /* GET home page */
 router.get('/', (req, res, next) => res.render('index'));
+
+router.get('/movies', (req, res, next) => {
+    moviesModel.find()
+        .then((movies) => {
+            res.render('movies', { movies });
+        })
+        .catch((err) => {
+            console.error(err);
+        });
+});
+
+
+
+router.get('/movie/:id', (req, res, next) => {
+    moviesModel.findById(req.params.id)
+        .then((movieId) => {
+            res.render('movie', movieId);
+        })
+        .catch((err) => {
+            next(err);
+        });
+});
 
 module.exports = router;
