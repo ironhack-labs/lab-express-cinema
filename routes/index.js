81672ac6e016160c5e44a4a3953a319f0dd47b69
Lenis Cervantes <leniserik@gmail.com>
2022-10-08 18:23:08 +0200

    all good

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,14 @@
 const express = require('express');
 const router = express.Router();
+const movieModel = require('../models/movie.model');
+
 
 /* GET home page */
 router.get('/', (req, res, next) => res.render('index'));
 
-module.exports = router;
+/* GET movies page */
+router.get('/movies', (req, res) => {
+    res.render('movies');
+  });
+
+module.exports = router; 
