559fdad0366d0854321e9160e00453bb3caa5f9e
MoniIgnacio <moniignacio02@gmail.com>
2022-10-14 23:12:14 +0400

    add It-2 done

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,21 @@
 const express = require('express');
 const router = express.Router();
+const Movie = require('../models/Movie.model.js');
 
 /* GET home page */
 router.get('/', (req, res, next) => res.render('index'));
+
+router.get('/movies', (req, res, next) => {
+    Movie.find()
+    .then((response)=>{
+        res.render('../views/movies/movies.hbs',{
+            Movies: response
+        })
+        console.log(response);
+    })
+    .catch((err)=>{
+        next(err);
+    })
+})
 
 module.exports = router;
