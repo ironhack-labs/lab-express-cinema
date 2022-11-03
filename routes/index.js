3db4dbb1dafa731688f4a8b3e836150f51f815d1
Shania <shaniaantezana@gmail.com>
2022-11-03 13:42:46 -0400

    iteration 3 done

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,25 @@
 const express = require('express');
 const router = express.Router();
+const data = require('../seeds/movies.seed')
+const Movie = require('../models/Movie.model')
 
 /* GET home page */
-router.get('/', (req, res, next) => res.render('index'));
+router.get('/', async(req, res, next) =>{ 
+try{
+  await Movie.deleteMany()
+  res.render('index')
+}catch(error){
+  console.log(error.message)
+}
+})
+router.get('/movies',async(req,res,next)=> {
+try{
+  await Movie.insertMany(data)
+  console.log("deu certo")
+  res.render('movies',data)
+}catch(error){
+    console.log(error.message)
+}
+});
 
 module.exports = router;
