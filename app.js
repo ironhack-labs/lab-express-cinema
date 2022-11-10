5e91915cd2b0f7cddad11af7c69efaac515392f8
PedroSG00 <suarezgallardopedro@gmail.com>
2022-11-10 21:58:02 +0100

    done

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ const express = require('express');
 const hbs = require('hbs');
 
 const app = express();
+require("./db");
 
 // ℹ️ This function is getting exported from the config folder. It runs most middlewares
 require('./config')(app);
--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,35 @@
 const express = require('express');
+const { get } = require('mongoose');
 const router = express.Router();
+
+const Movie = require('./../models/Movie.model')
 
 /* GET home page */
 router.get('/', (req, res, next) => res.render('index'));
+
+router.get('/movies/list', (req, res) => {
+
+    Movie
+        .find()
+        .then(movieDb => {
+            // console.log(movieDb)
+            res.render('movies', { movieDb })
+
+        })
+
+})
+router.get('/movies/:movie_id', (req, res) => {
+
+    const { movie_id } = req.params
+
+    Movie
+        .findById(movie_id)
+        .then(movieDb => {
+            console.log(movieDb)
+            res.render('details', { movieDb })
+
+        })
+
+})
 
 module.exports = router;
