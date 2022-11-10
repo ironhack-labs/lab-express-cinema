305068b03693c232b7d719b5de8448de0dad4554
Gonzalo García Corrales <115544633+gongarco@users.noreply.github.com>
2022-11-10 23:25:46 +0100

    done

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,33 @@
 const express = require('express');
 const router = express.Router();
 
+const Movie = require('./../models/Movie.model.js')
+
 /* GET home page */
+
 router.get('/', (req, res, next) => res.render('index'));
+
+
+router.get('/pelis/listado', (req, res) => {
+    Movie
+        .find()
+        .then(moviesFromDB => {
+            res.render('movies/list', { movies: moviesFromDB })
+        })
+        .catch(err => console.log(err))
+})
+
+
+
+router.get('/pelis/detalles/:movieId', (req, res) => {
+    const { movieId } = req.params
+
+    Movie
+        .findById(movieId)
+        .then(moviesFromDB => {
+            res.render('movies/details', { movies: moviesFromDB })
+        })
+        .catch(err => console.log(err))
+})
 
 module.exports = router;
