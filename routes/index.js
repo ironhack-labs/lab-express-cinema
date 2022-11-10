fdb71806f66a0833ad76974a3d504727d06abdc3
victor moreno <oliverio89@gmail.com>
2022-11-10 22:02:36 +0100

    done no css

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,37 @@
 const express = require('express');
+const { route } = require('../app');
 const router = express.Router();
+const Movie = require('./../models/Movie.model')
 
 /* GET home page */
-router.get('/', (req, res, next) => res.render('index'));
+router.get('/', (req, res) => res.render('index'));
+
+
+router.get('/movies', (req, res) => {
+
+    Movie
+        .find()
+        .then(moviesFromDB => {
+
+            res.render('movies', { movies: moviesFromDB })
+        })
+        .catch(err => console.log(err))
+
+}
+);
+
+router.get('/movies/:moviesId', (req, res) => {
+
+    const { moviesId } = req.params
+    Movie
+        .findById(moviesId)
+        .then(moviesFromDB => {
+            console.log(moviesFromDB)
+            res.render('detalles', { movies: moviesFromDB })
+        })
+        .catch(err => console.log(err))
+
+})
+
 
 module.exports = router;
