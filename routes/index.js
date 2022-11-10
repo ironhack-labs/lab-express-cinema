049f6a61f464d5211e988c19607e4554d3971b45
Manuel Atance <ryuusei3@MacBook-Pro-de-Manuel.local>
2022-11-10 21:04:20 +0100

    Terminadooooo

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,29 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
+const Movie = require("./../models/Movie.model");
+
 /* GET home page */
-router.get('/', (req, res, next) => res.render('index'));
+router.get("/", (req, res) => res.render("index"));
+
+// Movies list
+
+router.get("/movies", (req, res) => {
+	Movie.find()
+		.then((moviesFromDB) => {
+			res.render("movies", { movies: moviesFromDB });
+		})
+		.catch((err) => console.log(err));
+});
+
+router.get("/movies/:id", (req, res) => {
+	const { id } = req.params;
+
+	Movie.findById(id)
+		.then((movieFromDB) => {
+			res.render("details", movieFromDB);
+		})
+		.catch((err) => console.log(err));
+});
 
 module.exports = router;
