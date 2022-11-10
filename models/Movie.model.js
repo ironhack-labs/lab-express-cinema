98ba42e50bf5274a450ed12a574ee7ce05014eb1
DavidD95~ <daviddaganzofraile@gmail.com~>
2022-11-10 23:58:38 +0100

    fnish

---
+++ models/Movie.model.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+
+const movieSchema = new mongoose.Schema(
+  {
+    title: String,
+    director: String,
+    stars: Array,
+    image: String,
+    description: String,
+    showtimes: Array,
+  },
+  {
+    timestamps: true
+  }
+);
+
+module.exports = mongoose.model('movie', movieSchema);
+
--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,29 @@
 const express = require('express');
 const router = express.Router();
-
+const Movie = require('./../models/Movie.model')
 /* GET home page */
 router.get('/', (req, res, next) => res.render('index'));
+router.get('/movies', (req, res, next) => {
 
-module.exports = router;
+  Movie
+    .find()
+    .then(MoviesFromDB => {
+      res.render('movies', { movies: MoviesFromDB })
+    })
+    .catch(err => console.log(err))
+
+}),
+
+  router.get('/details/movD/:movie_id', (req, res, next) => {
+
+    const { movie_id } = req.params
+
+    Movie
+      .findById(movie_id)
+      .then(movieFromDB => {
+        res.render('details/movD', movieFromDB)
+      })
+      .catch(err => console.log(err))
+
+  }),
+  module.exports = router;
