57c5fc36d0e868e34de720ad6318994836baa5e5
Alberto Naval <albertonaval.dev@gmail.com>
2022-11-10 23:35:31 +0100

    Done with Style

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ require('./config')(app);
 const projectName = 'IronHack Cinema';
 const capitalized = string => string[0].toUpperCase() + string.slice(1).toLowerCase();
 
-app.locals.title = `${capitalized(projectName)}- Generated with Ironlauncher`;
+app.locals.title = `${capitalized(projectName)}`;
 
 const index = require('./routes/index.routes');
 app.use('/', index);
