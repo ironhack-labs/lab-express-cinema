7fb084b315c50ebb3b12e3b8e98d74f60f42534d
Alex Marquez <alexis.marquez@icloud.com>
2022-11-10 21:54:12 +0100

    Done

---
+++ models/Movie.model.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+
+const movieSchema = new mongoose.Schema(
+    {
+        title: String,
+        director: String,
+        stars: [String],
+        image: String,
+        description: String,
+        showtimes: [String]
+
+
+    },
+    {
+        timestamps: true
+    }
+);
+
+module.exports = mongoose.model('Movie', movieSchema);
+
+
--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,35 @@
 const express = require('express');
 const router = express.Router();
 
+const Movie = require('../models/Movie.model')
+
 /* GET home page */
 router.get('/', (req, res, next) => res.render('index'));
+
+router.get('/movies', (req, res, next) => {
+    Movie
+        .find()
+        .then(moviesFromDB => {
+            res.render('movies', { movies: moviesFromDB })
+
+        })
+        .catch(err => console.log(`An error occurred while creating books from the DB: ${err}`));
+
+});
+router.get('/movies/:id', (req, res, next) => {
+    const { id } = req.params
+    Movie
+        .findById(id)
+        .then(moviesFromDB => {
+            res.render('movie-details', moviesFromDB)
+
+        })
+        .catch(err => console.log(`An error occurred while creating books from the DB: ${err}`));
+
+});
+
+
+
+
 
 module.exports = router;
