cb803ce0b55a8940be904fbdf0202729291b3b86
Juan <jpcarta16@gmail.com>
2022-11-10 21:54:16 +0100

    Done

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,43 @@ const express = require('express');
 const router = express.Router();
 
 /* GET home page */
+
+const Movie = require('../models/Movies.model')
 router.get('/', (req, res, next) => res.render('index'));
+
+
+router.get('/movies', (req, res) => {
+
+    Movie
+        .find()
+        .select({ title: 1, image: 1 })
+        .then(movieFromDB => {
+            console.log({ movies: movieFromDB })
+            res.render('movies', { movies: movieFromDB })
+        })
+        .catch(err => console.log(err))
+})
+
+router.get("/movies/:id", (req, res) => {
+    const { id } = req.params;
+
+    Movie.findById(id)
+        .then((movieFromDB) => {
+            res.render("details", movieFromDB);
+        })
+        .catch((err) => console.log(err));
+});
+// app.get("/mvoies", (req, res) => {
+
+//     const { movies } = req.query
+
+//     spotifyApi
+//         .searchArtists(movies)
+//         .then(data => {
+//             res.render("movies", { director: data })
+//         })
+//         .catch(err => console.log(err));
+
+// })
 
 module.exports = router;
