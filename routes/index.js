e9556f208b087f51f9dcd564e56050cd1c1eee45
cbrioneslopez <cbrioneslopez96@gmail.com>
2022-11-10 21:38:55 +0100

    Finished Lab

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,26 @@
 const express = require('express');
 const router = express.Router();
-
+const Movie = require("../models/Movie.model")
 /* GET home page */
 router.get('/', (req, res, next) => res.render('index'));
+
+router.get("/movies", (req, res) => {
+    Movie
+        .find()
+        .then(movieDB => {
+            console.log(movieDB)
+            res.render("movies", { movie: movieDB })
+        })
+})
+router.get("/movies/:movieID", (req, res) => {
+    const { movieID } = req.params
+    Movie
+        .findById(movieID)
+        .then(movieDB => {
+            console.log(movieDB)
+            res.render("movie-details", { movieDB })
+        })
+})
+
 
 module.exports = router;
