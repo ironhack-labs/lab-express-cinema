aec6bc8b23421a84b0e7b58734ab7df302bc5cbc
davidmunoz626 <115696661+davidmunoz626@users.noreply.github.com>
2022-11-10 21:42:50 +0100

    done

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,31 @@
 const express = require('express');
 const router = express.Router();
+const Movie = require('./../models/movie.model')
 
 /* GET home page */
-router.get('/', (req, res, next) => res.render('index'));
+router.get('/', (req, res) => res.render('index'))
+
+router.get('/movies', (req, res) => {
+    Movie
+        .find()
+
+        .then(moviesFromDB => {
+            res.render('movies', { movies: moviesFromDB })
+        })
+        .catch(err => console.log(err))
+})
+
+router.get('/movies/details/:movies_id', (req, res) => {
+
+    const { movies_id } = req.params
+
+    Movie
+        .findById(movies_id)
+        .then(moviesFromDB => {
+            res.render('movies/details', moviesFromDB)
+        })
+        .catch(err => console.log(err))
+
+})
 
 module.exports = router;
