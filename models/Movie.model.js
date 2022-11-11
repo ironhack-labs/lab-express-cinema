6b1b4e0f4720326bf740ca5afc2d087c186230ea
CibránMC <cibran.melendez@gmail.com>
2022-11-11 16:33:00 +0100

    done

---
+++ models/Movie.model.js
@@ -0,0 +1,47 @@
+const mongoose = require('mongoose')
+const { Schema, model } = mongoose
+
+
+const movieSchema = new Schema(
+    {
+        title:
+        {
+            type: String,
+            trim: true,
+            required: true,
+        },
+        director: {
+            type: String,
+            trim: true,
+            required: true,
+        },
+        stars: [{
+            type: String,
+            trim: true,
+            required: true,
+
+        }],
+        image: {
+            type: String,
+            trim: true,
+            required: true,
+        },
+        description: {
+            type: String,
+            trim: true,
+            required: true,
+        },
+        showtimes: [{
+            type: String,
+            trim: true,
+            required: true,
+        }]
+    },
+    {
+        timestamps: true,
+    }
+)
+
+const MovieModel = model('Movie', movieSchema)
+
+module.exports = MovieModel
--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,23 @@
 const express = require('express');
 const router = express.Router();
 
+const MovieModel = require('../models/Movie.model')
+
 /* GET home page */
 router.get('/', (req, res, next) => res.render('index'));
 
+router.get('/movies', (req, res) => {
+    MovieModel
+        .find()
+        .then((moviesFromDB) => {
+            res.render('movies', { movies: moviesFromDB })
+        }
+        )
+}
+)
+
 module.exports = router;
+
+router.get('/movies', (req, res) => {
+    res.render('movies')
+})
