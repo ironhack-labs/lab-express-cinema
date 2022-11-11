c1554d8e1597c06d7f7654953e28456b466af600
Catalina Sabogal <csabogalortiz@gmail.com>
2022-11-11 09:19:13 +0100

    Final Cine

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,44 @@
 const express = require('express');
 const router = express.Router();
 
+const Movie = require('./../models/Movie.model')
+
+
+
 /* GET home page */
 router.get('/', (req, res, next) => res.render('index'));
 
 module.exports = router;
+
+
+// movie list
+
+router.get("/movies", (req, res, next) => {
+
+    Movie
+        .find()
+        .then(moviesFromDB => {
+            console.log(moviesFromDB)
+            res.render('movies', { movies: moviesFromDB })
+        })
+        .catch(err => console.log(err))
+
+    // res.render("movies");
+})
+
+// Movie details
+router.get('/movieDetails/:movies_id', (req, res) => {
+
+    const { movies_id } = req.params
+
+    Movie
+        .findById(movies_id)
+        .then(moviesFromDB => {
+            res.render('movieDetails', moviesFromDB)
+        })
+        .catch(err => console.log(err))
+
+})
+
+
+
