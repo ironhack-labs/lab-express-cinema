0acf5e8e6868c290d3830d1e0e553a5f8466a9bf
Alex Lino <alejandrolinojaraquemada@gmail.com>
2022-11-11 10:44:11 +0100

    done

---
+++ models/Movie.model.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+
+const movieSchema = new mongoose.Schema(
+    {
+        title: String,
+        director: String,
+        stars: [String],
+        image: String,
+        description: String,
+        showtime: [String],
+
+
+    },
+    {
+        timestamps: true
+    }
+);
+
+module.exports = mongoose.model('Movie', movieSchema);
