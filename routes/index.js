9b26adcf6b7a5de520adee778f81e82f936f267c
yoaomoreira <jpmoreiraes@gmail.com>
2022-11-11 08:59:45 +0100

    lab done

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,33 @@
 const express = require('express');
 const router = express.Router();
+const Movie = require("./../models/Movie.model")
 
 /* GET home page */
 router.get('/', (req, res, next) => res.render('index'));
+
+// Movies list
+router.get('/movies', (req, res) => {
+    Movie
+        .find()
+        .select()
+        .then(moviesFromDB => {
+            console.log(moviesFromDB)
+            res.render('movies', { movies: moviesFromDB })
+        })
+        .catch(err => console.log(err))
+})
+
+
+// Movie details
+router.get('/movie/:moviesId', (req, res) => {
+    const { moviesId } = req.params
+    Movie
+        .findById(moviesId)
+        .then(moviesFromDB => {
+            res.render('details', { movies: moviesFromDB })
+        })
+        .catch(err => console.log(err))
+
+})
 
 module.exports = router;
