9461f37485f3271a812267873cd937181926030f
Marta <martinha_peres@hotmail.com>
2022-11-15 21:22:23 +0000

    missing styling

---
+++ routes/movies.route.js
@@ -0,0 +1,30 @@
+const router = require("express").Router();
+
+const Movie = require("../models/Movie.model.js");
+
+router.get("/movies", (req, res, next) => {
+  Movie.find()
+    .then((theMovies) => {
+      res.render("movies.hbs", { moviesDB: theMovies });
+    })
+    .catch((error) => {
+      console.log("Error while getting the movies from the DB: ", error);
+      next(error);
+    });
+});
+
+router.get("/movie/:id", (req, res, next) => {
+  let movieID = req.params.id;
+  console.log(movieID);
+  Movie.findById(movieID)
+    .then((theMovie) => {
+      console.log("Retrieved movie from DB:", theMovie);
+      res.render("movie-details.hbs", theMovie);
+    })
+    .catch((error) => {
+      console.log("Error while getting the movie from the DB: ", error);
+      next(error);
+    });
+});
+
+module.exports = router;
