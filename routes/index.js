8f17648eb0ca16ad656f0f24839b0c6f9e9b3cd0
Alfie Collins <alfiecollins44@hotmail.co.uk>
2022-11-15 16:58:04 +0000

    unfinished

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const Movie = require('./../models/Movie.model')
+const Movie = require('../models/Movie.model')
 
 /* GET home page */
 router.get('/', (req, res, next) => res.render('index'));
@@ -13,18 +13,6 @@ router.get('/movies', (req, res) => {
             res.render('movies', { movies: moviesFromDB })
         })
         .catch(err => console.log(err))
-})
-router.get('/movies/moviesDetails/:movie_id', (req, res) => {
-
-    const { movie_id } = req.params
-
-    Movie
-        .findById(movie_id)
-        .then(moviesFromDB => {
-            res.render('movies/moviesDetails', moviesFromDB)
-        })
-        .catch(err => console.log(err))
-
 })
 
 module.exports = router;
