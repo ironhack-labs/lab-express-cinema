db49e388133cc2c93e7f7221db37d7c7c5195caa
sijincodes <sijinmathews03@gmail.com>
2022-11-20 15:50:23 +0100

    iteration3

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,16 @@
 const express = require('express');
 const router = express.Router();
+const Movies = require("../models/Movie.model");
 
 /* GET home page */
 router.get('/', (req, res, next) => res.render('index'));
+
+router.get('/movies',(req,res,next)=>{
+    Movies.find()
+    .then((moviefromDB)=>res.render('movies',{movie:moviefromDB}))
+    .catch(error => next(error))
+})
+
+
 
 module.exports = router;
