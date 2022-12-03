713c9cac70742cc31a7b2493a9b9c262aabb65ec
Cristian Gomez <gmz.mrs.cristian@gmail.com>
2022-12-02 21:51:44 -0600

    Ya quedo sin muchos estilos

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,27 @@
 const express = require('express');
 const router = express.Router();
 
+const Movie = require('../models/Movie.model')
+
 /* GET home page */
 router.get('/', (req, res, next) => res.render('index'));
+
+router.get('/movies', (req,res,next)=>{
+    Movie.find()
+    .then(movies => {
+        res.render('movies',{ movies })
+    })
+    .catch(error=> console.log("ocurrio un error", error))
+})
+
+router.get('/:id/detail', (req,res,next)=>{
+    const { id } = req.params
+    Movie.findById(id)
+    .then(movie=>{
+        res.render('movieDetail',{ movie })
+    })
+    .catch(error=> console.log("ocurrio un error", error))
+})
+
 
 module.exports = router;
