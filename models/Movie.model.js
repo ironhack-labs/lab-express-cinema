75f85077c8f65e527e30c56bf34a3efd437679eb
Arcadio REYES <arcadioreyesph@gmail.com>
2022-12-27 09:53:12 +0100

    iteration 1 done

---
+++ models/Movie.model.js
@@ -0,0 +1,14 @@
+mongoose = require("mongoose");
+const { Schema, model } = mongoose;
+
+const movieSchema = new Schema({
+  title: String,
+  director: String,
+  stars: [String],
+  image: String,
+  description: String,
+  showtimes: [String],
+});
+
+const Movie = mongoose.model("Movie", movieSchema);
+module.exports = Movie;
