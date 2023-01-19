36a2a1c3a26bc7eb29cd928b844a0a37d99c2f53
Daniel Olea <oleaonize@gmail.com>
2023-01-19 20:12:48 +0100

    Error para revisión

--- db/index.js
+++ db/index.js
@@ -1,11 +1,12 @@
 // ℹ️ package responsible to make the connection with mongodb
 // https://www.npmjs.com/package/mongoose
 const mongoose = require("mongoose");
+mongoose.set('strictQuery', true)
 
 // ℹ️ Sets the MongoDB URI for our app to have access to it.
 // If no env has been set, we dynamically set it to whatever the folder name was upon the creation of the app
 
-const MONGO_URI = process.env.MONGODB_URI || "mongodb://localhost/lab-express-cinema";
+const MONGO_URI = process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/lab-express-cinema";
 
 mongoose
   .connect(MONGO_URI)
@@ -15,3 +16,21 @@ mongoose
   .catch((err) => {
     console.error("Error connecting to mongo: ", err);
   });
+
+
+// const mongoose = require("mongoose")
+// mongoose.set('strictQuery', true)
+
+// mongoose.connect("mongodb://127.0.0.1:27017/lab-express-cinema", {
+//   useNewUrlParser: true,
+//   autoIndex: true, //make this also true
+// })
+//   .then(response => console.log(`Connected to Mongo! Database name: "${response.connections[0].name}"`))
+//   .catch(err => console.error('Error connecting to mongo', err))
+
+// process.on("SIGINT", function () {
+//   mongoose.connection.close(function () {
+//     console.log("Mongoose disconnected on app termination");
+//     process.exit(0);
+//   });
+// });
