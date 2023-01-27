097557e1bc6a4190e34a39f1af2cdbadd0a4727d
leandropereyradev <leandro-pereyra@hotmail.es>
2023-01-27 19:34:51 +0100

    Iteration 2 | The Home Page

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,8 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
 /* GET home page */
-router.get('/', (req, res, next) => res.render('index'));
+router.get("/", (req, res, next) => res.render("index"));
+router.get("/movies", (req, res, next) => res.render("movies"));
 
 module.exports = router;
