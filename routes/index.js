322de4d5c39db48ee886e90faae76e3210668ce9
elena992 <elena.fernandez1992@gmail.com>
2023-01-28 12:50:24 +0100

    all done

--- routes/index.js
+++ routes/index.js
@@ -1,26 +1,27 @@
-const express = require('express');
-const Movie = require('../models/Movie.model');
+const express = require("express");
+const Movie = require("../models/Movie.model");
 const router = express.Router();
 
 /* GET home page */
-router.get('/', (req, res, next) => res.render('index'));
+router.get("/", (req, res, next) => res.render("index"));
 
-router.get('/movies', (req, res, next) => {
-    Movie.find()
-        .then(movies => {
-            res.render('movies', {moviefromDB: movies});
-        })
-        .catch(error => console.log('Error while getting the movies from the DB: ', error))
-})
+router.get("/movies", (req, res, next) => {
+  Movie.find()
+    .then((movies) => {
+      console.log(movies);
+      res.render("movies", { movies: movies });
+    })
+    .catch((error) =>
+      console.log("Error while getting the movies from the DB: ", error)
+    );
+});
 
-router.get('/movie/:id', (req, res, next) =>{
-    Movie.findById(req.params.id)
-    .then(movie => {
-        res.render("movieDetails", {movie})
-})
-.catch(error => 
-    console.log("Error", error))
-})
+router.get("/movie/:id", (req, res, next) => {
+  Movie.findById(req.params.id)
+    .then((movie) => {
+      res.render("movie-details", { movie });
+    })
+    .catch((error) => console.log("Error", error));
+});
 
 module.exports = router;
-
