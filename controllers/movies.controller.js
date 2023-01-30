81a777935e374c8217bff1261b3d7aa2f71f17d0
Ana Gutiérrez Ruiz <ana.gutierrez.ruiz@Anas-iMac.local>
2023-01-30 17:08:01 +0100

    rendered list of movies (iteration 3)

--- controllers/movies.controller.js
+++ controllers/movies.controller.js
@@ -2,6 +2,9 @@ const Movie = require('../models/movie.model');
 
 module.exports.list = (req, res, next) => {
   Movie.find()
-    .then(movies => console.log(movies))
+    .then(movies => {
+      console.log(movies);
+      res.render('movies/list', { movies });
+    })
     .catch((error) => next(error))
 }
