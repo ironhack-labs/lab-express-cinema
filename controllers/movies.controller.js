7057d0b2e607a8644d8264acc39eb7110f830e29
Victor Lopez <victorlpz2301@gmail.com>
2023-01-30 17:06:53 +0100

    iteration3

--- controllers/movies.controller.js
+++ controllers/movies.controller.js
@@ -3,6 +3,7 @@ const Movie = require('../models/movie.model')
 module.exports.list = (req, res,next) => {
   Movie.find()
     .then((movies) => {
+      res.render('movies/list.hbs', { movies })
       console.log(movies)
     })
     .catch((error) => next(error))
