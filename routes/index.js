9d8103229854fa2c27202136044e662e7459a6a6
Cristina Gutiérrez <cgutierrezgm@gmail.com>
2023-02-09 21:22:55 +0100

    v1

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,29 @@
 const express = require('express');
+const Movie = require('../models/Movie.model');
 const router = express.Router();
 
 /* GET home page */
 router.get('/', (req, res, next) => res.render('index'));
+
+router.get('/movies', (req, res) => {
+
+    Movie
+        .find()
+        .then(movies => {
+            res.render('movies', { movies })
+        })
+        .catch(err => console.log(err))
+})
+
+router.get('/movies/:movie_title', (req, res) => {
+    const { movie_id } = req.params
+
+    Movie
+        .findById()
+        .then(movies => {
+            res.send(movies)
+            res.render('details'), { movies }
+        })
+})
 
 module.exports = router;
