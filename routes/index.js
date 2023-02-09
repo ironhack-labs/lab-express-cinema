39339895835114eac26bb7272988acbbb17c160d
Gimena Motto <gimenapimba@gmail.com>
2023-02-09 23:22:04 +0100

    done

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,35 @@
 const express = require('express');
 const router = express.Router();
-
+const Movie = require('./../models/Movie.model')
 /* GET home page */
 router.get('/', (req, res, next) => res.render('index'));
 
+
+router.get('/movies', (req, res) => {
+
+    Movie
+        .find()
+        .then(movie => res.render('movies', { movie }))
+        .catch(err => console.log(err))
+})
+
+router.get('/movies/:movie_id', (req, res) => {
+
+    const { movie_id } = req.params
+
+    Movie
+        .findById(movie_id)
+        .then(movie => res.render('movie-details', movie))
+        .catch(err => console.log(err))
+})
+
+
+
+
+
 module.exports = router;
+
+
+
+
+
