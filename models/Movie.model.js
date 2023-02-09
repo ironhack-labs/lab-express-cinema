899388f4460292df35d6cf3b13a83c5197ef21fc
Catalina Fernandez <catalinaf97@gmail.com>
2023-02-09 21:12:35 +0100

    done

---
+++ models/Movie.model.js
@@ -0,0 +1,17 @@
+const { Schema, model } = require('mongoose');
+
+const movieSchema = new Schema(
+    {
+        title: String,
+        director: String,
+        stars: [String],
+        image: String,
+        description: String,
+        showtimes: [String]
+    },
+    {
+        timestamps: true
+    }
+);
+
+module.exports = model('movie', movieSchema)
--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,32 @@
 const express = require('express');
 const router = express.Router();
 
-/* GET home page */
+require('../db/index')
+
+const Movie = require('../models/Movie.model')
+
 router.get('/', (req, res, next) => res.render('index'));
+
+router.get('/movies', (req, res, next) => {
+    Movie
+        .find()
+        .sort({ title: 1 })
+        .then(allMovies => {
+            res.render('movies', { allMovies })
+        })
+})
+
+router.get('/movie/:movie_id', (req, res) => {
+
+    const { movie_id } = req.params
+
+    Movie
+        .findById(movie_id)
+        .then(allMovies => {
+            console.log(allMovies)
+            res.render('movieDetails', allMovies)
+        })
+        .catch(err => console.log(err))
+})
 
 module.exports = router;
