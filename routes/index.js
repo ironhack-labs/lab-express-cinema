38aa9e60ad5896a6b95970653cc938832a885a28
YadMorSal <yadmina@gmail.com>
2023-02-09 22:03:27 +0100

    Done

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,20 @@
 const express = require('express');
 const router = express.Router();
 
-/* GET home page */
+
+require('../db/index')
+const movie = require('../models/Movie.model')
 router.get('/', (req, res, next) => res.render('index'));
 
+
+router.get('/movies', (req, res) => {
+
+    movie
+        .find()
+        .then(movies => {
+            res.render('movies', { movies })
+
+        })
+        .catch(err => console.log(err))
+})
 module.exports = router;
