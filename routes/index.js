b0de6ffb4cac05d23c695b14af78508dd5ed1d31
Bernardo Vera <bvera.ruiz@gmail.com>
2023-02-09 20:45:32 +0100

    done

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,31 @@
 const express = require('express');
 const router = express.Router();
+const Movies = require('./../models/Movie.model')
 
 /* GET home page */
 router.get('/', (req, res, next) => res.render('index'));
 
+router.get('/allmovies', (req, res) => {
+
+
+    Movies
+        .find()
+        .then(movies => res.render('movies', { movies }))
+        .catch(err => console.log(err))
+
+
+}
+)
+
+router.get('/movies/:movies_id', (req, res) => {
+
+    const { movies_id } = req.params
+    console.log('HOLLLLAAAA', movies_id)
+    Movies
+        .findById(movies_id)
+        .then(movies => res.render('moviedetail', movies))
+        .catch(err => console.log(err))
+
+
+})
 module.exports = router;
