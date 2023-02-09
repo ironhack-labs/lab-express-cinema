ea4742339e58676d80ef9b3258de60e14da81f03
jelloreda <jaimelloredavelasco@gmail.com>
2023-02-09 21:53:57 +0100

    lab done

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,27 @@
 const express = require('express');
+const Movie = require('../models/Movie.models');
 const router = express.Router();
+
+require('../db')
 
 /* GET home page */
 router.get('/', (req, res, next) => res.render('index'));
+
+router.get('/movies', (req, res) => {
+    Movie
+        .find()
+        .then(movies => res.render('movies', { movies }))
+        .catch(err => console.log(err))
+})
+
+router.get('/details/:movie_id', (req, res) => {
+    const { movie_id } = req.params
+
+    Movie
+        .findById(movie_id)
+        .then(movie => res.render('details', movie))
+        .catch(err => console.log(err))
+
+})
 
 module.exports = router;
