a4a3338142a23124e30c5cda1e709275f18c7b93
Andrei Costea <andreicst.97@gmail.com>
2023-02-10 08:39:50 +0100

    [MAD DE 0123]ANDREI COSTEA

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,33 @@
 const express = require('express');
 const router = express.Router();
 
+const movie = require('./../models/Movie.model')
+
 /* GET home page */
-router.get('/', (req, res, next) => res.render('index'));
+router.get('/', (req, res) => res.render('index'));
+
+router.get('/movies', (req, res) => {
+    movie
+        .find()
+        .then(movies => {
+            res.render('movies', { movies })
+        })
+        .catch(err => console.log(err))
+})
+
+router.get('/movies/:movieID', (req, res) => {
+    const { movieID } = req.params
+    movie
+        .find({ _id: movieID })
+        .then(movie => {
+            res.render('movie-details', { movie })
+            // console.log(movie)
+        })
+})
+
+router.get('/movies/:movieID/reserve', (req, res) => {
+    const { movieID } = req.params
+    res.render('reserve')
+})
 
 module.exports = router;
