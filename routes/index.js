23b9c311a84a4b5ade18fba894c961898149f136
Cristina Chaparro <cricbau@gmail.com>
2023-02-12 10:09:22 +0100

    iteration 3 almost done

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,21 @@
-const express = require('express');
+const express = require("express");
+const MovieModel = require("../models/Movie.model");
 const router = express.Router();
 
 /* GET home page */
-router.get('/', (req, res, next) => res.render('index'));
+router.get("/", (req, res, next) => res.render("index.hbs"));
+
+router.get("/movies", (req, res, next) => {
+  MovieModel.find()
+    .select("title")
+    .then((response) => {
+      res.render("movies.hbs", {
+        allMovies: response,
+      });
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+});
 
 module.exports = router;
