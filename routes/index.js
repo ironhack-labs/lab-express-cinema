fcfa05854a3bd7b106aa771e86640c3d27301049
Joanadgomes <joanadiogog@gmail.com>
2023-02-13 20:59:27 +0000

    lab extra solved w/ style

--- routes/index.js
+++ routes/index.js
@@ -4,4 +4,34 @@ const router = express.Router();
 /* GET home page */
 router.get('/', (req, res, next) => res.render('index'));
 
+
+
+const Movie = require('../models/Movie.model');
+
+router.get('/movies', async (req, res, next) => {
+    try {
+        let movies = await Movie.find();
+
+        res.render('movies/movies', {movies});
+
+    } catch (error) {
+        console.log(error);
+        next(error);
+    }
+})
+
+router.get('/movies/:id', async (req, res, next) => {
+    try {
+        const {id} = req.params;
+
+        const movie = await Movie.findById(id);
+
+        res.render('movies/movie-details', movie);
+
+    } catch (error) {
+        console.log(error);
+        next(error);
+    }
+})
+
 module.exports = router;
