fc4c477edca1f52336675bf7f25f63f3a8cf8817
Marisha Deroubaix <deroubaix.marisha@gmail.com>
2023-02-13 22:53:57 +0000

    finished but no css

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,32 @@
 const express = require('express');
 const router = express.Router();
+const Movie = require('../models/Movie.model')
 
 /* GET home page */
 router.get('/', (req, res, next) => res.render('index'));
+
+router.get('/movies', async (req, res, next) => {
+    
+    try {
+      let movies = await Movie.find()
+      res.render("movies", {movies})
+    } catch (error) {
+      console.log(error)
+      next(error)
+    } 
+  });
+
+  router.get('/movies/:id', async (req, res, next) => {
+    try {
+        const {id} = req.params
+        const movie = await Movie.findById(id)
+        res.render('details', movie)
+        
+    } catch (error) {
+      console.log(error)
+      next(error)
+    }
+    
+  })
 
 module.exports = router;
