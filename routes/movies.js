245c23f08038aa0e6abc4e5d6b712c30d73d43a7
valyy151 <marin.valenta1@gmail.com>
2023-03-19 15:18:48 +0100

    Iteration 3-4 complete

--- routes/movies.js
+++ routes/movies.js
@@ -1,6 +1,18 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/', (req, res) => res.render('movies/movies'));
+const Movie = require('../models/Movie.model');
+
+router.get('/', (req, res) => {
+	Movie.find()
+		.then((data) => res.render('movies/movies', { data }))
+		.catch((err) => console.log(err));
+});
+
+router.get('/:movieId', (req, res) => {
+	Movie.findById(req.params.movieId)
+		.then((movie) => res.render('movies/movie', movie))
+		.catch((err) => console.log(err));
+});
 
 module.exports = router;
