79e3cb0f1e928830cf666cf48fef7854cdd5ef5e
MaaX59 <Maax.strandberg@gmail.com>
2023-04-06 21:30:34 +0200

    first

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,26 @@
 const express = require('express');
 const router = express.Router();
+const MoviesModel = require("../models/Movie.model");
 
 /* GET home page */
-router.get('/', (req, res, next) => res.render('index'));
+router.get('/', async (req, res, next) => {
+    const moviesFromDb = await MoviesModel.find();
+
+    
+    console.log("from db", moviesFromDb);
+    
+    res.render('index', {moviesFromDb})
+});
+
+router.get('/movies', async (req, res, next) => {
+    const moviesFromDb = await MoviesModel.find();
+
+    
+    console.log("from db", moviesFromDb);
+    
+    res.render('movies', {moviesFromDb})
+});
+
+
 
 module.exports = router;
