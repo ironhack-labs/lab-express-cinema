4e849872db2e51d0977b647ebe9f5062c59eaed1
RonnySk <ronny_sk@hotmail.com>
2023-04-06 21:54:12 +0200

    Finished all iterations

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,21 @@
 const express = require('express');
 const router = express.Router();
+const movieModel = require("../models/Movie.model");
+
 
 /* GET home page */
 router.get('/', (req, res, next) => res.render('index'));
 
+router.get('/movies', async (req, res)=>{
+    const moviesFromDB = await movieModel.find();
+//  console.log("data Base information", moviesFromDB)
+    res.render('movies', {moviesFromDB})
+})
+
+router.get('/movie/:id', async (req, res) =>{
+    const {id} = req.params;
+    const oneMovie = await movieModel.findById(id);
+    console.log("movie Id", oneMovie )
+    res.render("movie-description", { oneMovie });
+})
 module.exports = router;
