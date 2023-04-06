e1f153f97337a90e132251c65e3054cc20ed85a4
Lena Imdahl <mail@lenaimdahl.de>
2023-04-06 22:38:21 +0200

    connected db to hbs

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,16 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
+const Movies = require("../models/Movie.model");
 
 /* GET home page */
-router.get('/', (req, res, next) => res.render('index'));
+router.get("/", (req, res, next) => res.render("index"));
+
+//display moviesArray from mongoDB to movies page
+router.get("/movies", (req, res) => {
+  Movies.find().then((moviesArray) => {
+    console.log(moviesArray);
+    res.render("movies", { moviesArray });
+  });
+});
 
 module.exports = router;
