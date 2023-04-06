8607e7e85e3391e70dc544db378383d529c7d537
olgam <olgamartyniuk88@gmail.com>
2023-04-06 22:51:03 +0200

    it. 3 done

--- db/index.js
+++ db/index.js
@@ -1,6 +1,7 @@
 // ℹ️ package responsible to make the connection with mongodb
 // https://www.npmjs.com/package/mongoose
 const mongoose = require("mongoose");
+const MovieModel = require("../models/Movie.model");
 
 // ℹ️ Sets the MongoDB URI for our app to have access to it.
 // If no env has been set, we dynamically set it to whatever the folder name was upon the creation of the app
@@ -11,6 +12,7 @@ mongoose
   .connect(MONGO_URI)
   .then((x) => {
     console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`);
+    // return MovieModel.deleteMany();
   })
   .catch((err) => {
     console.error("Error connecting to mongo: ", err);
--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,19 @@
 const express = require('express');
 const router = express.Router();
+const MovieModel = require("../models/Movie.model");
 
 /* GET home page */
 router.get('/', (req, res, next) => res.render('index'));
+
+router.get('/movies', async (req, res, next) => {
+    const moviesFromDB = await MovieModel.find();
+    res.render('movies', { movies: moviesFromDB});
+});
+
+
+
+
+
+
 
 module.exports = router;
