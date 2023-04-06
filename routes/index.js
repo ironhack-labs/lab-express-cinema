2c9f4f7250826d62153e0060af696844ea1ecfe5
Wiebke Lueneburg <git@fraulueneburg.de>
2023-04-06 21:33:40 +0200

    in the middle of iteration 3

--- routes/index.js
+++ routes/index.js
@@ -4,4 +4,8 @@ const router = express.Router();
 /* GET home page */
 router.get('/', (req, res, next) => res.render('index'));
 
+router.get('/movies', async(req, res) => {
+    const moviesFromSeed = await MoviesModel.find();
+    res.render('');
+});
 module.exports = router;
