cc93981d4b858c9ed3cf85245531f431a147c3f7
Michel Saber <michel-saber@hotmail.com>
2023-04-06 21:33:51 +0200

    initial coding commit

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,17 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
+const MovieModel = require("../models/Movie.model");
 
 /* GET home page */
-router.get('/', (req, res, next) => res.render('index'));
+router.get("/", (req, res, next) => res.render("index"));
+
+router.get("/movies", async (req, res) => {
+  try {
+    const movies = await MovieModel.find();
+    res.render("movies", { movies });
+  } catch (err) {
+    console.log(err);
+  }
+});
 
 module.exports = router;
