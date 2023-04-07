a55f0a85cb07f96b21e557d59116081ed8c71af9
Wiebke Lueneburg <git@fraulueneburg.de>
2023-04-07 18:29:07 +0200

    solved iteration 3-4 (+ some styling)

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,22 @@
 const express = require('express');
 const router = express.Router();
+const MoviesModel = require('../models/Movie.model');
 
 /* GET home page */
 router.get('/', (req, res, next) => res.render('index'));
 
+/* GET movies */
 router.get('/movies', async(req, res) => {
     const moviesFromSeed = await MoviesModel.find();
-    res.render('');
+    res.render('movies', {moviesFromSeed});
 });
 module.exports = router;
+
+/* GET single movie */
+router.get("/movies/:id", async (req, res) => {
+
+  const { id } = req.params;
+  const singleMovie = await MoviesModel.findById(id);
+  console.log("that movie:", singleMovie);
+  res.render("movie-detail", { singleMovie });
+});
