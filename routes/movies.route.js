14aac73c647267dd676d56ed762f55e6c650a5e7
Mauricio Benavente <mauriciobenavente97@icloud.com>
2023-04-07 11:30:33 +0200

    working on iteration 2

--- routes/movies.route.js
+++ routes/movies.route.js
@@ -1,21 +1,20 @@
-
- 
-
 const express = require('express');
 const router = express.Router();
 const Movie = require('../models/Movie.model.js');
 
+// ROUTES:
+
 router.get('/movies', async (req, res, next) => {
 
+    const movies = await Movie.find()
+    res.render('movies', movies);
+});
+
+
+
+  router.get('/movies/:moviesId', async (req, res, next) => {
+     const {id} = await Movie.findbyId(req.params.moviesId);
+    res.render('movies', id);
+  });
   
-
-  res.render('movies');
-});
-
-
-router.get('/movies/:moviesId', async (req, res, next) => {
-
-  res.render('movies');
-});
-
-module.exports = router;
+  module.exports = router;
