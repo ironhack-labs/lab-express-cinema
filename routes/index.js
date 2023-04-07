e4ff3122cd5e5b388a34cbbb90d8839a66d5854c
Yo-Jia Chen <donna880815@gmail.com>
2023-04-07 12:00:57 +0200

    finish iteration 4

--- routes/index.js
+++ routes/index.js
@@ -16,11 +16,20 @@ router.get('/movies', async (req, res, next) => {
     const moviesFromDb = await MoviesModel.find();
 
     
-    console.log("from db", moviesFromDb);
+    // console.log("from db", moviesFromDb);
     
     res.render('movies', {moviesFromDb})
 });
 
+router.get("/movies/:movieId",async(req, res)=>{
+    try{    
+        const {movieId} = req.params;
+    const movieDetail = await MoviesModel.findById(movieId);
+    console.log(movieDetail)
+    res.render('movieDetail', {movieDetail})
+}
+    catch(err){console.log("There's an error", err)}
 
+})
 
 module.exports = router;
