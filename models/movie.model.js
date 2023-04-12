35c80b73ad9b0808516615c17f0c129a6861ae3e
Rayane <alirayane14@gmail.com>
2023-04-12 20:22:35 +0200

    iteration1

---
+++ models/movie.model.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+
+const movieSchema = new mongoose.Schema({
+  title: String,
+  director: String,
+  stars: [String],
+  image: String,
+  description: String,
+  showtimes: [String],
+});
+
+module.exports = mongoose.model("Movie", movieSchema);
