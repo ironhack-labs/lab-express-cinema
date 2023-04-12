4ce7c4137a7731f89ff7cb06752cfec52d472f96
rachelpytse <rachelpytse@gmail.com>
2023-04-12 21:01:08 +0200

    finished iteration2-3

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,24 @@
 const express = require('express');
 const router = express.Router();
+const Movie = require("../models/movie.model.js")
 
 /* GET home page */
 router.get('/', (req, res, next) => res.render('index'));
+
+router.get('/movies', (req, res, next) => {
+    Movie.find()
+    .then((data) => {
+        console.log("Data is ", data)
+
+        res.render('movies', {
+            allMovies : data
+        })
+    })
+    .catch((err) => {
+        console.log("oops ", err)
+    })
+
+
+});
 
 module.exports = router;
