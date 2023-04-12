851499db44a8903a4cef73056e3900e6dc7d9e85
byref17 <124827023+byref17@users.noreply.github.com>
2023-04-12 21:35:31 +0200

    iteration 2

--- routes/index.js
+++ routes/index.js
@@ -3,5 +3,5 @@ const router = express.Router();
 
 /* GET home page */
 router.get('/', (req, res, next) => res.render('index'));
-
+router.get('/movies', (req, res, next) => res.render('movies'));
 module.exports = router;
