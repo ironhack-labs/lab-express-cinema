50dbc7b976e9aafd7a74705db5433e0f94d9f8f7
Rayane <alirayane14@gmail.com>
2023-04-12 21:32:45 +0200

    lab done

--- routes/index.js
+++ routes/index.js
@@ -1,24 +1,36 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const Movie = require("../models/movie.model.js")
+const Movie = require("../models/movie.model.js");
 
 /* GET home page */
-router.get('/', (req, res, next) => res.render('index'));
+router.get("/", (req, res, next) => res.render("index"));
 
-router.get('/movies', (req, res, next) => {
-    Movie.find()
+router.get("/movies", (req, res, next) => {
+  Movie.find()
     .then((data) => {
-        console.log("Data is ", data)
+      console.log("Data is ", data);
 
-        res.render('movies', {
-            allMovies : data
-        })
+      res.render("movies", {
+        allMovies: data,
+      });
     })
     .catch((err) => {
-        console.log("oops ", err)
+      console.log("oops ", err);
+    });
+});
+
+router.get("/movies/:id", (req, res, next) => {
+  Movie.findById(req.params.id)
+    .then((data) => {
+      console.log("Data is ", data);
+
+      res.render("movie-details", {
+        oneMovie: data,
+      });
     })
-
-
+    .catch((err) => {
+      console.log("oops ", err);
+    });
 });
 
 module.exports = router;
