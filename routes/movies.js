aeaf7a42cf95f42f4525eace59bb9b914bfa9a40
Ayoze Martin <ayozemt@gmail.com>
2023-04-14 21:05:24 +0200

    ECONNREFUSED

--- routes/movies.js
+++ routes/movies.js
@@ -1,6 +1,31 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-router.get('/movies', (req, res, next) => res.render('movies'));
+const Movie = require("../models/Movie.model");
+
+router.get("/movies", (req, res, next) => {
+  Movie.find()
+    .then((allTheMoviesFromDB) => {
+      console.log("Retrieved movies from DB:", allTheMoviesFromDB);
+      res.render("movies.hbs", { movies: allTheMoviesFromDB });
+    })
+    .catch((error) => {
+      console.log("Error while getting the movies from the DB: ", error);
+
+      next(error);
+    });
+});
+
+router.get("/movies/:movieId", (req, res, next) => {
+  const { movieId } = req.params;
+
+  Movie.findById(movieId)
+    .then((theMovie) => res.render("movie-details.hbs", { movie: theMovie }))
+    .catch((error) => {
+      console.log("Error while retrieving movie details: ", error);
+
+      next(error);
+    });
+});
 
 module.exports = router;
