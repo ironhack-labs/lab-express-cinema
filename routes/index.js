c33f171c54651be82b28a3d793574fb5fb178c33
pepealacid <ppalacid@gmail.com>
2023-04-14 02:06:53 +0200

    seeds and models

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,10 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
 /* GET home page */
-router.get('/', (req, res, next) => res.render('index'));
+router.get("/", (req, res, next) => res.render("index"));
+router.get("/movies", (req, res, next) => {
+  res.render("movies", { movies });
+});
 
 module.exports = router;
