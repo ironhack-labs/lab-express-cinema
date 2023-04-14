a23a85da8b2b1ca1640f26e0a5068caa0cfe0f1a
pepealacid <ppalacid@gmail.com>
2023-04-14 12:27:08 +0200

    no carga index

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,13 @@
 const express = require("express");
 const router = express.Router();
+const Movie = require("../models/Movie.model");
 
 /* GET home page */
-router.get("/", (req, res, next) => res.render("index"));
-router.get("/movies", (req, res, next) => {
+router.get("/", (req, res, next) => {
+  res.render("index");
+});
+router.get("/movies", async (req, res, next) => {
+  const movies = await Movie.find();
   res.render("movies", { movies });
 });
 
