01a7e3c5c106ccd09ed192a0ce6fb3144704e110
Jon <jon_ander_23@protonmail.com>
2023-04-14 18:59:16 +0200

    Code creates document and collection in mongoDBcompass, but not the data :'(

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const router = express.Router();
+const Movie = require("../models/Movie.model");
 
 /* GET home page */
-router.get('/', (req, res, next) => res.render('index'));
+router.get("/", async (req, res) => res.render('index'));
+
+router.get("/movies", (req, res, next) => res.render('movies'));
 
 module.exports = router;
