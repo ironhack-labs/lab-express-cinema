549f3eedbe0e8643241fdd7bf116bfe6a63ff425
pepealacid <ppalacid@gmail.com>
2023-04-15 01:47:59 +0200

    solved without styles

--- routes/index.js
+++ routes/index.js
@@ -10,5 +10,9 @@ router.get("/movies", async (req, res, next) => {
   const movies = await Movie.find();
   res.render("movies", { movies });
 });
+router.get("/movies/:id", async (req, res, next) => {
+  const movie = await Movie.findById(req.params.id);
+  res.render("movie-details", movie);
+});
 
 module.exports = router;
