4773073944cf2fed54a4540991a973fcc0fc30a6
Jon <jon_ander_23@protonmail.com>
2023-04-15 09:04:14 +0200

    Loaded documents to db

--- seeds/movies.seed.js
+++ seeds/movies.seed.js
@@ -3,11 +3,11 @@ require("dotenv").config();
 const movies = require("./movies.json");
 const Movie = require("../models/Movie.model");
 
-const MONGO_URI =
+const MONGODB_URI =
   process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/lab-express-cinema";
 
 mongoose
-  .connect(process.env.MONGODB_URI)
+  .connect(MONGODB_URI)
   .then(async () => {
     console.log("connected to DB by movies.seed.js");
     await Movie.deleteMany();
