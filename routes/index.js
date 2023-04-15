8da0a4b61ad78cae3989b345988c32e2ac2e676d
AZRGUI-MOHAMED <azrgui.m@gmail.com>
2023-04-15 12:20:46 +0200

    done

--- routes/index.js
+++ routes/index.js
@@ -3,5 +3,13 @@ const router = express.Router();
 
 /* GET home page */
 router.get('/', (req, res, next) => res.render('index'));
+router.get('/movies', async (req, res, next) => {
+    const moviesFromDB = await MovieModel.find();
+    res.render('movies', { movies: moviesFromDB});
+});
+
+
+
+
 
 module.exports = router;
