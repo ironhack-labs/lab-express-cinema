3c4b17c718875671852c5bcacd9fac7b251618a2
José Antonio Escudero García <jaeg90@gmail.com>
2023-04-17 11:15:06 +0200

    Lab done

--- routes/index.js
+++ routes/index.js
@@ -8,9 +8,13 @@ router.get("/", (req, res, next) => res.render("index"));
 // GET movies page
 router.get("/movies", async (req, res, netx) => {
   const movies = await Movies.find();
-  console.log(movies);
-
   res.render("movies", { movies });
+});
+
+// GET movie-detail page
+router.get("/movies/:id", async (req, res, netx) => {
+  const movie = await Movies.findById(req.params.id);
+  res.render("movie-detail", movie);
 });
 
 module.exports = router;
