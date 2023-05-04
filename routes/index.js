ae2768852fdf46aa4c30250327cff13eeb73b84a
Helena Sánchez <helena.sangar@gmail.com>
2023-05-04 22:53:20 +0200

    finished sin bonus

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,43 @@
 const express = require('express');
 const router = express.Router();
 
-/* GET home page */
+const Movie = require('./../models/Movie.model')
+
+
 router.get('/', (req, res, next) => res.render('index'));
 
+
+router.get("/movies", (req, res, next) => {
+
+    Movie
+        .find()
+        .sort({ title: 1 })           // Ordenamos por título ASC
+        .then(moviesList =>
+            res.render('movies', { movies: moviesList }))
+        .catch(err => console.log(err))
+})
+
+router.get("/movie-details/:id", (req, res, next) => {
+    const { id } = req.params
+    Movie
+        .findById(id)
+        .then(moviesDetails =>
+            res.render('movie-details', moviesDetails))
+        .catch(err => console.log(err))
+})
+
+
+
+
 module.exports = router;
+
+/* 
+
+app.get('/beers', (req, res) => {
+    punkAPI
+        .getBeers()
+        .then(beersFromApi =>
+            res.render('beers', { beers: beersFromApi }))
+        .catch(error => console.log(error));
+});
+ */
