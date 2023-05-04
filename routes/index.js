e98d6a344acbfffca40bc77ffa87b3f80bfae00a
Gan-Dev <60898077+Gan-dev@users.noreply.github.com>
2023-05-04 22:07:14 +0200

    Todo Ok

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,23 @@
 const express = require('express');
 const router = express.Router();
+const Movie = require('./../models/Movie.model')
 
 /* GET home page */
 router.get('/', (req, res, next) => res.render('index'));
 
+//movies
+router.get('/movies', (req, res, next) => {
+    Movie
+        .find()
+        .then(movie => res.render("movies", { movies: movie }))
+        .catch(err => console.log(err))
+})
+
+router.get('/moviesSpects/:id', (req, res, next) => {
+    const { id } = req.params
+    Movie
+        .findById(id)
+        .then(movie => res.render('moviesSpects/movieSpect', movie))
+        .catch(err => console.log(err))
+})
 module.exports = router;
