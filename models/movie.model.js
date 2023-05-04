5d1368dcd2d86254927f67d466c01c7a3903fc70
Manuel Pérez <mperezpradosjose@gmail.com>
2023-05-04 20:39:19 +0200

    Realizado con exito, este lab me resulto mas sencillo que el de spotify

---
+++ models/movie.model.js
@@ -0,0 +1,28 @@
+const { Schema, model } = require('mongoose');
+
+
+const movieSchema = new Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    director: {
+        type: String,
+        required: true
+    },
+    stars: {
+        type: [String]
+    },
+    image: {
+        type: String
+    },
+    description: {
+        type: String
+    },
+    showtimes: {
+        type: [String]
+    }
+})
+
+
+module.exports = model('Movie', movieSchema)
--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,27 @@
 const express = require('express');
 const router = express.Router();
 
+const Coaster = require('../models/movie.model.js')
 /* GET home page */
 router.get('/', (req, res, next) => res.render('index'));
+
+
+//// Movies Page//////
+router.get('/movies', (req, res, next) => {
+    Coaster
+        .find()
+        .then(allMovies => res.render('movies', { movie: allMovies }))
+        .catch(err => console.log(err))
+})
+//// single movie page///
+router.get('/movie/:id', (req, res) => {
+    const id = req.params.id
+    Coaster
+        .findById(id)
+        .then((movie) => {
+            res.render('movie', movie)
+        })
+        .catch(err => console.log(err))
+})
 
 module.exports = router;
