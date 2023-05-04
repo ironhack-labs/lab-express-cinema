8a84ff0d867c9f392dae80002c546e86d8615781
LourdesGonzalezT <LourdesGonzalezT@github.com>
2023-05-05 00:39:53 +0200

    en proceso

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,30 @@
 const express = require('express');
 const router = express.Router();
 
-/* GET home page */
-router.get('/', (req, res, next) => res.render('index'));
+const Movie = require('./../models/Movie.model')
 
+router.get("/", (req, res, next) => {
+    res.render("index")
+})
+
+router.get('/movies', (req, res) => {
+
+    Movie
+        .find()
+        .then(allMovies => {
+            res.render('movies-page', { movies: allMovies })
+        })
+        .catch(err => console.log(err))
+})
+
+
+router.get('/movie/:id', (req, res) => {
+
+    const { _id } = req.params
+
+    Movie
+        .findById(_id)
+        .then(movie => res.render('movies-details', movie))
+        .catch(err => console.log(err))
+})
 module.exports = router;
