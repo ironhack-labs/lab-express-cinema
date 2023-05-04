bf12de8e9e934ef9244115142330f3319bc42f02
Alvaro Barba <abarba.lod@gmail.com>
2023-05-04 20:48:56 +0200

    lab hecho sin estilos

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,28 @@
 const express = require('express');
 const router = express.Router();
 
+const Movie = require('./../models/Movie.model')
+
 /* GET home page */
 router.get('/', (req, res, next) => res.render('index'));
+
+router.get('/movies', (req, res) => {
+    Movie
+        .find()
+        .then(allMovies => {
+            res.render('movies', { movies: allMovies })
+        })
+        .catch(err => console.log(err))
+
+})
+
+router.get('/movieDetails/:id', (req, res) => {
+    const { id } = req.params
+    Movie
+        .findById(id)
+        .then(moviedata => {
+            res.render('movie', moviedata)
+        })
+})
 
 module.exports = router;
