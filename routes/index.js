7a696871bc8c8104b939be4712702a46602b3fdc
arturhonores <luis.arturhonores@gmail.com>
2023-05-05 12:36:13 +0200

    sólo falta agregar detalles de movie

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,24 @@
 const express = require('express');
 const router = express.Router();
+const Movies = require("../models/Movie.model")
 
 /* GET home page */
 router.get('/', (req, res, next) => res.render('index'));
+
+router.get("/movies", (req, res) => {
+    Movies
+        .find()
+        .then(movies => res.render('movies', { movies: movies }))
+        .catch(err => console.log(err))
+})
+
+router.get("/details-movie/:id", (req, res) => {
+    const { id } = req.params
+
+    Movies
+        .findById(id)
+        .then(movie => res.render('details-movie', { movie }))
+        .catch(err => console.log(err))
+})
 
 module.exports = router;
