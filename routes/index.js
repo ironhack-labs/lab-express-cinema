fbb988f097c341845e83d0863cc9089817114c48
CrisRamosLazaro <crislr@hotmail.com>
2023-05-07 20:54:28 +0200

    lab completed

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,32 @@
 const express = require('express');
 const router = express.Router();
 
-/* GET home page */
-router.get('/', (req, res, next) => res.render('index'));
 
-module.exports = router;
+const Movie = require('./../models/Movie.model')
+
+
+router.get("/", (req, res, next) => {
+    res.render("index")
+})
+
+
+router.get("/movies", (req, res, next) => {
+    Movie
+        .find()
+        .select({ image: 1, title: 1 })
+        .sort({ title: 1 })
+        .then(movies => res.render('movies', { movies }))
+        .catch(err => console.log(err))
+})
+
+router.get('/movie-details/:movie_id', (req, res) => {
+
+    const { movie_id } = req.params
+
+    Movie
+        .findById(movie_id)
+        .then(movie => res.render('movie-details', { movie }))
+        .catch(err => console.log(err))
+})
+
+module.exports = router
