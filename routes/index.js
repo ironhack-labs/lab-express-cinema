6937c9733227199e82ea8950a4922543e8edaa83
Paula <94paulala@gmail.com>
2023-05-11 13:28:02 +0200

    solved

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,26 @@
 const express = require('express');
 const router = express.Router();
 
+const MovieModel = require('./../models/Movie.model');
+
 /* GET home page */
-router.get('/', (req, res, next) => res.render('index'));
+router.get('/', (req, res, next) => {
+    res.render('index')
+})
+
+router.get('/movies', (req, res, next) => {
+    MovieModel
+    .find()
+    .then(movies => res.render('movies', { movies: movies}))
+    .catch(err => console.log(err))
+})
+
+router.get('/details/:movie_id', (req, res) => {
+    const { movie_id } = req.params
+    MovieModel
+    .findById(movie_id)
+    .then(movie => res.render('movie-details', movie))
+    .catch(err => console.log(err))
+})
 
 module.exports = router;
