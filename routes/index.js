1b514f0df8b815da351732dda15493c441749b46
Bernard Cardoso <be1.0@hotmail.com>
2023-06-02 00:21:58 +0100

    first iterations

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,21 @@
 const express = require('express');
 const router = express.Router();
+const Movie = require('../models/Movie.model');
+
 
 /* GET home page */
-router.get('/', (req, res, next) => res.render('index'));
+router.get('/', (req, res, next) => { 
+    res.render('index')});
+
+router.get('/movies', (req, res, next) => {
+    Movie.find()
+      .then((movies) => {
+        res.render('movies', { movies });
+      })
+      .catch((err) => {
+        console.log('Error retrieving movies: ', err);
+        res.render('error'); // Render an error page if there is an error
+      });
+  });
 
 module.exports = router;
