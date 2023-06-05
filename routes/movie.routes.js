060b35fe2510693fd17e0c2ab425325e57e3611e
Philippe Miossec <pmiossec@gmail.com>
2023-06-05 14:04:52 +0200

    It4: Movie details

--- routes/movie.routes.js
+++ routes/movie.routes.js
@@ -8,10 +8,19 @@ router.get('/movies', async (req, res, next) => {
     {
         const movies = await Movie.find();
         console.log("movies from db", movies);
-        res.render('movies/movies-list.hbs', {movies});
+        res.render('movies/movies-list', {movies});
     }catch(err) {
         res.send(err)
     }
 });
-
+router.get('/movie/:id', async (req, res, next) => {
+    try
+    {
+        const movie = await Movie.findById(req.params.id);
+        console.log("movie", movie);
+        res.render('movies/movie-details', movie);
+    }catch(err) {
+        res.send(err)
+    }
+});
 module.exports = router;
