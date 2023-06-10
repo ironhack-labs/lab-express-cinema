a396d61b595391a7ae10e98a8f447ef7ad33f43d
Andresfrla <125943749+Andresfrla@users.noreply.github.com>
2023-06-10 17:21:26 -0600

    Iteration 0 and Iteration 1 Done

---
+++ models/Movie.model.js
@@ -0,0 +1,27 @@
+const { Schema,model } = require('mongoose')
+
+const movieSchema = new Schema ({
+    title: {
+        type: String
+    },
+    director: {
+        type: String
+    },
+    stars: {
+        type: [String]
+    },
+    image: {
+        type: String
+    },
+    description: {
+        type: String
+    },
+    showtimes: {
+        type: [String]
+    }},
+    {
+        timestamps: true
+    }
+)
+
+module.exports = model("Movie". movieSchema)
