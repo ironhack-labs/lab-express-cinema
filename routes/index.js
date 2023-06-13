01c5e709991b1ecaa49be86250aa90ed1aa83233
Andresfrla <125943749+Andresfrla@users.noreply.github.com>
2023-06-12 18:14:50 -0600

    Iteration 3 Done

--- routes/index.js
+++ routes/index.js
@@ -12,7 +12,14 @@ router.get('/movies', (req, res, next) => {
     Movie.find()
     .then(movies => {
         res.render('movies', {movies})
-    }).catch()
+    }).catch(err => console.log('Err sending all the movies'))
+})
+
+router.get('/movies/:id/movie', (req, res, next) => {
+    Movie.findById(req.params.id)
+    .then(movie => {
+        res.render('movie', {movie})
+    }).catch(err => console.log('Err finding single movie'))
 })
 
 module.exports = router;
