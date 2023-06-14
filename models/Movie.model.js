6f7b7ac2f49f73b06f67c32539c167aa7557f490
Diogo Parreira <diogomiguel.nunes@hotmail.com>
2023-06-14 12:19:34 +0100

    All Done

---
+++ models/Movie.model.js
@@ -0,0 +1,33 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const movieSchema = new Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    director: {
+        type: String,
+        required: true
+    },
+    stars: {
+        type: Array,
+        required: true
+    },
+    image: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    showtimes: {
+        type: Array,
+        required: true
+    }
+});
+
+const Movie = mongoose.model('Movie', movieSchema);
+
+  module.exports = Movie;
