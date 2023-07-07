8fe7940177c21f5e27882fb21a0cba68d46e4c68
Juan Diego Sanchez <diegosanchezperez93@gmail.com>
2023-07-08 01:42:24 +0200

    New changes

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,25 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
+const Movie = require("../models/Movie.model.js");
 
 /* GET home page */
-router.get('/', (req, res, next) => res.render('index'));
+router.get("/", (req, res, next) => res.render("index"));
+
+// GET movies page
+router.get("/movies", (req, res, next) => {
+  Movie.find()
+    .then((moviesFromDB) => res.render("movies", { moviesFromDB }))
+    .catch((err) => res.status(500).send(err));
+});
+
+// GET movie detail
+router.get("/movie/:movieId", (req, res) => {
+  Movie.findById(req.params.movieId)
+    .then((theMovie) => {
+      console.log(theMovie);
+      res.render("movies-pages/movie", { theMovie });
+    })
+    .catch((err) => res.status(500).send(err));
+});
 
 module.exports = router;
