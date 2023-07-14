b3c7e397e34c8ad171c891442b5a066a2fe885d4
Kiruba Chelladurai <kirubaberlin@gmail.com>
2023-07-14 22:13:46 +0200

    kiruba-express-cinema

--- app.js
+++ app.js
@@ -1,3 +1,33 @@
+// const mongoose = require('mongoose');
+// const Movie = require('./models/Movie.model');
+
+// mongoose.connect('mongodb://localhost:27017/myapp', {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true
+// });
+
+// const movies = [
+//   {
+//     title: 'A Wrinkle in Time',
+//     director: 'Ava DuVernay',
+//     stars: ['Storm Reid', 'Oprah Winfrey', 'Reese Witherspoon'],
+//     image: 'https://example.com/image1.jpg',
+//     description: 'Following the discovery of a new form of space travel...',
+//     showtimes: ['13:00', '15:30', '18:00', '20:10', '22:40']
+//   },
+//   // Add more movie objects here
+// ];
+
+// Movie.insertMany(movies)
+//   .then(() => {
+//     console.log('Movies seeded successfully');
+//     mongoose.connection.close();
+//   })
+//   .catch((error) => {
+//     console.error('Error seeding movies:', error);
+//     mongoose.connection.close();
+//   });
+
 // ℹ️ Gets access to environment variables/settings
 // https://www.npmjs.com/package/dotenv
 require('dotenv/config');
@@ -27,6 +57,9 @@ app.locals.title = `${capitalized(projectName)}- Generated with Ironlauncher`;
 // 👇 Start handling routes here
 const index = require('./routes/index');
 app.use('/', index);
+
+const moviesIndex = require('./routes/movies')
+app.use('/movies', moviesIndex)
 
 // ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
 require('./error-handling')(app);
