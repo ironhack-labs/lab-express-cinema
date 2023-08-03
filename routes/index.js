02efae50504f534b159e29824c2230ee242298ae
Alejandro <123700084+recheitor@users.noreply.github.com>
2023-08-03 19:05:24 +0200

    done

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,25 @@
 const express = require('express');
 const router = express.Router();
 
+const Movie = require('./../models/Movie.model')
 /* GET home page */
 router.get('/', (req, res, next) => res.render('index'));
+
+router.get('/movies', (req, res) => {
+    Movie
+        .find()
+        .select({ image: 1, title: 1 })
+        .then((allMovies) => {
+            res.render('movies', { allMovies })
+        })
+})
+
+router.get('/movie/:id', (req, res) => {
+    Movie
+        .findById(req.params.id)
+        .then((movieDetail) => {
+            res.render('movie-detail', movieDetail)
+        })
+})
 
 module.exports = router;
