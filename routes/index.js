85474e866348522ab84fbf56414e859d8c7c0068
Irene Buceta <irenebuzalocen@gmail.com>
2023-08-03 21:22:15 +0200

    done

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,34 @@
 const express = require('express');
 const router = express.Router();
+const Movies = require('../models/movie.model')
+
 
 /* GET home page */
 router.get('/', (req, res, next) => res.render('index'));
 
+//ir a pagina de peliculas//
+router.get('/movies', (req, res, next) => {
+
+    Movies
+        .find()
+        .then(movies => res.render('movies', { movies }))
+        .catch(err => console.log(err))
+})
+
 module.exports = router;
+
+//params
+router.get('/movies/:id', (req, res) => {
+    const { id } = req.params
+
+    Movies
+        .findById(id)
+        .then(movie => res.render('movie-detail', movie))
+        .catch(err => console.log(err))
+
+    //res.send('<h1>¡Mira la terminal!</h1>')
+})
+
+module.exports = router;
+
+
