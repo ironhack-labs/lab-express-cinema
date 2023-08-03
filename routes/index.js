983b711580242748b46a2896036529c3cf756618
Arian <arianraiesteban_20@hotmail.com>
2023-08-03 23:14:20 +0200

    Ironhack cinema LAB

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,21 @@
-const express = require('express');
-const router = express.Router();
+const express = require('express')
+const router = express.Router()
+
+const Movie = require('./../models/Movie.model')
 
 /* GET home page */
-router.get('/', (req, res, next) => res.render('index'));
+router.get('/', (req, res, next) => res.render('index'))
 
-module.exports = router;
+router.get('/movies', (req, res) => {
+	Movie.find()
+		.then(moviesFromDB => res.render('movies', { movies: moviesFromDB }))
+		.catch(error => console.log('There was an error while retreiving the movies', error))
+})
+
+router.get('/movie/:id', (req, res) => {
+	Movie.findById(req.params.id)
+		.then(movie => res.render('movie-details', movie))
+		.catch(error => console.log('There was an error while retreiving the movie', error))
+})
+
+module.exports = router
