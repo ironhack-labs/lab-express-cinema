3e010ade05d2b66d37a96ae3500e7370883fccaf
Naiim Taefy <naiim.tj@gmail.com>
2023-08-03 21:32:17 +0200

    feat: Fix style and others

--- controllers/movies.controller.js
+++ controllers/movies.controller.js
@@ -3,7 +3,6 @@ const Movie = require("../models/movie.model");
 module.exports.list = (req, res, next) => {
   Movie.find()
     .then((movies) => {
-      console.log(movies);
       res.render("movies/list", { movies });
     })
     .catch(() => {});
