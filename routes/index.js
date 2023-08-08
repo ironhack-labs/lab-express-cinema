ea94eb25c0963b5bbc1fce0c555e46664058567e
AlexOcana <a.ocana.monsalve@hotmail.com>
2023-08-08 11:47:19 +0200

    DONE

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,33 @@
 const express = require('express');
 const router = express.Router();
 
-/* GET home page */
-router.get('/', (req, res, next) => res.render('index'));
 
+
+const MovieModel = require('./../models/Movie.model');
+
+router.get("/", (req, res) => {
+
+    res.render('home')
+
+})
+
+router.get("/peliculas", (req, res) => {
+
+    MovieModel
+
+        .find()
+        .then(movies => res.render('peliculas', { movies }))
+        .catch(err => console.log(err))
+})
+
+router.get('/detalles/:movies_id', (req, res) => {
+
+    const { movies_id } = req.params
+
+    MovieModel
+        .findById(movies_id)
+        .then(movie => res.render('detalles', movie))
+        .catch(err => console.log(err))
+
+})
 module.exports = router;
