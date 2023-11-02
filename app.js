d55d427e2c5d87c86caebca64fcea70a1330e8e6
Elena Ceinos <elenaceinos@hotmail.com>
2023-11-02 22:40:47 +0100

    lab solved, trustable web

--- app.js
+++ app.js
@@ -1,34 +1,52 @@
-// ℹ️ Gets access to environment variables/settings
-// https://www.npmjs.com/package/dotenv
+
 require('dotenv/config');
 
-// ℹ️ Connects to the database
-require('./db');
 
-// Handles http requests (express is node js framework)
-// https://www.npmjs.com/package/express
+require('./db');
+const Movie = require("./models/movies.model")
 const express = require('express');
 
-// Handles the handlebars
-// https://www.npmjs.com/package/hbs
+
 const hbs = require('hbs');
 
 const app = express();
 
-// ℹ️ This function is getting exported from the config folder. It runs most middlewares
+
 require('./config')(app);
 
-// default value for title local
+
 const projectName = 'lab-express-cinema';
 const capitalized = string => string[0].toUpperCase() + string.slice(1).toLowerCase();
 
-app.locals.title = `${capitalized(projectName)}- Generated with Ironlauncher`;
+app.locals.title = "IronCinema"
 
-// 👇 Start handling routes here
+
 const index = require('./routes/index');
 app.use('/', index);
 
-// ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
+app.get("/movieList", (req, res) => {
+    Movie
+        .find()
+        .then(data => {
+            res.render('movie-list', { data })
+        })
+
+})
+app.get('/details/:id', (req, res) => {
+    const { id } = req.params
+
+
+    Movie
+        .findById(id)
+        .then(data => {
+            console.log(data)
+            res.render('details', data)
+
+        })
+        .catch(err => console.log('The error while searching albums occurred: ', err))
+
+});
+
 require('./error-handling')(app);
 
 module.exports = app;
