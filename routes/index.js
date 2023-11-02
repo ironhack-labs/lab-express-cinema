4f1def5fc8f3de395c9da5b54c712a4514ca328f
ricardo <richu1094@gmail.com>
2023-11-02 22:48:42 +0100

    >^.^<

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,27 @@
 const express = require('express');
 const router = express.Router();
+const Movie = require('./../models/Movie.model');
 
-/* GET home page */
-router.get('/', (req, res, next) => res.render('index'));
+router.get('/', (req, res) => {
+  res.render('index');
+});
+
+router.get('/movies', (req, res) => {
+  Movie.find()
+    .then(movies => {
+      res.render('movies', { movies });
+    })
+    .catch(err => console.log(err));
+});
+
+router.get('/movie/:id', (req, res) => {
+  const { id } = req.params;
+  Movie.findById(id)
+    .then(movies => {
+      console.log(movies);
+      res.render('movie-details', movies);
+    })
+    .catch(err => console.log(err));
+});
 
 module.exports = router;
