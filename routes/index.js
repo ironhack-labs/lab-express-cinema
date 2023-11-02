eb8600afd899abe085758f3a1b1f7d469b784299
Dixamo <juandarm02@gmail.com>
2023-11-02 19:49:51 +0100

    done

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,29 @@
-const express = require('express');
-const router = express.Router();
+const express = require('express')
+const router = express.Router()
 
-/* GET home page */
-router.get('/', (req, res, next) => res.render('index'));
+const Movie = require('./../models/Movie.model')
+
+router.get('/', (req, res) => {
+    res.render('index')
+})
+
+router.get('/movies', (req, res) => {
+    Movie
+        .find()
+        .then(movies => {
+            res.render('movies', { movies: movies })
+        })
+        .catch(err => console.log(err))
+})
+
+router.get('/movie/:id', (req, res) => {
+    const { id } = req.params
+    Movie
+        .findById(id)
+        .then(movie => {
+            res.render('movie',movie)
+        })
+        .catch(err => console.log(err))
+})
 
 module.exports = router;
