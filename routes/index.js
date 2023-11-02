71bb3e68b98c1534014bb4bf0347a2f25afb9d18
Mohamed khamkham <mohamkham1999@gmail.com>
2023-11-02 21:24:00 +0100

    lab-cine

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,34 @@
 const express = require('express');
 const router = express.Router();
 
-/* GET home page */
-router.get('/', (req, res, next) => res.render('index'));
+const Movie = require('../models/Movie.model')
 
-module.exports = router;
+router.get("/", (req, res) => {
+    res.render("index")
+})
+
+
+router.get('/movies', (req, res) => {
+
+    Movie
+        .find()
+        .then(movies => res.render('movies/movies', { movies }))
+        .catch(err => console.log(err))
+})
+
+
+
+
+
+// books details
+router.get('/movies/detalles/:_id', (req, res) => {
+
+    const { _id } = req.params
+
+    Movie
+        .findById(_id)
+        .then(movies => res.render('movies/details', movies))
+        .catch(err => console.log(err))
+})
+
+module.exports = router
