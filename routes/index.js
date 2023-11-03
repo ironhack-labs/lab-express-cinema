b08ad9c59e9cc5b9ec89aa601f993eddd791fbe2
Cesaria Colazzo <csr.colazzo@gmail.com>
2023-11-03 09:24:34 +0100

    done

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,34 @@
 const express = require('express');
+const MoviesModel = require('../models/Movies.model');
 const router = express.Router();
 
-/* GET home page */
-router.get('/', (req, res, next) => res.render('index'));
 
-module.exports = router;
+router.get('/', (req, res,) =>
+    res.render("Index"));
+
+
+
+router.get("/movies", (req, res) => {
+
+    MoviesModel
+        .find()
+        .then(movie => {
+            res.render("movies", { movie })
+
+        })
+})
+
+
+
+
+router.get("/movies/:id", (req, res) => {
+    const { _id } = req.params
+
+    MoviesModel
+        .find()
+        .then(movieinfo => {
+            res.render("moviesinfo", { movieinfo: movie })
+
+        })
+})
+module.exports = router
