f2458b2deac745c71435a0b8dcba2ccea5221ef6
Marina Viejo <100945399+marinavp8@users.noreply.github.com>
2023-11-03 11:43:33 +0100

    done

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,32 @@
 const express = require('express');
 const router = express.Router();
+const Movie = require("../models/Movie.model")
 
 /* GET home page */
-router.get('/', (req, res, next) => res.render('index'));
+router.get('/', (req, res) => res.render('index'));
+
+router.get('/movies', (req, res) => {
+    Movie
+        .find()
+        .then(data => {
+            const movies = []
+            data.forEach(elm => {
+                movies.push({ id: elm.id, title: elm.title, image: elm.image })
+            })
+            res.render("movies", { movies })
+        })
+        .catch(err => console.log("fallaste con las movies, ", err))
+}
+);
+
+router.get("/movie-details/:movieId", (req, res) => {
+    const { movieId } = req.params
+
+    Movie
+        .findById(movieId)
+        .then(data => res.render("movie-details", data))
+        .catch(err => console.log("fallaste con los details", err))
+})
 
 module.exports = router;
+
