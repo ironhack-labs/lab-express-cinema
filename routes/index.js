228c7d8c88f4737b47a67c3525d9efca915e54ba
agdoy <145876715+agdoy@users.noreply.github.com>
2023-11-03 16:27:59 +0100

    done

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,20 @@
 const express = require('express');
 const router = express.Router();
-
+const Movie = require("./../models/movie.model")
 /* GET home page */
-router.get('/', (req, res, next) => res.render('index'));
+router.get('/', (req, res) =>
+    res.render('index'));
+
+router.get('/movies', (req, res) => {
+
+    Movie
+        .find()
+        .then(moviesDB => {
+            res.render('movies', { moviesDB })
+        })
+        .catch(err => console.log('Se produjo un error:', err))
+})
+
 
 module.exports = router;
+
