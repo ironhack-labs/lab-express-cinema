c3ccaaf5a83d446a0526c973da41cde7385557cb
jltorres29 <jltorres29@hotmail.com>
2023-11-24 18:44:12 +0100

    done

---
+++ models/Movie.model.js
@@ -0,0 +1,36 @@
+const mongoose = require("mongoose")
+
+
+const moviesSchema = new mongoose.Schema ({
+    title: {
+        type: String,
+        required: [true, 'error']
+    },
+    director: {
+      type: String,
+      required: [true, 'error']
+    },
+    stars: {
+        type: [String],
+        required: [true, 'error']
+    },
+    image: {
+        type: String,
+        required: [true, 'error']
+    },
+    description: {
+        type: String,
+        required: [true, 'error']
+    },
+      description: {
+        type: String,
+        required: [true, 'error']
+    },
+      showtimes: {
+        type: [String],
+        required: [true, 'error']
+    }
+})
+
+const Movie = mongoose.model("Movie", moviesSchema)
+module.exports = Movie;
--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,15 @@
 const express = require('express');
 const router = express.Router();
+const Movie = require('../models/Movie.model')
 
 /* GET home page */
 router.get('/', (req, res, next) => res.render('index'));
 
+router.get('/movies', async (req, res, next) => {
+    try {
+        const movies = await Movie.find()
+        console.log(movies);
+        res.render('allMovies', {movies})
+    } catch (err) { console.log(err) }
+})
 module.exports = router;
