fbd1e9954ab6a1d16ed89574ddc55f75f12a21c4
paussaanz <paula@sanz.eu>
2023-12-06 14:27:32 +0100

    done

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const router = express.Router();
+const dataController = require('../controllers/dataController');
+
 
 /* GET home page */
 router.get('/', (req, res, next) => res.render('index'));
+router.get('/movies',dataController.getMovies)
+router.get('/movieDetail/:id', dataController.getMovieDetail)
 
 module.exports = router;
