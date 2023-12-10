dedca401d5415287975217d8d9767ff661544c79
Ensha C Latheef <enshalatheef@gmail.com>
2023-12-10 13:44:09 +0100

    homepage created

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,9 @@ const express = require('express');
 const router = express.Router();
 
 /* GET home page */
-router.get('/', (req, res, next) => res.render('index'));
+router.get('/', (req, res, next) => res.render('index', { heroTitle: "Cinema Ironhack", actionButtonLabel: "Check the movies!" }));
+
+/* GET movies page */
+router.get('/movies', (req, res, next) => res.render('movies', { heroTitle: "Cinema Ironhack", actionButtonLabel: "Check the movies!" }));
 
 module.exports = router;
