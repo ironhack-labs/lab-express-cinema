f14063f880fb7a413912b18cb715f66f824580ec
marcosAlvarezCalabria <marcosalvarezcalabria@gmail.com>
2024-02-19 19:27:16 +0100

    feat:iteration 5

--- controllers/movies.controllers.js
+++ controllers/movies.controllers.js
@@ -1,16 +1,22 @@
-const Movie = require("../models/Movie.model")
-const mongoose = require ("mongoose")
-
+const Movie = require("../models/Movie.model");
+const mongoose = require("mongoose");
 
 module.exports.list = (req, res, next) => {
-    Movie.find ()
-        .then ((movies) =>{
-            res.render ('movies', { movies })
-            console.debug(`this is  movies ${movies}`)
-        } )
-        .catch ((error) => {
-            console.error (`this is an error `)
-            next(error)})
+  Movie.find()
+    .then((movies) => {
+      res.render("movies", { movies });
+    })
+    .catch((error) => {
+      next(error);
+    });
+};
+module.exports.detail = (req, res, next) => {
+  const { id } = req.params;
 
-
-}
+  Movie.findById(id)
+    .then((movie) => {
+      console.debug(`esto es movie ${movie}`);
+      res.render("details", { movie });
+    })
+    .catch((error) => next(error));
+};
--- routes/index.js
+++ routes/index.js
@@ -6,5 +6,6 @@ const movies = require("../controllers/movies.controllers")
 /* GET home page */
 router.get('/', (req, res, next) => res.render('index'));
 router.get("/movies",movies.list)
+router.get ('/:id', movies.detail)
 
 module.exports = router;
