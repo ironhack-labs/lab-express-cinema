348202f981b32939c89c93612b295867ed234bda
martab <mbsuarez.92@gmail.com>
2024-02-22 18:19:02 +0100

    finish

---
+++ controllers/movie.controller.js
@@ -0,0 +1,14 @@
+const Movie = require('../models/movie.model');
+const mongoose = require('mongoose');
+
+module.exports.list = (req, res, next) => {
+    Movie.find()
+        .then((movies) => res.render('movies/list', {movies}))
+        .catch((error) => console.error(error));
+}
+
+module.exports.details = (req, res, next) => {
+    Movie.findById(req.params.id)
+        .then((movie) => res.render('/movies/details', {movie}))
+        .catch((error) => console.error(error));
+}
---
+++ models/movie.model.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const movieSchema = new Schema (
+    {
+        title: {
+            type: String,
+            required: [true, 'Title is required'],
+            minlength: [2, 'Title needs at least 2 chars']
+        },
+        director: {
+            type: String
+        },
+        stars: {
+            type: String
+        },
+        image: {
+            type: String
+        }, 
+        description: {
+            type: String, 
+        },
+        showtimes: {
+            type: [String]
+        },
+    },
+    {timestamps: true}
+);
+
+const Movie = mongoose.model('Movie', movieSchema);
+module.exports = Movie;
